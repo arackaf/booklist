f544c789c0b001013e06cbcc1b1905d64f440f09
Adam Rackis <adamrackis@gmail.com>
2015-08-22 19:40:46 -0500

    Initial and entirely unsatisfactory error processing added for Mongo data access :-|

--- dataAccess/BookDAO-es6.js
+++ dataAccess/BookDAO-es6.js
@@ -1,13 +1,42 @@
 var MongoClient = require('mongodb').MongoClient;
 
-class BookDAO{
+class DAO{
+    open(){
+        let result = MongoClient.connect('mongodb://localhost:27017/mongotest');
+        Promise.resolve(result).then(db => this.db = db);
+        return result;
+    }
+    confirmSingleResult(res){
+        if (+res.result.n !== 1) {
+            throw 'Object not inserted';
+        }
+    }
+    processSingleResultAndClose(p){
+        return p.then(result => {
+            this.confirmSingleResult(result);
+            this.dispose();
+        }, err => {
+            this.dispose();
+            console.log(err);
+            throw err;
+        });
+    }
+    dispose(){
+        this.db.close();
+    }
+}
+
+class BookDAO extends DAO {
     constructor(userId){
+        super();
         this.userId = userId;
     }
     saveBook(book){
-        return MongoClient.connect('mongodb://localhost:27017/mongotest').then(db => {
+        return super.open().then(db => {
             book.userId = this.userId;
-            return db.collection('books').insert(book).then(result => db.close());
+            return super.processSingleResultAndClose(
+                db.collection('books').insert(book)
+            );
         });
     }
 }
