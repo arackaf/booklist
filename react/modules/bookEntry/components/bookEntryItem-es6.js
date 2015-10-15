93372034e2695a97be67161e1226032b99ea7c8c
Adam Rackis <adamrackis@gmail.com>
2015-10-15 13:34:48 -0500

    Focus working better

--- react/modules/bookEntry/components/bookEntryItem-es6.js
+++ react/modules/bookEntry/components/bookEntryItem-es6.js
@@ -1,8 +1,23 @@
 class BookEntryItem extends React.Component{
     render(){
         return (
-            <span>Input ISBN<input value={this.props.isbn} onChange={this.props.isbnChange} /></span>
+            <span>Input ISBN<input ref='input' value={this.props.isbn} onChange={this.props.isbnChange} onKeyDown={evt => this.keyDown(evt)}  /></span>
         );
+    }
+    componentDidMount(){
+        if (this.props.index == 0){
+            ReactDOM.findDOMNode(this.refs.input).focus();
+        }
+    }
+    componentDidUpdate(){
+        if (this.props.index == this.props.activeInput){
+            ReactDOM.findDOMNode(this.refs.input).focus();
+        }
+    }
+    keyDown(evt){
+        if (evt.keyCode == 13){
+            this.props.entryFinished();
+        }
     }
 }
 
--- react/modules/bookEntry/components/bookEntryList-es6.js
+++ react/modules/bookEntry/components/bookEntryList-es6.js
@@ -2,28 +2,31 @@ let BookEntryItem = require('./bookEntryItem');
 
 class BookEntryList extends React.Component {
     componentDidMount(){
-        console.log('MOUNTED');
-        this.props.dispatch({ type: 'INITIALIZE_ENTRY_LIST', count: 10 })
+        this.props.dispatch({ type: 'INITIALIZE_ENTRY_LIST', count: 10 });
     }
     render() {
         return (
             <div>
-                <div>Hello World from { this.props.from }</div>
-                <br />
-                <div>
-                    { this.props.entryList.map((obj, i) =>
-                            <div key={'Book' + i}>
-                                <BookEntryItem isbn={obj.isbn} isbnChange={e => this.isbnChanged(obj, e)} />
-                                <br /><br />
-                            </div>
-                    )}
-                </div>
+                { this.props.entryList.map((entry, i) =>
+                        <div key={'Book' + i}>
+                            <BookEntryItem
+                                isbn={entry.isbn}
+                                isbnChange={e => this.isbnChanged(entry, e)}
+                                entryFinished={() => this.entryFinished(entry)}
+                                index={i}
+                                activeInput={this.props.activeInput}
+                            />
+                            <br /><br />
+                        </div>
+                )}
             </div>
-        )
+        );
     }
     isbnChanged(entry, e){
         this.props.dispatch({ type: 'UPDATE_ISBN', isbn: e.target.value, entry });
-        console.log('changed', entry.isbn, e.target.value);
+    }
+    entryFinished(entry){
+        this.props.dispatch({ type: 'CURRENT_INPUT_FINISHED', entry });
     }
 
 }
