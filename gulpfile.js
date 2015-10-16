1c2733b5b89e1b216784986e364532bf8d5559ed
Adam Rackis <adamrackis@hotmail.com>
2015-10-15 20:44:25 -0500

    re-transpiling, removing last of the transpiled files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    mocha = require('gulp-mocha'),
+    gprint = require('gulp-print'),
+    babel = require('gulp-babel'),
+    fs = require('fs');
 
 gulp.task('test', function () {
     gulp.src('tests/**/!(*-es6.js)') //we don't want es6 files - just the transpiled results
         .pipe(mocha());
+});
+
+gulp.task('initial-transpile', function () {
+    gulp.src('**/**-es6.js')
+        .pipe(babel({ stage: 1 }))
+        .pipe(rename(function (path) {
+            path.basename = path.basename.replace(/-es6$/, '');
+        }))
+        .pipe(gulp.dest(''))
+        .pipe(gprint(function(filePath){ return "File processed: " + filePath; }));
 });
