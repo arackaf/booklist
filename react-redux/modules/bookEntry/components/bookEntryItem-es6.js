5c10453df97e60976bebf63a1243cc68542bef87
Adam Rackis <adamrackis@gmail.com>
2015-11-12 21:51:33 -0600

    small bug fix

--- react-redux/modules/bookEntry/components/bookEntryItem-es6.js
+++ react-redux/modules/bookEntry/components/bookEntryItem-es6.js
@@ -7,7 +7,7 @@ class BookEntryItem extends React.Component{
                 Input ISBN<input ref='input' value={this.props.isbn} onChange={this.props.isbnChange} onKeyDown={evt => this.keyDown(evt)} disabled={this.props.retrieving}  />
                 { this.props.retrieving ? <span>loading....</span> : null }
                 { this.props.fetchedTitle ? <span>Saved: { this.props.fetchedTitle } <AjaxButton running={this.props.deleting} onClick={this.props.deleteBook} text={'Delete'} runningText='Deleting' /></span> : null }
-                { this.props.retrieveFailure ? <span>Could not find isbn: { this.props.isbn }</span> : null }
+                { this.props.retrieveFailure && !this.props.retrieving ? <span>Could not find isbn: { this.props.fetchedIsbn }</span> : null }
             </span>
         );
     }
