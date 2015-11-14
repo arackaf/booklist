3e9342207d8019a5338dc3839e7a7fe5696b0157
Adam Rackis <adamrackis@hotmail.com>
2015-11-13 21:10:31 -0600

    Tests running against reducer function

--- tests/reduxBookEntryTests-es6.js
+++ tests/reduxBookEntryTests-es6.js
@@ -1,14 +1,19 @@
+const { updateIsbn, currentInputFinished, getBook, getBookResults, loadAndSaveBook, deleteBook, saveAllPending, resetList } = require('../react-redux/modules/bookEntry/actions/bookActionCreators');
+
 global.Redux = require('../scripts/redux');
 const assert = require('chai').assert;
-const { store, getNewReducer } = require('../react-redux/applicationRoot/store');
 const reducer = require('../react-redux/modules/bookEntry/reducers/bookEntryReducer');
-
-getNewReducer({ name: 'bookEntry', reducer: reducer });
 
 describe('book entry tests', function(){
 
-    it('should pass', function(){
-        assert.isTrue(Array.isArray(store.getState().bookEntry.entryList));
-    })
+    it('should get an initialized store', function(){
+        assert.isTrue(Array.isArray(reducer(undefined).entryList));
+    });
+
+    it('should accept dispatches', function(){
+        let initState = reducer(undefined);
+        let state = reducer(initState, updateIsbn('123', initState.entryList[0]));
+        assert.strictEqual(state.entryList[0].isbn, '123');
+    });
 
 });
