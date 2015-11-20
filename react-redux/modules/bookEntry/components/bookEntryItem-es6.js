a6c4dfcf7ebde6a453a0c0258785d007e486f714
Adam Rackis <adamrackis@hotmail.com>
2015-11-20 10:52:33 -0600

    Basic info div added. Not wired up

--- react-redux/modules/bookEntry/components/bookEntryItem-es6.js
+++ react-redux/modules/bookEntry/components/bookEntryItem-es6.js
@@ -1,13 +1,24 @@
 const AjaxButton = require('/react-redux/applicationRoot/rootComponents/ajaxButton');
 
 class BookEntryItem extends React.Component{
+    constructor(){
+        super();
+        this.state = { expanded: false };
+    }
+    expand(){
+        this.setState({ expanded: true });
+    }
+    collapse(){
+        this.setState({ expanded: false });
+    }
     render(){
         return (
             <span>
                 Input ISBN<input ref='input' value={this.props.isbn} onChange={this.props.isbnChange} onKeyDown={evt => this.keyDown(evt)} disabled={this.props.retrieving}  />
                 { this.props.retrieving ? <span>loading....</span> : null }
-                { this.props.fetchedTitle ? <span>Saved: { this.props.fetchedTitle } <AjaxButton running={this.props.deleting} onClick={this.props.deleteBook} text={'Delete'} runningText='Deleting' /></span> : null }
+                { this.props.fetchedTitle ? <span>Saved: { this.props.fetchedTitle } <AjaxButton running={this.props.deleting} onClick={this.props.deleteBook} text={'Delete'} runningText='Deleting' /> <button onClick={() => this.expand()}>D</button></span> : null }
                 { this.props.retrieveFailure && !this.props.retrieving ? <span>Could not find isbn: { this.props.fetchedIsbn }</span> : null }
+                { this.state.expanded ? <div>Blah blah blah<br />Blah blah blah<br />Blah blah blah <br/><button onClick={() => this.collapse()}>U</button></div> : null }
             </span>
         );
     }
