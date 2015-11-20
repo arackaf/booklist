154d3bc742c10cf47f523ff8aa96d4f21a969c33
Adam Rackis <adamrackis@gmail.com>
2015-11-19 20:35:45 -0600

    testing against a proper store, mocking ajax calls.

--- tests/reduxBookEntryTests-es6.js
+++ tests/reduxBookEntryTests-es6.js
@@ -3,47 +3,67 @@ const { updateIsbn, currentInputFinished, getBook, getBookResults, loadAndSaveBo
 global.Redux = require('../scripts/redux');
 const assert = require('chai').assert;
 const reducer = require('../react-redux/modules/bookEntry/reducers/bookEntryReducer');
+const sinon = require('sinon');
+
+const Redux = require('redux');
+const thunkMiddleware = require('../react-redux/util/redux-thunk');
 
 const bookLookupResults = {
     '123': { title: 'title 123' },
     '234': { failure: true }
 };
 
+var store;
+
+const createStoreWithMiddleware = Redux.applyMiddleware(
+    thunkMiddleware
+)(Redux.createStore);
+
 global.ajaxUtil = {
-    post(url, data, callback){
-        callback(bookLookupResults[data.isbn]);
-    }
+    post(){ }
 };
 
 describe('book entry tests', function(){
+
+    beforeEach(function(){
+        store = createStoreWithMiddleware(Redux.combineReducers({ bookEntry: reducer, x: () => ({}) }));
+    });
 
     it('should get an initialized store', function(){
         assert.isTrue(Array.isArray(reducer(undefined).entryList));
     });
 
     it('should set isbn', function(){
-        let state = applyToReducer(updateIsbn('123', 0), updateIsbn('234', 1));
+        let state = applyToStore(updateIsbn('123', 0), updateIsbn('234', 1));
         assert.strictEqual(state.entryList[0].isbn, '123');
         assert.strictEqual(state.entryList[1].isbn, '234');
     });
 
     it('should get a book properly', function(){
-        let state = applyToReducer(updateIsbn('123', 0), updateIsbn('234', 1), getBook(1));
+        let state = applyToStore(updateIsbn('123', 0), updateIsbn('234', 1), getBook(1));
         checkBook({ isbn: '123', retrieving: undefined }, state.entryList[0]);
         checkBook({ isbn: '234', retrieving: true }, state.entryList[1]);
     });
 
+
     it('should get multiple books properly', function(){
-        let state = applyToReducer(updateIsbn('123', 1), updateIsbn('234', 5), updateIsbn('345', 2), saveAllPending());
+        var mock = sinon.mock(ajaxUtil);
+
+        mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '123' }));
+        mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '234' }));
+        mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '345' }));
+
+        let state = applyToStore(updateIsbn('123', 1), updateIsbn('234', 5), updateIsbn('345', 2), saveAllPending());
+
+        mock.verify();
         //checkBook({ fetchedTitle: 'title 123',  isbn: '123', retrieveFailure: undefined }, state.entryList[1]);
         //checkBook({ isbn: '234', retrieving: true }, state.entryList[1]);
     });
 
 
-    function applyToReducer(...actions){
-        let state = reducer();
-        actions.forEach(a => state = reducer(state, a));
-        return state;
+    function applyToStore(...actions){
+        actions.forEach(a => store.dispatch(a));
+        return store.getState().bookEntry;
     }
 
     function checkBook(bookProperties, bookInState){
