2e04505a6aa85e1606947c66deeeed7c1fab0e95
Adam Rackis <adamrackis@gmail.com>
2015-11-20 14:37:54 -0600

    cleaning up button components

--- react-redux/applicationRoot/rootComponents/ajaxButton-es6.js
+++ react-redux/applicationRoot/rootComponents/ajaxButton-es6.js
@@ -1,7 +1,9 @@
-class AjaxButton extends React.Component{
+const BootstrapButton = require('/react-redux/applicationRoot/rootComponents/bootstrapButton');
+
+class AjaxButton extends BootstrapButton {
     render(){
         return (
-            <button className={'btn ' + (this.props.css || '')} onClick={this.props.onClick} disabled={this.props.running}>{this.props.running ? this.props.runningText || this.props.text : this.props.text}</button>
+            <button className={this.state.btnCss} onClick={this.props.onClick} disabled={this.props.running}>{ this.props.running ? this.props.runningText || this.props.text : this.props.text }</button>
         )
     }
 }
