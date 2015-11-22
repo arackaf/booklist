933f5714b292b6a0108f5259b9fa5ad871ba1fbb
Adam Rackis <adamrackis@gmail.com>
2015-11-21 20:17:53 -0600

    desktop and mobile view list skeleton UIs loading

--- react-redux/modules/bookList/components/bookViewList-es6.js
+++ react-redux/modules/bookList/components/bookViewList-es6.js
@@ -1,14 +1,26 @@
 let { } = require('../actions/actionCreators');
 
-const BookViewListItem = require('./bookViewListItem');
-
 class BookEntryList extends React.Component {
+    constructor(){
+        super();
+        this.state = { listItem: null };
+        this.switchToDesktop();
+    }
+    switchToDesktop(){
+        System.import('./modules/bookList/components/bookViewListItem-desktop').then(component => this.setState({ listItem: component }));
+    }
+    switchToMobile(){
+        System.import('./modules/bookList/components/bookViewListItem-mobile').then(component => this.setState({ listItem: component }));
+    }
     render() {
         return (
             <div>
+                <button onClick={() => this.switchToDesktop()}>Desktop</button>
+                <button onClick={() => this.switchToMobile()}>Mobile</button>
                 Root list -> <br/><br/>
 
-                { Array.from({ length: 10 }).map(o => <BookViewListItem />) }
+                { this.state.listItem ?
+                    Array.from({ length: 10 }).map((o, i) => React.createElement(this.state.listItem, { key: 'el' + i })) : '<Loading...>' }
 
             </div>
         );
