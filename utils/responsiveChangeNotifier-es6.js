382de4dec80861d3ecab6d15001a7af9e566b1d9
Adam Rackis <adamrackis@hotmail.com>
2015-11-26 15:27:12 -0600

    responsive swapping broken into mixin - decorator eventually

---
+++ utils/responsiveChangeNotifier-es6.js
@@ -0,0 +1,29 @@
+class ResponsiveNotifier{
+    constructor(cb){
+        this.notifySize = function notifySize(){
+            if(ResponsiveBootstrapToolkit.is("xs") ) {
+                cb('xs');
+            }
+
+            if(ResponsiveBootstrapToolkit.is("sm") ) {
+                cb('sm');
+            }
+
+            if(ResponsiveBootstrapToolkit.is("md") ) {
+                cb('md');
+            }
+
+            if(ResponsiveBootstrapToolkit.is(">md") ) {
+                cb('lg');
+            }
+        };
+
+        $(window).on('resize', this.notifySize);
+        this.notifySize();
+    }
+    dispose(){
+        $(window).off(this.notifySize);
+    }
+}
+
+module.exports = ResponsiveNotifier;
