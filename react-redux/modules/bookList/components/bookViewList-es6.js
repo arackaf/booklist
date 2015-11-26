bf7ba0c61e6f9ee9e7e5c0510e7219c7c2d67db5
Adam Rackis <adamrackis@gmail.com>
2015-11-26 15:27:12 -0600

    responsive swapping broken into mixin - decorator eventually

--- react-redux/modules/bookList/components/bookViewList-es6.js
+++ react-redux/modules/bookList/components/bookViewList-es6.js
@@ -1,20 +1,58 @@
+let ResponsiveNotifier = require('/utils/responsiveChangeNotifier');
 let { loadBooks } = require('../actions/actionCreators');
 
-class BookEntryList extends React.Component {
-    constructor(){
-        super();
+const responsiveBsSizes = ['xs', 'sm', 'md', 'lg'];
 
-        this.state = { listComponent: null };
-        this.switchToDesktop();
+function responsiveMobileDesktopMixin(self, stateName, config){
+    let currentlyMobile;
+
+    const cutoff = config.cutoff || 'sm';
+    const mobileCutoffIndex = responsiveBsSizes.indexOf(cutoff);
+
+    if (!self.state){
+        self.state = { [stateName]: null };
+    } else {
+        self.state[stateName] = null;
     }
-    componentDidMount(){
+
+    self.responsiveNotifier = new ResponsiveNotifier(val => checkSize(val));
+
+    self.switchToMobile = function(){
+        this.overridden = true;
+        loadComponent.call(self, config.mobile);
+    };
+
+    self.switchToDesktop = function(){
+        this.overridden = true;
+        loadComponent.call(self, config.desktop);
+    };
+
+    function checkSize(currentSize){
+        if (self.overridden) return;
+
+        let isMobile = responsiveBsSizes.indexOf(currentSize) <= mobileCutoffIndex;
+        if (isMobile !== currentlyMobile){
+            currentlyMobile = isMobile;
+            loadComponent(currentlyMobile ? config.mobile : config.desktop);
+        }
+    }
+
+    function loadComponent(componentPath){
+        System.import(componentPath).then(component => self.setState({ [stateName]: component }));
+    }
+}
+
+    class BookEntryList extends React.Component {
+        constructor(){
+            super();
+
+            responsiveMobileDesktopMixin(this, 'listComponent', {
+                mobile:  './modules/bookList/components/bookViewList-mobile',
+                desktop: './modules/bookList/components/bookViewList-desktop'
+            });
+        }
+        componentDidMount(){
         this.props.dispatch(loadBooks());
-    }
-    switchToDesktop(){
-        System.import('./modules/bookList/components/bookViewList-desktop').then(component => this.setState({ listComponent: component }));
-    }
-    switchToMobile(){
-        System.import('./modules/bookList/components/bookViewList-mobile').then(component => this.setState({ listComponent: component }));
     }
     render() {
         return (
