1ecda165c563978efbc3b9755570f61a8158e5d5
Adam Rackis <adamrackis@hotmail.com>
2015-11-26 16:38:51 -0600

    trying to dynamically swap between modules - redux cleanup issues

---
+++ react-redux/util/responsiveUiLoaders-es6.js
@@ -0,0 +1,51 @@
+let ResponsiveNotifier = require('/utils/responsiveChangeNotifier');
+
+const responsiveBsSizes = ['xs', 'sm', 'md', 'lg'];
+
+function responsiveMobileDesktopMixin(self, stateName, config){
+    let currentlyMobile;
+
+    const cutoff = config.cutoff || 'sm';
+    const mobileCutoffIndex = responsiveBsSizes.indexOf(cutoff);
+
+    if (!self.state){
+        self.state = { [stateName]: null };
+    } else {
+        self.state[stateName] = null;
+    }
+
+    self.responsiveNotifier = new ResponsiveNotifier(val => checkSize(val));
+
+    self.switchToMobile = function(){
+        this.overridden = true;
+        loadComponent.call(self, config.mobile);
+    };
+
+    self.switchToDesktop = function(){
+        this.overridden = true;
+        loadComponent.call(self, config.desktop);
+    };
+
+    const originalComponentWillDismount = self.componentWillUnmount;
+    self.componentWillUnmount = function(){
+        this.responsiveNotifier.dispose();
+        console.log('notifier diposed');
+        typeof originalComponentWillDismount === 'function' && originalComponentWillDismount.call(this);
+    };
+
+    function checkSize(currentSize){
+        if (self.overridden) return;
+
+        let isMobile = responsiveBsSizes.indexOf(currentSize) <= mobileCutoffIndex;
+        if (isMobile !== currentlyMobile){
+            currentlyMobile = isMobile;
+            loadComponent(currentlyMobile ? config.mobile : config.desktop);
+        }
+    }
+
+    function loadComponent(componentPath){
+        System.import(componentPath).then(component => self.setState({ [stateName]: component }));
+    }
+}
+
+module.exports = { responsiveMobileDesktopMixin };
