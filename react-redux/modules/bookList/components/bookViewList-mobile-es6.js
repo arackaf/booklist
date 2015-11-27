bd0950ec363473f47c831d84c15c911095606249
Adam Rackis <adamrackis@hotmail.com>
2015-11-26 21:46:31 -0600

    initial mobile ui started

--- react-redux/modules/bookList/components/bookViewList-mobile-es6.js
+++ react-redux/modules/bookList/components/bookViewList-mobile-es6.js
@@ -1,5 +1,27 @@
-const BootstrapButton = require('/react-redux/applicationRoot/rootComponents/bootstrapButton');
-const AjaxButton = require('/react-redux/applicationRoot/rootComponents/ajaxButton');
+class BookViewListMobileItem extends React.Component{
+    constructor(){
+        super();
+        this.state = { expanded: false };
+    }
+    toggle(){
+        this.setState({ expanded: !this.state.expanded });
+    }
+    render(){
+        return (
+            <a onClick={() => this.toggle()} className="list-group-item" style={{ cursor: 'pointer' }}>
+                <h4 className="list-group-item-heading">{this.props.title}</h4>
+                <p className="list-group-item-text">{this.props.author || 'no author'}</p>
+                { !this.state.expanded ? null :
+                    <div>
+                        {this.props.publicationDate ? <p className="list-group-item-text">{'Published ' + this.props.publicationDate}</p> : null}
+                        {this.props.pages ? <p className="list-group-item-text">{this.props.pages + ' pages'}</p> : null}
+                        {this.props.isbn ? <p className="list-group-item-text">{'ISBN ' + this.props.isbn}</p> : null}
+                    </div>
+                }
+            </a>
+        );
+    }
+}
 
 class BookViewListMobile extends React.Component{
     constructor(){
@@ -7,12 +29,8 @@ class BookViewListMobile extends React.Component{
     }
     render(){
         return (
-            <div>
-                { this.props.list.map((book, i) =>
-                    <div key={'book' + i}>
-                        Hey there - MOBILE
-                    </div>
-                )}
+            <div className="list-group">
+                { this.props.list.map((book, i) => <BookViewListMobileItem key={'book' + i} {...book} /> )}
             </div>
         );
     }
