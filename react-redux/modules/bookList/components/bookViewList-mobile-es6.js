4be68a9fa12254fa80609ecbdc5389d52ead5d98
Adam Rackis <adamrackis@gmail.com>
2015-11-26 22:03:09 -0600

    mobile ui expanded

--- react-redux/modules/bookList/components/bookViewList-mobile-es6.js
+++ react-redux/modules/bookList/components/bookViewList-mobile-es6.js
@@ -9,15 +9,22 @@ class BookViewListMobileItem extends React.Component{
     render(){
         return (
             <a onClick={() => this.toggle()} className="list-group-item" style={{ cursor: 'pointer' }}>
-                <h4 className="list-group-item-heading">{this.props.title}</h4>
-                <p className="list-group-item-text">{this.props.author || 'no author'}</p>
-                { !this.state.expanded ? null :
-                    <div>
-                        {this.props.publicationDate ? <p className="list-group-item-text">{'Published ' + this.props.publicationDate}</p> : null}
-                        {this.props.pages ? <p className="list-group-item-text">{this.props.pages + ' pages'}</p> : null}
-                        {this.props.isbn ? <p className="list-group-item-text">{'ISBN ' + this.props.isbn}</p> : null}
+                <div className="row">
+                    <div className="col-xs-3 col-sm-1">
+                        {this.state.expanded || this.props.showImg ? <img src={this.props.smallImage} /> : null}
                     </div>
-                }
+                    <div className="col-xs-9 col-sm-11">
+                        <h4 className="list-group-item-heading">{this.props.title}</h4>
+                        <p className="list-group-item-text">{this.props.author || 'no author'}</p>
+                        { !this.state.expanded ? null :
+                            <div>
+                                {this.props.publicationDate ? <p className="list-group-item-text">{'Published ' + this.props.publicationDate}</p> : null}
+                                {this.props.pages ? <p className="list-group-item-text">{this.props.pages + ' pages'}</p> : null}
+                                {this.props.isbn ? <p className="list-group-item-text">{'ISBN ' + this.props.isbn}</p> : null}
+                            </div>
+                        }
+                    </div>
+                </div>
             </a>
         );
     }
@@ -26,11 +33,18 @@ class BookViewListMobileItem extends React.Component{
 class BookViewListMobile extends React.Component{
     constructor(){
         super();
+        this.state = { showImages: false };
+    }
+    toggleImages(){
+        this.setState({ showImages: !this.state.showImages });
     }
     render(){
         return (
-            <div className="list-group">
-                { this.props.list.map((book, i) => <BookViewListMobileItem key={'book' + i} {...book} /> )}
+            <div>
+                <a style={{ cursor: 'pointer' }} onClick={() => this.toggleImages()}>Show images</a>
+                <div className="list-group">
+                    { this.props.list.map((book, i) => <BookViewListMobileItem showImg={this.state.showImages} key={'book' + i} {...book} /> )}
+                </div>
             </div>
         );
     }
