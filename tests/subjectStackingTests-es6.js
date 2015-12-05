9c4b4d557c2f40189fbe7d6cbca6873e4ac623ef
Adam Rackis <adamrackis@hotmail.com>
2015-12-05 16:19:00 -0600

    Very basic initial stacking done

---
+++ tests/subjectStackingTests-es6.js
@@ -0,0 +1,42 @@
+const { updateIsbn, currentInputFinished, getBook, getBookResults, loadAndSaveBook, deleteBook, saveAllPending, resetList } = require('../react-redux/modules/bookList/actions/actionCreators');
+
+const { LOAD_SUBJECTS_RESULTS } = require('../react-redux/modules/bookList/actions/actionNames');
+
+const Redux = require('redux');
+const sinon = require('sinon');
+const assert = require('chai').assert;
+
+const reducer = require('../react-redux/modules/bookList/reducers/reducer');
+
+describe('subject stacking', function() {
+
+    it('should stack a flat list', function () {
+        let subjects = loadSubjects([{ _id: 1, name: 'a' }, { _id: 2, name: 'b' }, { _id: 3, name: 'c' }]);
+        verifySubjects(subjects, [{ _id: 1, name: 'a' }, { _id: 2, name: 'b' }, { _id: 3, name: 'c' }]);
+    });
+
+    it('should stack list with one child', function () {
+        let subjects = loadSubjects([{ _id: 1, name: 'a' }, { _id: 2, name: 'b' }, { _id: 3, name: 'c', path: ',2,' }]);
+        verifySubjects(subjects, [{ _id: 1, name: 'a' }, { _id: 2, name: 'b', children: [{ _id: 3, name: 'c' }] }]);
+    });
+
+    function loadSubjects(subjects){
+        return apply({ type: LOAD_SUBJECTS_RESULTS, subjects }).subjects;
+    }
+
+    function apply(...actions){
+        let state = reducer(undefined);
+        actions.forEach(a => state = reducer(state, a));
+        return state;
+    }
+
+    function verifySubjects(actual, expected){
+        assert.strictEqual(actual.length, expected.length, 'Top level subjects dont match');
+
+        expected.forEach(se => {
+            let matchingSubject = actual.find(sa => se._id == sa._id && se.name == sa.name);
+            assert.isObject(matchingSubject, `subject not found ${se._id} ${se.name}`);
+            verifySubjects(matchingSubject.children, se.children || []); //I don't feel like adding children to every test object
+        });
+    }
+});
