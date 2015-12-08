5c5cdfe6436bfa760f508144e39bfa59fa825fa6
Adam Rackis <adamrackis@gmail.com>
2015-12-07 22:13:47 -0600

    hierarchical template in modal

--- react-redux/modules/bookList/components/bookViewList-es6.js
+++ react-redux/modules/bookList/components/bookViewList-es6.js
@@ -1,6 +1,5 @@
 const { loadBooks, editSubjectsForBook, addSubjectToBook, loadBooksAndSubjects } = require('../actions/actionCreators');
 const { responsiveMobileDesktopMixin } = require('/react-redux/util/responsiveUiLoaders');
-const Collapse = ReactBootstrap.Collapse;
 
 function BookListLoading() {
     return <div style={{ height: '150' }}>Loading <i className="fa fa-spinner fa-spin"></i></div>
@@ -8,42 +7,6 @@ function BookListLoading() {
 
 function BookListNoResults() {
     return <div style={{ height: '150' }}>No results</div>
-}
-
-
-class HierarchicalSubjectItem extends React.Component {
-    constructor(){
-        super();
-        this.state = { childrenVisible: false };
-    }
-    toggleChildren(){
-        this.setState({childrenVisible: !this.state.childrenVisible});
-    }
-    render(){
-        return (
-            <li key={this.props._id}>
-                {this.props.children.length ?
-                    <div>
-                        <a onClick={() => this.toggleChildren()}>{this.props.name}</a>
-                        <Collapse in={this.state.childrenVisible}>
-                            <div>
-                                <HierarchicalSubjectList subjects={this.props.children} />
-                            </div>
-                        </Collapse>
-                    </div>: <span>{this.props.name}</span>}
-            </li>
-        )
-    }
-}
-
-class HierarchicalSubjectList extends React.Component {
-    render() {
-        return (
-            <ul>
-                { this.props.subjects.map(s => <HierarchicalSubjectItem key={`s-${s._id}`} {...s} />) }
-            </ul>
-        )
-    }
 }
 
 class BookViewingList extends React.Component {
@@ -68,9 +31,6 @@ class BookViewingList extends React.Component {
                     <button onClick={() => this.switchToDesktop()}>Desktop</button>
                     <button onClick={() => this.switchToMobile()}>Mobile</button>
                     <br/><br/>
-
-                    <HierarchicalSubjectList subjects={this.props.subjects} />
-
 
                     { !this.state.listComponent || this.props.loading ? <BookListLoading /> :
                         (this.props.bookList.length ?
