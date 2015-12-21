be483ad8c6b81afa3cd6ecf84f224555aeee3b0c
Adam Rackis <adamrackis@hotmail.com>
2015-12-21 17:00:47 -0600

    selecting books re-implemented with memoized selector

--- react-redux/modules/bookList/components/bookViewList-desktop-es6.js
+++ react-redux/modules/bookList/components/bookViewList-desktop-es6.js
@@ -59,7 +59,7 @@ class BookViewListDesktop extends React.Component{
                     { this.props.books.list.map(book =>
                         <tr key={book._id}>
                             <td>
-                                <i onClick={() => this.props.toggleSelectBook(book._id)} className={'fa ' + (book.selected ? 'fa-check-square-o' : 'fa-square-o')} style={{ cursor: 'pointer' }}></i>
+                                <i onClick={() => this.props.toggleSelectBook(book._id)} className={'fa ' + (this.props.books.selectedBooks[book._id] ? 'fa-check-square-o' : 'fa-square-o')} style={{ cursor: 'pointer' }}></i>
                             </td>
                             <td><img src={book.smallImage} /></td>
                             <td>{book.title}</td>
