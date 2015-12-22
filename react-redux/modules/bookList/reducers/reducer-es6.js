fdbe188a486564102b2d6581aff3d7e038d3f092
Adam Rackis <adamrackis@hotmail.com>
2015-12-22 11:20:04 -0600

    big overhaul of reducers

--- react-redux/modules/bookList/reducers/reducer-es6.js
+++ react-redux/modules/bookList/reducers/reducer-es6.js
@@ -2,6 +2,11 @@ const { booksReducer } = require('./booksReducer');
 const { subjectsReducer } = require('./subjectsReducer');
 const filtersReducer = require('./filtersReducer');
 
+const actionCreators = require('../actions/actionCreators');
+const { createSelector } = require('/node_modules/reselect/lib/index');
+const { setBookResultsSubjects } = require('../util/booksSubjectsHelpers');
+const { stackAndGetTopLevelSubjects } = require('../util/booksSubjectsHelpers');
+
 const initialState = () => ({
     books: booksReducer(),
     subjects: subjectsReducer(),
@@ -16,4 +21,17 @@ function reducer(state = initialState(), action = {}){
     };
 }
 
-module.exports = reducer;
+const booksWithSubjectsSelector = createSelector(
+    [state => state.books.list, state => state.subjects.list],
+    setBookResultsSubjects
+);
+
+const stackedSubjectsSelector = state => stackAndGetTopLevelSubjects(state.list);
+
+const bookListSelector = state => ({
+    subjects: Object.assign({}, state.bookList.subjects, {list: stackedSubjectsSelector(state.bookList.subjects)}),
+    books: Object.assign({}, state.bookList.books, {list: booksWithSubjectsSelector(state.bookList)}),
+    filters: state.bookList.filters
+});
+
+module.exports = { reducer, selector: bookListSelector };
