b6529ada2f80b2afa57996c7642c6e7478d3e14e
Adam Rackis <adamrackis@gmail.com>
2016-01-02 11:52:15 -0500

    Begin to clean up filtering reducer

--- react-redux/modules/bookList/reducers/filtersReducer-es6.js
+++ react-redux/modules/bookList/reducers/filtersReducer-es6.js
@@ -1,28 +1,15 @@
-const { TOGGLE_FILTER_SUBJECT, OPEN_SUBJECTS_FILTER_MODAL, CLOSE_SUBJECTS_FILTER_MODAL, TOGGLE_FILTERED_SUBJECT } = require('../actions/actionNames');
-const Immutable = require('../../../../utils/immutable');
+const { TOGGLE_FILTERED_SUBJECT } = require('../actions/actionNames');
 
 const initialState = () => ({
     searchText: '',
-    subjects: Immutable.Set(),
-    subjectsFilterModal: null
+    subjects: {},
+    withChildSubjects: false
 });
 
 function filtersReducer(state = initialState(), action = {}){
     switch(action.type){
-        case TOGGLE_FILTER_SUBJECT:
-            let newSubjects = Object.assign({}, state.subjects);
-            if (newSubjects[action._id]) delete newSubjects[action._id];
-            else newSubjects[action._id] = true;
-
-            return Object.assign({}, state, { subjects: newSubjects });
-        case OPEN_SUBJECTS_FILTER_MODAL:
-            return Object.assign({}, state, { subjectsFilterModal: { subjects: Immutable.Set(state.subjects) } });
         case TOGGLE_FILTERED_SUBJECT:
-            let newFilterSubjects = state.subjectsFilterModal.subjects;
-            newFilterSubjects = newFilterSubjects.has(action._id) ? newFilterSubjects.delete(action._id) : newFilterSubjects.add(action._id);
-            return Object.assign({}, state, { subjectsFilterModal: Object.assign({}, state.subjectsFilterModal, { subjects: newFilterSubjects }) });
-        case CLOSE_SUBJECTS_FILTER_MODAL:
-            return Object.assign({}, state, { subjectsFilterModal: null });
+            return Object.assign({}, state, { subjects: { ...subjects, [action._id]: !state.subjects[action._id] } });
     }
     return state;
 }
