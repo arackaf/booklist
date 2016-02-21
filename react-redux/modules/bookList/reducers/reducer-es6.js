b2fa6bfd3e26dc90ff14efe9727289ca668926ae
Adam Rackis <adamrackis@hotmail.com>
2016-02-20 19:52:00 -0600

    rename filters to bookSearch

--- react-redux/modules/bookList/reducers/reducer-es6.js
+++ react-redux/modules/bookList/reducers/reducer-es6.js
@@ -1,6 +1,6 @@
 const { booksReducer: books, booksSelector } = require('./booksReducer');
 const { subjectsReducer: subjects, subjectsSelector } = require('./subjectsReducer');
-const { filtersReducer: filters, filtersSelector } = require('./filtersReducer');
+const { bookSearchReducer: bookSearch, bookSearchSelector } = require('./bookSearchReducer');
 const { bookSubjectManagerReducer: booksSubjectsModifier, booksSubjectsModifierSelector } = require('./booksSubjectModifierReducer');
 
 const actionCreators = require('../actions/actionCreators');
@@ -8,7 +8,7 @@ const actionCreators = require('../actions/actionCreators');
 const reducer = Redux.combineReducers({
     books,
     subjects,
-    filters,
+    bookSearch,
     booksSubjectsModifier
 });
 
@@ -16,7 +16,7 @@ const bookListSelector = state => {
     return {
         subjects: subjectsSelector(state.bookList),
         books: booksSelector(state.bookList),
-        filters: filtersSelector(state.bookList),
+        bookSearch: bookSearchSelector(state.bookList),
         booksSubjectsModifier: booksSubjectsModifierSelector(state.bookList)
     }
 };
