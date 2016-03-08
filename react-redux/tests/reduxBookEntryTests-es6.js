e2e46b0625a533c4e76e91bcf1e41428ee822354
Adam Rackis <adamrackis@hotmail.com>
2016-03-08 12:54:00 -0600

    Disabling some soon-to-be obsolete tests

--- react-redux/tests/reduxBookEntryTests-es6.js
+++ react-redux/tests/reduxBookEntryTests-es6.js
@@ -23,39 +23,39 @@ describe('book entry tests', function(){
         store = createStoreWithMiddleware(Redux.combineReducers({ bookEntry: reducer, x: () => ({}) }));
     });
 
-    it('should get an initialized store', function(){
-        assert.isTrue(Array.isArray(reducer(undefined).entryList));
-    });
-
-    it('should set isbn', function(){
-        let state = applyToStore(updateIsbn('123', 0), updateIsbn('234', 1));
-        assert.strictEqual(state.entryList[0].isbn, '123');
-        assert.strictEqual(state.entryList[1].isbn, '234');
-    });
-
-    it('should get a book properly', function(){
-        let state = applyToStore(updateIsbn('123', 0), updateIsbn('234', 1), getBook(1));
-        checkBook({ isbn: '123', retrieving: undefined }, state.entryList[0]);
-        checkBook({ isbn: '234', retrieving: true }, state.entryList[1]);
-    });
-
-
-    it('should get and set multiple books properly', function(){
-        var mock = sinon.mock(ajaxUtil);
-
-        mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '123' })).callsArgWith(2, { title: 'title 123',  isbn: '123' });
-        mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '234' })).callsArgWith(2, { title: 'title 234',  isbn: '234' });
-        mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '345' })).callsArgWith(2, { title: 'title 345',  isbn: '345' });
-
-        let state = applyToStore(updateIsbn('123', 1), updateIsbn('234', 5), updateIsbn('345', 2), saveAllPending());
-
-        mock.verify();
-        checkBooks(state, {
-            1: { fetchedTitle: 'title 123',  isbn: '123', retrieveFailure: undefined },
-            5: { fetchedTitle: 'title 234',  isbn: '234', retrieveFailure: undefined },
-            2: { fetchedTitle: 'title 345',  isbn: '345', retrieveFailure: undefined }
-        });
-    });
+    //it('should get an initialized store', function(){
+    //    assert.isTrue(Array.isArray(reducer(undefined).entryList));
+    //});
+    //
+    //it('should set isbn', function(){
+    //    let state = applyToStore(updateIsbn('123', 0), updateIsbn('234', 1));
+    //    assert.strictEqual(state.entryList[0].isbn, '123');
+    //    assert.strictEqual(state.entryList[1].isbn, '234');
+    //});
+    //
+    //it('should get a book properly', function(){
+    //    let state = applyToStore(updateIsbn('123', 0), updateIsbn('234', 1), getBook(1));
+    //    checkBook({ isbn: '123', retrieving: undefined }, state.entryList[0]);
+    //    checkBook({ isbn: '234', retrieving: true }, state.entryList[1]);
+    //});
+    //
+    //
+    //it('should get and set multiple books properly', function(){
+    //    var mock = sinon.mock(ajaxUtil);
+    //
+    //    mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '123' })).callsArgWith(2, { title: 'title 123',  isbn: '123' });
+    //    mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '234' })).callsArgWith(2, { title: 'title 234',  isbn: '234' });
+    //    mock.expects('post').withArgs(sinon.match.any, sinon.match({ isbn: '345' })).callsArgWith(2, { title: 'title 345',  isbn: '345' });
+    //
+    //    let state = applyToStore(updateIsbn('123', 1), updateIsbn('234', 5), updateIsbn('345', 2), saveAllPending());
+    //
+    //    mock.verify();
+    //    checkBooks(state, {
+    //        1: { fetchedTitle: 'title 123',  isbn: '123', retrieveFailure: undefined },
+    //        5: { fetchedTitle: 'title 234',  isbn: '234', retrieveFailure: undefined },
+    //        2: { fetchedTitle: 'title 345',  isbn: '345', retrieveFailure: undefined }
+    //    });
+    //});
 
 
     function applyToStore(...actions){
