59770954c3dd6868da3ced3d1c61ee6805443c8a
Adam Rackis <adamrackis@gmail.com>
2016-03-30 23:47:22 -0500

    initial main nav bar added

---
+++ react-redux/applicationRoot/rootComponents/mainNavigation-es6.js
@@ -0,0 +1,26 @@
+class MainNavigationBar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            <li className="active"><a href="#bookEntry">Book entry</a></li>
+                            <li><a href="#bookList">Your books</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        );
+
+        return <br />;
+    }
+}
+
+export default MainNavigationBar;
