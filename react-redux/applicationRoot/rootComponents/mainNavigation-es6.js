07138e638174fb43a5eaf15caeb07a7c6f2ec421
Adam Rackis <adamrackis@gmail.com>
2016-04-02 17:17:25 -0500

    NavBar converted to use React bindings

--- react-redux/applicationRoot/rootComponents/mainNavigation-es6.js
+++ react-redux/applicationRoot/rootComponents/mainNavigation-es6.js
@@ -1,26 +1,28 @@
+const Navbar = ReactBootstrap.Navbar;
+const Nav = ReactBootstrap.Nav;
+const NavItem = ReactBootstrap.NavItem;
+
 class MainNavigationBar extends React.Component {
+    logout(){
+        alert('log out');
+    }
     render() {
         return (
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a style={{ cursor: 'default' }} className="navbar-brand visible-xs-block visible-md-block">Brand</a>
-                    </div>
-
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav">
-                            <li className={this.props.isBookEntry ? 'active' : ''}><a href={this.props.isBookEntry ? undefined : '#bookEntry'}>Book entry</a></li>
-                            <li className={this.props.isBookList ? 'active' : ''}><a href={this.props.isBookList ? undefined : '#bookList'}>You books</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a style={{ cursor: 'default' }}>Book Tracker</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem href={this.props.isBookEntry ? undefined : '#bookEntry'}>Book entry</NavItem>
+                        <NavItem href={this.props.isBookList ? undefined : '#bookList'}>Your books</NavItem>
+                        <NavItem onClick={this.logout}>Logout</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
