4b5bc4523d16cc187c3157c54404457ee2437675
Adam Rackis <adamrackis@gmail.com>
2016-04-09 18:20:18 -0500

    style login screen, show ui message for failed login

--- react-redux/applicationRoot/rootComponents/ajaxButton-es6.js
+++ react-redux/applicationRoot/rootComponents/ajaxButton-es6.js
@@ -4,11 +4,11 @@ class AjaxButton extends BootstrapButton {
     render(){
 
         let result = this.props.running
-            ? <button className={this.state.btnCss} disabled={true}><i className="fa fa-fw fa-spin fa-spinner"></i>{ ' ' + this.props.runningText || this.props.text}</button>
+            ? <button className={this.state.btnCss} disabled={true}><i className="fa fa-fw fa-spin fa-spinner"></i>{ (this.props.runningText || this.props.text) ? ' ' + this.props.runningText || this.props.text : this.props.children}</button>
             : <button className={this.state.btnCss} onClick={this.props.onClick}>{ this.props.children }</button>;
 
         return result;
     }
 }
 
-module.exports = AjaxButton;
+export default AjaxButton;
