f8a2e38431cfdcd535cba1256cc0f9207f09b307
Adam Rackis <adamrackis@gmail.com>
2016-04-10 18:06:36 -0500

    book entry work

--- react-redux/modules/bookEntry/reducers/reducer-es6.js
+++ react-redux/modules/bookEntry/reducers/reducer-es6.js
@@ -9,7 +9,8 @@ import {
     RESET_LIST,
     SET_PENDING,
     BOOK_SAVED,
-    INCREMENT_PENDING
+    INCREMENT_PENDING,
+    BOOK_QUEUED
 } from '../actions/actionNames';
 
 const initialArray = () => Array.from({ length: 10 }).map(() => ({ isbn: '', fetched: false, fetching: false }));
@@ -23,7 +24,7 @@ function reducer(state = initialState, action){
     switch(action.type) {
         case UPDATE_ISBN:
             var newEntryList = state.entryList.concat();
-            Object.assign(newEntryList[action.index], { isbn: action.isbn });
+            Object.assign(newEntryList[action.index], { isbn: action.isbn, retrieving: false, queued: false });
 
             return Object.assign({}, state, { entryList: newEntryList });
         case GET_BOOK:
@@ -32,28 +33,11 @@ function reducer(state = initialState, action){
 
             newEntryList[action.index] = updatedObject;
             return Object.assign({}, state, { entryList: newEntryList });
-        case GETTING_BOOKS:
-            var newEntryList = state.entryList.concat();
-            action.indexes.forEach(i => newEntryList[i].retrieving = true);
-
-            return Object.assign({}, state, { entryList: newEntryList });
-        case GET_BOOK_RESULTS:
-            let searchResult = action.bookInfo;
-
-            var updatedObject = Object.assign({}, state.entryList[action.index], { fetched: true, retrieving: false, retrieveFailure: searchResult.failure, fetchedTitle: searchResult.title, fetchedInfo: searchResult, fetchedIsbn: state.entryList[action.index].isbn }),
+        case BOOK_QUEUED:
+            var updatedObject = Object.assign({}, state.entryList[action.index], { retrieving: false, queued: true }),
                 newEntryList = state.entryList.concat();
 
             newEntryList[action.index] = updatedObject;
-            return Object.assign({}, state, { entryList: newEntryList });
-        case BOOK_DELETED:
-            var newEntryList = state.entryList.concat();
-
-            newEntryList[action.index] = { isbn: '', fetched: false, fetching: false };
-            return Object.assign({}, state, { entryList: newEntryList });
-        case BOOK_DELETING:
-            var newEntryList = state.entryList.concat();
-
-            newEntryList[action.index].deleting = true;
             return Object.assign({}, state, { entryList: newEntryList });
         case RESET_LIST:
             return Object.assign({}, state, { entryList: initialArray() });
