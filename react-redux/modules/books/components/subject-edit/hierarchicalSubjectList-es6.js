5767a873836c1c37e87ce76d55897315ed63144d
Adam Rackis <adamrackis@gmail.com>
2016-04-20 00:31:57 -0500

    Edit subject modal styling

--- react-redux/modules/books/components/subject-edit/hierarchicalSubjectList-es6.js
+++ react-redux/modules/books/components/subject-edit/hierarchicalSubjectList-es6.js
@@ -14,18 +14,19 @@ class HierarchicalSubjectItem extends React.Component {
             <li key={this.props._id}>
                 {this.props.children.length ?
                     <div>
-                        <a onClick={() => this.toggleChildren()}>{this.props.name}</a>&nbsp;
-                        <a onClick={() => this.props.onEdit(this.props._id)}><i className="fa fa-edit"></i></a>
+                        <a onClick={() => this.props.onEdit(this.props._id)}><i className="fa fa-edit"></i></a>&nbsp;
+                        <a onClick={() => this.toggleChildren()}>{this.props.name}</a>
                         <Collapse in={this.state.childrenVisible}>
                             <div>
-                                <HierarchicalSubjectList onEdit={this.props.onEdit} subjects={this.props.children} />
+                                <HierarchicalSubjectList style={{ paddingLeft: 25 }} onEdit={this.props.onEdit} subjects={this.props.children} />
                             </div>
                         </Collapse>
                     </div>
                     :
                     <div>
-                        <span>{this.props.name}</span>&nbsp;
-                        <a onClick={() => this.props.onEdit(this.props._id)}><i className="fa fa-edit"></i></a>
+                        <a onClick={() => this.props.onEdit(this.props._id)}><i className="fa fa-edit"></i></a>&nbsp;
+                        <span>{this.props.name}</span>
+
                     </div>}
             </li>
         )
@@ -36,7 +37,7 @@ class HierarchicalSubjectList extends React.Component {
     render() {
         return (
             <div>
-                <ul>
+                <ul style={{ ...(this.props.style || {}), listStyle: 'none' }}>
                     { this.props.subjects.map(s => <HierarchicalSubjectItem onEdit={this.props.onEdit} key={s._id} {...s} />) }
                 </ul>
             </div>
