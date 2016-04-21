1a6449e077cabdd0fd04ca9e9be19b3497fd0b45
Adam Rackis <adamrackis@gmail.com>
2016-04-20 19:46:59 -0500

    Styling edit subjects modal

--- react-redux/modules/books/components/subject-edit/subjectEditModal-es6.js
+++ react-redux/modules/books/components/subject-edit/subjectEditModal-es6.js
@@ -22,22 +22,32 @@ class subjectEditModal extends React.Component {
                     <HierarchicalSubjectList style={{ paddingLeft: 5 }} subjects={this.props.subjects} onEdit={_id => this.props.editSubject(_id)} />
 
                     { editSubjectsPacket && editSubjectsPacket.editing ?
-                        <div>
-                            { editSubjectsPacket.editingSubject ? `Edit subject ${editSubjectsPacket.editingSubject.name}` : 'New Subject' }
-                            <br/>
-                            New name: <input value={editSubjectsPacket.newSubjectName} onChange={(e) => this.props.setNewSubjectName(e.target.value)} />
-                            New Parent:
-                            <select value={editSubjectsPacket.newSubjectParent} onChange={(e) => this.props.setNewSubjectParent(e.target.value)}>
-                                <option value="">None</option>
-                                { editSubjectsPacket.eligibleParents.map(s => <option key={s._id} value={s._id}>{s.name}</option>) }
-                            </select>
-                            <BootstrapButton onClick={this.props.createOrUpdateSubject}>Save</BootstrapButton>
+                        <div className="panel panel-info">
+                            <div className="panel-heading">
+                                { editSubjectsPacket.editingSubject ? `Edit ${editSubjectsPacket.editingSubject.name}` : 'New Subject' }
+                            </div>
+                            <div className="panel-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label>Subject name</label>
+                                        <input className="form-control" value={editSubjectsPacket.newSubjectName} onChange={(e) => this.props.setNewSubjectName(e.target.value)} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Parent</label>
+                                        <select className="form-control" value={editSubjectsPacket.newSubjectParent} onChange={(e) => this.props.setNewSubjectParent(e.target.value)}>
+                                            <option value="">None</option>
+                                            { editSubjectsPacket.eligibleParents.map(s => <option key={s._id} value={s._id}>{s.name}</option>) }
+                                        </select>
+                                    </div>
+                                    <BootstrapButton preset="primary" onClick={e => { this.props.createOrUpdateSubject(); e.preventDefault();} }>Save</BootstrapButton>
+                                </form>
+                            </div>
                         </div>
                         : null
                     }
                 </Modal.Body>
                 <Modal.Footer>
-                    <button onClick={this.props.stopEditingSubjects}>Close</button>
+                    <BootstrapButton onClick={this.props.stopEditingSubjects}>Close</BootstrapButton>
                 </Modal.Footer>
             </Modal>
         );
