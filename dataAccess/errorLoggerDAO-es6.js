414897a074fbe890e03146baafcdfd33f636297e
Adam Rackis <adamrackis@gmail.com>
2016-04-21 23:33:18 -0500

    error logging

---
+++ dataAccess/errorLoggerDAO-es6.js
@@ -0,0 +1,18 @@
+const DAO = require('./DAO');
+
+class ErrorLoggerDAO extends DAO {
+    constructor(userId) {
+        super();
+        this.userId = userId;
+    }
+    async log(type, err){
+        let db = await super.open();
+        try {
+            return await db.collection('errorLog').insert({ type, error: err, stack: err.stack || '' });
+        } finally {
+            super.dispose(db);
+        }
+    }
+}
+
+export default ErrorLoggerDAO;
