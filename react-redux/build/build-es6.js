fdf3b2837e649e5157314a7f7dd61142625956b0
Adam Rackis <adamrackis@gmail.com>
2016-04-29 20:39:46 -0500

    build work - path simplification remaining. Build file with proper object key names remaining

--- react-redux/build/build-es6.js
+++ react-redux/build/build-es6.js
@@ -3,6 +3,7 @@ const gulpUglify = require('gulp-uglify');
 const gulpRename = require('gulp-rename');
 const gulp = require('gulp');
 const glob = require('glob');
+const fs = require('fs');
 
 let builder = new Builder({
     baseURL: '../'
@@ -23,11 +24,15 @@ const files = glob.sync('../../react-redux/applicationRoot/**/*.js')
 
 let paths = files.join(' + ');
 
-let p1 = builder.bundle('reactStartup + ' + paths, '../dist/shared-unminified.js');
-let p2 = builder.bundle('modules/books/books', '../dist/books/books-unminified.js');
+let p1 = builder.bundle('reactStartup + ' + paths, '../dist/shared-unminified.js').then(results => ({ name: 'reactStartup', results }));
+let p2 = builder.bundle('modules/books/books', '../dist/books/books-unminified.js').then(results => ({ name: 'modules/books/books', results }));
+let p3 = builder.bundle('modules/scan/scan', '../dist/scan/scan-unminified.js').then(results => ({ name: 'modules/scan/scan', results }));
 
-Promise.all([p1, p2]).then(([shared, p1Results]) => {
-    p1Results.modules.forEach(m => console.log(m, '\n'));
+let buildOutputs = {};
+Promise.all([p1, p2, p3]).then(results => {
+    results.forEach(({ name, results }) => {
+        buildOutputs[name] = { modules: results.modules };
+    });
 
     gulp.src(['../dist/**/*-unminified.js'], {base: './'})
         .pipe(gulpUglify())
@@ -36,6 +41,23 @@ Promise.all([p1, p2]).then(([shared, p1Results]) => {
             console.log(`Finished compressing ${path.basename}`);
         }))
         .pipe(gulp.dest(''))
-        .on('end', () => console.log('minified'));
+        .on('end', createBundlesFileForBrowser);
 }).catch(err => console.log(err));
 
+function createBundlesFileForBrowser(){
+    console.log(Object.keys(buildOutputs).length);
+    let moduleEntries = Object.keys(buildOutputs).map(name => {
+        let entry = buildOutputs[name],
+            modulesArray = entry.modules.map(n => `'${n}'`).join(', ');
+        return `'${name}': [${modulesArray}]`;
+    }).join(',\n\t');
+
+    let fileContents =
+`
+var gBundlePaths = {
+\t${moduleEntries}
+}
+`
+
+    fs.writeFileSync('../dist/bundlePaths.js', fileContents);
+}
