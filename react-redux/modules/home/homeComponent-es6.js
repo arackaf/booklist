203c8affc9ba47b1aafaf2c1ffae03c7827ae853
Adam Rackis <adamrackis@gmail.com>
2016-05-08 17:47:00 -0500

    home page filled out a bit more

--- react-redux/modules/home/homeComponent-es6.js
+++ react-redux/modules/home/homeComponent-es6.js
@@ -5,24 +5,28 @@ const Nav = ReactBootstrap.Nav;
 const NavItem = ReactBootstrap.NavItem;
 import MainNavigationBar from 'root-components/mainNavigation';
 
+const MainHomePane = props =>
+    <div className="row">
+        <div className="hidden-xs hidden-sm col-md-1 col-lg-3"></div>
+        <div style={{ marginLeft: 10, marginRight: 10 }} className="col-md-10 col-lg-6">
+            <div className="panel panel-default">
+                <div className="panel-body">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+        <div className="hidden-xs hidden-sm col-md-1 col-lg-3"></div>
+    </div>
+
 class HomeIfLoggedIn extends React.Component{
     render(){
         return (
             <div>
                 <MainNavigationBar></MainNavigationBar>
-
-                <div className="row">
-                    <div className="hidden-xs hidden-sm col-md-1 col-lg-3"></div>
-                    <div style={{ marginLeft: 10, marginRight: 10 }} className="col-md-10 col-lg-6">
-                        <div className="panel panel-default">
-                            <div className="panel-body">
-                                Welcome to <i>My Library</i>.  Eventually there'll be some sort of interesting dashboard here.  Until then, just use the menu above
-                                to either view your library, or scan some books in.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="hidden-xs hidden-sm col-md-1 col-lg-3"></div>
-                </div>
+                <MainHomePane>
+                    Welcome to <i>My Library</i>.  Eventually there'll be some sort of interesting dashboard here.  Until then, just use the menu above
+                    to either view your library, or scan some books in.
+                </MainHomePane>
             </div>
         )
     }
@@ -35,7 +39,7 @@ class HomeIfNotLoggedIn extends React.Component{
                 <Navbar fluid={true}>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            <a style={{ cursor: 'default' }}>Book Tracker</a>
+                            <a style={{ cursor: 'default' }}>My Library</a>
                         </Navbar.Brand>
                         <Navbar.Toggle />
                     </Navbar.Header>
@@ -45,7 +49,20 @@ class HomeIfNotLoggedIn extends React.Component{
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
-                <div>Welcome to My-Library.  Use this site to blah blah blah. More complete description coming</div>
+                <MainHomePane>
+                    Welcome to <i>My Library</i>.
+                    <br /><br />
+                    This site is my own little passion project, the purpose of which is to track your library.  You scan in your books (or manually type in the isbn)
+                    and the books' info is fetched from Amazon, and stored for you.  You can then flexibly search and categorize your library.
+                    <br /><br />
+                    So basically this site is of use to the extremely small percentage of people who read a lot, own the books they read, and read non-eBooks.  As I said, this is more
+                    of a passion project than anything.
+                    <br /><br />
+                    It's free to sign up, and store up to 500 books.  In the remote chance someone actually wants to store more than that, there'll be some sort of nominal
+                    fee to help defray storage costs.
+                    <br /><br />
+                    <a className="btn btn-primary" href="#login">Login or create an account</a>
+                </MainHomePane>
             </div>
         )
     }
