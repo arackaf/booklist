08d0bfb9645f46572bc27567fd8ea7c99a2d39b2
Adam Rackis <adamrackis@gmail.com>
2016-05-18 20:29:01 -0500

    book entering tweaks

--- react-redux/modules/scan/components/bookEntryList-es6.js
+++ react-redux/modules/scan/components/bookEntryList-es6.js
@@ -74,6 +74,10 @@ class BookEntryList extends React.Component {
                                     <div style={{ margin: 0 }} className="alert alert-info alert-slim">
                                         Enter each isbn below, and press "Retrieve and save all" to search for all entered books. Or, use a barcode
                                         scanner to search for each book immediately (pressing enter after typing in a 10 or 13 digit isbn has the same effect).
+                                        <br /> <br />
+                                        After you enter the isbn in the last textbox, focus will jump back to the first.  This is to make scanning a large number
+                                        of books with a barcode scanner as smooth as possible; just make sure you don't have any partially-entered ISBNs up top, or else
+                                        they may get overridden.
                                     </div>
                                 </div>
                             </Collapse>
@@ -135,6 +139,8 @@ class BookEntryList extends React.Component {
         let index = this.props.entryList.indexOf(entry);
         if (index < this.props.entryList.length - 1){
             this.refs['Book' + (index + 1)].focusInput();
+        } else {
+            this.refs['Book0'].focusInput();
         }
 
         if (entry.isbn.length == 10 || entry.isbn.length == 13){
