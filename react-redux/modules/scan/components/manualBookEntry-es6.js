e08ee9a99a70f378380aaf46bf9b1790eec95cc8
Adam Rackis <adamrackis@gmail.com>
2016-05-23 19:07:02 -0500

    form laid out

--- react-redux/modules/scan/components/manualBookEntry-es6.js
+++ react-redux/modules/scan/components/manualBookEntry-es6.js
@@ -20,7 +20,61 @@ class ManualBookEntry extends React.Component {
                 </Modal.Header>
                 <Modal.Body>
                     <form>
+                        <div className="form-group">
+                            <label>Title</label>
+                            <input ref="title" className="form-control" placeholder="Title (required)" />
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>ISBN</label>
+                                    <input ref="isbn" className="form-control" placeholder="ISBN" />
+                                </div>
+                            </div>
 
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>Pages</label>
+                                    <input ref="pages" type="number" className="form-control" placeholder="Number of pages" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>Published</label>
+                                    <input ref="publisher" className="form-control" placeholder="Publisher" />
+                                </div>
+                            </div>
+
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>Published</label>
+                                    <input ref="publication-date" className="form-control" placeholder="Publication date" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-4">
+                                <div className="form-group">
+                                    <label>Author</label>
+                                    <input ref="author" className="form-control" placeholder="Author" />
+                                </div>
+                            </div>
+
+                            <div className="col-xs-4">
+                                <div className="form-group">
+                                    <label>Author 2</label>
+                                    <input ref="author2" className="form-control" placeholder="Author 2" />
+                                </div>
+                            </div>
+                            <div className="col-xs-4">
+                                <div className="form-group">
+                                    <label>Author 3</label>
+                                    <input ref="author3" className="form-control" placeholder="Author 3" />
+                                </div>
+                            </div>
+                        </div>
                     </form>
                 </Modal.Body>
                 <Modal.Footer>
