5b7a5467db96fd8ce9f58b7efa043808ac603b1a
Adam Rackis <adamrackis@hotmail.com>
2016-05-28 11:11:49 -0500

    style tweak

--- react-redux/modules/home/homeComponent-es6.js
+++ react-redux/modules/home/homeComponent-es6.js
@@ -83,7 +83,7 @@ class Home extends React.Component{
     }
     render(){
         return (
-            <div>
+            <div className="container-fluid">
                 { this.state.isLoggedIn ? <HomeIfLoggedIn /> : <HomeIfNotLoggedIn /> }
             </div>
         );
