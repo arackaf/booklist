8277ebe7b4ed845fc1e33de1887ca449550d1c11
Adam Rackis <adamrackis@gmail.com>
2016-06-03 00:57:34 -0500

    starting to clean up book search stuff

--- react-redux/modules/books/reducers/bookSearch/actionCreators-es6.js
+++ react-redux/modules/books/reducers/bookSearch/actionCreators-es6.js
@@ -1,5 +1,7 @@
 import { BEGIN_FILTER_CHANGE, TOGGLE_PENDING_SUBJECT, END_FILTER_CHANGE } from './actionNames';
 
+import { globalHashManager } from 'react-startup';
+
 export function beginFilterChange(){
     return { type: BEGIN_FILTER_CHANGE };
 }
@@ -8,6 +10,19 @@ export function togglePendingSubject(_id){
     return { type: TOGGLE_PENDING_SUBJECT, _id };
 }
 
-export function closeSubjectsFilterModal(){
+export function endFilterChanging(){
     return { type: END_FILTER_CHANGE };
+}
+
+export function applyFilters(){
+    return function(dispatch, getState) {
+        let state = getState().books.bookSearch,
+            filterSubjectsVal = Object.keys(state.pendingSubjects).filter(k => state.pendingSubjects[k]).join('-');
+
+        globalHashManager.setValues(
+            'filterSubjects', filterSubjectsVal,
+            'searchChildSubjects', state.searchChildSubjects && filterSubjectsVal ? 'true' : null
+        );
+        dispatch(endFilterChanging());
+    }
 }
