85ca4eaede94d7b2d0f8399bd6cb10359bb71fa6
Adam Rackis <adamrackis@hotmail.com>
2016-06-06 23:03:54 -0500

    restoring bookDAO

---
+++ dataAccess/bookDAO-es6.js
@@ -0,0 +1,94 @@
+const ObjectId = require('mongodb').ObjectID;
+const DAO = require('./dao');
+
+class BookDAO extends DAO {
+    constructor(userId){
+        super();
+        this.userId = userId;
+    }
+    async searchBooks({ search, subjects, searchChildSubjects, sort, sortDirection }){
+        subjects = subjects || [];
+        let db = await super.open();
+        try {
+            let query = { userId: this.userId },
+                sortObj = { _id: -1 };
+
+            if (search){
+                query.title = new RegExp(search, 'gi');
+            }
+
+            if (sort){
+                console.log('setting', sort, +sortDirection);
+                sortObj = { [sort]: +sortDirection };
+            }
+
+            if (subjects.length){
+                if (searchChildSubjects){
+                    let allPaths = subjects.map(s => `,${s},`).join('|');
+                    let childIds = (await db.collection('subjects').find({ path: { $regex: allPaths }, userId: this.userId }, { _id: 1 }).toArray()).map(o => '' + o._id);
+
+                    subjects.push(...childIds);
+                }
+
+                query.subjects = { $in: subjects };
+            }
+            //may implement $or another way
+            //if (query.title && query.subjects){
+            //    query.$or = [
+            //        { subjects: query.subjects },
+            //        { title: query.title }
+            //    ];
+            //    delete query.subjects;
+            //    delete query.title;
+            //}
+            return (await db.collection('books').find(query).sort(sortObj).toArray()).map(addCreatedOn);
+        } catch(err){
+            console.log(err);
+        } finally {
+            super.dispose(db);
+        }
+    }
+    async saveBook(book){
+        let db = await super.open();
+        try {
+            book.userId = this.userId;
+            let result = await db.collection('books').insert(book);
+
+            super.confirmSingleResult(result);
+        } finally {
+            super.dispose(db);
+        }
+    }
+    async deleteBook(id){
+        let db = await super.open();
+        try {
+            await db.collection('books').remove({ _id: ObjectId(id) });
+        } finally {
+            super.dispose(db);
+        }
+    }
+    async setBooksSubjects(books, add, remove){
+        let db = await super.open();
+        try{
+            await db.collection('books').update(
+                { _id: { $in: books.map(_id => ObjectId(_id)) } },
+                { $addToSet: { subjects: { $each: (add || []) } } }, { upsert: false, multi: true }
+            );
+
+            await db.collection('books').update(
+                { _id: { $in: books.map(_id => ObjectId(_id)) } },
+                { $pullAll: { subjects: (remove || []) } }, { upsert: false, multi: true }
+            );
+
+        } finally {
+            super.dispose(db);
+        }
+    }
+}
+
+function addCreatedOn(book){
+    book.dateAdded = +book._id.getTimestamp();
+    return book;
+}
+
+export default BookDAO;
