e6feb72ab386c526d7d8c883eb14982ffe7a2190
Adam Rackis <adamrackis@gmail.com>
2016-06-17 21:51:36 -0500

    gitignore tweaks

--- transpileTest/testFile.js
+++ transpileTest/testFile.js
@@ -24,19 +24,56 @@ class c {
         let objClone = _extends({}, obj);
     }
     asyncFunc() {
-        return _asyncToGenerator(function* () {
-            let a = yield something(),
-                b = yield somethingElse();
+        var _this = this;
 
-            yield Promise.all([somethingWithA(a), somethingWithB(b)]);
+        return _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+            var a, b;
+            return regeneratorRuntime.wrap(function _callee$(_context) {
+                while (1) switch (_context.prev = _context.next) {
+                    case 0:
+                        _context.next = 2;
+                        return something();
 
-            return 'Done!';
-        })();
+                    case 2:
+                        a = _context.sent;
+                        _context.next = 5;
+                        return somethingElse();
+
+                    case 5:
+                        b = _context.sent;
+                        _context.next = 8;
+                        return Promise.all([somethingWithA(a), somethingWithB(b)]);
+
+                    case 8:
+                        return _context.abrupt('return', 'Done!');
+
+                    case 9:
+                    case 'end':
+                        return _context.stop();
+                }
+            }, _callee, _this);
+        }))();
     }
-    *generatorFunc() {
-        yield 1;
-        yield 2;
-        yield 3;
+    generatorFunc() {
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+            while (1) switch (_context2.prev = _context2.next) {
+                case 0:
+                    _context2.next = 2;
+                    return 1;
+
+                case 2:
+                    _context2.next = 4;
+                    return 2;
+
+                case 4:
+                    _context2.next = 6;
+                    return 3;
+
+                case 6:
+                case 'end':
+                    return _context2.stop();
+            }
+        }, _callee2, this);
     }
     render() {
         return React.createElement(
