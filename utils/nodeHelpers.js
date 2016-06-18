8cc6c897424c18adb7eb53f1467fe8468acaaf63
Adam Rackis <adamrackis@hotmail.com>
2016-06-17 21:49:49 -0500

    transpile upgrade started

--- utils/nodeHelpers.js
+++ utils/nodeHelpers.js
@@ -1,19 +1,14 @@
 "use strict";
 
 function nodeCallback(f) {
-    return function (err) {
+    return function (err, ...args) {
         if (err) {
             throw err;
         }
-
-        for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-            args[_key - 1] = arguments[_key];
-        }
-
-        f.apply(undefined, args);
+        f(...args);
     };
 }
 
 module.exports = {
-    nodeCallback: nodeCallback
+    nodeCallback
 };
