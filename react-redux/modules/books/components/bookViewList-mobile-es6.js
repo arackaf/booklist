cd908af0d8de4a36bff02a422612de6dc647eea1
Adam Rackis <adamrackis@gmail.com>
2016-06-25 11:52:33 -0500

    Mobile updates

--- react-redux/modules/books/components/bookViewList-mobile-es6.js
+++ react-redux/modules/books/components/bookViewList-mobile-es6.js
@@ -10,25 +10,37 @@ import * as actionCreatorsEditBook from '../reducers/editBook/actionCreators';
 
 import { selector } from '../reducers/reducer';
 
-const BookViewListMobileItem = props => (
-    <a className="list-group-item" style={{ cursor: 'pointer' }}>
-        <div className="row">
-            <div className="col-xs-3 col-sm-1">
-                <img src={props.smallImage} />
-            </div>
-            <div className="col-xs-9 col-sm-11">
-                <h4 className="list-group-item-heading">{props.title}</h4>
-                <p className="list-group-item-text">{props.authors.join(', ') || 'No author'}</p>
-                <div>
-                    {props.publicationDate ? <p className="list-group-item-text">{'Published ' + props.publicationDate}</p> : null}
-                    {props.pages ? <p className="list-group-item-text">{props.pages + ' pages'}</p> : null}
-                    {props.isbn ? <p className="list-group-item-text">{'ISBN ' + props.isbn}</p> : null}
+const BookViewListMobileItem = props => {
+    let publisherDisplay = null,
+        isbnPages = null;
+    if (props.publisher || props.publicationDate) {
+        publisherDisplay = [props.publisher, props.publicationDate].filter(s => s).join(' ');
+    }
+    if (props.isbn || props.pages){
+        isbnPages = [
+            props.pages ? `${props.pages} pages` : null,
+            props.isbn ? `ISBN ${props.isbn}` : null
+        ].filter(o => o).join('; ');
+    }
+
+    return (
+        <a className="list-group-item" style={{ cursor: 'pointer' }}>
+            <div className="row">
+                <div className="col-xs-3 col-sm-1">
+                    <img src={props.smallImage} />
+                </div>
+                <div className="col-xs-9 col-sm-11">
+                    <h4 className="list-group-item-heading">{props.title}</h4>
+                    <p className="list-group-item-text">{props.authors.length ? <b>{props.authors.join(', ')}</b> : 'No author'}</p>
+                    <div>
+                        {publisherDisplay ? <p className="list-group-item-text">{publisherDisplay}</p> : null}
+                        {isbnPages ? <p className="list-group-item-text">{isbnPages}</p> : null}
+                    </div>
                 </div>
             </div>
-        </div>
-    </a>
-);
-
+        </a>
+    )
+}
 const BookViewListMobile = props => (
     <div>
         <div style={{ paddingBottom: 15 }}>
