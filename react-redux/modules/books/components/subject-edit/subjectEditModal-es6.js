0fac1e752b5f46d347e3971864e54a16116b1eb5
Adam Rackis <adamrackis@gmail.com>
2016-06-28 23:12:54 -0500

    Subjects ui

--- react-redux/modules/books/components/subject-edit/subjectEditModal-es6.js
+++ react-redux/modules/books/components/subject-edit/subjectEditModal-es6.js
@@ -24,7 +24,7 @@ class subjectEditModal extends React.Component {
                         Edit subjects
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
+                <Modal.Body style={{ paddingBottom: 0 }}>
                     <BootstrapButton onClick={this.props.newSubject} preset="info-xs">New subject</BootstrapButton>
                     <br />
                     <br />
@@ -37,31 +37,46 @@ class subjectEditModal extends React.Component {
                             </div>
                             <div className="panel-body">
                                 <form>
-                                    <div className="form-group">
-                                        <label>Subject name</label>
-                                        <input className="form-control" value={editSubjectsPacket.name} onChange={(e) => this.props.setNewSubjectName(e.target.value)} />
-                                    </div>
-                                    <div className="form-group">
-                                        <label>Parent</label>
-                                        <select className="form-control" value={editSubjectsPacket.parentId} onChange={(e) => this.props.setNewSubjectParent(e.target.value)}>
-                                            <option value="">None</option>
-                                            { editSubjectsPacket.eligibleParents.map(s => <option key={s._id} value={s._id}>{s.name}</option>) }
-                                        </select>
-                                    </div>
-                                    <div>
-                                        <div>Label color</div>
-                                        { this.props.colors.map(cp => <div className="color-choice" onClick={() => this.props.setNewSubjectBackgroundColor(cp.backgroundColor) } style={{ backgroundColor: cp.backgroundColor }}></div>) }
+                                    <div className="row">
+                                        <div className="col-xs-6">
+                                            <div className="form-group">
+                                                <label>Subject name</label>
+                                                <input className="form-control" value={editSubjectsPacket.name} onChange={(e) => this.props.setNewSubjectName(e.target.value)} />
+                                            </div>
+                                        </div>
+                                        <div className="col-xs-6">
+                                            <div className="form-group">
+                                                <label>Parent</label>
+                                                <select className="form-control" value={editSubjectsPacket.parentId} onChange={(e) => this.props.setNewSubjectParent(e.target.value)}>
+                                                    <option value="">None</option>
+                                                    { editSubjectsPacket.eligibleParents.map(s => <option key={s._id} value={s._id}>{s.name}</option>) }
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="col-xs-9">
+                                            <div className="form-group">
+                                                <label>Label color</label>
+                                                <div>
+                                                    { this.props.colors.map(cp => <div className="color-choice" onClick={() => this.props.setNewSubjectBackgroundColor(cp.backgroundColor) } style={{ backgroundColor: cp.backgroundColor }}></div>) }
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-xs-3">
+                                            <div className="form-group">
+                                                <label>Text color</label>
+                                                <div>
+                                                    { textColors.map(tc => <div className="color-choice" onClick={() => this.props.setNewSubjectTextColor(tc) } style={{ backgroundColor: tc }}></div>) }
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-xs-12">
+                                            <div style={{ marginTop: '10px' }} className="form-group">
+                                                <label>Preview &nbsp;&nbsp;</label>
+                                                <div className="label label-default" style={{ backgroundColor: editSubjectsPacket.backgroundColor, color: editSubjectsPacket.textColor }}>{ editSubjectsPacket.name }</div>
+                                            </div>
+                                        </div>
                                     </div>
                                     <br style={{ clear: 'both' }} />
-                                    <div>
-                                        <div>Text color</div>
-                                        { textColors.map(tc => <div className="color-choice" onClick={() => this.props.setNewSubjectTextColor(tc) } style={{ backgroundColor: tc }}></div>) }
-                                    </div>
-                                    <br style={{ clear: 'both' }} />
-                                    <br />
-                                    Preview: <div className="label label-default" style={{ backgroundColor: editSubjectsPacket.backgroundColor, color: editSubjectsPacket.textColor }}>{ editSubjectsPacket.name }</div>
-
-                                    <br /><br />
 
                                     <BootstrapButton preset="primary" onClick={e => { this.props.createOrUpdateSubject(); e.preventDefault();} }>Save</BootstrapButton>
                                     <BootstrapButton className="pull-right" preset="danger" onClick={e => { this.props.deleteSubject(); e.preventDefault();} }>Delete</BootstrapButton>
