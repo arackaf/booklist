4473ee5bca88c68f4d00b74514aedf297d2b8037
Adam Rackis <adamrackis@hotmail.com>
2016-07-08 17:24:01 -0500

    mobX hello world

--- react-mobx/modules/home/homeComponent-es6.js
+++ react-mobx/modules/home/homeComponent-es6.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { isLoggedIn, goHome } from 'reactStartup';
+import { render } from 'react-dom';
 
 import {
     Nav,
@@ -8,6 +9,62 @@ import {
 } from 'react-bootstrap';
 
 import MainNavigationBar from 'applicationRoot/rootComponents/mainNavigation';
+
+import { observable, computed } from 'mobx';
+import { observer } from "mobx-react";
+
+class BookList {
+    @observable books = []
+    @computed get readBooks(){
+        return this.books.filter(b => b.isRead);
+    }
+    loadBooks(){
+        setTimeout(() => {
+            this.books = [
+                { title: 'The Language Instinct', author: 'Steven Pinker', isRead: true },
+                { title: 'History of The United States During the Administration of Thomas Jefferson', author: 'Henry Adams', isRead: true },
+                { title: 'History of The United States During the Administration of James Madison', author: 'Henry Adams', isRead: false }
+            ].map(b => new Book(b));
+        }, 50);
+    }
+}
+
+class Book {
+    @observable isRead = false
+    constructor(props){
+        Object.assign(this, props)
+    }
+    toggleRead(){
+        this.isRead = !this.isRead;
+    }
+}
+
+@observer
+class MobXTest extends React.Component {
+    constructor(props){
+        super();
+        props.bookList.loadBooks();
+    }
+    render(){
+        return (
+            <div>
+                <span>All books:</span>
+                <ul>{ this.props.bookList.books.map(b => <li>{b.title} <button onClick={() => b.toggleRead()}>Toggle Read</button></li>) }</ul>
+
+                <br />
+
+                <span>Read books:</span>
+                <ul>{ this.props.bookList.readBooks.map(b => <li>{b.title} <button onClick={() => b.toggleRead()}>Toggle Read</button></li>) }</ul>
+            </div>
+        );
+    }
+}
+
+const bookList = new BookList();
+
+render(<MobXTest bookList={bookList} />, document.getElementById('mobx-temp-home'));
+
+
 
 const MainHomePane = props =>
     <div className="row">
