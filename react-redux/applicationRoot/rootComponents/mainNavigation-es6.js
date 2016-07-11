349419eedffedc7e29f6d30a33c363d0c0b6af7e
Adam Rackis <adamrackis@gmail.com>
2016-07-10 22:03:33 -0500

    Cleanup

--- react-redux/applicationRoot/rootComponents/mainNavigation-es6.js
+++ react-redux/applicationRoot/rootComponents/mainNavigation-es6.js
@@ -9,61 +9,51 @@ import {
 
 import { goHome, globalHashManager } from 'reactStartup';
 
-class NonPublicMainNavigationBar extends Component {
-    constructor(){
-        super();
-        this.state = { viewingPublic: !!globalHashManager.currentModule };
-    }
-    logout(){
-        ajaxUtil.post('/react-redux/logout', { }, () => window.location.reload());
-    }
-    render() {
-        let isBookEntry = this.props.isBookEntry,
-            isBookList = this.props.isBookList;
+const NonPublicMainNavigationBar = props => {
+    let logout = () => ajaxUtil.post('/react-redux/logout', { }, () => window.location.reload());
 
-        return (
-            <Navbar style={{ borderRadius: 0, borderRight: 0, borderLeft: 0, borderTop: 0 }} fluid={true}>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a onClick={goHome} style={{ cursor: 'pointer' }}>My Library</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav>
-                        <NavItem active={isBookEntry} href={isBookEntry ? undefined : '#scan'}>Book entry</NavItem>
-                        <NavItem active={isBookList} href={isBookList ? undefined : '#books'}>Your books</NavItem>
-                        <NavItem onClick={this.logout}>Logout</NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+    let isBookEntry = props.isBookEntry,
+        isBookList = props.isBookList;
+
+    return (
+        <Navbar style={{ borderRadius: 0, borderRight: 0, borderLeft: 0, borderTop: 0 }} fluid={true}>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a onClick={goHome} style={{ cursor: 'pointer' }}>My Library</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav>
+                    <NavItem active={isBookEntry} href={isBookEntry ? undefined : '#scan'}>Book entry</NavItem>
+                    <NavItem active={isBookList} href={isBookList ? undefined : '#books'}>Your books</NavItem>
+                    <NavItem onClick={logout}>Logout</NavItem>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
 
-class PublicViewingMainNavBar extends React.Component {
-    render(){
-        let isBookEntry = this.props.isBookEntry,
-            isBookList = this.props.isBookList;
-        
-        return (
-            <Navbar style={{ borderRadius: 0, borderRight: 0, borderLeft: 0, borderTop: 0 }} fluid={true}>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a onClick={goHome} style={{ cursor: 'pointer' }}>My Library</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav>
-                        <NavItem disabled={true}>Book entry</NavItem>
-                        <NavItem active={true} href={isBookList ? undefined : '#books'}>XXX</NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
-}
+const PublicViewingMainNavBar = props => {
+    let isBookList = props.isBookList;
+
+    return (
+        <Navbar style={{ borderRadius: 0, borderRight: 0, borderLeft: 0, borderTop: 0 }} fluid={true}>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a onClick={goHome} style={{ cursor: 'pointer' }}>My Library</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav>
+                    <NavItem disabled={true}>Book entry</NavItem>
+                    <NavItem active={true} href={isBookList ? undefined : '#books'}>XXX</NavItem>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
 const ConnectedPublicViewingNav = connect(state => state.root)(PublicViewingMainNavBar);
 
 
