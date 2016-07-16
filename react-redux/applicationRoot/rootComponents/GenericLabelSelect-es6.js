c6c4ff074e3e6d9e406e6ae58e4bf312f1ce8a4d
Adam Rackis <adamrackis@gmail.com>
2016-07-16 12:13:00 -0500

    multi-select as a component

---
+++ react-redux/applicationRoot/rootComponents/GenericLabelSelect-es6.js
@@ -0,0 +1,44 @@
+import Autosuggest from 'react-autosuggest';
+
+function getSuggestionValue(suggestion) { // when suggestion selected, this function tells what should be the value of the input
+    return suggestion.name;
+}
+
+function renderSuggestion(s) {
+    return (
+        <span style={{ backgroundColor: s.backgroundColor, color: s.textColor || 'white', marginLeft: (s.childLevel * 15) + 'px' }} className="label label-default">
+            {s.name}
+        </span>
+    );
+}
+
+class GenericLabelSelect extends React.Component {
+    constructor() {
+        super();
+
+        this.onChange = (event, { newValue }) => {
+            this.props.onChange(newValue);
+        }
+
+        this.onSuggestionSelected = (evt, { suggestion }) => {
+            this.props.onSuggestionSelected({ ...suggestion });
+            setTimeout(() => this.input.blur(), 1);
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Autosuggest className="auto-suggest-label"
+                             suggestions={this.props.suggestions}
+                             shouldRenderSuggestions={() => true}
+                             getSuggestionValue={getSuggestionValue}
+                             onSuggestionSelected={this.onSuggestionSelected}
+                             renderSuggestion={renderSuggestion}
+                             ref={el => { if (el && el.input){ this.input = el.input; } }}
+                             inputProps={ {...this.props.inputProps} } />
+            </div>
+        );
+    }
+}
+
+export default GenericLabelSelect;
