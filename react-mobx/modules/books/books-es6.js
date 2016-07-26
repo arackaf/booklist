e69dfcf437ee75e6ca508040834f54a34b72099c
Adam Rackis <adamrackis@hotmail.com>
2016-07-26 14:50:32 -0500

    Cleaning out all books stuff - clean slate

--- react-mobx/modules/books/books-es6.js
+++ react-mobx/modules/books/books-es6.js
@@ -1,8 +1,6 @@
 import BookViewList from './components/bookViewList';
-import { reducer, selector } from './reducers/reducer';
 
 export default {
     name: 'books',
-    reducer: reducer,
     component: BookViewList
 };
--- react-mobx/reactStartup-es6.js
+++ react-mobx/reactStartup-es6.js
@@ -1,26 +1,31 @@
 import HashUtility, { SerializedHash } from 'util/hashManager';
 import { renderUI, clearUI } from 'applicationRoot/renderUI';
-import { store, getNewReducer } from 'applicationRoot/store';
 import { createElement } from 'react';
 
 import 'util/ajaxUtil';
 
 let currentModule;
+let publicUserCache = {};
 
 window.onhashchange = function () {
     loadCurrentModule();
 };
 
-const validModules = new Set(['books', 'scan', 'home', 'activate']);
+let initial = true;
+const validModules = new Set(['books', 'scan', 'home', 'activate', 'view']);
+
+export const globalHashManager = new HashUtility();
 
 loadCurrentModule();
 export function loadCurrentModule() {
     let hash = window.location.hash.replace('#', ''),
         originalModule = hash.split('/')[0] || '',
-        module = (hash.split('/')[0] || 'home').toLowerCase();
+        module = (hash.split('/')[0] || 'home').toLowerCase(),
+        publicModule = module === 'view';
 
     let loggedIn = isLoggedIn();
-    if (!loggedIn){
+
+    if (!loggedIn && !publicModule){
         if (originalModule && module != 'home'){
             module = 'authenticate';
         } else {
@@ -33,19 +38,41 @@ export function loadCurrentModule() {
         }
     }
 
-    if (module === currentModule) return;
+    if (publicModule){
+        var userId = globalHashManager.currentParameters.userId;
+
+        //switching to a new public viewing - reload page
+        //if (!initial && store.getState().root.publicUserId != userId){
+        //    location.reload();
+        //    return;
+        //}
+
+        var publicUserPromise = userId ? (publicUserCache[userId] || (publicUserCache[userId] = fetchPublicUserInfo(userId))) : null;
+
+        if (module === 'view') {
+            module = 'books';
+        }
+    } //else if (store.getState().root.publicUserId){
+    //    //leaving public viewing - reload page
+    //    location.reload();
+    //    return;
+    //}
+
+    initial = false;
+
+    //if (module === currentModule) return;
     currentModule = module;
 
-    System.import(`./modules/${module}/${module}`).then(({ default: module }) => {
-        clearUI();
-        if (module.reducer) {
-            getNewReducer({name: module.name, reducer: module.reducer});
-        }
-        renderUI(createElement(module.component));
+    Promise.all([
+        System.import(`./modules/${module}/${module}`),
+        publicUserPromise
+    ]).then(([{ default: module }, publicUserInfo]) => {
+        //if (publicUserInfo){
+        //    store.dispatch({ type: 'SET_PUBLIC_INFO', name: publicUserInfo.name, booksHeader: publicUserInfo.booksHeader, _id: userId });
+        //}
+        renderUI(createElement(module.component, { hashParameters: globalHashManager.currentParameters }));
     });
 }
-
-export const globalHashManager = new HashUtility();
 
 export function isLoggedIn(){
     return /logged_in/ig.test(document.cookie);
@@ -55,4 +82,12 @@ export function goHome(){
     let currentModule = globalHashManager.getCurrentHashInfo().module || 'home';
     if (currentModule === 'home') return;
     globalHashManager.setHash(new SerializedHash('home'));
+}
+
+function fetchPublicUserInfo(userId){
+    return new Promise((res, rej) => {
+        ajaxUtil.post('/user/getPubliclyAvailableUsersName', { _id: userId }, resp => {
+            res({ name: resp.name, booksHeader: resp.booksHeader  });
+        });
+    });
 }
