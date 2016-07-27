77c0726cc87d6a08b1fdcdbaaebf652439be76b6
Adam Rackis <adamrackis@gmail.com>
2016-07-27 15:14:03 -0500

    desktop ui imported - not running yet - property passing started with odd results

--- react-mobx/modules/books/model/bookLoader-es6.js
+++ react-mobx/modules/books/model/bookLoader-es6.js
@@ -2,7 +2,11 @@ import { observable, computed } from 'mobx';
 import { observer } from "mobx-react";
 
 class BookLoader {
-    @observable books = []
+    @observable rawBooks = []
+    @computed get books(){
+        debugger;
+        return this.rawBooks.map(b => this.adjustBookForDisplay(b));
+    }
     load(bookSearch = {}, publicUserId = ''){
         Promise.resolve(ajaxUtil.get('/book/searchBooks', {
             search: bookSearch.search,
@@ -17,8 +21,20 @@ class BookLoader {
             pagesOperator: bookSearch.pagesOperator,
             userId: publicUserId
         })).then(resp => {
-            this.books = resp.results;
+            debugger;
+            this.rawBooks = resp.results;
         });
+    }
+    adjustBookForDisplay(rawBook){
+        debugger;
+        let book = Object.assign({}, rawBook);
+        book.subjectObjects = (book.subjects || []).map(_id => ({ _id, name: 'soon' })); //.map(s => subjectsHash[s]).filter(s => s);
+        book.tagObjects = (book.tags || []).map(_id => ({ _id, name: 'soon' })); //.map(s => tagHash[s]).filter(s => s);
+        book.authors = book.authors || [];
+
+        let d = new Date(+book.dateAdded);
+        book.dateAddedDisplay = `${(d.getMonth()+1)}/${d.getDate()}/${d.getFullYear()}`;
+        return book;
     }
 }
 
