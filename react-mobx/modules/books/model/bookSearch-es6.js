1fe8eb66078916f6cea7225d9928f7d7290f9ee4
Adam Rackis <adamrackis@gmail.com>
2016-07-28 12:47:13 -0500

    bookSearch started

--- react-mobx/modules/books/model/bookSearch-es6.js
+++ react-mobx/modules/books/model/bookSearch-es6.js
@@ -16,13 +16,37 @@ class BookSearchFields{
 }
 
 class BookSearch{
+    @observable searchChanging = false
+    beginSearchChange = () => this.searchChanging = true;
+    endSearchChange = () => this.searchChanging = false;
+
     @observable pendingSearch = new BookSearchFields()
     @observable activeSearch = new BookSearchFields()
 
-    setSearchFields(packet){
-        Object.assign(this.activeSearch);
+    setPendingSearchChildSubjects = evt => this.pendingSearch.searchChildSubjects = evt.target.checked;
+    setPendingPagesOperator = evt => this.pendingSearch.pagesOperator = evt.target.value;
+    setPendingField(property, evt){
+        this.pendingSearch[property] = evt.target.value;
+    }
+
+    setSearchFieldsFromHashPacket(hashPacket){
+        Object.assign(this.activeSearch, this.getNextFilters(hashPacket));
         this.pendingSearch = this.activeSearch;
     }
+
+    getNextFilters(hashPacket) {
+        return Object.assign({}, hashPacket, {
+            subjects: this.idStringToObject(hashPacket.subjects),
+            tags: this.idStringToObject(hashPacket.tags),
+            searchChildSubjects: hashPacket.searchChildSubjects ? true : null,
+            sortDirection: hashPacket.sortDirection == 'asc' ? 1 : -1
+        });
+    }
+
+    idStringToObject(str = '') {
+        return str.split('-').filter(s => s).reduce((obj, val) => (obj[val] = true, obj), {});
+    }
+
     setPendingSearch(){
         this.activeSearch = this.pendingSearch;
     }
