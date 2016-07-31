7639c7ef804d60b8ba527d6de37cf874f77493a2
Adam Rackis <adamrackis@hotmail.com>
2016-07-30 22:55:21 -0500

    stacking and unwinding

--- react-mobx/modules/books/model/bookModuleStore-es6.js
+++ react-mobx/modules/books/model/bookModuleStore-es6.js
@@ -23,18 +23,40 @@ class Subject{
     }
 }
 
+const subjectCompare = ({ name: name1 }, { name: name2 }) => {
+    let name1After = name1.toLowerCase() > name2.toLowerCase(),
+        bothEqual = name1.toLowerCase() === name2.toLowerCase();
+    return bothEqual ? 0 : (name1After ? 1 : -1);
+};
+
+const addAndRecurse = subjects => {
+    let result = [];
+    subjects.forEach(s => {
+        result.push(s);
+        if (s.children.length){
+            result.push(...addAndRecurse(addAndRecurse(s.children)));
+        }
+    })
+    return result;
+}
+
 class Store {
     @observable books = []
-    @observable subjects = []
+    @observable subjects = {}
     @computed get stackedSubjects(){
-        let result = this.subjects.map(s => toJS(s));
-        result.forEach(s => s.children = result.filter(sc => new RegExp(`,${s._id},$`).test(sc.path)));
-        return result.filter(s => s.path == null);
+        let result = Object.keys(this.subjects).map(_id => Object.assign(toJS(this.subjects[_id]), { childLevel: this.subjects[_id].childLevel }));
+        result.forEach(s => s.children = result.filter(sc => new RegExp(`,${s._id},$`).test(sc.path)).sort(subjectCompare));
+        return result.filter(s => s.path == null).sort(subjectCompare);
+    }
+    @computed get unwoundSubjects(){
+        return addAndRecurse(this.stackedSubjects);
     }
 
     loadSubjects(){
         Promise.resolve(ajaxUtil.get('/subject/all', { })).then(resp => {
-            this.subjects = resp.results.map(s => new Subject(s));
+            this.subjects = resp.results.map(s => new Subject(s)).reduce((hash, s) => (hash[s._id] = s, hash), {});
+
+            let s = this.subjects['565bb7e37365ef7636df64fa'];
         });
     }
     loadBooks(bookSearch = {}, publicUserId = ''){
