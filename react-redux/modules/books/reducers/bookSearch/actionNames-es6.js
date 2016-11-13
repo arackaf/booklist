d602f3bc6cf4af44cec92ad7ea8b31df35cd7d1a
Adam Rackis <adamrackis@gmail.com>
2016-11-12 21:36:00 -0600

    more bookList view refactorings

--- react-redux/modules/books/reducers/bookSearch/actionNames-es6.js
+++ react-redux/modules/books/reducers/bookSearch/actionNames-es6.js
@@ -8,3 +8,9 @@ export const SET_FILTERS = 'SET_FILTERS';
 export const SET_VIEWING_USERID = 'SET_VIEWING_USERID';
 export const SET_SEARCH_SUBJECTS_VALUE = 'SET_SEARCH_SUBJECTS_VALUE';
 export const SET_SEARCH_TAGS_VALUE = 'SET_SEARCH_TAGS_VALUE';
+
+export const SET_GRID_VIEW = 'booksSearch.SET_GRID_VIEW';
+export const SET_BASIC_LIST_VIEW = 'booksSearch.SET_BasicList_VIEW';
+
+export const GRID_VIEW = 'books table view';
+export const BASIC_LIST_VIEW = 'books mobile view';
