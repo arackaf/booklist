91bdf23d65b189fd4cdf44297fecc5f610511936
Adam Rackis <adamrackis@gmail.com>
2016-11-20 16:32:33 -0600

    Dragging started

--- react-redux/modules/subjects/components/subjectsList-es6.js
+++ react-redux/modules/subjects/components/subjectsList-es6.js
@@ -2,14 +2,35 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {selector} from 'modules/subjects/reducers/reducer';
 import * as actionCreators from 'modules/subjects/reducers/actionCreators';
+import {DragSource, DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+@DragSource('subject', {
+    beginDrag(props, monitor, component){
+        return props;
+    }
+}, (connect, monitor) => ({
+    // Call this function inside render()
+    // to let React DnD handle the drag events:
+    connectDragSource: connect.dragSource(),
+    // You can ask the monitor about the current drag state:
+    isDragging: monitor.isDragging()
+}))
+class SubjectDisplay extends Component {
+    render(){
+        let {_id, name, connectDragSource} = this.props;
+
+        return (
+            connectDragSource(<li key={_id}>{name}</li>)
+        )
+    }
+}
 
 export default class SubjectsList extends Component{
     render(){
         return (
             <div style={{ margin: '50px' }}>
-                <ul>{this.props.subjects.map(s =>
-                    <li key={s._id}>{s.name}</li>
-                )}</ul>
+                <ul>{this.props.subjects.map(subject => <SubjectDisplay {...subject} />)}</ul>
             </div>
         )
     }
@@ -17,4 +38,4 @@ export default class SubjectsList extends Component{
 
 const SubjectsListConnected = connect(selector, { ...actionCreators })(SubjectsList);
 
-export default SubjectsListConnected;
+export default DragDropContext(HTML5Backend)(SubjectsListConnected);
