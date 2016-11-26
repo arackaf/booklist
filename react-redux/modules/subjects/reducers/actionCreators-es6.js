617328af9e447d6f00d94823cf35143c54baa4c6
Adam Rackis <adamrackis@hotmail.com>
2016-11-26 10:28:13 -0600

    actually saving

--- react-redux/modules/subjects/reducers/actionCreators-es6.js
+++ react-redux/modules/subjects/reducers/actionCreators-es6.js
@@ -38,33 +38,38 @@ export const beginSubjectEdit = _id => (dispatch, getState) =>{
 
 export const setEditingSubjectField = (_id, field, value) => ({ type: SET_EDITING_SUBJECT_FIELD, _id, field, value });
 
-export function saveChanges(subject, original){
-    return function(dispatch, getState) {
-        let { _id, name, parentId, backgroundColor, textColor } = subject,
-            request = { _id, name, parentId, backgroundColor, textColor };
+export const saveChanges = (subject, original) => (dispatch, getState) => {
+    let { _id, name, parentId, backgroundColor, textColor } = subject,
+        request = { _id, name, parentId, backgroundColor, textColor };
 
-        let oldParentId = computeParentId(getState().app.subjectHash[_id].path);
-        let subjectsSavingHash;
-        if (oldParentId != subject.parentId){
-            subjectsSavingHash = toIdHash(unwindSubjects([original]))
-            subjectsSavingHash[subject.parentId] = true;
+    let oldParentId = computeParentId(getState().app.subjectHash[_id].path);
+    let subjectsSavingHash;
+    if (oldParentId != subject.parentId){
+        subjectsSavingHash = toIdHash(unwindSubjects([original]))
+        subjectsSavingHash[subject.parentId] = true;
+
+        if (subject.parentId) {
             let newParentPath = getState().app.subjectHash[subject.parentId].path;
-            if (newParentPath){
+            if (newParentPath) {
                 newParentPath.split(',').filter(s => s).forEach(_id => subjectsSavingHash[_id] = true);
             }
-        } else {
-            subjectsSavingHash = toIdHash([subject]);
         }
-
-        dispatch({ type: SUBJECTS_SAVING, subjects: subjectsSavingHash });
-        //setTimeout(() => saveSubjectRoot(request, dispatch), 1500);
-        setTimeout(() => dispatch({ type: CLEAR_SAVING_STATE, subjects: subjectsSavingHash }), 1500);
+    } else {
+        subjectsSavingHash = toIdHash([subject]);
     }
+
+    dispatch({ type: SUBJECTS_SAVING, subjects: subjectsSavingHash });
+    Promise.resolve(saveSubjectRoot(request, dispatch))
+           .then(() => {
+               dispatch({ type: CLEAR_SAVING_STATE, subjects: subjectsSavingHash });
+               dispatch({ type: CANCEL_SUBJECT_EDIT, _id });
+           });
 }
 
 export const setNewParent = (subject, newParent) => (dispatch, getState) => {
-    let _id = subject._id,
-        adjustedSubject = {...subject};
+    let { _id, name, backgroundColor, textColor } = subject,
+        adjustedSubject = {...subject},
+        request = {_id, name, backgroundColor, textColor, parentId: newParent._id};
 
     if (!newParent.path){
         adjustedSubject.path = `,${newParent._id},`;
@@ -72,17 +77,14 @@ export const setNewParent = (subject, newParent) => (dispatch, getState) => {
         adjustedSubject.path = `${newParent.path},${newParent._id},`;
     }
 
-    let adjustedSubjectsHash = toIdHash(unwindSubjects([adjustedSubject]));
-    adjustedSubject.path.split(',').filter(s => s).forEach(_id => adjustedSubjectsHash[_id] = true);
+    let subjectsSavingHash = toIdHash(unwindSubjects([adjustedSubject]));
+    adjustedSubject.path.split(',').filter(s => s).forEach(_id => subjectsSavingHash[_id] = true);
 
     //provide immediate feedback, so the DnD "sticks"
     dispatch({ type: SAVE_SUBJECT_RESULTS, affectedSubjects: [adjustedSubject] });
     //disable dragging and editing on the entire hierarchy until the save is done
-    dispatch({ type: SUBJECTS_SAVING, subjects: adjustedSubjectsHash });
+    dispatch({ type: SUBJECTS_SAVING, subjects: subjectsSavingHash });
 
-    setTimeout(() => {
-        dispatch({ type: SUBJECTS_DONE_SAVING, subjects: adjustedSubjectsHash })
-
-        setTimeout(() => dispatch({ type: CLEAR_SAVING_STATE, subjects: adjustedSubjectsHash }), 1000);
-    }, 1500);
-}
+    Promise.resolve(saveSubjectRoot(request, dispatch))
+           .then(() => dispatch({ type: CLEAR_SAVING_STATE, subjects: subjectsSavingHash }));
+};
