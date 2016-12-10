03f1f94574d7fde64e77f7ad010961be08b01a38
Adam Rackis <adamrackis@gmail.com>
2016-12-10 16:55:10 -0600

    caching files

--- swRoot.js
+++ swRoot.js
@@ -1,15 +1,25 @@
 self.addEventListener('install', function(event) {
     console.log('INSTALLED');
+
+    console.log('ADDING CACHE FILES');
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                '/react-redux/node_modules/react/dist/react-with-addons.js',
+                '/react-redux/node_modules/react-dom/dist/react-dom.js'
+            ]).then(function(){ console.log('cache filling success'); }).catch(function(){ console.log('cache filling failure') });
+        })
+    );
 });
 
 console.log('ADDING FETCH at root level');
 self.addEventListener('fetch', function(event) {
-    //console.log('fetching ->', event.request);
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
                 // Cache hit - return response
                 if (response) {
+                    console.log('cache hit', event.request);
                     return response;
                 }
                 return fetch(event.request);
