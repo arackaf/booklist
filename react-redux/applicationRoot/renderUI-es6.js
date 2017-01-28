fc12f928d9cc083520fe4c311cf95f20acb76020
Adam Rackis <adamrackis@hotmail.com>
2017-01-27 21:26:36 -0600

    Lots of nice style tweaks

--- react-redux/applicationRoot/renderUI-es6.js
+++ react-redux/applicationRoot/renderUI-es6.js
@@ -15,7 +15,7 @@ const WellUiSwitcher = connect(state => state.app, {requestDesktop, requestMobil
         showSwitchBackMobile = props.isMobile && props.showingDesktop;
 
     return (
-        <div className="well well-sm" style={{ position: 'fixed', bottom: 0, left: 0, right: 0, marginBottom: 0 }}>
+        <div className="well well-sm" style={{ position: 'fixed', bottom: 0, left: 0, right: 0, marginBottom: 0, borderRadius: 0 }}>
             <img width="16" height="16" src="/static/main-icon2.png"/>
             <span style={{marginLeft: '5px', marginRight: '5px'}}>My Library</span>
             { showChooseDesktop ? <a onClick={props.requestDesktop}>Use desktop version</a> : null }
@@ -35,7 +35,7 @@ export function clearUI(){
 export function renderUI(component){
     render(
         <Provider store={store}>
-            <div>
+            <div style={{position: 'fixed', top: 55, left: 0, right: 0, bottom: 35, overflowX: 'hidden', overflowY: 'scroll'}}>
                 <MobileMeta />
                 <MainNavigationBar />
                 { component }
