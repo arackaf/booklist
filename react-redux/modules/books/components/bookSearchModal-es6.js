8ebeee76e118040464fcb2ea004f31f5a09203ca
Adam Rackis <adamrackis@gmail.com>
2017-01-29 16:21:56 -0600

    refactor book search modal - end goal fix z-index

---
+++ react-redux/modules/books/components/bookSearchModal-es6.js
@@ -0,0 +1,137 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bookSearchSelector} from 'modules/books/reducers/bookSearch/reducer';
+import {Modal} from 'simple-react-bootstrap';
+import BootstrapButton from 'applicationRoot/components/bootstrapButton';
+
+import * as booksActionCreators from '../reducers/books/actionCreators';
+import * as bookSearchActionCreators from '../reducers/bookSearch/actionCreators';
+import * as subjectsActionCreators from '../reducers/subjects/actionCreators';
+import * as tagsActionCreators from '../reducers/tags/actionCreators';
+import * as booksSubjectModificationActionCreators from '../reducers/booksSubjectModification/actionCreators';
+import * as booksTagModificationActionCreators from '../reducers/booksTagModification/actionCreators';
+
+import GenericLabelSelect from 'applicationRoot/components/genericLabelSelect'
+import {RemovableLabelDisplay} from 'applicationRoot/components/labelDisplay';
+import {InputForPending, RadioForPending} from './pendingInputs';
+
+class BookSearchModal extends Component {
+    render(){
+        return (
+            <Modal className="fade" show={this.props.editingFilters} onHide={this.props.endFilterChanging}>
+                <Modal.Header closeButton>
+                    <button type="button" className="close" onClick={this.props.endFilterChanging} aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 className="modal-title">Full search</h4>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <div className="row">
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>Title</label>
+                                    <InputForPending name="search" parentProps={this.props} placeholder="Search title" />
+                                </div>
+                            </div>
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>Pages</label>
+                                    <div className="form-inline">
+                                        <div style={{ marginRight: 10 }} className="form-group">
+                                            <select onChange={this.props.setPendingPagesOperator} value={this.props.pending.pagesOperator} className="form-control">
+                                                <option value="lt">{'<'}</option>
+                                                <option value="gt">{'>'}</option>
+                                            </select>
+                                        </div>
+                                        <div className="form-group">
+                                            <InputForPending name="pages" parentProps={this.props} type="number" placeholder="Number of pages" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>Publisher</label>
+                                    <InputForPending name="publisher" parentProps={this.props} placeholder="Publisher" />
+                                </div>
+                            </div>
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>Author</label>
+                                    <InputForPending name="author" parentProps={this.props} placeholder="Author" />
+                                </div>
+                            </div>
+                            <div className="col-xs-6">
+                                <div className="form-group">
+                                    <label>Is read?</label>
+                                    <br />
+                                    <div style={{display: 'inline'}} className="radio">
+                                        <label>
+                                            <RadioForPending name="isRead" parentProps={this.props} value={''} />
+                                            Either
+                                        </label>
+                                    </div>
+                                    <div style={{display: 'inline', marginLeft: '20px'}} className="radio">
+                                        <label>
+                                            <RadioForPending name="isRead" parentProps={this.props} value='1' />
+                                            Yes
+                                        </label>
+                                    </div>
+                                    <div style={{display: 'inline', marginLeft: '20px'}} className="radio">
+                                        <label>
+                                            <RadioForPending name="isRead" parentProps={this.props} value='0' />
+                                            No
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                    <div className="row" style={{ position: 'relative' }}>
+                        <div className="col-xs-3">
+                            <GenericLabelSelect
+                                inputProps={{ placeholder: 'Tags', value: this.props.searchTagsValue, onChange: this.props.setSearchTagsValue }}
+                                suggestions={this.props.eligibleFilterTags}
+                                onSuggestionSelected={this.props.addPendingTag} />
+                        </div>
+                        <div className="col-xs-9">
+                            <div>
+                                {this.props.pendingSelectedTags.map(t =>
+                                    <RemovableLabelDisplay className="margin-left" item={t} doRemove={() => this.props.removePendingTag(t._id)} />)}
+                            </div>
+                        </div>
+                    </div>
+
+                    <br />
+
+                    <div className="row" style={{ position: 'relative' }}>
+                        <div className="col-xs-3">
+                            <GenericLabelSelect
+                                inputProps={{ placeholder: 'Subjects', value: this.props.searchSubjectsValue, onChange: this.props.setSearchSubjectsValue }}
+                                suggestions={this.props.eligibleFilterSubjects}
+                                onSuggestionSelected={this.props.addPendingSubject} />
+                        </div>
+                        <div className="col-xs-9">
+                            <div>
+                                {this.props.pendingSelectedSubjects.map(s =>
+                                    <RemovableLabelDisplay className="margin-left" item={s} doRemove={() => this.props.removePendingSubject(s._id)} />)}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="checkbox">
+                        <label>
+                            <input type="checkbox" onChange={this.props.setPendingSearchChildSubjects} checked={this.props.pending.searchChildSubjects} /> Also search child subjects
+                        </label>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <BootstrapButton preset="primary" className="pull-left" onClick={this.props.applyFilters}>Filter</BootstrapButton>
+                    <BootstrapButton preset="default" onClick={this.props.endFilterChanging}>Close</BootstrapButton>
+                </Modal.Footer>
+            </Modal>
+        )
+    }
+}
+
+export default connect(bookSearchSelector, { ...bookSearchActionCreators, ...booksActionCreators, ...subjectsActionCreators, ...booksSubjectModificationActionCreators, ...booksTagModificationActionCreators, ...tagsActionCreators })(BookSearchModal);
--- react-redux/modules/books/components/booksMenuBar-es6.js
+++ react-redux/modules/books/components/booksMenuBar-es6.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Modal, NavBar} from 'simple-react-bootstrap';
+import {NavBar} from 'simple-react-bootstrap';
 
-import BootstrapButton, {BootstrapAnchorButton} from 'applicationRoot/components/bootstrapButton';
+import {BootstrapAnchorButton} from 'applicationRoot/components/bootstrapButton';
 
 import {bookSearchSelector} from 'modules/books/reducers/bookSearch/reducer';
 
@@ -14,22 +14,8 @@ import * as booksSubjectModificationActionCreators from '../reducers/booksSubjec
 import * as booksTagModificationActionCreators from '../reducers/booksTagModification/actionCreators';
 import {globalHashManager} from 'reactStartup';
 
-import GenericLabelSelect from 'applicationRoot/components/genericLabelSelect'
-import { RemovableLabelDisplay } from 'applicationRoot/components/labelDisplay';
-
-const InputForPending = props => {
-    let name = props.name,
-        actionName = `setPending${name[0].toUpperCase()}${name.slice(1)}`,
-        parentProps = props.parentProps;
-    return <input { ...props } className={`form-control ${props.className || ''}`} onKeyDown={parentProps[actionName]} onChange={parentProps[actionName]} value={parentProps.pending[name]} />;
-}
-
-const RadioForPending = props => {
-    let {name, value, parentProps, ...rest} = props,
-        actionName = `setPending${name[0].toUpperCase()}${name.slice(1)}`;
-
-    return <input { ...rest } type="radio" className={`${props.className || ''}`} onClick={parentProps[actionName]} checked={parentProps.pending[name] === value} value={value} />;
-}
+import {RemovableLabelDisplay} from 'applicationRoot/components/labelDisplay';
+import {InputForPending, RadioForPending} from './pendingInputs';
 
 class BooksMenuBar extends React.Component {
     removeFilterSubject(_id){
@@ -140,119 +126,6 @@ class BooksMenuBar extends React.Component {
                         </NavBar.Nav> : null
                     }
                 </NavBar>
-
-                <Modal className="fade" show={this.props.editingFilters} onHide={this.props.endFilterChanging}>
-                    <Modal.Header closeButton>
-                        <button type="button" className="close" onClick={this.props.endFilterChanging} aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 className="modal-title">Full search</h4>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <form>
-                            <div className="row">
-                                <div className="col-xs-6">
-                                    <div className="form-group">
-                                        <label>Title</label>
-                                        <InputForPending name="search" parentProps={this.props} placeholder="Search title" />
-                                    </div>
-                                </div>
-                                <div className="col-xs-6">
-                                    <div className="form-group">
-                                        <label>Pages</label>
-                                        <div className="form-inline">
-                                            <div style={{ marginRight: 10 }} className="form-group">
-                                                <select onChange={this.props.setPendingPagesOperator} value={this.props.pending.pagesOperator} className="form-control">
-                                                    <option value="lt">{'<'}</option>
-                                                    <option value="gt">{'>'}</option>
-                                                </select>
-                                            </div>
-                                            <div className="form-group">
-                                                <InputForPending name="pages" parentProps={this.props} type="number" placeholder="Number of pages" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-xs-6">
-                                    <div className="form-group">
-                                        <label>Publisher</label>
-                                        <InputForPending name="publisher" parentProps={this.props} placeholder="Publisher" />
-                                    </div>
-                                </div>
-                                <div className="col-xs-6">
-                                    <div className="form-group">
-                                        <label>Author</label>
-                                        <InputForPending name="author" parentProps={this.props} placeholder="Author" />
-                                    </div>
-                                </div>
-                                <div className="col-xs-6">
-                                    <div className="form-group">
-                                        <label>Is read?</label>
-                                        <br />
-                                        <div style={{display: 'inline'}} className="radio">
-                                            <label>
-                                                <RadioForPending name="isRead" parentProps={this.props} value={''} />
-                                                Either
-                                            </label>
-                                        </div>
-                                        <div style={{display: 'inline', marginLeft: '20px'}} className="radio">
-                                            <label>
-                                                <RadioForPending name="isRead" parentProps={this.props} value='1' />
-                                                Yes
-                                            </label>
-                                        </div>
-                                        <div style={{display: 'inline', marginLeft: '20px'}} className="radio">
-                                            <label>
-                                                <RadioForPending name="isRead" parentProps={this.props} value='0' />
-                                                No
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-
-                        <div className="row" style={{ position: 'relative' }}>
-                            <div className="col-xs-3">
-                                <GenericLabelSelect
-                                    inputProps={{ placeholder: 'Tags', value: this.props.searchTagsValue, onChange: this.props.setSearchTagsValue }}
-                                    suggestions={this.props.eligibleFilterTags}
-                                    onSuggestionSelected={this.props.addPendingTag} />
-                            </div>
-                            <div className="col-xs-9">
-                                <div>
-                                    {this.props.pendingSelectedTags.map(t =>
-                                        <RemovableLabelDisplay className="margin-left" item={t} doRemove={() => this.props.removePendingTag(t._id)} />)}
-                                </div>
-                            </div>
-                        </div>
-
-                        <br />
-
-                        <div className="row" style={{ position: 'relative' }}>
-                            <div className="col-xs-3">
-                                <GenericLabelSelect
-                                    inputProps={{ placeholder: 'Subjects', value: this.props.searchSubjectsValue, onChange: this.props.setSearchSubjectsValue }}
-                                    suggestions={this.props.eligibleFilterSubjects}
-                                    onSuggestionSelected={this.props.addPendingSubject} />
-                            </div>
-                            <div className="col-xs-9">
-                                <div>
-                                    {this.props.pendingSelectedSubjects.map(s =>
-                                        <RemovableLabelDisplay className="margin-left" item={s} doRemove={() => this.props.removePendingSubject(s._id)} />)}
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="checkbox">
-                            <label>
-                                <input type="checkbox" onChange={this.props.setPendingSearchChildSubjects} checked={this.props.pending.searchChildSubjects} /> Also search child subjects
-                            </label>
-                        </div>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <BootstrapButton preset="primary" className="pull-left" onClick={this.props.applyFilters}>Filter</BootstrapButton>
-                        <BootstrapButton preset="default" onClick={this.props.endFilterChanging}>Close</BootstrapButton>
-                    </Modal.Footer>
-                </Modal>
             </div>
         )
     }
