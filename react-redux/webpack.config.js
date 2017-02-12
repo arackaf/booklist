e194ea57a20a4f16220b2f5d7546f76ccee7a4c0
Adam Rackis <adamrackis@gmail.com>
2017-02-12 00:29:37 -0600

    webpack started

---
+++ react-redux/webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+
+module.exports = {
+    entry: './reactStartup.js',
+    context: __dirname,
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            'simple-react-bootstrap': 'node_modules/simple-react-bootstrap/dist/simple-react-bootstrap.js',
+            'jscolor': 'util/jscolor.js'
+        },
+        modules: [
+            path.resolve('./'),
+            path.resolve('./node_modules'),
+        ]
+    },
+    module: {
+        loaders: [
+            {
+                test: /(\.js$|\.es6$)/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+};
