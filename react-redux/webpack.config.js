57c8271809633ecdcdb200e491fa238a24e7d7da
Adam Rackis <adamrackis@gmail.com>
2017-02-12 20:24:24 -0600

    Webpack setup

--- react-redux/webpack.config.js
+++ react-redux/webpack.config.js
@@ -1,5 +1,6 @@
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
     entry: './reactStartup.js',
@@ -30,7 +31,36 @@ module.exports = {
             }
         ]
     },
-    plugins: [new BundleAnalyzerPlugin({
-        analyzerMode: 'static'
-    })]
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static'
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            filename: 'all-node.js',
+            async: 'all-node',
+            minChunks(module, count) {
+                var context = module.context;
+                return context && context.indexOf('node_modules') >= 0;
+            },
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            filename: 'react-dnd.js',
+            async: 'react-dnd',
+            minChunks(module, count) {
+                var context = module.context;
+                var targets = ['react-dnd', 'react-dnd-html5-backend', 'react-dnd-touch-backend']
+                return context && context.indexOf('node_modules') >= 0 && targets.find(t => new RegExp('\\\\' + t + '\\\\', 'i').test(context));
+            },
+        }),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     async: true,
+        //     filename: 'react-bundle.js',
+        //     minChunks(module, count) {
+        //         var context = module.context;
+        //         return context && context.indexOf('node_modules\\react\\') >= 0;
+        //     },
+        // }),
+    ]
 };
