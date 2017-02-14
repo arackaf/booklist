a82b5917e80d0c11f8e417181809669e5d6156ef
Adam Rackis <adamrackis@hotmail.com>
2017-02-14 00:24:48 -0600

    WebPack config

--- react-redux/webpack.config.js
+++ react-redux/webpack.config.js
@@ -4,6 +4,74 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: './reactStartup.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            'simple-react-bootstrap': 'node_modules/simple-react-bootstrap/dist/simple-react-bootstrap.js',
+            'jscolor': 'util/jscolor.js'
+        },
+        modules: [
+            path.resolve('./'),
+            path.resolve('./node_modules'),
+        ]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static'
+        }),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'node-static',
+            filename: 'node-static.js',
+            minChunks(module, count) {
+                var context = module.context;
+                return context && context.indexOf('node_modules') >= 0;
+            },
+        }),
+
+        //*********************************** async chunks*************************
+
+        //catch all - anything used in more than one place
+        new webpack.optimize.CommonsChunkPlugin({
+            filename: 'used-twice.js',
+            async: 'used-twice',
+            minChunks(module, count) {
+                return count >= 2;
+            },
+        }),
+
+        //specifically bundle these large things
+        new webpack.optimize.CommonsChunkPlugin({
+            filename: 'react-dnd.js',
+            async: 'react-dnd',
+            minChunks(module, count) {
+                var context = module.context;
+                var targets = ['react-dnd', 'react-dnd-html5-backend', 'react-dnd-touch-backend', 'dnd-core']
+                return context && context.indexOf('node_modules') >= 0 && targets.find(t => new RegExp('\\\\' + t + '\\\\', 'i').test(context));
+            },
+        }),
+    ]
+};
+
+
+
+module.exportsFINAL = {
+    entry: './reactStartup.js',
     context: __dirname,
     output: {
         filename: 'bundle.js',
@@ -54,13 +122,5 @@ module.exports = {
                 return context && context.indexOf('node_modules') >= 0 && targets.find(t => new RegExp('\\\\' + t + '\\\\', 'i').test(context));
             },
         }),
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     async: true,
-        //     filename: 'react-bundle.js',
-        //     minChunks(module, count) {
-        //         var context = module.context;
-        //         return context && context.indexOf('node_modules\\react\\') >= 0;
-        //     },
-        // }),
     ]
 };
