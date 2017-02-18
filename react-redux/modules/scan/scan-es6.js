d086bbddeb6af506048790d7f5badc94c051e0a3
Adam Rackis <adamrackis@gmail.com>
2017-02-17 19:40:22 -0600

    mass migration away from old transpiler

--- react-redux/modules/scan/scan-es6.js
+++
@@ -1,8 +0,0 @@
-import BookEntryList from './components/bookEntryList';
-import reducer from './reducers/reducer';
-
-export default {
-    name: 'scan',
-    reducer: reducer,
-    component: BookEntryList
-};
--- react-redux/util/immutableHelpers-es6.js
+++
@@ -1,5 +0,0 @@
-export const removeKeysFromObject = (obj, keys) =>{
-    let result = {...obj};
-    keys.forEach(k => delete result[k]);
-    return result;
-}
