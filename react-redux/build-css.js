c690437d9af815760f170b948c90bbd409cfeb0f
Adam Rackis <adamrackis@gmail.com>
2017-02-19 20:23:48 -0600

    wrap up

--- react-redux/build-css.js
+++ react-redux/build-css.js
@@ -4,6 +4,8 @@ var content = ['./dist/**/*.js']; //webpack-built content
 
 var minify = true;
 
+console.log('\ncreating css builds...');
+
 purify(content, ['./static/bootstrap/css/bootstrap.css'], {
   minify: minify,
   output: './static/bootstrap/css/bootstrap-booklist-build.css'
@@ -14,4 +16,4 @@ purify(content, ['./static/fontawesome/css/font-awesome.css'], {
   output: './static/fontawesome/css/font-awesome-booklist-build.css'
 });
 
-
+console.log('css builds complete');
