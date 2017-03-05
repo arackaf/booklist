2c05369630ad7e673dff6e39fcb6fa9e3084cfaa
Adam Rackis <adamrackis@gmail.com>
2017-03-05 10:16:55 -0600

    add link to github repo

--- react-redux/modules/home/homeComponent.js
+++ react-redux/modules/home/homeComponent.js
@@ -41,13 +41,15 @@ class HomeIfNotLoggedIn extends React.Component{
                     Welcome to <i>My Library</i>.
                     <br /><br />
                     This site is my own little passion project, the purpose of which is to track your library.  You scan in your books (or manually type in the isbn)
-                    and the books' info is fetched from Amazon, and stored for you.  You can then flexibly search and categorize your library.
+                    and the books' info is fetched from Amazon, and stored for you.  You can then flexibly search and categorize your library. 
                     <br /><br />
                     So basically this site is of use to the extremely small percentage of people for whom the following are <i>all</i> true: they read a lot,
                     own the books they read, and read non-eBooks.  As I said, this is more of a passion project than anything.
                     <br /><br />
                     It's free to sign up, and store up to 500 books.  In the remote chance someone actually wants to store more than that, there'll be some sort
                     of nominal fee to help defray storage costs.
+                    <br /><br />
+                    For those interested in seeing the code for this site, The GitHub repository is <a target='_blank' href='https://github.com/arackaf/booklist'>here</a>
                     <br /><br />
                     <a className="btn btn-primary" href="#login">Login or create an account</a>
                 </MainHomePane>
