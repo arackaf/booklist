d51aa133e5a92eeaaca7a9dd72b8a84140178393
Adam Rackis <adamrackis@gmail.com>
2017-03-05 23:16:51 -0600

    basic user settings working

--- react-redux/modules/settings/reducers/userSettings/actionCreators.js
+++ react-redux/modules/settings/reducers/userSettings/actionCreators.js
@@ -1,4 +1,4 @@
-import {LOAD_USER_INFO, USER_INFO_LOADING, USER_INFO_LOADED} from './actionNames';
+import {LOAD_USER_INFO, USER_INFO_LOADING, USER_INFO_LOADED, SET_EDITING_INFO} from './actionNames';
 
 export function loadUserSettings(){
     return (dispatch, getState) => {
@@ -8,3 +8,9 @@ export function loadUserSettings(){
         });
     }
 }
+
+const createEditingChange = (name, evtName = 'value') => evt => dispatch => dispatch({type: SET_EDITING_INFO, name, value: evt.target[evtName]});
+
+export const editIsPublic = createEditingChange('isPublic', 'checked');
+export const editPublicName = createEditingChange('publicName');
+export const editPublicBooksHeader = createEditingChange('publicBooksHeader');
--- react-redux/modules/settings/reducers/userSettings/reducer.js
+++ react-redux/modules/settings/reducers/userSettings/reducer.js
@@ -1,9 +1,14 @@
-import {USER_INFO_LOADING, USER_INFO_LOADED} from './actionNames';
+import {createSelector} from 'reselect';
+import {USER_INFO_LOADING, USER_INFO_LOADED, SET_EDITING_INFO} from './actionNames';
 const defaultState = {
     loading: true,
     isPublic: false,
     publicBooksHeader: '',
-    publicName: ''
+    publicName: '',
+    editing: {
+        publicBooksHeader: '',
+        publicName: ''
+    }
 }
 
 export default (state = defaultState, action) => {
@@ -11,9 +16,17 @@ export default (state = defaultState, action) => {
         case USER_INFO_LOADING: 
             return {...state, loading: true};
         case USER_INFO_LOADED: 
-            return {...state, loading: false, ...action.info};
+            return {...state, loading: false, ...action.info, editing: {...action.info}};
+        case SET_EDITING_INFO:
+            return {...state, editing: {...state.editing, [action.name]: action.value}};
     }
     return state;
 };
 
-export const selector = state => state.settingsModule.userSettings;
+const dirtyProps = ['isPublic', 'publicName', 'publicBooksHeader'];
+export const selector = createSelector([
+    state => state.settingsModule.userSettings
+], (userSettings) => ({
+    ...userSettings,
+    isDirty: dirtyProps.some(p => userSettings[p] != userSettings.editing[p])
+}));
