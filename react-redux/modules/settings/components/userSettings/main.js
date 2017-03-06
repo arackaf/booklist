d56b091fe1ae171ba69a13cd655631b9564cdc27
Adam Rackis <adamrackis@hotmail.com>
2017-03-05 23:16:51 -0600

    basic user settings working

--- react-redux/modules/settings/components/userSettings/main.js
+++ react-redux/modules/settings/components/userSettings/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {selector} from '../../reducers/userSettings/reducer';
-import * as actionCreators from '../../reducers/userSettings/reducer';
+import * as actionCreators from '../../reducers/userSettings/actionCreators';
 import Loading from 'applicationRoot/components/loading';
 
 @connect(selector, {...actionCreators})
@@ -14,8 +14,27 @@ export default class UserSettings extends Component {
                     : 
                     <div style={{padding: '10px'}}>
                         <div className="form-group">
-                            <label className='checkbox'>Allow your book collection to be viewed publicly? <input defaultChecked={this.props.isPublic} type='checkbox' style={{marginLeft: '5px'}} /></label>
+                            <label className='checkbox'>Allow your book collection to be viewed publicly? <input checked={this.props.editing.isPublic} onChange={this.props.editIsPublic} style={{marginLeft: '5px'}} type='checkbox' /></label>
                         </div>
+                        {this.props.editing.isPublic ? 
+                            <div>
+                                <div className="form-group">
+                                    <label htmlFor="publicNameInput">Publicly display your name as</label>
+                                    <input value={this.props.editing.publicName} onChange={this.props.editPublicName} className="form-control" id="publicNameInput" placeholder="Public name" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="publicBooksHeader">Publicly display your name as</label>
+                                    <input value={this.props.editing.publicBooksHeader} onChange={this.props.editPublicBooksHeader} className="form-control" id="publicBooksHeader" placeholder="Book header" />
+                                </div>
+                                <div className="alert alert-info" role="alert">
+                                    This is what will show in the navigation when people view your book collection. Leave it blank
+                                    to default to [public name]'s Books.  See below
+                                </div>
+                                <img src='/react-redux/static/publicBookHeaderInfo.png' />
+                                <br />
+                                {this.props.isDirty ? <button className='btn btn-primary'>Save</button> : null}
+                            </div> : null
+                        }
                     </div>
                 }
 
