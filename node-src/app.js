3582a8b31430211e10ba5abe2c1835814d140b93
Adam Rackis <adamrackis@hotmail.com>
2017-05-09 23:38:23 -0500

    refresh main app file

--- node-src/app.js
+++ node-src/app.js
@@ -1,55 +1,55 @@
-'use strict';
-
 require('dotenv').config();
 
 require('regenerator/runtime');
 
 require('./app-helpers/promiseUtils');
-var dao = require('./dataAccess/dao').default;
+const dao = require('./dataAccess/dao').default;
 
-var express = require('express');
-var app = express();
-var path = require("path");
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var lwip = require('lwip');
-var exif = require('exif-parser');
-var compression = require('compression');
+const express = require('express');
+const app = express();
+const path = require("path");
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const lwip = require('lwip');
+const exif = require('exif-parser');
+const compression = require('compression');
 
-var bookEntryQueueManager = require('./app-helpers/bookEntryQueueManager').default;
-var PendingBookEntryDao = require('./dataAccess/pendingBookEntryDAO').default;
-var ErrorLoggerDao = require('./dataAccess/errorLoggerDAO').default;
-var UserDao = require('./dataAccess/userDAO').default;
+const bookEntryQueueManager = require('./app-helpers/bookEntryQueueManager').default;
+const PendingBookEntryDao = require('./dataAccess/pendingBookEntryDAO').default;
+const ErrorLoggerDao = require('./dataAccess/errorLoggerDAO').default;
+const UserDao = require('./dataAccess/userDAO').default;
 
-var hour = 3600000;
-var rememberMeExpiration = 2 * 365 * 24 * hour; //2 years
+const hour = 3600000;
+const rememberMeExpiration = 2 * 365 * 24 * hour; //2 years
 
-var multer = require('multer');
+const multer  = require('multer');
 
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     RememberMeStrategy = require('passport-remember-me').Strategy;
 
-passport.use(new LocalStrategy(function (email, password, done) {
-    var userDao = new UserDao();
+passport.use(new LocalStrategy(
+    function(email, password, done) {
+        let userDao = new UserDao();
 
-    userDao.lookupUser(email, password).then(function (userResult) {
-        if (userResult) {
-            userResult.id = '' + userResult._id;
-            done(null, userResult);
-        } else {
-            done(null, false, { message: 'Incorrect login' });
-        }
-    });
-}));
+        userDao.lookupUser(email, password).then(userResult => {
+            if (userResult) {
+                userResult.id = '' + userResult._id;
+                done(null, userResult);
+            } else {
+                done(null, false, {message: 'Incorrect login'});
+            }
+        });
+    }
+));
 
 function consumeRememberMeToken(token, done) {
-    var userDao = new UserDao();
+    let userDao = new UserDao();
 
-    userDao.lookupUserByToken(token).then(function (userResult) {
+    userDao.lookupUserByToken(token).then(userResult => {
         if (userResult) {
             userResult.id = '' + userResult._id;
             done(null, userResult);
@@ -59,32 +59,31 @@ function consumeRememberMeToken(token, done) {
     });
 }
 
-passport.use(new RememberMeStrategy(function (token, done) {
-    consumeRememberMeToken(token, function (err, userResult) {
-        if (err) {
-            return done(err);
-        }
-        if (!userResult) {
-            return done(null, false);
-        }
+passport.use(new RememberMeStrategy(
+    function(token, done) {
+        consumeRememberMeToken(token, function(err, userResult) {
+            if (err) { return done(err); }
+            if (!userResult) { return done(null, false); }
 
-        done(null, userResult);
-    });
-}, function (user, done) {
-    return done(null, user.token);
-}));
+            done(null, userResult);
+        });
+    },
+    function(user, done) {
+        return done(null, user.token);
+    }
+));
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
+passport.deserializeUser(function(id, done) {
     return done(undefined, { id: '' + id, _id: '' + id });
 });
 
 app.use(compression());
-app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 app.use(cookieParser());
@@ -101,14 +100,12 @@ app.use('/react-redux/', express.static(__dirname + '/react-redux/'));
 app.use('/utils/', express.static(__dirname + '/utils/'));
 app.use('/uploads/', express.static(__dirname + '/uploads/'));
 
-app.ws('/bookEntryWS', function (ws, req) {
+app.ws('/bookEntryWS', function(ws, req) {
     bookEntryQueueManager.subscriberAdded(req.user.id, ws);
 });
 
 var easyControllers = require('easy-express-controllers').easyControllers;
-easyControllers.createAllControllers(app, { fileTest: function fileTest(f) {
-        return !/-es6.js$/.test(f);
-    } });
+easyControllers.createAllControllers(app, { fileTest: f => !/-es6.js$/.test(f) });
 
 app.get('/', browseToReactRedux);
 app.get('/books', browseToReactRedux);
@@ -120,7 +117,7 @@ app.get('/home', browseToReactRedux);
 app.get('/view', browseToReactRedux);
 app.get('/react-redux', browseToReactRedux);
 
-function browseToReactRedux(request, response) {
+function browseToReactRedux(request, response){
     if (!!request.user) {
         response.cookie('logged_in', 'true', { maxAge: 900000 });
     } else {
@@ -133,58 +130,54 @@ app.get('/favicon.ico', function (request, response) {
     response.sendFile(path.join(__dirname + '/favicon.ico'));
 });
 
-app.post('/react-redux/login', passport.authenticate('local'), function (req, response) {
+app.post('/react-redux/login', passport.authenticate('local'), function(req, response) {
     // If this function gets called, authentication was successful. `req.user` contains the authenticated user.
     response.cookie('logged_in', 'true', { maxAge: 900000 });
     response.cookie('userId', req.user.id, { maxAge: 900000 });
     if (req.body.rememberme == 1) {
-        response.cookie('remember_me', req.user.token, { path: '/', httpOnly: true, maxAge: rememberMeExpiration });
+        response.cookie('remember_me', req.user.token, {path: '/', httpOnly: true, maxAge: rememberMeExpiration });
     }
     response.send(req.user);
 });
 
-app.post('/react-redux/logout', function (req, response) {
+app.post('/react-redux/logout', function(req, response){
     response.clearCookie('remember_me');
     response.clearCookie('userId');
     req.logout();
     response.send({});
 });
 
-var multerBookCoverUploadStorage = multer.diskStorage({
-    destination: function destination(req, file, cb) {
-        if (!req.user.id) {
+const multerBookCoverUploadStorage = multer.diskStorage({
+    destination(req, file, cb){
+        if (!req.user.id){
             cb('Not logged in');
         } else {
-            (function () {
-                var path = './uploads/' + req.user.id + '/coverUpload';
+            let path = `./uploads/${req.user.id}/coverUpload`;
 
-                fs.stat(path, function (err) {
-                    if (err) {
-                        mkdirp(path, function (err, res) {
-                            return cb(err, path);
-                        });
-                    } else {
-                        cb(null, path);
-                    }
-                });
-            })();
+            fs.stat(path, function(err){
+                if (err){
+                    mkdirp(path, (err, res) => cb(err, path));
+                } else {
+                    cb(null, path);
+                }
+            });
         }
     },
-    filename: function filename(req, file, cb) {
+    filename(req, file, cb) {
         cb(null, file.originalname);
     }
 });
-var upload = multer({ storage: multerBookCoverUploadStorage });
+const upload = multer({ storage: multerBookCoverUploadStorage });
 
 //TODO: refactor to be a controller action - will require middleware in easy-express-controllers which doesn't currently exist
-app.post('/react-redux/upload', upload.single('fileUploaded'), function (req, response) {
+app.post('/react-redux/upload', upload.single('fileUploaded'), function(req, response){
     //req.body.___ still has manual fields sent over
-    if (req.file.size > 900000) {
+    if (req.file.size > 900000){
         return response.send({ success: false, error: 'Max size is 500K' });
     }
 
-    var pathResult = path.normalize(req.file.destination).replace(/\\/g, '/'),
-        pathToFileUploaded = pathResult + '/' + req.file.originalname,
+    let pathResult = path.normalize(req.file.destination).replace(/\\/g, '/'),
+        pathToFileUploaded = `${pathResult}/${req.file.originalname}`,
         ext = (path.extname(pathToFileUploaded) || '').toLowerCase();
 
     try {
@@ -194,11 +187,11 @@ app.post('/react-redux/upload', upload.single('fileUploaded'), function (req, re
             }
 
             if (ext == '.jpg' || ext == '.jpeg') {
-                fs.readFile(pathToFileUploaded, function (err, data) {
+                fs.readFile(pathToFileUploaded, (err, data) => {
                     if (err) {
                         console.log('ERROR', pathToFileUploaded, err);
                     }
-                    var exifData = exif.create(data).parse(),
+                    let exifData = exif.create(data).parse(),
                         batchImage = null;
 
                     if (exifData && exifData.tags) {
@@ -228,9 +221,7 @@ app.post('/react-redux/upload', upload.single('fileUploaded'), function (req, re
                     }
 
                     if (batchImage) {
-                        batchImage.exec(function (err, image) {
-                            return processImageAsNeeded(image);
-                        });
+                        batchImage.exec((err, image) => processImageAsNeeded(image))
                     } else {
                         processImageAsNeeded(image);
                     }
@@ -239,40 +230,40 @@ app.post('/react-redux/upload', upload.single('fileUploaded'), function (req, re
                 processImageAsNeeded(image);
             }
         });
-    } catch (err) {
+    } catch (err){
         return response.send({ success: false, error: 'Error opening file. Is it a valid image?' });
     }
 
     function processImageAsNeeded(image) {
         if (image.width() > 55) {
-            var width = image.width(),
+            let width = image.width(),
                 height = image.height(),
-                newWidth = height * 50 / width;
+                newWidth = (height * 50) / width;
 
             image.resize(50, newWidth, function (err, image) {
-                var resizedDestination = pathResult + '/resized_' + req.file.originalname;
+                let resizedDestination = `${pathResult}/resized_${req.file.originalname}`;
 
-                image.writeFile(resizedDestination, function (err) {
-                    response.send({ success: true, smallImagePath: '/' + resizedDestination }); //absolute for client, since it'll be react-redux base (or something else someday, perhaps)
+                image.writeFile(resizedDestination, err => {
+                    response.send({success: true, smallImagePath: '/' + resizedDestination}); //absolute for client, since it'll be react-redux base (or something else someday, perhaps)
                 });
             });
         } else {
-            response.send({ success: true, smallImagePath: '/' + pathResult + '/' + req.file.originalname }); //absolute for client, since it'll be react-redux base (or something else someday, perhaps)
+            response.send({success: true, smallImagePath: `/${pathResult}/${req.file.originalname}`}); //absolute for client, since it'll be react-redux base (or something else someday, perhaps)
         }
     }
 });
 
-app.post('/react-redux/createUser', function (req, response) {
-    var userDao = new UserDao(),
+app.post('/react-redux/createUser', function(req, response){
+    let userDao = new UserDao(),
         username = req.body.username,
         password = req.body.password,
         rememberMe = req.body.rememberme == 1;
 
-    userDao.checkUserExists(username, password).then(function (exists) {
+    userDao.checkUserExists(username, password).then(exists => {
         if (exists) {
             response.send({ errorCode: 's1' });
         } else {
-            userDao.createUser(username, password, rememberMe).then(function () {
+            userDao.createUser(username, password, rememberMe).then(() => {
                 userDao.sendActivationCode(username);
                 response.send({});
             });
@@ -281,29 +272,28 @@ app.post('/react-redux/createUser', function (req, response) {
 });
 
 app.get('/activate', browseToReactRedux);
-app.get('/activate/:code', function (req, response) {
-    var userDao = new UserDao(),
+app.get('/activate/:code', function(req, response){
+    let userDao = new UserDao(),
         code = req.params.code;
 
     response.clearCookie('remember_me');
     req.logout();
 
-    userDao.activateUser(code).then(function (result) {
+    userDao.activateUser(code).then(result => {
         //console.log('activation results', 'success', success, 'already activated', alreadyActivated, 'invalid', invalid);
-        if (result.success) {
-            req.login(result, function () {
+        if (result.success){
+            req.login(result, function(){
                 response.cookie('userId', result._id, { maxAge: 900000 });
                 if (result.rememberMe) {
                     response.cookie('remember_me', result.token, { path: '/', httpOnly: true, maxAge: rememberMeExpiration });
                 }
                 response.redirect('/activate');
-            });
+            })
         } else {
             response.redirect(result.alreadyActivated ? '/activate?alreadyActivated=true' : '/activate');
         }
-    }, function (err) {
-        return console.log(':(', err);
-    });
+
+    }, err => console.log(':(', err));
 });
 
 process.on('uncaughtException', error);
@@ -311,19 +301,19 @@ process.on('unhandledRejection', error);
 process.on('exit', shutdown);
 process.on('SIGINT', shutdown);
 
-function shutdown() {
+function shutdown(){
     dao.shutdown();
     process.exit();
 }
 
-function error(err) {
-    try {
-        var logger = new ErrorLoggerDao();
+function error(err){
+    try{
+        let logger = new ErrorLoggerDao();
         logger.log('exception', err);
-    } catch (e) {}
+    } catch(e) { }
 }
 
-Promise.resolve(dao.init()).then(function () {
+Promise.resolve(dao.init()).then(() => {
     app.listen(process.env.PORT || 3000);
     bookEntryQueueManager.initialize();
 });
