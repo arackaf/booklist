d03b055803c8d379b210e91629eea18dc00584c3
Adam Rackis <adamrackis@gmail.com>
2017-05-10 23:39:50 -0500

    remove un-needed names

--- react-redux/modules/scan/scan.ts
+++ react-redux/modules/scan/scan.ts
@@ -2,7 +2,6 @@ import BookEntryList from './components/bookEntryList';
 import reducer from './reducers/reducer';
 
 export default {
-    name: 'scan',
     reducer: reducer,
     component: BookEntryList
 };
--- react-redux/modules/subjects/subjects.ts
+++ react-redux/modules/subjects/subjects.ts
@@ -3,7 +3,6 @@ import { reducer } from './reducers/reducer';
 import { loadSubjects } from 'applicationRoot/rootReducerActionCreators'
 
 export default {
-    name: 'subjects',
     reducer: reducer,
     component: SubjectsList,
     initialize: loadSubjects
