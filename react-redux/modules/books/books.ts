cbda66cf6d2a5dea191588e1ef8f729971e4108d
Adam Rackis <adamrackis@hotmail.com>
2017-05-10 23:39:50 -0500

    remove un-needed names

--- react-redux/modules/books/books.ts
+++ react-redux/modules/books/books.ts
@@ -5,7 +5,6 @@ import {booksActivated} from './reducers/bookSearch/actionCreators';
 import {store} from 'applicationRoot/store';
 
 export default {
-    name: 'books',
     reducer: reducer,
     component: BookViewList,
     initialize: booksActivated
--- react-redux/modules/settings/settings.ts
+++ react-redux/modules/settings/settings.ts
@@ -2,7 +2,6 @@ import SettingsComponent from './components/settingsComponent';
 import reducer, {initialize} from './reducers/reducer';
 
 export default {
-    name: 'settings',
     reducer,
     component: SettingsComponent,
     initialize
