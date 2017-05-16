5e957e4c50f3396f8dbc928b46914ca44bdebbd7
Adam Rackis <adamrackis@gmail.com>
2017-05-16 16:55:09 -0500

    amazon search fixes

--- node-src/amazonDataAccess/amazonSearch.js
+++ node-src/amazonDataAccess/amazonSearch.js
@@ -18,14 +18,12 @@ class AmazonSearch{
                 'ResponseGroup': 'ItemAttributes,EditorialReview,Images',
                 'ItemId': isbn
             }).then(({result}) => {
-                debugger;
                 if (!result.ItemLookupResponse || !result.ItemLookupResponse.Items || !result.ItemLookupResponse.Items.Item || !result.ItemLookupResponse.Items.Item.ItemAttributes){
                     resolve({ failure: true });
                 } else {
                     resolve(projectResponse(result.ItemLookupResponse.Items.Item));
                 }
             }).catch(err => {
-                debugger;
                 resolve({ failure: true });
             })
         })
@@ -38,26 +36,34 @@ function projectResponse(item){
             title: safeAccess(attributes, 'Title'),
             isbn: safeAccess(attributes, 'ISBN'),
             ean: safeAccess(attributes, 'EAN'),
-            author: safeAccess(attributes, 'Author'),
             pages: +safeAccess(attributes, 'NumberOfPages') || undefined,
             smallImage: safeAccess(safeAccessObject(item, 'SmallImage'), 'URL'),
             mediumImage: safeAccess(safeAccessObject(item, 'MediumImage'), 'URL'),
             publicationDate: safeAccess(attributes, 'PublicationDate'),
             publisher: safeAccess(attributes, 'Publisher'),
-            editorialReviews: []
-        },
-        editorialReviews = item.EditorialReviews && item.EditorialReviews[0] && item.EditorialReviews[0].EditorialReview;
-
-        result.authors = attributes.Author;
+            authors: safeArray(attributes, attributes => attributes.Author),
+            editorialReviews: safeArray(item, item => item.EditorialReviews.EditorialReview)
+        };
 
     if (typeof result.pages === 'undefined'){
         delete result.pages;
     }
-
-    if (editorialReviews){
-        result.editorialReviews = editorialReviews.map(({ Source, Content }) => ({ source: Source[0], content: Content[0]  }));
-    }
     return result;
+
+    function safeArray(item, lambda){
+        try {
+            let val = lambda(item);
+            if (!val){
+                return [];
+            } else if (Array.isArray(val)){
+                return val;
+            } else {
+                return [val];
+            }
+        } catch(err){
+            return [];
+        }
+    }
 
     function safeAccess(obj, path){
         return obj[path] || '';
