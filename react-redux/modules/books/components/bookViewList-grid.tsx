65da46680d9b8b5810dd55727e6b24b2ea178a2e
Adam Rackis <adamrackis@gmail.com>
2017-06-03 23:15:39 -0500

    loading book details started

--- react-redux/modules/books/components/bookViewList-grid.tsx
+++ react-redux/modules/books/components/bookViewList-grid.tsx
@@ -51,57 +51,65 @@ export default class BookViewListGrid extends Component<BookListComponentStateTy
                         </thead>
                         <tbody>
                         { this.props.booksList.map(book =>
-                            <tr key={book._id}>
-                                <td>
-                                    <input type="checkbox" onClick={() => this.props.toggleSelectBook(book._id)} checked={!!this.props.selectedBooks[book._id]} disabled={this.props.viewingPublic} />
-                                </td>
-                                <td><img src={book.smallImage} /></td>
-                                <td>
-                                    {book.title}
-                                    <br />
-                                    { book.editorialReviewsLoading 
-                                        ? <a target="_new" className="margin-right grid-hover-filter inline-filter"><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-spin fa-spinner"></i></a>
-                                        : (
-                                            book.expanded
-                                                ? <a target="_new" className="margin-right grid-hover-filter inline-filter"><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-minus show-on-hover-parent-td"></i></a>
-                                                : <a target="_new" onClick={() => this.props.loadEditorialReviews(book._id)} className="margin-right grid-hover-filter inline-filter"><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-plus show-on-hover-parent-td"></i></a>
-                                        )
-                                    }
-                                    { book.isbn ? <a target="_new" className="margin-right grid-hover-filter inline-filter" href={`https://www.amazon.com/gp/product/${book.isbn}/?tag=zoomiec-20`}><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-amazon show-on-hover-parent-td"></i></a> : null }
-                                    { !this.props.viewingPublic ? <a className="margin-right grid-hover-filter inline-filter" onClick={() => this.props.editBook(book)}><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-pencil show-on-hover-parent-td"></i></a> : null }
-                                    { !this.props.viewingPublic ? <a className="margin-right grid-hover-filter inline-filter" onClick={() => this.props.setPendingDeleteBook(book)}><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-trash show-on-hover-parent-td"></i></a> : null }
-                                    { book.pendingDelete ? <AjaxButton running={book.deleting} runningText="Deleting" onClick={() => this.props.deleteBook(book)} className="btn btn-xs btn-danger margin-right">Confirm delete</AjaxButton> : null }
-                                    { book.pendingDelete ? <button onClick={() => this.props.cancelPendingDeleteBook(book)} className="btn btn-xs btn-primary">Cancel</button> : null }
-                                </td>
-                                <td>
-                                    <ul className="list-unstyled">
-                                        {book.authors.map((author, i) => <li key={i}>{author}</li>)}
-                                    </ul>
-                                </td>
-                                <td>
-                                    { book.subjectObjects.map((s, i) => <div key={i}><LabelDisplay item={s} /></div>) }
-                                    <div style={{ marginTop: 5, minHeight: 40 }}>
-                                        <button className="btn btn-default btn-xs" onClick={() => this.props.enableSubjectModificationSingleBook(book._id)} disabled={this.props.viewingPublic}>Modify</button>
-                                    </div>
-                                </td>
-                                <td>
-                                    { book.tagObjects.map((s, i) => <div key={i}><LabelDisplay item={s} /></div>) }
-                                    <div style={{ marginTop: 5, minHeight: 40 }}>
-                                        <button className="btn btn-default btn-xs" onClick={() => this.props.enableTagModificationSingleBook(book._id)} disabled={this.props.viewingPublic}>Modify</button>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div style={{ marginTop: 5 }}> { !!book.isRead
-                                        ? <AjaxButton running={!!book.readChanging} runningText=" " onClick={() => this.props.setUnRead(book._id)} disabled={this.props.viewingPublic} preset="success-xs">Read <i className="fa fa-fw fa-check"></i></AjaxButton>
-                                        : <AjaxButton running={!!book.readChanging} runningText=" " onClick={() => this.props.setRead(book._id)} disabled={this.props.viewingPublic} preset="default-xs">Set read</AjaxButton>
-                                    }
-                                    </div>
-                                </td>
-                                <td>{book.publisher}{book.publisher ? <br /> : null}{book.publicationDate}</td>
-                                <td>{book.isbn}</td>
-                                <td>{book.pages}</td>
-                                <td>{book.dateAddedDisplay}</td>
-                            </tr>
+                            [
+                                <tr key={book._id}>
+                                    <td>
+                                        <input type="checkbox" onClick={() => this.props.toggleSelectBook(book._id)} checked={!!this.props.selectedBooks[book._id]} disabled={this.props.viewingPublic} />
+                                    </td>
+                                    <td><img src={book.smallImage} /></td>
+                                    <td>
+                                        {book.title}
+                                        <br />
+                                        { book.detailsLoading 
+                                            ? <a target="_new" className="margin-right grid-hover-filter inline-filter"><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-spin fa-spinner"></i></a>
+                                            : (
+                                                book.expanded
+                                                    ? <a target="_new" onClick={() => this.props.collapseBook(book._id)} className="margin-right grid-hover-filter inline-filter"><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-minus show-on-hover-parent-td"></i></a>
+                                                    : <a target="_new" onClick={() => this.props.expandBook(book._id)} className="margin-right grid-hover-filter inline-filter"><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-plus show-on-hover-parent-td"></i></a>
+                                            )
+                                        }
+                                        { book.isbn ? <a target="_new" className="margin-right grid-hover-filter inline-filter" href={`https://www.amazon.com/gp/product/${book.isbn}/?tag=zoomiec-20`}><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-amazon show-on-hover-parent-td"></i></a> : null }
+                                        { !this.props.viewingPublic ? <a className="margin-right grid-hover-filter inline-filter" onClick={() => this.props.editBook(book)}><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-pencil show-on-hover-parent-td"></i></a> : null }
+                                        { !this.props.viewingPublic ? <a className="margin-right grid-hover-filter inline-filter" onClick={() => this.props.setPendingDeleteBook(book)}><i style={{ display: book.pendingDelete ? 'inline' : '' }} className="fa fa-fw fa-trash show-on-hover-parent-td"></i></a> : null }
+                                        { book.pendingDelete ? <AjaxButton running={book.deleting} runningText="Deleting" onClick={() => this.props.deleteBook(book)} className="btn btn-xs btn-danger margin-right">Confirm delete</AjaxButton> : null }
+                                        { book.pendingDelete ? <button onClick={() => this.props.cancelPendingDeleteBook(book)} className="btn btn-xs btn-primary">Cancel</button> : null }
+                                    </td>
+                                    <td>
+                                        <ul className="list-unstyled">
+                                            {book.authors.map((author, i) => <li key={i}>{author}</li>)}
+                                        </ul>
+                                    </td>
+                                    <td>
+                                        { book.subjectObjects.map((s, i) => <div key={i}><LabelDisplay item={s} /></div>) }
+                                        <div style={{ marginTop: 5, minHeight: 40 }}>
+                                            <button className="btn btn-default btn-xs" onClick={() => this.props.enableSubjectModificationSingleBook(book._id)} disabled={this.props.viewingPublic}>Modify</button>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        { book.tagObjects.map((s, i) => <div key={i}><LabelDisplay item={s} /></div>) }
+                                        <div style={{ marginTop: 5, minHeight: 40 }}>
+                                            <button className="btn btn-default btn-xs" onClick={() => this.props.enableTagModificationSingleBook(book._id)} disabled={this.props.viewingPublic}>Modify</button>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div style={{ marginTop: 5 }}> { !!book.isRead
+                                            ? <AjaxButton running={!!book.readChanging} runningText=" " onClick={() => this.props.setUnRead(book._id)} disabled={this.props.viewingPublic} preset="success-xs">Read <i className="fa fa-fw fa-check"></i></AjaxButton>
+                                            : <AjaxButton running={!!book.readChanging} runningText=" " onClick={() => this.props.setRead(book._id)} disabled={this.props.viewingPublic} preset="default-xs">Set read</AjaxButton>
+                                        }
+                                        </div>
+                                    </td>
+                                    <td>{book.publisher}{book.publisher ? <br /> : null}{book.publicationDate}</td>
+                                    <td>{book.isbn}</td>
+                                    <td>{book.pages}</td>
+                                    <td>{book.dateAddedDisplay}</td>
+                                </tr>,
+                                book.expanded ?
+                                    <tr>
+                                        <td colSpan={11}>
+                                            <h1>Hello World</h1>
+                                        </td>
+                                    </tr> : null
+                            ]
                         )}
                         </tbody>
                     </table>
