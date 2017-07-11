7b28e13d9d62a406954958be59068b7896ecffca
Adam Rackis <adamrackis@hotmail.com>
2017-07-11 18:34:33 -0500

    chart formatting

--- react-redux/modules/home/homeComponent.tsx
+++ react-redux/modules/home/homeComponent.tsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { isLoggedIn } from 'reactStartup';
-
 import {scaleLinear, scaleOrdinal, scaleBand, schemeCategory10} from 'd3-scale';
 import {max} from 'd3-array';
 import {select} from 'd3-selection';
@@ -22,13 +21,13 @@ class BarChart extends Component<any, any> {
     createBarChart = () => {
         let node = this.node,
             margin = {top: 20, right: 10, bottom: 80, left: 0},
-            {data, size} = this.props,
-            chartHeight = size[1] - margin.top - margin.bottom,
+            {data, width, height} = this.props,
+            chartHeight = height - margin.top - margin.bottom,
             dataMax = max(data),
             dataScale = scaleLinear().domain([0, dataMax]).range([0, chartHeight]),
             scaleX = scaleBand()
                         .domain(data.map((d, i) => getDisplay(i)))
-                        .range([0, 500])
+                        .range([0, width])
                         .paddingInner([0.1])
                         .paddingOuter([0.3])
                         .align([0.5]),
@@ -39,29 +38,30 @@ class BarChart extends Component<any, any> {
     render() {
         let node = this.node,
             margin = {top: 20, right: 10, bottom: 80, left: 0},
-            {data, size} = this.props,
-            chartHeight = size[1] - margin.top - margin.bottom,
+            {data, width, height} = this.props,
+            chartHeight = height - margin.top - margin.bottom,
             dataMax = max(data),
             dataScale = scaleLinear().domain([0, dataMax]).range([0, chartHeight]),
             scaleX = scaleBand()
                         .domain(data.map((d, i) => getDisplay(i)))
-                        .range([0, 500])
+                        .range([0, width])
                         .paddingInner([0.1])
                         .paddingOuter([0.3])
                         .align([0.5]),
             colorScale = scaleOrdinal()
                         .domain(data.map((d, i) => i))
-                        .range(schemeCategory10);                        
+                        .range(schemeCategory10),
+            style = {display: 'block', marginLeft: 'auto', marginRight: 'auto'};
 
         return (
-            <svg ref={node => this.node = node} width={500} height={500} style={{backgroundColor: 'lightblue'}}>
-                <g transform={`scale(1, -1) translate(${margin.left}, ${margin.bottom - 500})`}>
+            <svg style={style} ref={node => this.node = node} width={width} height={height}>
+                <g transform={`scale(1, -1) translate(${margin.left}, ${margin.bottom - height})`}>
                     {data.map((d, i) => (
-                        <Bar key={i} x={scaleX(getDisplay(i))} y={0} color={colorScale(i)} width={scaleX.bandwidth()} height={dataScale(d)} />
+                        <Bar key={i} x={scaleX(getDisplay(i))} y={0} color={colorScale(i)} width={scaleX.bandwidth()} height={dataScale(d)} graphWidth={width} />
                     ))}
                 </g>
                 <g transform={`translate(${margin.left}, ${-1 * margin.bottom})`}>
-                    <Axis scale={scaleX} transform="translate(0, 500)"></Axis>
+                    <Axis scale={scaleX} transform={`translate(0, ${height})`}></Axis>
                 </g>
             </svg>
         );
@@ -85,9 +85,9 @@ class Bar extends Component<any, any> {
             .attr("x", this.props.x)
     }
     render() {
-        let {x, height, width, color} = this.props;
+        let {x, height, width, color, graphWidth} = this.props;
         return (
-            <rect ref={el => this.el = el} x={500} y={0} height={0} fill={color} width={0} />
+            <rect ref={el => this.el = el} x={graphWidth} y={0} height={0} fill={color} width={0} />
         );
     }
 }
@@ -121,16 +121,14 @@ class Axis extends Component<any, any> {
 }
 
 const MainHomePane = props =>
-    <div className="row" style={{margin: 0}}>
-        <div className="hidden-xs hidden-sm col-md-1 col-lg-3"></div>
-        <div style={{ marginLeft: 10, marginRight: 10 }} className="col-md-10 col-lg-6">
+    <div style={{margin: 0}}>
+        <div style={{ marginLeft: 'auto', marginRight: 'auto', width: '1200px' }}>
             <div className="panel panel-default">
                 <div className="panel-body">
                     {props.children}
                 </div>
             </div>
         </div>
-        <div className="hidden-xs hidden-sm col-md-1 col-lg-3"></div>
     </div>
 
 class HomeIfLoggedIn extends Component<any, any> {
@@ -143,6 +141,10 @@ class HomeIfLoggedIn extends Component<any, any> {
         setTimeout(() => this.setState({data: [1, 3, 9, 11, 15, 17]}), 6000)
         setTimeout(() => this.setState({data: [17, 15, 11, 9, 3, 1, 5, 7]}), 7000)
     }
+    sized = sizeInfo => {
+        debugger;
+        this.setState({width: sizeInfo.client.width});
+    }
     render() {
         //[5, 10, 4, 5, 7, 11, /*6, 31, 3, 7, 9, 18, 5, 22, 5*/]
         return (
@@ -153,7 +155,7 @@ class HomeIfLoggedIn extends Component<any, any> {
                     <br />
                     <br />
 
-                    <BarChart data={this.state.data} size={[500, 500]} />
+                    <BarChart data={this.state.data} width={1100} height={500} />
                     <br />
                     <br />
                     <br />
