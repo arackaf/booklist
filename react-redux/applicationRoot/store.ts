bf71a081ea5bf069d2b4bef5cd8769f9cbe32e5f
Adam Rackis <adamrackis@hotmail.com>
2017-08-26 17:11:35 -0500

    Proxy test

--- react-redux/applicationRoot/store.ts
+++ react-redux/applicationRoot/store.ts
@@ -37,26 +37,52 @@ let codeSplitReducer = (state = aInit, action) => {
     return state;
 }
 
-let myStore = createStoreWithMiddleware(combineReducers({
-    app: rootReducer,
-    codeSplitSlice: state => state || null
-}), {
-    codeSplitSlice: {
+let initialReducer = {
+    app: rootReducer
+};
+
+let initialState = { // <----- from local storage 
+    codeSplitSlice: { // <----- from code-split reducers
         a: 12,
         b: 13
     }
-})
+};
 
-//later
+let handler = {
+    ownKeys(target){
+        return Array.from(new Set([...Reflect.ownKeys(target), ...Reflect.ownKeys(initialState)]));
+    },
+    get(target, key){
+        return target[key] || (state => state === void 0 ? null : state); // <--- stub for Redux if not present
+    },
+    getOwnPropertyDescriptor(target, key){
+        return Reflect.getOwnPropertyDescriptor(target, key) || Reflect.getOwnPropertyDescriptor(initialState, key);
+    }
+};
+
+let reducerProxy = new Proxy(initialReducer, handler);
+let myStore = createStoreWithMiddleware(combineReducers(reducerProxy), initialState);
+
+let state = myStore.getState();
+// state.codeSplitSplice === {a: 12, b: 13} hooray!
+
 
 myStore.replaceReducer(combineReducers({
     app: rootReducer,
     codeSplitSlice: codeSplitReducer
 }));
 
+state = myStore.getState();
+// state.codeSplitSplice === {a: 12, b: 13} still, as expected!
+
 myStore.dispatch({type: 'INC_A'});
+state = myStore.getState();
+// state.codeSplitSplice === {a: 13, b: 13} as expected!
+
 myStore.dispatch({type: 'INC_B'});
+state = myStore.getState();
+// state.codeSplitSplice === {a: 13, b: 14} as expected!
 
-
+debugger;
 
 export const store = createStoreWithMiddleware(getNewReducer());
