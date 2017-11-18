4e4b73a04eeab531bc636ec62d80304735b7e128
Adam Rackis <adamrackis@gmail.com>
2017-11-18 13:09:03 -0600

    push notification started

--- react-redux/reactStartup.ts
+++ react-redux/reactStartup.ts
@@ -20,10 +20,49 @@ import "util/ajaxUtil";
 import createHistory from "history/createBrowserHistory";
 
 (function() {
-  if ("serviceWorker" in navigator && !/localhost/.test(window.location as any)) {
+  //if ("serviceWorker" in navigator && !/localhost/.test(window.location as any)) {
+  if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/service-worker.js");
+
+    if (Notification) {
+      Notification.requestPermission().then(permission => {});
+    }
+
+    if (isLoggedIn()) {
+      let subscriptionOptions = {
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array("BCC0wqyL-OGz5duRO9-kOSUEv72BMGf0x0oaMGryF1eLa3FF-sW2YmunhNqQegrXHykP-Wa6xC1rEnDuBGtjgUo")
+      };
+
+      navigator.serviceWorker.ready.then(registration => {
+        registration.pushManager.subscribe(subscriptionOptions).then(subscription => {
+          ajaxUtil.post("/user/saveNotificationSubscription", { subscription: JSON.stringify(subscription) });
+        });
+      });
+    }
   }
 })();
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+function sendNotification(text) {
+  if ((Notification as any).permission == "granted") {
+    new Notification(text);
+  }
+}
+
+//sendNotification("Hi there");
 
 declare global {
   var require: any;
