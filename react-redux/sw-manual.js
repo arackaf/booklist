699184e9d5115e63dc3a8fdb55f3cd9fc9d68dd1
Adam Rackis <adamrackis@hotmail.com>
2017-11-21 01:30:37 -0600

    offline started

--- react-redux/sw-manual.js
+++ react-redux/sw-manual.js
@@ -1,4 +1,14 @@
+toolbox.router.get(/books$/, handleMain);
+toolbox.router.get(/subjects$/, handleMain);
+toolbox.router.get(/localhost:3000\/$/, handleMain);
+toolbox.router.get(/mylibrary.io$/, handleMain);
+
+function handleMain(request) {
+  return fetch(request);
+}
+
 self.addEventListener("push", () => {
+  console.log("typeof", typeof toolbox);
   console.log("Push notification received!!!");
   self.registration.showNotification("Push notification received!");
 });
