5446be2f4ac08d3b65ea12351a61bd86014709a6
Adam Rackis <adamrackis@hotmail.com>
2017-11-23 19:40:34 -0600

    more Node streams conversion stuff

--- node-src/maintenance/moveHttpCovers.js
+++ node-src/maintenance/moveHttpCovers.js
@@ -4,6 +4,7 @@ AWS.config.region = "us-east-1";
 
 import fs from "fs";
 import path from "path";
+import request from "request";
 import http from "http";
 import DAO from "../dataAccess/dao";
 import del from "del";
@@ -11,9 +12,35 @@ import { ObjectId } from "mongodb";
 
 let dao = new DAO();
 
-convertAll().then(() => {});
+//convertAllManual();
+convertAllAmazon();
 
-async function convertAll() {
+async function convertAllManual() {
+  let db = await DAO.init();
+  let toConvert = await db
+    .collection("books")
+    .find({ smallImage: { $regex: /http:\/\/my-library-cover-uploads/ } })
+    .toArray();
+
+  for (let book of toConvert) {
+    let namePart = book.smallImage
+      .toLowerCase()
+      .replace("http://my-library-cover-uploads.s3-website-us-east-1.amazonaws.com/", "")
+      .replace(/\.jpg$/, "")
+      .replace(/\.jpeg$/, "")
+      .replace(/\.png$/, "");
+
+    if (namePart.indexOf(".") >= 0 || namePart.indexOf(",") >= 0) {
+      let newURL = await convertFile(book.smallImage, book.userId, book._id);
+      await db.collection("books").update({ _id: ObjectId(book._id) }, { $set: { smallImage: newURL } });
+      console.log(book.title, "Converted", "\n");
+    }
+  }
+
+  DAO.shutdown();
+}
+
+async function convertAllAmazon() {
   let db = await DAO.init();
   let toConvert = await db
     .collection("books")
@@ -38,36 +65,30 @@ convertFile("http://ecx.images-amazon.com/images/I/513GMmespwL._SL75_.jpg", "112
 let i = 1;
 function convertFile(url, userId, _id) {
   return new Promise((res, rej) => {
-    http.get(url, response => {
-      try {
-        let s3bucket = new AWS.S3({ params: { Bucket: "my-library-cover-uploads" } });
-        let ext = path.extname(url);
-        let fileName = "junk-" + i++ + ext;
+    let s3bucket = new AWS.S3({ params: { Bucket: "my-library-cover-uploads" } });
+    let ext = path.extname(url);
+    let fileName = "junk-" + i++ + ext;
+    let file = fs.createWriteStream(fileName);
 
-        let file = fs.createWriteStream(fileName);
-        response.pipe(file);
+    request(url)
+      .pipe(file)
+      .on("finish", () => {
         file.close();
 
-        setTimeout(() => {
-          fs.readFile("./" + fileName, (err, data) => {
-            if (err) {
-              return rej(err);
-            }
-            let params = {
-              Key: `bookCovers/${userId}/converted-cover-${_id}${ext}`,
-              Body: data
-            };
+        fs.readFile("./" + fileName, (err, data) => {
+          if (err) {
+            return rej(err);
+          }
+          let params = {
+            Key: `bookCovers/${userId}/converted-cover-${_id}${ext}`,
+            Body: data
+          };
 
-            s3bucket.upload(params, function(err) {
-              del.sync("./" + fileName);
-              if (err) rej(err);
-              else res(`http://my-library-cover-uploads.s3-website-us-east-1.amazonaws.com/${params.Key}`);
-            });
+          s3bucket.upload(params, function(err) {
+            if (err) rej(err);
+            else res(`http://my-library-cover-uploads.s3-website-us-east-1.amazonaws.com/${params.Key}`);
           });
-        }, 500);
-      } catch (err) {
-        rej(err);
-      }
-    });
+        });
+      });
   });
 }
