d4144aaf0094e6563d2fc620bb5ed8eb59477ab9
Adam Rackis <adamrackis@hotmail.com>
2018-03-08 13:37:10 -0600

    upgrade @std/esm

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require = require("@std/esm")(module, { esm: "js", cjs: true });
+require = require("@std/esm")(module, { mode: "js", cjs: true });
 require("./node-dest/app-helpers/promiseUtils");
 require("dotenv").config();
 require("./startApp");
