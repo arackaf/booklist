7f8d02e1e26fc28afa59706f02cc70858e498b4b
Adam Rackis <adamrackis@gmail.com>
2018-04-23 20:27:43 -0500

    Remove decorator syntax from user controller

--- node-src/controllers/user.js
+++ node-src/controllers/user.js
@@ -1,34 +1,32 @@
-import { httpPost } from "easy-express-controllers";
+import { controller } from "easy-express-controllers";
 import UserDAO from "../dataAccess/userDAO";
 
-export default class userController {
-  @httpPost
+class userController {
   async getPubliclyAvailableUsersName({ _id }) {
     let user = await new UserDAO().getPublicDisplayInfo(_id);
     this.send({ publicName: user && user.publicName, publicBooksHeader: user && user.publicBooksHeader });
   }
-  @httpPost
   async getPublicSettings() {
     let userId = this.request.user.id;
     let result = await new UserDAO().getPublicSettings(userId);
     this.send({ info: result });
   }
-  @httpPost
   async setPublicSettings({ isPublic, publicName, publicBooksHeader }) {
     let userId = this.request.user.id;
     let result = await new UserDAO().setPublicSettings(userId, isPublic, publicName, publicBooksHeader);
     this.send({});
   }
-  @httpPost
   async resetPassword({ oldPassword, newPassword }) {
     let userId = this.request.user.id;
     let result = await new UserDAO().resetPassword(userId, oldPassword, newPassword);
     this.send({ ...result });
   }
-  @httpPost
   async saveNotificationSubscription({ subscription }) {
     let userId = this.request.user.id;
     await new UserDAO().updateSubscription(userId, JSON.parse(subscription));
     this.send({ success: true });
   }
 }
+controller({ defaultVerb: "post" })(userController);
+
+export default userController;
