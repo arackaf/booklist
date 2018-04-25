f04e6277cfe3931ccc8ae1838c2895e8ed56d935
Adam Rackis <adamrackis@hotmail.com>
2018-04-24 20:53:43 -0500

    Next action removed

--- node-src/controllers/user.js
+++ node-src/controllers/user.js
@@ -2,10 +2,6 @@ import { controller } from "easy-express-controllers";
 import UserDAO from "../dataAccess/userDAO";
 
 class userController {
-  async getPubliclyAvailableUsersName({ _id }) {
-    let user = await new UserDAO().getPublicDisplayInfo(_id);
-    this.send({ publicName: user && user.publicName, publicBooksHeader: user && user.publicBooksHeader });
-  }
   async setPublicSettings({ isPublic, publicName, publicBooksHeader }) {
     let userId = this.request.user.id;
     let result = await new UserDAO().setPublicSettings(userId, isPublic, publicName, publicBooksHeader);
