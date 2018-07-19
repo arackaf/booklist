c6d2327b4b01f45934179df4bb0371b340e4db1a
Adam Rackis <adamrackis@gmail.com>
2018-07-19 12:18:02 -0500

    tweaks

--- node-src/graphQL-public/resolver.js
+++ node-src/graphQL-public/resolver.js
@@ -1,38 +1,19 @@
-import GraphQLJSON from 'graphql-type-json';
+import GraphQLJSON from "graphql-type-json";
 
-import Book, { Book as BookRest } from './Book/resolver';
-import Subject, { Subject as SubjectRest } from './Subject/resolver';
-import LabelColor, { LabelColor as LabelColorRest } from './LabelColor/resolver';
-import User, { User as UserRest } from './User/resolver';
-import PublicUser, { PublicUser as PublicUserRest } from './PublicUser/resolver';
-import Tag, { Tag as TagRest } from './Tag/resolver';
+import Book, { Book as BookRest } from "./Book/resolver";
+import Subject, { Subject as SubjectRest } from "./Subject/resolver";
+import LabelColor, { LabelColor as LabelColorRest } from "./LabelColor/resolver";
+import Tag, { Tag as TagRest } from "./Tag/resolver";
 
 const { Query: BookQuery, Mutation: BookMutation } = Book;
 const { Query: SubjectQuery, Mutation: SubjectMutation } = Subject;
 const { Query: LabelColorQuery, Mutation: LabelColorMutation } = LabelColor;
-const { Query: UserQuery, Mutation: UserMutation } = User;
-const { Query: PublicUserQuery, Mutation: PublicUserMutation } = PublicUser;
 const { Query: TagQuery, Mutation: TagMutation } = Tag;
 
 export default {
   JSON: GraphQLJSON,
-  Query: Object.assign(
-    {},
-    BookQuery,
-    SubjectQuery,
-    LabelColorQuery,
-    UserQuery,
-    PublicUserQuery,
-    TagQuery
-  ),
-  Mutation: Object.assign({},
-    BookMutation,
-    SubjectMutation,
-    LabelColorMutation,
-    UserMutation,
-    PublicUserMutation,
-    TagMutation
-  ),
+  Query: Object.assign({}, BookQuery, SubjectQuery, LabelColorQuery, TagQuery),
+  Mutation: Object.assign({}, BookMutation, SubjectMutation, LabelColorMutation, TagMutation),
   Book: {
     ...BookRest
   },
@@ -42,14 +23,7 @@ export default {
   LabelColor: {
     ...LabelColorRest
   },
-  User: {
-    ...UserRest
-  },
-  PublicUser: {
-    ...PublicUserRest
-  },
   Tag: {
     ...TagRest
   }
 };
-
