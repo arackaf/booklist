c31e33639252cf3967fa4a3e030f9fbd9d9c110f
Adam Rackis <adamrackis@gmail.com>
2018-09-01 22:29:12 -0500

    New primary NavBar!

--- react-redux/applicationRoot/components/mainNavigation.tsx
+++ react-redux/applicationRoot/components/mainNavigation.tsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import NavBar from "simple-react-bootstrap/lib/navBar";
 import { goto } from "reactStartup";
 import ajaxUtil from "util/ajaxUtil";
+
+const spreadClassNames = (baseCssClasses = "", ...userClasses) => `${baseCssClasses} ${userClasses.join(" ")}`;
+
+const NavBarItem = props => {
+  let { disabled, className, active, href, children, ...rest } = props;
+
+  return (
+    <li disabled={!!disabled} className={spreadClassNames(className, !!disabled ? "disabled" : "", active ? "active" : "")} {...rest}>
+      <a href={href}>{children}</a>
+    </li>
+  );
+};
 
 @connect(state => state.app)
 export default class MainNavigationBar extends Component<any, any> {
@@ -12,13 +23,9 @@ export default class MainNavigationBar extends Component<any, any> {
     localStorage.setItem("reduxState", "");
     ajaxUtil.post("/react-redux/logout", {}, () => window.location.reload());
   };
-  componentDidUpdate(prevProps) {
-    if (prevProps.module != this.props.module) {
-      this.el.closeIfOpen();
-    }
-  }
   render() {
     let { isPublic, publicBooksHeader, publicName, module, isLoggedIn } = this.props,
+      isHome = module == "home",
       isBookEntry = module == "scan",
       isBookList = module == "books",
       isSubjects = module == "subjects",
@@ -26,55 +33,68 @@ export default class MainNavigationBar extends Component<any, any> {
       isSettings = module == "settings";
 
     return (
-      <NavBar
-        ref={el => (this.el = el)}
+      <nav
+        className="navbar navbar-default main-navbar"
         style={{ borderRadius: 0, borderRight: 0, borderLeft: 0, borderTop: 0, position: "fixed", top: 0, left: 0, right: 0, zIndex: 500 }}
       >
-        <NavBar.Header>
-          <NavBar.Brand>
+        <div className="container-fluid">
+          <div className="navbar-header hidden-xs" ref={el => (this.el = el)}>
             <a className="navbar-brand" onClick={() => goto("home")} style={{ cursor: "pointer" }}>
               <img height="32" width="32" style={{ display: "inline-block", marginTop: "-5px" }} src="react-redux/static/main-icon2.png" />
               <span style={{ display: "inline-block", verticalAlign: "top", marginLeft: "5px" }}>My Library</span>
             </a>
-          </NavBar.Brand>
-        </NavBar.Header>
-        <NavBar.Nav>
-          {isLoggedIn || isPublic ? (
-            <NavBar.Item disabled={isPublic} onClick={isBookEntry || isPublic ? null : () => goto("scan")} active={isBookEntry}>
-              Book entry
-            </NavBar.Item>
+          </div>
+
+          <ul className="nav navbar-nav">
+            <NavBarItem className="visible-xs" disabled={isPublic} onClick={() => goto("home")} active={isHome}>
+              <i className="fal fa-home visible-xs" />
+            </NavBarItem>
+            {isLoggedIn || isPublic ? (
+              <NavBarItem disabled={isPublic} onClick={isBookEntry || isPublic ? null : () => goto("scan")} active={isBookEntry}>
+                <span className="hidden-xs">Book entry</span>
+                <i className="visible-xs fal fa-scanner" />
+              </NavBarItem>
+            ) : null}
+            {isLoggedIn || isPublic ? (
+              <NavBarItem active={isBookList} onClick={isBookList ? null : () => goto("books")}>
+                <span className="hidden-xs">Books</span>
+                <i className="visible-xs fal fa-books" />
+              </NavBarItem>
+            ) : null}
+            {isLoggedIn || isPublic ? (
+              <NavBarItem disabled={isPublic} onClick={() => goto("subjects")} active={isSubjects}>
+                <span className="hidden-xs">Subjects</span>
+                <i className="visible-xs fal fa-tags" />
+              </NavBarItem>
+            ) : null}
+            {isLoggedIn && isPublic ? (
+              <NavBarItem onClick={() => goto("books")}>
+                <span className="hidden-xs">View your collection</span>
+                <i className="visible-xs fal fa-eye" />
+              </NavBarItem>
+            ) : null}
+            {isLoggedIn || isPublic ? (
+              <NavBarItem disabled={isPublic} onClick={isPublic ? null : () => goto("settings")} active={isSettings}>
+                <span className="hidden-xs">Settings</span>
+                <i className="visible-xs fal fa-cogs" />
+              </NavBarItem>
+            ) : null}
+            {!isLoggedIn && !isLoginModule ? (
+              <NavBarItem onClick={() => goto("login")}>
+                <span>Login</span>
+              </NavBarItem>
+            ) : null}
+          </ul>
+          {isLoggedIn ? (
+            <ul className={"nav navbar-nav pull-right"}>
+              <NavBarItem className="pull-right" onClick={this.logout}>
+                <span className="hidden-xs">Logout</span>
+                <i className="visible-xs fal fa-sign-out" />
+              </NavBarItem>
+            </ul>
           ) : null}
-          {isLoggedIn || isPublic ? (
-            <NavBar.Item active={isBookList} onClick={isBookList ? null : () => goto("books")}>
-              Books
-            </NavBar.Item>
-          ) : null}
-          {isLoggedIn || isPublic ? (
-            <NavBar.Item disabled={isPublic} onClick={() => goto("subjects")} active={isSubjects}>
-              Subjects
-            </NavBar.Item>
-          ) : null}
-          {isLoggedIn && isPublic ? <NavBar.Item onClick={() => goto("books")}>View your collection</NavBar.Item> : null}
-          {isLoggedIn || isPublic ? (
-            <NavBar.Item disabled={isPublic} onClick={isPublic ? null : () => goto("settings")} active={isSettings}>
-              Settings
-            </NavBar.Item>
-          ) : null}
-          {null && isLoggedIn ? (
-            <NavBar.Item onClick={() => {}} active={false} disabled={false}>
-              <i className="fa fa-fw fa-refresh" />
-            </NavBar.Item>
-          ) : null}
-          {!isLoggedIn && !isLoginModule ? <NavBar.Item onClick={() => goto("login")}>Login</NavBar.Item> : null}
-        </NavBar.Nav>
-        {isLoggedIn ? (
-          <NavBar.Nav className="pull-right">
-            <NavBar.Item className="pull-right" onClick={this.logout}>
-              Logout
-            </NavBar.Item>
-          </NavBar.Nav>
-        ) : null}
-      </NavBar>
+        </div>
+      </nav>
     );
   }
 }
