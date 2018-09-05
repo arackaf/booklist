15472d640ce841f174dbc3715ca8c0fd4b601104
Adam Rackis <adamrackis@hotmail.com>
2018-09-05 15:07:00 -0500

    Small tweak to to aws queue

--- node-src/amazonDataAccess/amazonOperationQueue.js
+++ node-src/amazonDataAccess/amazonOperationQueue.js
@@ -1,12 +1,10 @@
-let _timer = Symbol("aws"),
-  _lastRun = Symbol("lastRun"),
-  _startOp = Symbol("startItem");
+let _timer = Symbol("aws");
+let _startOp = Symbol("startItem");
 
 //Amazon only allows 1 request per second.  This class implements a queue that ensures this limit is not violated
 export class AmazonQueue {
   constructor() {
     this.queue = [];
-    this[_lastRun] = 0;
   }
   push(op) {
     if (!op.start) {
@@ -21,11 +19,10 @@ export class AmazonQueue {
   }
   runNext() {
     let opToRun = this.queue.shift();
-    this[_timer] = setTimeout(() => this[_startOp](opToRun), currentlyNeededDelay(this[_lastRun]));
+    this[_timer] = setTimeout(() => this[_startOp](opToRun), AmazonQueue.AWS_DELAY);
   }
   [_startOp](opToRun) {
     opToRun.start().then(() => {
-      this[_lastRun] = +new Date();
       this.finishedItem();
     });
   }
@@ -38,13 +35,5 @@ export class AmazonQueue {
 }
 
 AmazonQueue.AWS_DELAY = 1050; //1 second plus a teeny safety factor because why not
-
-function currentlyNeededDelay(lastRun = 0) {
-  let currentTime = +new Date(),
-    currentlyElapsedSinceLastRun = currentTime - lastRun,
-    neededDelay = AmazonQueue.AWS_DELAY - currentlyElapsedSinceLastRun;
-
-  return Math.max(neededDelay, 0); //might be negative, in which case we need no delay
-}
 
 export default new AmazonQueue();
