299eef5a80f540e180741d4fbb5479a4bf14b842
Adam Rackis <adamrackis@hotmail.com>
2018-09-04 23:41:04 -0500

    similarity lookup seems to work

---
+++ node-src/amazonDataAccess/amazonSimilarLookup.js
@@ -0,0 +1,138 @@
+import AWS from "aws-sdk";
+AWS.config.region = "us-east-1";
+
+import request from "request";
+import uuid from "uuid/v4";
+import del from "del";
+import path from "path";
+import fs from "fs";
+import mkdirp from "mkdirp";
+
+const awsCredentials = {
+  awsId: process.env.AWS_ID,
+  awsSecret: process.env.AWS_SECRET,
+  assocId: process.env.ASSOC_ID
+};
+
+const OperationHelper = require("apac").OperationHelper;
+const opHelper = new OperationHelper(awsCredentials);
+
+export default class AmazonSearch {
+  lookupBook(isbn) {
+    return new Promise(function(resolve, reject) {
+      if (!isbn) {
+        return resolve([]);
+      }
+      opHelper
+        .execute("SimilarityLookup", {
+          SearchIndex: "Books",
+          IdType: "ISBN",
+          ResponseGroup: "ItemAttributes,Images",
+          ItemId: isbn
+        })
+        .then(async ({ result }) => {
+          if (!result.SimilarityLookupResponse || !result.SimilarityLookupResponse.Items || !result.SimilarityLookupResponse.Items.Item) {
+            return resolve([]);
+          } else {
+            let itemsArray = result.SimilarityLookupResponse.Items.Item;
+            if (Array.isArray(itemsArray)) {
+              let resultsP = itemsArray.map(item => projectResponse(item)).filter(item => item);
+              let results = [];
+              for (let p of resultsP) {
+                results.push(await p);
+              }
+              resolve(results);
+            } else {
+              resolve([projectResponse(item)].filter(item => item));
+            }
+          }
+        })
+        .catch(err => {
+          resolve([]);
+        });
+    });
+  }
+}
+
+async function projectResponse(item) {
+  let attributes = item.ItemAttributes;
+  let result = {
+    title: safeAccess(attributes, "Title"),
+    asin: safeAccess(item, "ASIN"),
+    isbn: safeAccess(attributes, "ISBN"),
+    ean: safeAccess(attributes, "EAN"),
+    smallImage: safeAccess(safeAccessObject(item, "SmallImage"), "URL"),
+    mediumImage: safeAccess(safeAccessObject(item, "MediumImage"), "URL"),
+    authors: safeArray(attributes, attributes => attributes.Author)
+  };
+
+  if (/^http:\/\//.test(result.smallImage)) {
+    try {
+      let newImage = await saveImageToS3(result.smallImage, ean);
+      result.smallImage = newImage;
+    } catch (e) {}
+  }
+
+  return result;
+
+  function safeArray(item, lambda) {
+    try {
+      let val = lambda(item);
+      if (!val) {
+        return [];
+      } else if (Array.isArray(val)) {
+        return val;
+      } else {
+        return [val];
+      }
+    } catch (err) {
+      return [];
+    }
+  }
+
+  function safeAccess(obj, path) {
+    return obj[path] || "";
+  }
+
+  function safeAccessObject(obj, path) {
+    return obj[path] || {};
+  }
+}
+
+export function saveImageToS3(url, ean) {
+  return new Promise((res, rej) => {
+    let s3bucket = new AWS.S3({ params: { Bucket: "my-library-cover-uploads" } });
+    let ext = path.extname(url);
+    let uniqueId = uuid();
+    let fileName = "file-" + uniqueId + ext;
+    let fullName = path.resolve("./conversions/" + fileName);
+    mkdirp.sync(path.resolve("./conversions"));
+    let file = fs.createWriteStream(fullName);
+
+    request(url)
+      .pipe(file)
+      .on("finish", () => {
+        file.close();
+
+        fs.readFile(fullName, (err, data) => {
+          if (err) {
+            return rej(err);
+          }
+          let params = {
+            Key: `bookCovers/${ean}/converted-similar-cover-${uniqueId}${ext}`,
+            Body: data
+          };
+
+          s3bucket.upload(params, function(err) {
+            try {
+              del.sync(fullName);
+            } catch (e) {}
+
+            if (err) rej(err);
+            else res(`http://my-library-cover-uploads.s3-website-us-east-1.amazonaws.com/${params.Key}`);
+          });
+        });
+      })
+      .on("error", () => rej());
+  });
+}
--- node-src/dataAccess/bookDAO.js
+++ node-src/dataAccess/bookDAO.js
@@ -59,6 +59,48 @@ class BookDAO extends DAO {
     }
   }
 
+  async booksWithoutSimilarity() {
+    let db = await super.open();
+    try {
+      let query = { similarItems: { $exists: false } };
+      let project = { _id: 1, isbn: 1 };
+
+      try {
+        let books = await db
+          .collection("books")
+          .aggregate([{ $match: query }, { $project: project }, { $limit: 10 }])
+          .toArray();
+
+        return books;
+      } catch (err) {
+        console.log(err);
+      }
+    } finally {
+      super.dispose(db);
+    }
+  }
+
+  async updateBookSimilarity(book, results) {
+    let db = await super.open();
+    try {
+      await db.collection("books").update(
+        { _id: book._id },
+        {
+          $set: { similarItems: results.map(result => result.asin), similarItemsLastUpdate: +new Date() }
+        }
+      );
+
+      for (let book of results) {
+        let user = await db.collection("amazonReference").findOne({ asin: book.asin });
+        if (!user) {
+          await db.collection("amazonReference").insert(book);
+        }
+      }
+    } finally {
+      super.dispose(db);
+    }
+  }
+
   saveToAws(webPath) {
     return new Promise((res, rej) => {
       fs.readFile("." + webPath, (err, data) => {
