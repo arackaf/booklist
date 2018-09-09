23dfed1fb25b4f0ebc514e4095d01f890578967a
Adam Rackis <adamrackis@gmail.com>
2018-09-09 16:30:53 -0500

    create a proper modal component

---
+++ react-redux/applicationRoot/components/modal.tsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+import { Dialog, DialogOverlay, DialogContent } from "@reach/dialog";
+import { Transition, config } from "react-spring";
+
+export class StandardModalHeader extends Component<{ onHide: any; caption: any }, any> {
+  render() {
+    let { onHide, caption } = this.props;
+    return (
+      <>
+        <button type="button" className="close" onClick={onHide} aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 className="modal-title">{caption}</h4>
+        <hr />
+      </>
+    );
+  }
+}
+
+export default class Modal extends Component<{ isOpen: boolean; onHide: any; headerCaption?: any }, any> {
+  render() {
+    let { isOpen, onHide, headerCaption, children } = this.props;
+    return (
+      <Transition
+        config={{ ...config.gentle, overshootClamping: true }}
+        from={{ opacity: 0, y: -10 }}
+        enter={{ opacity: 1, y: 0 }}
+        leave={{ opacity: 0, y: 10 }}
+      >
+        {isOpen
+          ? (styles: any) => (
+              <DialogOverlay onDismiss={onHide} isOpen={isOpen} style={{ opacity: styles.opacity }}>
+                <DialogContent
+                  style={{
+                    transform: `translate3d(0px, ${styles.y}px, 0px)`,
+                    border: "4px solid hsla(0, 0%, 0%, 0.5)",
+                    borderRadius: 10
+                  }}
+                >
+                  {headerCaption ? <StandardModalHeader caption={headerCaption} onHide={onHide} /> : null}
+                  {children}
+                </DialogContent>
+              </DialogOverlay>
+            )
+          : null}
+      </Transition>
+    );
+  }
+}
