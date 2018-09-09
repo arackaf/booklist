fe7831d6cfef1526152c16343d002867dfd2a995
Adam Rackis <adamrackis@hotmail.com>
2018-09-09 15:14:08 -0500

    typings shit and code cleanup

---
+++ react-redux/typings/modules/@reach/dialog/index.d.ts
@@ -0,0 +1,5 @@
+import { Component } from "react";
+
+export class Dialog extends Component<any, any> {}
+export class DialogOverlay extends Component<any, any> {}
+export class DialogContent extends Component<any, any> {}
