9e219da2becb3acbd64f0aa045e62ff50b543e06
Adam Rackis <adamrackis@hotmail.com>
2018-12-01 18:19:25 -0500

    More cleanup

--- react-redux/modules/home/homeComponent.tsx
+++ react-redux/modules/home/homeComponent.tsx
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import Measure from "react-measure";
 import "d3-transition";
