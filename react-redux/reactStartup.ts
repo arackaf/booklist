e6de4186720d535a5b452d310958d858089ba4cb
Adam Rackis <adamrackis@gmail.com>
2018-12-01 13:30:26 -0600

    reactStartup cleanup

--- react-redux/reactStartup.ts
+++ react-redux/reactStartup.ts
@@ -31,94 +31,13 @@ import "util/ajaxUtil";
 
 import createHistory from "history/createBrowserHistory";
 import { loadTags } from "applicationRoot/tags/actionCreators";
+import setupServiceWorker from "./util/setupServiceWorker";
 
-declare var window: any;
-
-(function() {
-  if ("serviceWorker" in navigator && !/localhost/.test(window.location as any)) {
-    navigator.serviceWorker.register("/service-worker.js").then(registration => {
-      if (registration.waiting && registration.active) {
-        newerSwAvailable(registration.waiting);
-      }
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === "installed") {
-            if (navigator.serviceWorker.controller) {
-              newerSwAvailable(installingWorker);
-            }
-          }
-        };
-      };
-    });
-
-    function newerSwAvailable(sw) {
-      try {
-        navigator.serviceWorker.addEventListener("message", event => {
-          if (event.data == "sw-updated") {
-            location.reload();
-          }
-        });
-        import("toastify-js").then(({ default: Toastify }) => {
-          window.addEventListener("click", evt => {
-            try {
-              if (evt.target.classList.contains("do-sw-update")) {
-                sw.postMessage("sw-update-accepted");
-              }
-            } catch (e) {}
-          });
-          Toastify({
-            text: `
-              <h4 style='display: inline'>An update is available!</h4>
-              <br><br>
-              <a class='do-sw-update' style='color: white'>Click to update and reload</a>&nbsp;&nbsp;
-            `.trim(),
-            duration: 7000,
-            gravity: "bottom",
-            close: true
-          }).showToast();
-        });
-      } catch (er) {}
-    }
-
-    try {
-      navigator.serviceWorker.controller.postMessage({ command: "sync-images" });
-    } catch (er) {}
-
-    // if (Notification) {
-    //   Notification.requestPermission().then(permission => {});
-    // }
-
-    if (isLoggedIn()) {
-      // let subscriptionOptions = {
-      //   userVisibleOnly: true,
-      //   applicationServerKey: urlBase64ToUint8Array("BCC0wqyL-OGz5duRO9-kOSUEv72BMGf0x0oaMGryF1eLa3FF-sW2YmunhNqQegrXHykP-Wa6xC1rEnDuBGtjgUo")
-      // };
-      // navigator.serviceWorker.ready.then(registration => {
-      //   registration.pushManager.subscribe(subscriptionOptions).then(subscription => {
-      //     ajaxUtil.post("/user/saveNotificationSubscription", { subscription: JSON.stringify(subscription) });
-      //   });
-      // });
-      /*
-      
-        async saveNotificationSubscription({ subscription }) {
-          let userId = this.request.user.id;
-          await new UserDAO().updateSubscription(userId, JSON.parse(subscription));
-          this.send({ success: true });
-        }
-      
-      */
-    }
-  }
-})();
+setupServiceWorker();
 
 if ("ontouchstart" in window || "onmsgesturechange" in window) {
   store.dispatch(setIsTouch(true));
 }
-
-try {
-  var desktopRequested = !!localStorage.getItem("useDesktop");
-} catch (x) {}
 
 if (window.screen.width < 700) {
   store.dispatch(setMobile());
@@ -126,7 +45,7 @@ if (window.screen.width < 700) {
   store.dispatch(setDesktop());
 }
 
-if (desktopRequested) {
+if (!!localStorage.getItem("useDesktop")) {
   store.dispatch(setRequestDesktop());
 }
 
@@ -138,8 +57,7 @@ if (isLoggedIn().logged_in) {
   store.dispatch(loadSubjects());
 }
 
-const history = createHistory();
-export { history };
+export const history = createHistory();
 
 const validModules = new Set(["books", "scan", "home", "activate", "view", "subjects", "settings"]);
 let initial = true;
