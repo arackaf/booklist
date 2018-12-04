35d821528d1a958a7ae1cccb1ae746a07d2374a6
Adam Rackis <adamrackis@gmail.com>
2018-12-04 08:26:49 -0500

    Don't service worker cache activate page

--- react-redux/webpack.config.js
+++ react-redux/webpack.config.js
@@ -115,7 +115,8 @@ module.exports = {
         getCache({ pattern: /^https:\/\/s3.amazonaws.com\/my-library-cover-uploads/, name: "local-images1" }),
         getCache({ pattern: /fontawesome\/webfonts/, name: "fontawesome-fonts" })
       ],
-      importScripts: ["react-redux/sw-manual-update-sync.js"]
+      importScripts: ["react-redux/sw-manual-update-sync.js"],
+      navigateFallbackBlacklist: [/\/activate\b/]
     }),
     //new BundleAnalyzerPlugin({ analyzerMode: "static" }),
     isProd ? new MinifyPlugin() : null
