6d363468fb272f1c895a06b89e35def1cfd7c1f8
Adam Rackis <adamrackis@gmail.com>
2019-01-05 15:07:38 -0600

    use Rollup like a civilized human

---
+++ react-redux/sw-manual/rollup.config.js
@@ -0,0 +1,12 @@
+const graphqlPlugin = require("./rollupGraphqlPlugin");
+const minify = require("rollup-plugin-babel-minify");
+const path = require("path");
+
+module.exports = {
+  input: "./index.js",
+  output: {
+    format: "iife",
+    file: "./sw-index-bundle.js"
+  },
+  plugins: [/*minify({}),*/ graphqlPlugin({ path: path.resolve(__dirname, "../extracted_queries.json") })]
+};
