f7265fc0068f2ed2a7270f70467a04e9d0f85296
Adam Rackis <adamrackis@gmail.com>
2019-01-24 21:15:21 -0600

    More cleanup

---
+++ react-redux/sw-manual/src/incrementalSync.js
@@ -0,0 +1,47 @@
+import { deleteItem } from "./indexedDbUpdateUtils";
+
+export async function syncResultsFor({ request, response }, name, transform = item => item) {
+  let createNameSingle = `create${name}`;
+  if (response && response.data && response.data[createNameSingle] && response.data[createNameSingle][name]) {
+    syncItem(transform(response.data[createNameSingle][name]), `${name.toLowerCase()}s`);
+  }
+  let updateNameSingle = `update${name}`;
+  if (response && response.data && response.data[updateNameSingle] && response.data[updateNameSingle][name]) {
+    syncItem(transform(response.data[updateNameSingle][name]), `${name.toLowerCase()}s`);
+  }
+  let updateNamePlural = `update${name}s`;
+  if (response && response.data && response.data[updateNamePlural] && response.data[updateNamePlural][name + "s"]) {
+    response.data[updateNamePlural][name + "s"].forEach(item => syncItem(transform(item), `${name.toLowerCase()}s`));
+  }
+  let deleteNameSingle = `delete${name}`;
+  if (response && response.data && response.data[deleteNameSingle]) {
+    let reqJson = await request.json();
+    deleteItem(reqJson.variables._id, name.toLowerCase() + "s");
+  }
+}
+
+export function syncSubjectsResults(resp) {
+  if (resp && resp.data && resp.data.updateSubject) {
+    resp.data.updateSubject.forEach(s => syncItem(s, `subjects`));
+  }
+}
+
+export function syncItem(item, table, transform = item => item) {
+  let open = indexedDB.open("books", 1);
+
+  return new Promise(res => {
+    open.onsuccess = evt => {
+      let db = open.result;
+      let tran = db.transaction(table, "readwrite");
+      let objStore = tran.objectStore(table);
+      objStore.get(item._id).onsuccess = ({ target: { result: itemToUpdate } }) => {
+        if (!itemToUpdate) {
+          objStore.add(transform(item)).onsuccess = res;
+        } else {
+          Object.assign(itemToUpdate, transform(item));
+          objStore.put(itemToUpdate).onsuccess = res;
+        }
+      };
+    };
+  });
+}
---
+++ react-redux/sw-manual/src/initialSync.js
@@ -0,0 +1,128 @@
+import { updateSyncInfo, insertItems } from "./indexedDbUpdateUtils";
+
+import { doFetch } from "./util";
+
+import allSubjects from "../../graphQL/subjects/allSubjects.graphql";
+import allTags from "../../graphQL/tags/getTags.graphql";
+import allLabelColors from "../../graphQL/misc/allLabelColors.graphql";
+import initialOfflineBookSync from "../../graphQL/books/initialOfflineBookSync.graphql";
+
+export function fullSync(page = 1) {
+  let open = indexedDB.open("books", 1);
+
+  // Set up the database schema
+  open.onsuccess = evt => {
+    let db = open.result;
+    Promise.all([new Promise(res => doBooksSync(db, res)), doSubjectsSync(db), doTagsSync(db), doLabelColorsSync(db)]).then(() => {
+      updateSyncInfo(db, { lastSync: +new Date() });
+    });
+  };
+}
+
+function doBooksSync(db, onFinish, page = 1) {
+  let pageSize = 50;
+  getGraphqlResults(initialOfflineBookSync, { page, pageSize }, "allBooks", "Books").then(books => {
+    insertItems(db, books, "books", { transformItem: book => Object.assign(book, { imgSync: 0, title_ci: (book.title || "").toLowerCase() }) }).then(
+      () => {
+        if (books.length == pageSize) {
+          doBooksSync(db, onFinish, page + 1);
+        } else {
+          syncImages(db, onFinish);
+        }
+      }
+    );
+  });
+}
+
+function doSubjectsSync(db, page = 1) {
+  return getGraphqlResults(allSubjects, {}, "allSubjects", "Subjects").then(subjects => insertItems(db, subjects, "subjects"));
+}
+
+function doTagsSync(db, page = 1) {
+  return getGraphqlResults(allTags, {}, "allTags", "Tags").then(tags => insertItems(db, tags, "tags"));
+}
+
+function doLabelColorsSync(db, page = 1) {
+  return getGraphqlResults(allLabelColors, {}, "allLabelColors", "LabelColors").then(labelColors => insertItems(db, labelColors, "labelColors"));
+}
+
+async function syncImages(db, onComplete) {
+  let tran = db.transaction("books");
+  let booksStore = tran.objectStore("books");
+  let idx = booksStore.index("imgSync");
+  let booksCursor = idx.openCursor(0);
+  let booksToUpdate = [];
+
+  booksCursor.onsuccess = evt => {
+    let cursor = evt.target.result;
+    if (!cursor) return runIt();
+
+    let book = cursor.value;
+    booksToUpdate.push({ _id: book._id, smallImage: book.smallImage });
+    cursor.continue();
+  };
+
+  async function runIt() {
+    if (!booksToUpdate.length) return;
+
+    for (let _book of booksToUpdate) {
+      try {
+        let imgCached = await preCacheBookImage(_book);
+        if (imgCached) {
+          let tran = db.transaction("books", "readwrite");
+          let booksStore = tran.objectStore("books");
+          await new Promise(res => {
+            let req = booksStore.get(_book._id);
+            req.onsuccess = ({ target: { result: bookToUpdate } }) => {
+              bookToUpdate.imgSync = 1;
+              booksStore.put(bookToUpdate).onsuccess = res;
+            };
+            req.onerror = () => res();
+          });
+        }
+      } catch (er) {
+        console.log("ERROR", er);
+      } finally {
+      }
+    }
+    onComplete();
+  }
+}
+
+async function preCacheBookImage(book) {
+  let smallImage = book.smallImage;
+  if (!smallImage) return;
+
+  let cachedImage = await caches.match(smallImage);
+  if (cachedImage) return;
+
+  if (/https:\/\/s3.amazonaws.com\/my-library-cover-uploads/.test(smallImage)) {
+    let cache = await caches.open("local-images1");
+    let img = await fetch(smallImage, { mode: "cors", credentials: "omit" });
+
+    await cache.put(smallImage, img);
+    return true;
+  }
+
+  if (/https:\/\/images-na\.ssl-images-amazon\.com/.test(smallImage)) {
+    let cache = await caches.open("amazon-images1");
+    let img = await fetch(smallImage, { mode: "cors", credentials: "omit" });
+    await cache.put(smallImage, img);
+    return true;
+  }
+
+  if (/https:\/\/ecx\.images-amazon\.com/.test(smallImage)) {
+    let cache = await caches.open("amazon-images2");
+    let img = await fetch(smallImage, { mode: "cors", credentials: "omit" });
+    await cache.put(smallImage, img);
+    return true;
+  }
+}
+
+function getGraphqlResults(query, variables, op, name) {
+  return doFetch(`/graphql/?query=${query}&variables=${JSON.stringify(variables)}`)
+    .then(resp => resp.json())
+    .then(resp => {
+      return resp.data && resp.data[op] && resp.data[op][name];
+    });
+}
