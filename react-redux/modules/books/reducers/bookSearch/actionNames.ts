6958203cb3c0fc35504215d1614e6b33698f2a12
Adam Rackis <adamrackis@gmail.com>
2019-02-23 19:59:30 -0600

    Remove books reducer code

--- react-redux/modules/books/reducers/bookSearch/actionNames.ts
+++
@@ -1,6 +0,0 @@
-export const SET_GRID_VIEW = "booksSearch.SET_GRID_VIEW";
-export const SET_BASIC_LIST_VIEW = "booksSearch.SET_BasicList_VIEW";
-
-export const GRID_VIEW = "books table view";
-export const BASIC_LIST_VIEW = "books mobile view";
-export const HASH_CHANGED = "books search hash changed";
