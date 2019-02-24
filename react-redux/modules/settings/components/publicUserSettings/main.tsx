8dfb05f5699e19159dc2f2acdfe4db89f90f4a48
Adam Rackis <adamrackis@gmail.com>
2019-02-24 14:27:49 -0600

    Public user settings on hooks

--- react-redux/modules/settings/components/publicUserSettings/main.tsx
+++ react-redux/modules/settings/components/publicUserSettings/main.tsx
@@ -1,92 +1,119 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { selector } from "../../reducers/publicUserSettings/reducer";
-import * as actionCreators from "../../reducers/publicUserSettings/actionCreators";
+import React, { FunctionComponent, useContext, useState, useRef } from "react";
 import { SectionLoading } from "applicationRoot/components/loading";
 import { AjaxButton } from "applicationRoot/components/bootstrapButton";
 
-const styleTwenty: any = { left: "20%" };
+import PublicUserSettingsQuery from "graphQL/settings/getPublisUserSettingsQuery.graphql";
+import UpdatePublisUserSettingsMutation from "graphQL/settings/updatePublicUserSettings.graphql";
+import { useQuery, buildQuery, useMutation, buildMutation } from "micro-graphql-react";
+import { AppContext } from "applicationRoot/renderUI";
 
-@connect(
-  selector,
-  { ...actionCreators }
-)
-export default class PublicUserSettings extends Component<ReturnType<typeof selector> & typeof actionCreators, any> {
-  render() {
-    return (
-      <div>
-        <div className="row">
-          <div className="col-md-6 col-sm-12" style={{ position: "relative", minHeight: "200px" }}>
-            {this.props.loading ? (
-              <SectionLoading style={styleTwenty} />
-            ) : (
-              <div style={{ padding: "10px" }}>
-                {this.props.publicLink ? (
-                  <div className="panel panel-default" style={{ padding: "10px" }}>
-                    Your collection is currently public, viewable at <br />
-                    <br />
-                    <a target="_blank" href={this.props.publicLink}>
-                      {this.props.publicLink}
-                    </a>
-                  </div>
-                ) : null}
-                <div className="form-group">
-                  <label className="checkbox">
-                    Allow your book collection to be viewed publicly?{" "}
-                    <input
-                      checked={this.props.editing.isPublic}
-                      disabled={this.props.saving}
-                      onChange={this.props.editIsPublic}
-                      style={{ marginLeft: "5px" }}
-                      type="checkbox"
-                    />
-                  </label>
-                </div>
-                {this.props.editing.isPublic ? (
-                  <div style={{ marginLeft: "20px" }}>
-                    <div className="form-group">
-                      <label htmlFor="publicNameInput">Publicly display your name as</label>
-                      <input
-                        value={this.props.editing.publicName}
-                        disabled={this.props.saving}
-                        onChange={this.props.editPublicName}
-                        className="form-control"
-                        id="publicNameInput"
-                        placeholder="Public name"
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="publicBooksHeader">Publicly display your name as</label>
-                      <input
-                        value={this.props.editing.publicBooksHeader}
-                        disabled={this.props.saving}
-                        onChange={this.props.editPublicBooksHeader}
-                        className="form-control"
-                        id="publicBooksHeader"
-                        placeholder="Book header"
-                      />
-                    </div>
-                    <div className="alert alert-info" role="alert">
-                      This is what will show in the navigation when people view your book collection. Leave it blank to default to [public name]'s
-                      Books. See below
-                    </div>
-                    <img src="/react-redux/static/publicBookHeaderInfo.png" />
-                  </div>
-                ) : null}
-                <AjaxButton
-                  disabled={!this.props.isDirty || !this.props.editing.publicName}
-                  onClick={this.props.savePublicInfo}
-                  running={this.props.saving}
-                  runningText="Saving"
-                  preset="primary"
-                >
-                  Save
-                </AjaxButton>
-              </div>
-            )}
-          </div>
+const PublicUserSettings: FunctionComponent<{}> = props => {
+  const { loading, loaded, data } = useQuery(buildQuery(PublicUserSettingsQuery, {}));
+
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-6 col-sm-12" style={{ position: "relative", minHeight: "200px" }}>
+          {!loaded ? <SectionLoading style={{ left: "20%" }} /> : <EditPublicUserSettings settings={data.getUser.User} />}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+interface UserSettings {
+  isPublic: boolean;
+  publicBooksHeader: string;
+  publicName: string;
 }
+
+const EditPublicUserSettings: FunctionComponent<{ settings: UserSettings }> = props => {
+  const [app] = useContext(AppContext);
+  const { runMutation, running: saving } = useMutation(buildMutation(UpdatePublisUserSettingsMutation));
+  const { settings } = props;
+  const { publicBooksHeader, publicName } = settings;
+  const [pendingIsPublic, setPendingIsPublic] = useState(settings.isPublic);
+  const [isPublic, setIsPublic] = useState(settings.isPublic);
+  const [isDirty, setDirtyState] = useState(false);
+  const setDirty = () => setDirtyState(true);
+
+  const publicLink = isPublic ? `http://${window.location.host}/view?userId=${app.userId}` : "";
+
+  const pubNameEl = useRef(null);
+  const pubHeaderEl = useRef(null);
+
+  const update = () => {
+    let isPublic = pendingIsPublic;
+    runMutation({
+      isPublic: pendingIsPublic,
+      publicBooksHeader: pubHeaderEl.current ? pubHeaderEl.current.value : "",
+      publicName: pubNameEl.current ? pubNameEl.current.value : ""
+    }).then(() => setIsPublic(isPublic));
+  };
+
+  return (
+    <div style={{ padding: "10px" }}>
+      {publicLink ? (
+        <div className="panel panel-default" style={{ padding: "10px" }}>
+          Your collection is currently public, viewable at <br />
+          <br />
+          <a target="_blank" href={publicLink}>
+            {publicLink}
+          </a>
+        </div>
+      ) : null}
+      <div className="form-group">
+        <label className="checkbox">
+          Allow your book collection to be viewed publicly?{" "}
+          <input
+            onChange={evt => {
+              setDirty();
+              setPendingIsPublic(evt.target.checked);
+            }}
+            defaultChecked={pendingIsPublic}
+            disabled={saving}
+            style={{ marginLeft: "5px" }}
+            type="checkbox"
+          />
+        </label>
+      </div>
+      {pendingIsPublic ? (
+        <div style={{ marginLeft: "20px" }}>
+          <div className="form-group">
+            <label htmlFor="pName">Publicly display your name as</label>
+            <input
+              ref={pubNameEl}
+              onChange={setDirty}
+              defaultValue={publicName}
+              disabled={saving}
+              className="form-control"
+              id="pName"
+              placeholder="Public name"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="publicBooksHeader">Publicly display your name as</label>
+            <input
+              ref={pubHeaderEl}
+              defaultValue={publicBooksHeader}
+              disabled={saving}
+              className="form-control"
+              id="publicBooksHeader"
+              placeholder="Book header"
+            />
+          </div>
+          <div className="alert alert-info" role="alert">
+            This is what will show in the navigation when people view your book collection. Leave it blank to default to [public name]'s Books. See
+            below
+          </div>
+          <img src="/react-redux/static/publicBookHeaderInfo.png" />
+        </div>
+      ) : null}
+      <AjaxButton disabled={!isDirty} onClick={update} running={saving} runningText="Saving" preset="primary">
+        Save
+      </AjaxButton>
+    </div>
+  );
+};
+
+export default PublicUserSettings;
