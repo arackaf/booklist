8bfcdd873ccd14131f0cd9413f12b2fe4b7b46b4
Adam.Rackis <adam.rackis@riotgames.com>
2019-02-23 19:59:30 -0600

    Remove books reducer code

--- react-redux/modules/books/books.ts
+++ react-redux/modules/books/books.ts
@@ -1,9 +1,5 @@
 import { BookModuleRoot } from "./components/bookViewList";
-import { reducer } from "./reducers/reducer";
-import { booksInitialized } from "./reducers/bookSearch/actionCreators";
 
 export default {
-  reducer: reducer,
-  component: BookModuleRoot,
-  initialize: booksInitialized
+  component: BookModuleRoot
 };
