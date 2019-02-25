129a96f172996b0bc34fd70695ef694cd42da315
Adam.Rackis <adam.rackis@riotgames.com>
2019-02-24 21:34:50 -0600

    Minor cleanup in home

--- react-redux/modules/home/components/barChart.tsx
+++ react-redux/modules/home/components/barChart.tsx
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent } from "react";
+import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 
 import scaleLinear from "d3-scale/src/linear";
