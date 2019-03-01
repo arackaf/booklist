771a617497a360874baa4f3160ea9ee2de0b95cc
Adam Rackis <adamrackis@gmail.com>
2019-03-01 13:42:31 -0600

    fix filters modal

--- react-redux/applicationRoot/components/selectAvailableSubjects.tsx
+++ react-redux/applicationRoot/components/selectAvailableSubjects.tsx
@@ -1,16 +1,18 @@
 import React, { FunctionComponent } from "react";
 import SelectAvailableItems from "./availableTagsOrSubjects";
 import { filterTags } from "applicationRoot/tagsState";
+import { useStackedSubjects } from "applicationRoot/subjectsState";
 
 type LocalProps = { currentlySelected: string[]; onSelect: any };
 
 const SelectAvailableSubjects: FunctionComponent<LocalProps> = props => {
+  const { subjectsUnwound } = useStackedSubjects();
   return (
     <SelectAvailableItems
       placeholder="Subjects"
-      items={this.props.subjectsUnwound}
-      currentlySelected={this.props.currentlySelected}
-      onSelect={this.props.onSelect}
+      items={subjectsUnwound}
+      currentlySelected={props.currentlySelected}
+      onSelect={props.onSelect}
       filter={filterTags}
     />
   );
