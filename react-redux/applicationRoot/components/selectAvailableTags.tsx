ad6b617fd40c050aed79396db9952546f03068f4
Adam.Rackis <adam.rackis@riotgames.com>
2019-03-01 13:42:31 -0600

    fix filters modal

--- react-redux/applicationRoot/components/selectAvailableTags.tsx
+++ react-redux/applicationRoot/components/selectAvailableTags.tsx
@@ -1,18 +1,14 @@
-import React, { Component, FunctionComponent } from "react";
+import React, { Component, FunctionComponent, useContext } from "react";
 import SelectAvailableItems from "./availableTagsOrSubjects";
 import { filterTags } from "applicationRoot/tagsState";
+import { TagsContext } from "modules/books/components/bookViewList";
 
 type LocalProps = { currentlySelected: string[]; onSelect: any };
 
 const SelectAvailableTags: FunctionComponent<LocalProps> = props => {
+  const { tags } = useContext(TagsContext);
   return (
-    <SelectAvailableItems
-      placeholder="Tags"
-      items={this.props.allTagsSorted}
-      currentlySelected={this.props.currentlySelected}
-      onSelect={this.props.onSelect}
-      filter={filterTags}
-    />
+    <SelectAvailableItems placeholder="Tags" items={tags} currentlySelected={props.currentlySelected} onSelect={props.onSelect} filter={filterTags} />
   );
 };
 
