86e84c82f59c21405ddc836d8ae6eae9e9f8d082
Adam Rackis <adamrackis@gmail.com>
2019-04-09 20:52:47 -0500

    convert sync task to esm, use shared db util

--- node-src/util/syncDemo.js
+++ node-src/util/syncDemo.js
@@ -1,51 +1,51 @@
-require("dotenv").config();
-const { MongoClient, ObjectId } = require("mongodb");
-
-const mongoClientPromise = MongoClient.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true });
-const mongoDbPromise = mongoClientPromise.then(client => client.db(process.env.DB_NAME));
+import { MongoClient, ObjectId } from "mongodb";
+import { getDbConnection } from "./dbUtils";
 
 let masterId = process.env.DEMO_REPLICATION_MASTER;
 let slaveId = process.env.DEMO_REPLICATION_SLAVE;
 
 async function sync() {
-  let client = await mongoClientPromise;
-  let db = await mongoDbPromise;
-
-  await db.collection("books").deleteMany({ userId: slaveId });
-  await db.collection("subjects").deleteMany({ userId: slaveId });
-
-  let newSubjects = await db
-    .collection("subjects")
-    .find({ userId: masterId })
-    .toArray();
-
-  let newBooks = await db
-    .collection("books")
-    .find({ userId: masterId })
-    .toArray();
-
-  let subjectMap = new Map([]);
-
-  for (let s of newSubjects) {
-    let oldId = s._id;
-    delete s._id;
-    s.userId = slaveId;
-    await db.collection("subjects").insertOne(s);
-    subjectMap.set("" + oldId, "" + s._id);
-  }
-
-  let booksToAdd = [];
-  for (let b of newBooks) {
-    delete b._id;
-    b.userId = slaveId;
-    b.subjects = b.subjects.map(id => subjectMap.get(id)).filter(id => id);
-    booksToAdd.push(b);
-  }
-  await db.collection("books").insertMany(booksToAdd);
+  let { client, db } = await getDbConnection();
 
   try {
-    client.close();
-  } catch (er) {}
+    await db.collection("books").deleteMany({ userId: slaveId });
+    await db.collection("subjects").deleteMany({ userId: slaveId });
+
+    let newSubjects = await db
+      .collection("subjects")
+      .find({ userId: masterId })
+      .toArray();
+
+    let newBooks = await db
+      .collection("books")
+      .find({ userId: masterId })
+      .toArray();
+
+    let subjectMap = new Map([]);
+
+    for (let s of newSubjects) {
+      let oldId = s._id;
+      delete s._id;
+      s.userId = slaveId;
+      await db.collection("subjects").insertOne(s);
+      subjectMap.set("" + oldId, "" + s._id);
+    }
+
+    let booksToAdd = [];
+    for (let b of newBooks) {
+      delete b._id;
+      b.userId = slaveId;
+      b.subjects = b.subjects.map(id => subjectMap.get(id)).filter(id => id);
+      booksToAdd.push(b);
+    }
+    await db.collection("books").insertMany(booksToAdd);
+  } catch (er) {
+    console.log("ERROR", er);
+  } finally {
+    try {
+      client.close();
+    } catch (e) {}
+  }
 }
 
 sync();
