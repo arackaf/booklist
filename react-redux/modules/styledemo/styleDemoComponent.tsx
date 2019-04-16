50d51e6027a19c52bf965f8d39874b1544ce8fc3
Adam.Rackis <adam.rackis@riotgames.com>
2019-04-16 16:03:09 -0500

    style demo added - small style tweaks

---
+++ react-redux/modules/styledemo/styleDemoComponent.tsx
@@ -0,0 +1,262 @@
+import React from "react";
+
+export default () => (
+  <div className="scheme1" id="home">
+    <div style={{ padding: "5px" }}>
+      <br />
+
+      <div className="btn-group">
+        <input style={{ width: "200px" }} className="form-control hidden-tiny" placeholder="Title search" />
+
+        <button title="Filter search" className="btn">
+          <i className="fal fa-filter" />
+        </button>
+
+        <button disabled={true} title="Edit subjects" className="btn btn-default">
+          <i className="fal fa-sitemap" />
+        </button>
+        <button title="Edit tags" className="btn btn-default">
+          <i className="fal fa-tags" />
+        </button>
+
+        <button className="btn active">
+          <i className="fal fa-table" />
+        </button>
+        <button className="btn">Hi there</button>
+        <button className="btn">
+          <i className="fal fa-list" />
+        </button>
+      </div>
+      <br />
+
+      <button className="btn btn-primary margin-right">Primary Button</button>
+      <button className="btn btn-primary btn-xs margin-right">Primary Button XS</button>
+      <button disabled={true} className="btn btn-primary margin-right">
+        Primary Button
+      </button>
+      <button disabled={true} className="btn btn-primary btn-xs margin-right">
+        Primary Button XS
+      </button>
+      <br />
+      <br />
+      <button className="btn btn-info margin-right">Success Button</button>
+      <button className="btn btn-info btn-xs margin-right">Success Button XS</button>
+      <button disabled={true} className="btn btn-info margin-right">
+        Success Button
+      </button>
+      <button disabled={true} className="btn btn-info btn-xs margin-right">
+        Success Button XS
+      </button>
+      <br />
+      <br />
+      <button className="btn btn-success margin-right">Success Button</button>
+      <button className="btn btn-success btn-xs margin-right">Success Button XS</button>
+      <button disabled={true} className="btn btn-success margin-right">
+        Success Button
+      </button>
+      <button disabled={true} className="btn btn-success btn-xs margin-right">
+        Success Button XS
+      </button>
+      <br />
+      <br />
+      <button className="btn btn-danger margin-right">Danger Button</button>
+      <button className="btn btn-danger btn-xs margin-right">Danger Button XS</button>
+      <button disabled={true} className="btn btn-danger margin-right">
+        Danger Button
+      </button>
+      <button disabled={true} className="btn btn-danger btn-xs margin-right">
+        Danger Button XS
+      </button>
+
+      <br />
+      <br />
+
+      <span className="label label-info margin-right">Info Label</span>
+      <span className="label label-success margin-right">Success Label</span>
+      <span className="label label-danger margin-right">Danger Label</span>
+      <span className="label margin-right" style={{ backgroundColor: "rgb(13, 230, 255)" }}>
+        Misc Label
+      </span>
+
+      <br />
+      <br />
+      <div className="alert alert-success">Success Alert</div>
+      <br />
+      <div className="alert alert-warning">Warning Alert</div>
+      <br />
+      <div className="alert alert-danger">Danger Alert</div>
+      <br />
+      <div className="alert alert-info">Info Alert</div>
+      <br />
+      <br />
+
+      <hr />
+
+      <br />
+
+      <div className="row">
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+        <div className="col-lg-1 col-md-3 col-sm-6 col-xs-12">l1 m3 s6 x12</div>
+      </div>
+
+      <br />
+      <br />
+
+      <div className="tab-headers">
+        <div className="tab-header">
+          <a>Section A</a>
+        </div>
+        <div className="tab-header active">
+          <a>Section B</a>
+        </div>
+        <div className="tab-header">
+          <a>Section C</a>
+        </div>
+        <div className="tab-header">
+          <a>Section D</a>
+        </div>
+      </div>
+
+      <br />
+      <br />
+
+      <table className="table table-condensed table-striped">
+        <thead>
+          <tr>
+            <th style={{ paddingLeft: "5px" }}>
+              <i className="fal fa-square" />
+            </th>
+            <th />
+            <th style={{ minWidth: "250px" }}>Title</th>
+            <th>Subjects</th>
+            <th>Tags</th>
+            <th style={{ minWidth: "90px" }}>Read</th>
+            <th>Info</th>
+            <th style={{ minWidth: "86px" }}>Pages</th>
+            <th>Added</th>
+          </tr>
+        </thead>
+        <tbody />
+      </table>
+    </div>
+
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </div>
+);
