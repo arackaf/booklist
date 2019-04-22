d3386af1ea0d4aef887fac89747bcfde382ad077
Adam.Rackis <adam.rackis@riotgames.com>
2019-04-21 23:49:02 -0700

    Track current theme in ui

--- react-redux/modules/settings/components/themeChooser/main.tsx
+++ react-redux/modules/settings/components/themeChooser/main.tsx
@@ -11,13 +11,16 @@ const themeNames = Array.from({ length: numThemes }, (v, i) => `scheme${i + 1}`)
 const arrayOfTen = Array.from({ length: 10 }, (v, i) => i + 1);
 
 const ThemeChooser = props => {
-  const [state, actions, dispatch] = useContext(AppContext);
+  const [{ colorTheme }, actions, dispatch] = useContext(AppContext);
   return (
     <div>
       {themeNames.map((name, index) => (
         <>
-          <div onClick={() => dispatch({ type: SET_THEME, theme: name })} className={classNames(name, themeChooser)}>
-            <span>{"Theme " + (index + 1)}</span>
+          <div onClick={() => dispatch({ type: SET_THEME, theme: name })} className={classNames(name, themeChooser, { active: colorTheme == name })}>
+            <span>
+              {"Theme " + (index + 1)}
+              {colorTheme == name ? <i className="far fa-check margin-left" /> : null}
+            </span>
             {arrayOfTen.map(val => (
               <div style={{ backgroundColor: `var(--primary-${val})` }} />
             ))}
