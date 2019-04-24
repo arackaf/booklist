cc5e0531ac3128880db247810aceebdf2ec778b5
Adam Rackis <adamrackis@gmail.com>
2019-04-23 17:46:19 -0700

    Start on StyleDemo

--- react-redux/modules/settings/components/themeChooser/main.tsx
+++ react-redux/modules/settings/components/themeChooser/main.tsx
@@ -3,7 +3,10 @@ import classNames from "classnames";
 import styles from "./styles.module.css";
 import { AppContext } from "app/renderUI";
 import { SET_THEME } from "app/appState";
-const { themeChooser } = styles;
+
+import DemoStyles from "./demoStyles";
+
+const { themeChooserRoot, themeChooser } = styles;
 
 const numThemes = 17;
 
@@ -13,21 +16,29 @@ const arrayOfTen = Array.from({ length: 10 }, (v, i) => i + 1);
 const ThemeChooser = props => {
   const [{ colorTheme }, actions, dispatch] = useContext(AppContext);
   return (
-    <div>
-      {themeNames.map((name, index) => (
-        <>
-          <div onClick={() => dispatch({ type: SET_THEME, theme: name })} className={classNames(name, themeChooser, { active: colorTheme == name })}>
-            <span>
-              {"Theme " + (index + 1)}
-              {colorTheme == name ? <i className="far fa-check margin-left" /> : null}
-            </span>
-            {arrayOfTen.map(val => (
-              <div style={{ backgroundColor: `var(--primary-${val})` }} />
-            ))}
-          </div>
-          <br />
-        </>
-      ))}
+    <div className={themeChooserRoot}>
+      <div>
+        {themeNames.map((name, index) => (
+          <>
+            <div
+              onClick={() => dispatch({ type: SET_THEME, theme: name })}
+              className={classNames(name, themeChooser, { active: colorTheme == name })}
+            >
+              <span>
+                {"Theme " + (index + 1)}
+                {colorTheme == name ? <i className="far fa-check margin-left" /> : null}
+              </span>
+              {arrayOfTen.map(val => (
+                <div style={{ backgroundColor: `var(--primary-${val})` }} />
+              ))}
+            </div>
+            <br />
+          </>
+        ))}
+      </div>
+      <div>
+        <DemoStyles />
+      </div>
     </div>
   );
 };
