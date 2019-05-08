5dcc990dd8c6ec15cbd73ce2b25e0daa1dc24195
Adam.Rackis <adam.rackis@riotgames.com>
2019-05-08 14:25:18 -0500

    tweak history bundling

--- sapper/src/util/history.js
+++ sapper/src/util/history.js
@@ -2,7 +2,6 @@ const historyModule = require("history");
 
 //FUCK SSR
 const historyManager = typeof window == "object" ? historyModule.createBrowserHistory : historyModule.createMemoryHistory;
-
 const history = historyManager();
 
 export default history;
