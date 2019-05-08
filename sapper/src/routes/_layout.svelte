a56f0d84cb4503622e0de88fc0d35a5ac0bb5145
Adam.Rackis <adam.rackis@riotgames.com>
2019-05-08 11:29:32 -0500

    History updating

--- sapper/src/routes/_layout.svelte
+++ sapper/src/routes/_layout.svelte
@@ -8,17 +8,12 @@
 
 <style>
   main {
-    position: relative;
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
+    padding: 20px;
   }
 </style>
 
 <Nav {segment} />
 
-<main>
+<main class="scheme3">
   <slot />
 </main>
--- sapper/src/routes/books/components/BooksMenuBar.svelte
+++ sapper/src/routes/books/components/BooksMenuBar.svelte
@@ -1,1 +1,16 @@
-<div>Menu Bar!</div>
+<script>
+  import history from "../../../util/history";
+
+  function keydown(e) {
+    if (e.keyCode == 13) {
+      history.push({
+        pathname: "/",
+        search: e.target.value ? `search=${e.target.value}` : void 0
+      });
+    }
+  }
+</script>
+
+<div>
+  <input class="form-control" placeholder="Search" on:keydown={keydown} />
+</div>
