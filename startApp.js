7a5d3b86bfb1c114cd9ce8a5c817b3108957e17a
Adam Rackis <adamrackis@gmail.com>
2019-05-15 23:23:57 -0500

    CORS tweak, clean up compare folder

--- startApp.js
+++ startApp.js
@@ -166,7 +166,7 @@ const { rootPublic, executableSchemaPublic } = getPublicGraphqlSchema();
 app.use("/graphql-public", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
   next();
 });
 
