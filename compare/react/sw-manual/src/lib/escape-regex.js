72e22b6f7ee18969702b39fa59dec6b00b534f08
Adam.Rackis <adam.rackis@riotgames.com>
2019-05-15 23:23:57 -0500

    CORS tweak, clean up compare folder

--- compare/react/sw-manual/src/lib/escape-regex.js
+++
@@ -1,9 +0,0 @@
-var matchOperatorsRe = /[|\\{}()[\]^$+*?.]/g;
-
-export default function(str) {
-  if (typeof str !== "string") {
-    throw new TypeError("Expected a string");
-  }
-
-  return str.replace(matchOperatorsRe, "\\$&");
-}
