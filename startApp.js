4c5b655284c2b8fe521e75d0944d1ace0832385d
Adam Rackis <adamrackis@gmail.com>
2019-05-15 23:12:14 -0500

    Update micro-graphql-react, update public endpoint CORS configuration

--- startApp.js
+++ startApp.js
@@ -165,6 +165,7 @@ const { rootPublic, executableSchemaPublic } = getPublicGraphqlSchema();
 
 app.use("/graphql-public", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
