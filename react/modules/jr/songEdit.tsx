9d9e539d782ee99f260b081e373878cb2f2afe70
Adam Rackis <adamrackis@hotmail.com>
2020-01-28 20:22:59 -0600

    Add new singers

--- react/modules/jr/songEdit.tsx
+++ react/modules/jr/songEdit.tsx
@@ -145,7 +145,7 @@ const SongDisplayEditing = props => {
   const artistRef = useRef(null);
   const [isGroup, setIsGroup] = useState(song.group);
   const [saving, setSaving] = useState(false);
-  const singers = ["Jason", "Jordan", "Matt", "Michael", "Ray", "Rob", "Scotty"];
+  const singers = ["Jason", "Jordan", "Matt", "Michael", "Ray", "Rob", "Scotty", "Sunshine", "Ingrid"];
   const [currentSingers, setCurrentSingers] = useState(new Set(song.singers || []));
 
   const toggleSinger = singer => {
