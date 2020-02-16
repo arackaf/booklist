b1313afe7058884f89d5ea3f1ec13d36b5aef761
Adam Rackis <adamrackis@hotmail.com>
2020-02-16 12:13:06 -0600

    use transition hook

--- react/app/components/modal.tsx
+++ react/app/components/modal.tsx
@@ -1,7 +1,8 @@
 import React, { Component, SFC } from "react";
 
 import { DialogOverlay, DialogContent } from "@reach/dialog";
-import { Transition, config } from "react-spring/renderprops";
+import { Transition } from "react-spring/renderprops";
+import { useTransition, animated } from "react-spring";
 
 import "css/reach-modal-overrides.scss";
 
@@ -20,37 +21,41 @@ export const StandardModalHeader: SFC<{ onHide: any; caption: any }> = props => 
   );
 };
 
+const AnimatedDialogOverlay = animated(DialogOverlay);
+const AnimatedDialogContent = animated(DialogContent);
+
 type ModalTypes = { isOpen: boolean; style?: any; onHide: any; headerCaption?: any; className?: string; focusRef?: any };
 const Modal: SFC<ModalTypes> = props => {
   let { isOpen, onHide, headerCaption, focusRef = null, style = { maxWidth: "600px" }, children } = props;
-  return (
-    <Transition
-      items={!!isOpen}
-      config={isOpen ? { mass: 1, tension: 350, friction: 30 } : { duration: 150 }}
-      from={{ opacity: 0, y: -10 }}
-      enter={{ opacity: 1, y: 0 }}
-      leave={{ opacity: 0, y: 10 }}
-    >
-      {isOpen =>
-        isOpen &&
-        ((styles: any) => (
-          <DialogOverlay initialFocusRef={focusRef} onDismiss={onHide} isOpen={isOpen} style={{ opacity: styles.opacity }}>
-            <DialogContent
-              style={{
-                transform: `translate3d(0px, ${styles.y}px, 0px)`,
-                border: "4px solid hsla(0, 0%, 0%, 0.5)",
-                borderRadius: 10,
-                ...style
-              }}
-            >
-              {headerCaption ? <StandardModalHeader caption={headerCaption} onHide={onHide} /> : null}
-              {children}
-            </DialogContent>
-          </DialogOverlay>
-        ))
-      }
-    </Transition>
-  );
+
+  const modalMaybe = useTransition(!!isOpen, null, {
+    config: isOpen ? { mass: 1, tension: 350, friction: 30 } : { duration: 150 },
+    from: { opacity: 0, transform: `translate3d(0px, -10px, 0px)` },
+    enter: { opacity: 1, transform: `translate3d(0px, 0px, 0px)` },
+    leave: { opacity: 0, transform: `translate3d(0px, 10px, 0px)` }
+  });
+
+  let results = modalMaybe.map(({ item: isOpen, props: styles }: any) => {
+    if (isOpen) console.log(isOpen, "---", styles);
+    return (
+      isOpen && (
+        <AnimatedDialogOverlay initialFocusRef={focusRef} onDismiss={onHide} isOpen={isOpen} style={{ opacity: styles.opacity }}>
+          <AnimatedDialogContent
+            style={{
+              transform: styles.transform,
+              border: "4px solid hsla(0, 0%, 0%, 0.5)",
+              borderRadius: 10,
+              ...style
+            }}
+          >
+            {headerCaption ? <StandardModalHeader caption={headerCaption} onHide={onHide} /> : null}
+            {children}
+          </AnimatedDialogContent>
+        </AnimatedDialogOverlay>
+      )
+    );
+  });
+  return results.find(item => item) || null;
 };
 
 export default Modal;
