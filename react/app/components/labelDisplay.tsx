556e43624c9dd7511817986cf684df0203c21206
Adam Rackis <adamrackis@hotmail.com>
2020-02-25 21:10:46 -0600

    style tweaks

--- react/app/components/labelDisplay.tsx
+++ react/app/components/labelDisplay.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cn from "classnames";
 
 export const LabelDisplay = props => {
   let item = props.item,
@@ -16,7 +17,7 @@ export const LabelDisplay = props => {
 };
 
 export const EditableExpandableLabelDisplay = props => {
-  let { item, onEdit, expanded, setExpanded } = props;
+  let { item, onEdit, expanded, setExpanded, childSubjects } = props;
   let extraStyles = props.style || {};
   let extraClasses = props.className || "";
 
@@ -25,11 +26,17 @@ export const EditableExpandableLabelDisplay = props => {
       style={{ backgroundColor: item.backgroundColor, color: item.textColor || "white", ...extraStyles }}
       className={"label label-default label-editable-expandable noselect " + extraClasses}
     >
-      <a className="toggle" onClick={() => setExpanded(val => !val)} style={{ color: item.textColor || "white" }}>
-        <i className={`fad fa-chevron-${expanded ? "down" : "right"}`}></i>
-      </a>
+      {childSubjects?.length ? (
+        <a
+          className={cn("toggle", { expanded })}
+          onClick={() => setExpanded(val => !val)}
+          style={{ color: item.textColor || "white", borderRight: `1px solid ${item.textColor || "white"}` }}
+        >
+          <i className="fad fa-chevron-right"></i>
+        </a>
+      ) : null}
 
-      <span style={{ marginLeft: 5, paddingLeft: 5, borderLeft: "1px solid white" }}>{props.name}</span>
+      <span>{props.name}</span>
 
       {props.children || item.name}
       <a onClick={onEdit} style={{ color: item.textColor || "white", cursor: "pointer", marginLeft: "5px" }}>
