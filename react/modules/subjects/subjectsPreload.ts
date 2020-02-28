6fc8ca4c838a8a6e149f22128fe019497d4369c1
Adam Rackis <adamrackis@hotmail.com>
2020-02-28 11:44:51 -0600

    Subjects preloading. Books preloading tweak.

--- react/modules/subjects/subjectsPreload.ts
+++ react/modules/subjects/subjectsPreload.ts
@@ -1,15 +1,11 @@
 import { graphqlClient } from "util/graphql";
-import GetBooksQuery from "graphQL/books/getBooks.graphql";
 import AllSubjectsQuery from "graphQL/subjects/allSubjects.graphql";
-import GetTags from "graphQL/tags/getTags.graphql";
 import AllLabelColorsQuery from "graphQL/misc/allLabelColors.graphql";
 
-import { bookSearchVariablesFromCurrentUrl } from "./booksLoadingUtils";
+import { getCurrentUrlState } from "util/urlHelpers";
 
 export default function preload() {
-  let variables = bookSearchVariablesFromCurrentUrl();
-  graphqlClient.preload(GetBooksQuery, variables);
+  let variables = getCurrentUrlState().searchState;
   graphqlClient.preload(AllSubjectsQuery, { publicUserId: variables.publicUserId });
-  graphqlClient.preload(GetTags, { publicUserId: variables.publicUserId });
   graphqlClient.preload(AllLabelColorsQuery , { cache: 9 });
 }
