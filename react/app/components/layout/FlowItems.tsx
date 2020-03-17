731c4accc3c0a05d9ca153f9ee242701005f3da9
Adam Rackis <adamrackis@hotmail.com>
2020-03-17 16:51:10 -0400

    Search modal radio buttons fixed, flow-items now appropriately parameterized

--- react/app/components/layout/FlowItems.tsx
+++ react/app/components/layout/FlowItems.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import cn from "classnames";
 
-export default ({ className = "", tighter = false, tightest = false, children }) => (
-  <div className={cn("flow-items", className, { tighter, tightest })}>
+export default ({ className = "", tighter = false, tightest = false, vCenter = false, children }) => (
+  <div className={cn("flow-items", className, { tighter, tightest, ["v-center"]: vCenter })}>
     <div>{children}</div>
   </div>
 );
