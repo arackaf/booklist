567914e25dd1f74e25aeed18ab9ba5b94f1a6f56
Adam Rackis <adamrackis@gmail.com>
2020-03-17 15:46:03 -0400

    Search modal isRead radios

--- react/app/components/layout/FlowItems.tsx
+++ react/app/components/layout/FlowItems.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import cn from "classnames";
 
-export default ({ tighter = false, tightest = false, children }) => (
-  <div className={cn("flow-items", { tighter, tightest })}>
+export default ({ className = "", tighter = false, tightest = false, children }) => (
+  <div className={cn("flow-items", className, { tighter, tightest })}>
     <div>{children}</div>
   </div>
 );
