dff1bbe6c507ecf580cc9d47a95347b1911ccf74
Adam Rackis <adamrackis@gmail.com>
2020-03-24 14:22:46 -0500

    tweak alias

--- react/app/components/ui/react-autosuggest-styled.tsx
+++ react/app/components/ui/react-autosuggest-styled.tsx
@@ -1,4 +1,4 @@
-import AutoSuggest from "react-autosuggest";
+import AutoSuggest from "node_modules/react-autosuggest/dist/index";
 import "css/react-autosuggest.scss";
 
 export default AutoSuggest;
