784e6bc33358226d6729805f70f03d41e4a8fe8c
Adam Rackis <adamrackis@gmail.com>
2020-04-30 18:43:43 -0500

    Get started

--- react/modules/subjects/subjects.tsx
+++ react/modules/subjects/subjects.tsx
@@ -1,21 +1,14 @@
-import React, { memo, createContext, useState, useCallback, useContext, FC, useRef } from "react";
+import React, { memo, createContext, useState, useContext, FC, useRef } from "react";
 import { useSpring, animated, config } from "react-spring";
-import { Button } from "app/components/ui/Button";
 import { useRootSubjects, useChildMapSelector, useSubjectMutations, useSubjectsState } from "app/state/subjectsState";
 
-import subjectsListStyles from "./subjectsList.module.scss";
+import "./subjectsList.scss";
 import { useColors } from "app/state/colorsState";
 import { EditableExpandableLabelDisplay } from "app/components/subjectsAndTags/LabelDisplay";
 
-import EditSubject from "app/components/subjectsAndTags/subjects/EditSubject";
-import Modal from "app/components/ui/Modal";
 import { useHeight, usePrevious } from "app/animationHelpers";
-import FlexRow from "app/components/layout/FlexRow";
 
-const AnimationContext = createContext(null);
 const EditContext = createContext(null);
-
-const { subjectsRoot, subjectRow, contentRoot } = subjectsListStyles;
 
 const SubjectDisplay: FC<any> = memo(props => {
   const { subject } = props;
@@ -42,7 +35,7 @@ const SubjectDisplay: FC<any> = memo(props => {
     onRest: () => (uiReady.current = true)
   }) as any;
 
-  let classes = `padding-bottom-med ${subjectRow}`;
+  let classes = `padding-bottom-med subjectRow`;
 
   const openEditModal = useContext(EditContext);
 
@@ -75,23 +68,9 @@ const SubjectList = props => {
   );
 };
 
-const defaultEditState = {
-  editingSubject: { name: "" }
-};
-
 export default () => {
-  const [subjectEditState, setSubjectEditState] = useState(defaultEditState);
-  const [editModalOpen, setEditModalOpen] = useState(false);
-  const { editingSubject } = subjectEditState;
-
   useColors();
   const topLevelSubjects = useRootSubjects();
-
-  const openEditModal = useCallback(editingSubject => {
-    setSubjectEditState({ editingSubject });
-    setEditModalOpen(true);
-  }, []);
-  const closeEditModal = useCallback(() => setEditModalOpen(false), []);
 
   const { opacity } = useSpring({
     config: { ...config.slow },
@@ -102,28 +81,10 @@ export default () => {
   }) as any;
 
   return (
-    <div className={subjectsRoot}>
-      <div className="padding-top" style={{ marginBottom: "60px" }}>
-        <FlexRow>
-          <div className="col-lg-6 col-md-8 col-xs-12">
-            <Button className="margin-bottom" preset="primary" onClick={() => openEditModal({ name: "" })}>
-              New Subject
-            </Button>
-
-            <EditContext.Provider value={openEditModal}>
-              <animated.div style={{ opacity }} className={contentRoot}>
-                <SubjectList subjects={topLevelSubjects} />
-              </animated.div>
-            </EditContext.Provider>
-          </div>
-        </FlexRow>
-      </div>
-
-      <Modal className="fade" isOpen={editModalOpen} onHide={closeEditModal} headerCaption={"Edit Subject"}>
-        <EditSubject subject={editingSubject} onCancelEdit={closeEditModal} />
-        <hr />
-        <Button onClick={closeEditModal}>Close</Button>
-      </Modal>
+    <div className="subjectsRoot">
+      <animated.div style={{ opacity }} className="contentRoot">
+        <SubjectList subjects={topLevelSubjects} />
+      </animated.div>
     </div>
   );
 };
