e26b11bca920b3a3faec344f42e243fd5a4bd5f5
Adam Rackis <adamrackis@gmail.com>
2020-07-05 11:51:39 -0500

    small animation tweak

--- react/modules/home/components/recommend/main.tsx
+++ react/modules/home/components/recommend/main.tsx
@@ -60,7 +60,7 @@ export default props => {
   const selectedBookTransitions = useTransition(selectedBooks, {
     config: book => ({ ...config.stiff, clamp: !selectedBooksSet.has(book._id) }),
     from: { opacity: 0, transform: "translate3d(-25%, 0px, 0px)" },
-    enter: { opacity: 1, height: "auto", transform: "translate3d(0%, 0px, 0px)" },
+    enter: book => ({ opacity: 1, height: displaySizes[book._id], transform: "translate3d(0%, 0px, 0px)" }),
     update: book => ({ height: displaySizes[book._id] }),
     leave: { opacity: 0, height: 0, transform: "translate3d(25%, 0px, 0px)" }
   });
