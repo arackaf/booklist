4fc1e96665c5316097ad84981f596c64377c01ae
Adam Rackis <adamrackis@gmail.com>
2020-08-13 10:29:31 -0500

    web socket tweak

--- react/app/renderUI.tsx
+++ react/app/renderUI.tsx
@@ -21,9 +21,12 @@ ws.onmessage = ({ data }) => {
   window.dispatchEvent(new CustomEvent("ws-info", { detail: { type: packet._messageType, packet } }));
 };
 
-window.addEventListener("sync-ws", () => {
+ws.onopen = () => {
   ws.send(`SYNC`);
-});
+  window.addEventListener("sync-ws", () => {
+    ws.send(`SYNC`);
+  });
+};
 
 window.onbeforeunload = function () {
   ws.onclose = function () {}; // disable onclose handler first
