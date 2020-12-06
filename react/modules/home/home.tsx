ac05191699dea51216c2a4361e4f47622dad2084
Adam Rackis <adamrackis@gmail.com>
2020-12-06 16:51:51 -0600

    Home style tweak

--- react/modules/home/home.tsx
+++ react/modules/home/home.tsx
@@ -83,10 +83,7 @@ const ChartHolder: FC<{}> = props => {
   const [chartWidth, setChartWidth] = useState(MAX_CHART_WIDTH);
 
   const getDrilldownChart = (index, subjects, header) => {
-    setChartPackets(charts => [
-      ...charts.slice(0, index + 1),
-      { subjects: subjects.concat(), header }
-    ]);
+    setChartPackets(charts => [...charts.slice(0, index + 1), { subjects: subjects.concat(), header }]);
   };
 
   return (
@@ -101,7 +98,16 @@ const ChartHolder: FC<{}> = props => {
       {({ measureRef }) => (
         <div ref={measureRef}>
           {chartPackets.map((packet, i) => (
-            <BarChart key={i} drilldown={getDrilldownChart} subjectHash={subjectHash} {...packet} chartIndex={i} maxWidth={MAX_CHART_WIDTH} width={chartWidth} height={600} />
+            <BarChart
+              key={i}
+              drilldown={getDrilldownChart}
+              subjectHash={subjectHash}
+              {...packet}
+              chartIndex={i}
+              maxWidth={MAX_CHART_WIDTH}
+              width={chartWidth}
+              height={600}
+            />
           ))}
         </div>
       )}
@@ -112,29 +118,33 @@ const ChartHolder: FC<{}> = props => {
 const HomeIfNotLoggedIn = () => (
   <div>
     <MainHomePane>
-      Welcome to <i>My Library</i>.<br />
-      <br />
-      This site is my own little passion project, the purpose of which is to track your library. You scan in your books (or manually type in the isbn)
-      and the books' info is fetched from Amazon, and stored for you. You can then flexibly search and categorize your library.
-      <br />
-      <br />
-      So basically this site is of use to the extremely small percentage of people for whom the following are <i>all</i> true: they read a lot, own
-      the books they read, and read non-eBooks. As I said, this is more of a passion project than anything.
-      <br />
-      <br />
-      It's free to sign up, and store up to 500 books. In the remote chance someone actually wants to store more than that, there'll be some sort of
-      nominal fee to help defray storage costs.
-      <br />
-      <br />
-      For those interested in seeing the code for this site, the GitHub repository is{" "}
-      <a target="_blank" href="https://github.com/arackaf/booklist">
-        here
-      </a>
-      <br />
-      <br />
-      <a style={{ textDecoration: "none" }} className="btn btn-primary" onClick={() => goto("login")}>
-        Login or create an account
-      </a>
+      <div style={{ display: "flex" }}>
+        <div style={{ maxWidth: "800px", marginTop: "25px", marginLeft: "auto", marginRight: "auto" }}>
+          Welcome to <i>My Library</i>.<br />
+          <br />
+          This site is my own little passion project, the purpose of which is to track your library. You scan in your books (or manually type in the
+          isbn) and the books' info is fetched from Amazon, and stored for you. You can then flexibly search and categorize your library.
+          <br />
+          <br />
+          So basically this site is of use to the extremely small percentage of people for whom the following are <i>all</i> true: they read a lot,
+          own the books they read, and read non-eBooks. As I said, this is more of a passion project than anything.
+          <br />
+          <br />
+          It's free to sign up, and store up to 500 books. In the remote chance someone actually wants to store more than that, there'll be some sort
+          of nominal fee to help defray storage costs.
+          <br />
+          <br />
+          For those interested in seeing the code for this site, the GitHub repository is{" "}
+          <a target="_blank" href="https://github.com/arackaf/booklist">
+            here
+          </a>
+          <br />
+          <br />
+          <a style={{ textDecoration: "none" }} className="btn btn-primary" onClick={() => goto("login")}>
+            Login or create an account
+          </a>
+        </div>
+      </div>
     </MainHomePane>
   </div>
 );
