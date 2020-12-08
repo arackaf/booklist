83a8fda704e341c373b60babba696b2e755624fe
Adam Rackis <adamrackis@gmail.com>
2020-12-07 22:04:56 -0600

    Update graphql client. Minor cleanup

--- react/app/state/colorsState.ts
+++ react/app/state/colorsState.ts
@@ -1,13 +1,12 @@
 import { useContext } from "react";
+import { useSuspenseQuery } from "micro-graphql-react";
 
 import AllLabelColorsQuery from "graphQL/misc/allLabelColors.graphql";
 import { AppContext } from "../renderUI";
-import { useSuspenseQuery } from "micro-graphql-react";
 import { QueryOf, Queries } from "graphql-typings";
 
 export function useColors() {
-  let appPacket = useContext(AppContext);
-
+  let appPacket = useContext(AppContext); 
   let userId = appPacket && appPacket.length ? appPacket[0].userId : "";
 
   let { data } = useSuspenseQuery<QueryOf<Queries["allLabelColors"]>>(AllLabelColorsQuery, { cache: 9 }, { active: !!userId });
--- svelte/app/state/colorsState.ts
+++ svelte/app/state/colorsState.ts
@@ -1,15 +1,14 @@
 import { derived } from "svelte/store";
+import { query } from "micro-graphql-svelte";
 
 import AllLabelColorsQuery from "graphQL/misc/allLabelColors.graphql";
-import { query } from "micro-graphql-svelte";
+import { appState } from "./appState";
 import { QueryOf, Queries } from "graphql-typings";
 
-import { appState } from "./appState";
-
-const { queryState, sync } = query(AllLabelColorsQuery);
+const { queryState, sync } = query<QueryOf<Queries["allLabelColors"]>>(AllLabelColorsQuery);
 appState.subscribe(state => sync({}, { active: !!state.userId }));
 
-export default derived(queryState, (state: any) => ({
-  loaded: !!state.data,
+export default derived(queryState, state => ({
+  loaded: !!state,
   colors: state.data ? state.data.allLabelColors.LabelColors.map(c => c.backgroundColor) : null,
 }));
