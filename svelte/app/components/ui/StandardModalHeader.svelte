8e4ec4f118548eb54fb8cc6eb97bbeb3e3b037e1
Adam Rackis <adamrackis@gmail.com>
2020-12-17 07:36:59 -0600

    Update svelte-helpers. Fix modal header to use the new close method.

--- svelte/app/components/ui/StandardModalHeader.svelte
+++ svelte/app/components/ui/StandardModalHeader.svelte
@@ -1,12 +1,12 @@
 <script>
   import { getContext } from "svelte";
-  let { onClose } = getContext("svelte-helpers-modal");
+  let { closeModal } = getContext("svelte-helpers-modal");
 
   export let caption = "";
 </script>
 
 <div class="standard-modal-header">
   <h4 class="modal-title">{caption}</h4>
-  <span style="margin-left: auto" class="close" on:click={onClose}> <span>&times;</span> </span>
+  <span style="margin-left: auto" class="close" on:click={closeModal}> <span>&times;</span> </span>
 </div>
 <hr />
