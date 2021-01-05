1a1034f0bef914da6f74145cb82c3e993e9dea59
Adam Rackis <adamrackis@hotmail.com>
2021-01-04 22:34:29 -0600

    Tweak Reacr SectionLoading component

--- react/app/components/loading.tsx
+++ react/app/components/loading.tsx
@@ -16,7 +16,7 @@ export const LongLoading = ({ ...rest }) => (
 
 export const SectionLoading = ({ style = {}, ...rest }) => (
   <i
-    style={{ position: "absolute", top: "50%", left: "50%", opacity: 0.2, transform: "translate(-50%,-50%)", ...style }}
+    style={{ position: "absolute", top: "50%", left: "50%", opacity: 0.2, ...style }}
     className="fa fa-5x fa-spin fa-spinner"
   />
 );
