7554c489a891f88ee62c350f3d65e0e03ae0b273
Adam Rackis <adamrackis@hotmail.com>
2021-01-04 21:57:05 -0600

    Start settings layout

--- svelte/modules/settings/Settings.svelte
+++ svelte/modules/settings/Settings.svelte
@@ -1,5 +1,54 @@
 <script>
+  import { appState } from "app/state/appState";
+
+  import Tabs from "app/components/layout/tabs/Tabs.svelte";
+  import TabHeaders from "app/components/layout/tabs/TabHeaders.svelte";
+  import TabHeader from "app/components/layout/tabs/TabHeader.svelte";
+  import TabContent from "app/components/layout/tabs/TabContent.svelte";
+  import TabContents from "app/components/layout/tabs/TabContents.svelte";
+
   import { history } from "util/urlHelpers";
+
+  $: ({ isPublic, isLoggedIn } = $appState);
 </script>
 
-<a href={`/view?userId=5df692fae34a83f9c74ac701`}>Go</a>
+<section>
+  <Tabs defaultTab={isPublic ? 'theme' : 'publicSettings'} localStorageName="settings-tab">
+    {#if isPublic || !isLoggedIn}
+      <TabHeaders>
+        <TabHeader disabled={true}><a>Public sharing</a></TabHeader>
+        <TabHeader disabled={true}><a>Reset password</a></TabHeader>
+        <TabHeader tabName="theme"><a>Theme</a></TabHeader>
+        <TabHeader tabName="miscSettings"><a>Misc</a></TabHeader>
+      </TabHeaders>
+    {:else}
+      <TabHeaders>
+        <TabHeader tabName="publicSettings"><a>Public sharing</a></TabHeader>
+        <TabHeader tabName="passwordReset"><a>Reset password</a></TabHeader>
+        <TabHeader tabName="theme"><a>Theme</a></TabHeader>
+        <TabHeader tabName="miscSettings"><a>Misc</a></TabHeader>
+      </TabHeaders>
+    {/if}
+    <TabContents>
+      {#if !isPublic && isLoggedIn}
+        <TabContent style={{ minHeight: '150px' }} tabName="publicSettings">
+          <!-- <PublicUserSettings /> -->
+          PublicUserSettings
+        </TabContent>
+        <TabContent style={{ minHeight: '150px' }} tabName="passwordReset">
+          <!-- <PasswordReset /> -->
+          PasswordReset
+        </TabContent>
+      {/if}
+
+      <TabContent style={{ minHeight: '150px' }} tabName="miscSettings">
+        <!-- <MiscSettings /> -->
+        MiscSettings
+      </TabContent>
+      <TabContent style={{ minHeight: '150px' }} tabName="theme">
+        <!-- <ThemeChooser /> -->
+        ThemeChooser
+      </TabContent>
+    </TabContents>
+  </Tabs>
+</section>
