6a69c2ce14035e417f513eb23c179b8240f5f8d4
Adam Rackis <adamrackis@hotmail.com>
2021-01-08 21:42:46 -0600

    test

--- react/app/renderUI.tsx
+++ react/app/renderUI.tsx
@@ -39,7 +39,7 @@ const MobileMeta = () => {
 
   useEffect(() => {
     setInterval(() => {
-      setContent("");
+      setContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1");
       setTimeout(() => { setContent("width=device-width, initial-scale=1") }, 10);
     }, 5000);
   }, []);
