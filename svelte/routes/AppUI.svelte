d93b5116223545678f01e06efa48197a4ca18131
Adam Rackis <adamrackis@hotmail.com>
2021-01-14 22:56:02 -0600

    Prettier fix, add mobile layout adjustment

--- svelte/routes/AppUI.svelte
+++ svelte/routes/AppUI.svelte
@@ -1,9 +1,38 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { appState } from "app/state/appState";
   import Footer from "app/components/Footer.svelte";
   import MainNavigation from "app/components/navigation/MainNavigation.svelte";
 
   export let content = null;
+
+  onMount(() => {
+    document.documentElement.scrollTop = 0;
+    if ($appState.isMobile) {
+      function setAdjustedVh() {
+        let vh = window.innerHeight * 0.01 + "px";
+        document.documentElement.style.setProperty("--adjusted-vh", vh);
+      }
+
+      window.addEventListener("resize", setAdjustedVh);
+
+      let pinching = false;
+      window.addEventListener("gesturestart", () => {
+        pinching = true;
+      });
+      window.addEventListener("gestureend", () => {
+        pinching = false;
+        setTimeout(() => setAdjustedVh(), 1);
+        setTimeout(() => setAdjustedVh(), 100);
+        setTimeout(() => setAdjustedVh(), 250);
+        setTimeout(() => setAdjustedVh(), 750);
+      });
+      setAdjustedVh();
+      setTimeout(setAdjustedVh, 10); //Chrome iOS
+      setTimeout(setAdjustedVh, 100);
+      setTimeout(setAdjustedVh, 250);
+    }
+  });
 </script>
 
 <style>
