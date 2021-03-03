ed0c09cbbfc5735c0545a1708bca018f98dbe8e3
Adam Rackis <adamrackis@hotmail.com>
2021-03-03 15:37:12 -0600

    Fix null reference exception

--- svelte/modules/books/booksState.ts
+++ svelte/modules/books/booksState.ts
@@ -171,8 +171,10 @@ const adjustBooks = (books, subjectsState, tagsState) => {
 
   return books.map((bookRaw: IBookDisplay) => {
     let result = { ...bookRaw };
-    result.subjectObjects = (result.subjects || []).map(s => subjectHash[s]).filter(s => s);
-    result.tagObjects = (result.tags || []).map(s => tagHash[s]).filter(s => s);
+    result.subjects = result.subjects ?? [];
+    result.tags = result.tags ?? [];
+    result.subjectObjects = result.subjects.map(s => subjectHash[s]).filter(s => s);
+    result.tagObjects = result.tags.map(s => tagHash[s]).filter(s => s);
     result.authors = result.authors || [];
 
     let d = new Date(+result.dateAdded);
