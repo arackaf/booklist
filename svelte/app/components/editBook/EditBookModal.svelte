edf1aeeebe68d02a0871c9b19466543197b11d86
Adam Rackis <adamrackis@gmail.com>
2021-03-29 19:50:16 -0500

    Refactor book edit content out of modal

--- svelte/app/components/editBook/EditBookModal.svelte
+++ svelte/app/components/editBook/EditBookModal.svelte
@@ -1,48 +1,13 @@
 <script lang="ts">
-  import Tabs from "../layout/tabs/Tabs.svelte";
-  import TabHeaders from "../layout/tabs/TabHeaders.svelte";
-  import TabHeader from "../layout/tabs/TabHeader.svelte";
-  import TabContents from "../layout/tabs/TabContents.svelte";
-  import TabContent from "../layout/tabs/TabContent.svelte";
-
   import Modal from "../ui/Modal.svelte";
-  import EditBookInfo from "./EditBookInfo.svelte";
-  import ManageBookCover from "./ManageBookCover.svelte";
+  import EditBook from "./EditBook.svelte";
 
   export let book;
   export let isOpen = false;
   export let onHide;
   export let saveBook;
-
-  const updateBook = fn => (book = fn(book));
 </script>
 
 <Modal headerCaption={`Edit: ${book.title}`} deferStateChangeOnClose={true} {isOpen} {onHide} standardFooter={false}>
-  <Tabs defaultTab="basic">
-    <TabHeaders>
-      <TabHeader tabName="basic"><a>Book info</a></TabHeader>
-      <TabHeader tabName="covers"><a>Covers</a></TabHeader>
-    </TabHeaders>
-    <TabContents>
-      <TabContent tabName="basic">
-        {#if book}
-          <EditBookInfo {saveBook} {book} />
-        {/if}
-      </TabContent>
-      <TabContent tabName="covers">
-        {#if book}
-          <div class="form-group">
-            <label>Small Cover</label>
-            <ManageBookCover _id={book._id} imgKey="smallImage" size="small" img={book.smallImage} updateBookObject={updateBook} />
-          </div>
-          <hr />
-          <div class="form-group">
-            <label>Medium Cover</label>
-            <ManageBookCover _id={book._id} imgKey="mediumImage" size="medium" img={book.mediumImage} updateBookObject={updateBook} />
-          </div>
-        {/if}
-      </TabContent>
-      <TabContent tabName="c">C Content</TabContent>
-    </TabContents>
-  </Tabs>
+  <EditBook {book} {saveBook} />
 </Modal>
