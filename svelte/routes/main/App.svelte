2a5b1dd3536c17c7439a3e1774c992f3e8565c9f
Adam Rackis <adamrackis@gmail.com>
2021-04-11 22:23:16 -0500

    test web socket

--- svelte/routes/main/App.svelte
+++ svelte/routes/main/App.svelte
@@ -1,48 +1,32 @@
 <script lang="ts">
-  import ModuleRoute from "app/components/navigation/ModuleRoute.svelte";
+  let ws;
 
-  import { history } from "util/urlHelpers";
-  import AppUI from "../AppUI.svelte";
-  import "util/graphql";
+  function connect() {
+    ws = new WebSocket("wss://08k4tjaorc.execute-api.us-east-1.amazonaws.com/dev");
+    ws.onopen = () => {
+      console.log("OPEN");
+    };
 
-  import booksPreload from "./booksPreload";
+    ws.onmessage = ({ data }) => {
+      console.log("DATA!!!", data);
+    };
 
-  import { appState, dispatch as appStateDispatch, URL_SYNC } from "app/state/appState";
+    setTimeout(() => {
+      ws.send(`{ action: "foo" }`);
+    }, 2000);
 
-  import setupServiceWorker from "../../util/setupServiceWorker";
+    setTimeout(() => {
+      console.log("---");
+      ws.send(`{ action: "foo" }`);
+    }, 4000);
 
-  history.listen(() => {
-    appStateDispatch({ type: URL_SYNC });
-  });
-
-  let ws = new WebSocket(webSocketAddress("/bookEntryWS"));
-
-  ws.onmessage = ({ data }) => {
-    let packet = JSON.parse(data);
-    window.dispatchEvent(new CustomEvent("ws-info", { detail: { type: packet._messageType, packet } }));
-  };
-
-  ws.onopen = () => {
-    ws.send(`SYNC`);
-    window.addEventListener("sync-ws", () => {
-      ws.send(`SYNC`);
-    });
-  };
-
-  window.onbeforeunload = function () {
-    ws.onclose = function () {}; // disable onclose handler first
-    ws.close();
-  };
-
-  setupServiceWorker();
+    setTimeout(() => {
+      console.log("---");
+      ws.send(`{ action: "foo" }`);
+    }, 6000);
+  }
 </script>
 
-<AppUI content={$appState.showingMobile ? 'width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0; user-scalable=0;' : ''}>
-  <ModuleRoute moduleName="home" loader={() => import(/* webpackChunkName: "home-module" */ 'modules/home/Home.svelte')} />
-  <ModuleRoute moduleName="scan" loader={() => import(/* webpackChunkName: "scan-module" */ 'modules/scan/Scan.svelte')} />
-  <ModuleRoute moduleName="books" preload={booksPreload} loader={() => import(/* webpackChunkName: "books-module" */ 'modules/books/Books.svelte')} />
-  <ModuleRoute moduleName="view" preload={booksPreload} loader={() => import(/* webpackChunkName: "books-module" */ 'modules/books/Books.svelte')} />
-  <ModuleRoute moduleName="subjects" loader={() => import(/* webpackChunkName: "subjects-module" */ 'modules/subjects/Subjects.svelte')} />
-  <ModuleRoute moduleName="settings" loader={() => import(/* webpackChunkName: "settings-module" */ 'modules/settings/Settings.svelte')} />
-  <ModuleRoute moduleName="activate" loader={() => import(/* webpackChunkName: "activate-module" */ 'modules/activate/Activate.svelte')} />
-</AppUI>
+<h1>Hello World!</h1>
+
+<button on:click={connect}>Connect</button>
