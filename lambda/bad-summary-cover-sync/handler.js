407e97ab3faa6b060c87c38eca243555d4148c26
Adam Rackis <adamrackis@gmail.com>
2021-04-13 02:15:52 -0500

    start on book summary bad cover lambda

---
+++ lambda/bad-summary-cover-sync/handler.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const updateBookSummaryCovers = require("./handlerCode");
+
+exports.badCoverSync = async event => {
+  console.log("STARTING");
+  await updateBookSummaryCovers();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Done!" })
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
---
+++ lambda/bad-summary-cover-sync/handlerCode.js
@@ -0,0 +1,116 @@
+"use strict";
+
+const request = require("request");
+const { v4: uuid } = require("uuid");
+const dlv = require("dlv");
+
+const { ObjectId } = require("mongodb");
+const moment = require("moment");
+
+const getDbConnection = require("./util/getDbConnection");
+
+const {
+  downloadBookCover,
+  removeFile,
+  resizeIfNeeded,
+  saveCoverToS3,
+  saveContentToS3,
+  getOpenLibraryCoverUri,
+  getGoogleLibraryUri
+} = require("./util/bookCoverHelpers");
+
+const getSecrets = require("./util/getSecrets");
+
+const dbPromise = getDbConnection();
+
+const delay = () => new Promise(res => setTimeout(res, 2500));
+
+module.exports = async function updateBookSummaryCovers() {
+  let count = 1;
+  const db = await dbPromise;
+  const secrets = await getSecrets();
+
+  const bookSummaries = await db
+    .collection("bookSummaries")
+    .aggregate([{ $match: { smallImage: new RegExp("nophoto") } }, { $limit: 3 }])
+    .toArray();
+
+  let title = "";
+
+  let _logs = [];
+  const log = (...args) => {
+    console.log(...args);
+    _logs.push(args.join(" "));
+  };
+
+  for (let bookSummary of bookSummaries) {
+    let { _id, isbn, title } = bookSummary;
+
+    await delay();
+    let res = await downloadBookCover(getOpenLibraryCoverUri(isbn), 1000);
+    if (!res) {
+      log("No cover found on OpenLibrary for", title);
+
+      log("Trying Google...");
+
+      let cover = await getGoogleCoverUrl(isbn, secrets);
+      if (!cover) {
+        log(`Cover not found on Google either for ${title}`);
+        continue;
+      }
+
+      log("Attempting Google cover", cover);
+
+      res = await downloadBookCover(cover, 1000);
+
+      if (!res) {
+        log(`Could not download from Google, either`);
+
+        continue;
+      }
+    }
+
+    let { fileName, fullName } = res;
+    let newPath = await resizeIfNeeded(fileName);
+
+    if (newPath) {
+      let s3Key = await saveCoverToS3(newPath, `bookCovers/bookSummary/${fileName}`);
+
+      log("#", count++, title, s3Key);
+      await db.collection("bookSummaries").updateOne(
+        { _id: ObjectId(_id) },
+        {
+          $set: { smallImage: s3Key }
+        }
+      );
+    } else {
+      log(`Google cover failed`);
+    }
+
+    removeFile(fullName);
+    newPath && removeFile(newPath);
+  }
+
+  saveContentToS3(_logs.join("\n"), `bookCoverSyncingLogs/${moment().format("YYYY-MM-DD--HH_mm")}.txt`);
+};
+
+function getGoogleCoverUrl(isbn, secrets) {
+  return new Promise(res => {
+    request(getGoogleLibraryUri(isbn, secrets["google-library-key"]), (err, resp, body) => {
+      if (err || !/^2/.test(resp.statusCode)) return res(null);
+
+      try {
+        let item = JSON.parse(body).items[0];
+        let imageLinks = dlv(item, "volumeInfo.imageLinks");
+        let smallImage = imageLinks.smallThumbnail || imageLinks.thumbnail;
+        if (smallImage) {
+          smallImage = smallImage.replace(/&edge=curl/gi, "");
+        }
+
+        return res(smallImage || null);
+      } catch (er) {
+        res(null);
+      }
+    });
+  });
+}
