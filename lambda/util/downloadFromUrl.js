2bae5cd91f102057032a382b8cc42be63842af31
Adam Rackis <adamrackis@gmail.com>
2021-04-23 17:44:57 -0500

    Download from url converted

--- lambda/util/downloadFromUrl.js
+++ lambda/util/downloadFromUrl.js
@@ -1,13 +1,10 @@
-import request from "request";
+import fetch from "node-fetch";
 
 export default url => {
   return new Promise(res => {
-    request({ uri: url, encoding: null }, function (err, resp, body) {
-      if (err || !body) {
-        res({ error: true, msg: err });
-      } else {
-        res({ body });
-      }
-    });
+    fetch(url)
+      .then(res => res.arrayBuffer())
+      .then(body => res({ body }))
+      .catch(err => res({ error: true, msg: err }));
   });
 };
