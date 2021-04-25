10828f0b14d5ccc4609c7370d49b7e03dd7c1dd8
Adam Rackis <adamrackis@gmail.com>
2021-04-24 21:06:46 -0500

    Dynamo put works

--- lambda/book-scan-dynamo-trigger/handler.js
+++ lambda/book-scan-dynamo-trigger/handler.js
@@ -34,3 +34,36 @@ module.exports.hello = async event => {
     )
   };
 };
+
+module.exports.stickItIn = async event => {
+  console.log("CALLED YO", +new Date());
+
+  try {
+    const db = new AWS.DynamoDB({
+      region: "us-east-1"
+    });
+
+    const params = {
+      TableName: "my_library_scan_state_live",
+      Item: {
+        id: { N: "1" },
+        items: { L: [{ S: "a" }, { S: "x" }] }
+      }
+    };
+    await db.putItem(params).promise();
+    console.log("INSERTED");
+  } catch (err) {
+    console.log("ERROR :(", err);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event
+      },
+      null,
+      2
+    )
+  };
+};
