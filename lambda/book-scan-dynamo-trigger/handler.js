cad648eded6ed3f10c32f691fcc57e05fbc458b2
Adam Rackis <adamrackis@gmail.com>
2021-04-24 20:50:13 -0500

    reading from Dynamo

--- lambda/book-scan-dynamo-trigger/handler.js
+++ lambda/book-scan-dynamo-trigger/handler.js
@@ -1,7 +1,27 @@
 "use strict";
 
+const AWS = require("aws-sdk");
+
 module.exports.hello = async event => {
   console.log("CALLED YO", +new Date());
+
+  try {
+    const db = new AWS.DynamoDB({
+      region: "us-east-1"
+    });
+
+    const params = {
+      TableName: "my_library_scan_state_live",
+      Key: {
+        id: { N: "1" }
+      }
+    };
+    const result = await db.getItem(params).promise();
+    console.log("FOUND", result.Item);
+    console.log("FOUND", JSON.stringify(result.Item));
+  } catch (err) {
+    console.log("ERROR :(", err);
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(
