21af358878526fb1c84defbd2307f7f6956f12b2
Adam Rackis <adamrackis@gmail.com>
2021-07-02 18:31:01 -0500

    Prettier format

--- node/dataAccess/connect.js
+++ node/dataAccess/connect.js
@@ -3,15 +3,11 @@ import { MongoClient } from "mongodb";
 export default () => {
   let connString = process.env.IS_PUBLIC ? process.env.MONGO_PUBLIC : process.env.MONGO_CONNECTION;
   let dbName = process.env.IS_PUBLIC ? process.env.DB_NAME_PUBLIC : process.env.DB_NAME;
-  return MongoClient.connect(
-    connString,
-    { useNewUrlParser: true }
-  ).then(client => client.db(dbName));
+  return MongoClient.connect(connString, { useNewUrlParser: true }).then(client => client.db(dbName));
 };
 
 export const connectPublic = () => {
-  return MongoClient.connect(
-    process.env.MONGO_CONNECTION || process.env.MONGO_PUBLIC,
-    { useNewUrlParser: true }
-  ).then(client => client.db(process.env.DB_NAME_PUBLIC));
+  return MongoClient.connect(process.env.MONGO_CONNECTION || process.env.MONGO_PUBLIC, { useNewUrlParser: true }).then(client =>
+    client.db(process.env.DB_NAME_PUBLIC)
+  );
 };
