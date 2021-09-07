b76473af97d9d54596b5e7d8843179a574878a00
Adam Rackis <adamrackis@gmail.com>
2021-09-06 23:32:28 -0500

    Svelte data vis tweaks. Clamp lower multi-bar sections to prevent weird jank. Add slide-in animation for bars like the React version has

--- svelte/modules/home/dataVis/barChart/bars/MultiBar.svelte
+++ svelte/modules/home/dataVis/barChart/bars/MultiBar.svelte
@@ -12,31 +12,43 @@
   $: _colors = data.entries.map(e => e.color);
   let colors = [];
 
+  let itemsClamped = {};
+
   $: {
     let heightUsed = 0;
     let count = _colors.length;
-    let height = $barSpring.height;
+    let currentHeight = $barSpring.height;
     colors = _colors.map((color, i) => {
       let isLast = i + 1 == count;
-      let sectionHeight = ~~(height / count);
-      let barHeight = isLast ? height - heightUsed : sectionHeight;
+      let sectionHeight = ~~(currentHeight / count);
+      let finalSectionHeight = ~~(height / count);
+      let barHeight = isLast ? currentHeight - heightUsed : sectionHeight;
 
-      const heightToUse = heightUsed;
+      if (!isLast && barHeight > finalSectionHeight) {
+        itemsClamped[i] = true;
+      }
+
+      if (itemsClamped[i]) {
+        barHeight = finalSectionHeight;
+      }
+
+      const sectionVerticalStartingPoint = heightUsed;
       heightUsed += barHeight;
 
-      return { x, y: heightToUse, height: barHeight, fill: color };
+      return { x, y: sectionVerticalStartingPoint, height: barHeight, fill: color };
     });
   }
 
-  let barSpring = spring({ height: 0, x, width }, { stiffness: 0.1, damping: 0.4 });
+  let barSpring = spring({ height: 0, x: totalSvgWidth }, { stiffness: 0.1, damping: 0.4 });
 
   $: {
-    barSpring.update(state => ({ ...state, height, x, width }));
+    itemsClamped = {};
+    barSpring.update(state => ({ ...state, height, x }));
   }
 </script>
 
-<g on:mouseover={() => hoverBar(data.groupId)} on:mouseout={() => unHoverBar(data.groupId)}>
+<g on:mouseover={() => hoverBar(data.groupId)} on:focus={null} on:blur={null} on:mouseout={() => unHoverBar(data.groupId)}>
   {#each colors as c}
-    <rect x={$barSpring.x} y={c.y} height={Math.max(c.height, 0)} width={$barSpring.width} fill={c.fill} />
+    <rect x={$barSpring.x} y={c.y} height={Math.max(c.height, 0)} {width} fill={c.fill} />
   {/each}
 </g>
--- svelte/modules/home/dataVis/barChart/bars/SingleBar.svelte
+++ svelte/modules/home/dataVis/barChart/bars/SingleBar.svelte
@@ -18,13 +18,13 @@
 
   //{...animatedValues}
 
-  let barSpring = spring({ height: 0, x, width }, { stiffness: 0.1, damping: 0.4 });
+  let barSpring = spring({ height: 0, x: totalSvgWidth, width }, { stiffness: 0.1, damping: 0.4 });
 
   $: {
     barSpring.update(state => ({ ...state, height, x, width }));
   }
 </script>
 
-<g on:mouseover={() => hoverBar(data.groupId)} on:mouseout={() => unHoverBar(data.groupId)}>
+<g on:mouseover={() => hoverBar(data.groupId)} on:focus={null} on:blur={null} on:mouseout={() => unHoverBar(data.groupId)}>
   <rect height={Math.max(0, $barSpring.height)} width={$barSpring.width} x={$barSpring.x} y={0} fill={color} />
 </g>
