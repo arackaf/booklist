a55fd8442746ba0d5e7ca7a62bbb37078940cc26
Adam Rackis <adamrackis@gmail.com>
2021-09-19 13:58:17 -0500

    Try useReducer

--- react/modules/home/recent-scans/index.tsx
+++ react/modules/home/recent-scans/index.tsx
@@ -1,46 +1,44 @@
-import React, { FunctionComponent, Suspense, useEffect, useState } from "react";
+import React, { FunctionComponent, Suspense, useEffect, useReducer, useState } from "react";
 const { unstable_useTransition: useTransition } = React as any;
 
 import { Queries, QueryOf } from "graphql-typings";
 import "./styles.scss";
 
 import RecentScansQuery from "../../../graphQL/recent-scans/recentScans.graphql";
-import { useSuspenseQuery } from "micro-graphql-react";
+import { useQuery } from "micro-graphql-react";
 import { LocalLoading } from "app/components/loading";
 
 type Props = any;
+
+const initialState = {
+  nextNextPageKey: "",
+  currentScans: []
+};
+function scanStateReducer(state, data) {
+  const newLastKey = data?.recentScanResults?.LastEvaluatedKey;
+  const nextScans = data?.recentScanResults?.ScanResults ?? [];
+
+  return { nextNextPageKey: newLastKey, currentScans: state.currentScans.concat(nextScans) };
+}
 
 const RecentScans: FunctionComponent<Props> = props => {
   const [startTransition, loading] = useTransition({ timeoutMs: 9000 });
 
   const [currentNextPageKey, setCurrentNextPageKey] = useState<any>(null);
-  const [nextNextPageKey, setNextNextPageKey] = useState<any>(null);
-  const { data } = useSuspenseQuery<QueryOf<Queries["recentScanResults"]>>(RecentScansQuery, { lastKey: currentNextPageKey });
-  const newLastKey = data?.recentScanResults?.LastEvaluatedKey;
+  const [{ nextNextPageKey, currentScans }, dispatch] = useReducer(scanStateReducer, initialState);
 
-  const [recentScans, setRecentScans] = useState<any>([]);
-
-  const nextScans = data?.recentScanResults?.ScanResults ?? [];
-
-  useEffect(() => {
-    setRecentScans(current => current.concat(nextScans));
-  }, [nextScans]);
-
-  useEffect(() => {
-    setNextNextPageKey(newLastKey);
-  }, [newLastKey]);
+  const { data } = useQuery<QueryOf<Queries["recentScanResults"]>>(RecentScansQuery, { lastKey: currentNextPageKey });
+  dispatch(data);
 
   const loadNextScans = () => {
-    startTransition(() => {
-      setCurrentNextPageKey(nextNextPageKey);
-    });
+    setCurrentNextPageKey(nextNextPageKey);
   };
 
   return (
     <div className="overlay-holder">
       {loading ? <LocalLoading /> : null}
       <div>
-        {recentScans.map(item => (
+        {currentScans.map(item => (
           <div>{item.title ?? `${item.isbn} Failure`}</div>
         ))}
 
