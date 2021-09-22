519dd58531976c556023e7ba3ff9f9a61ae48dca
Adam Rackis <adamrackis@gmail.com>
2021-09-22 00:04:37 -0500

    Move stackGraphData function

--- react/modules/home/components/barChart.tsx
+++ react/modules/home/components/barChart.tsx
@@ -10,66 +10,11 @@ import Bar from "./bar";
 import Axis from "./axis";
 
 import barCharQuery from "graphQL/home/barChart.graphql";
-import { computeSubjectParentId, getChildSubjectsSorted, useSubjectsState } from "app/state/subjectsState";
+import { useSubjectsState } from "app/state/subjectsState";
 import { useQuery } from "micro-graphql-react";
 import { AppContext } from "app/renderUI";
 import SvgTooltip from "./svgTooltip";
-
-const stackGraphData = (subjectHash, subjectIds, data) => {
-  if (!data) return null;
-
-  let targetSubjectsLookup = new Set(subjectIds);
-
-  let subjectResultsMap = new Map<string, number>([]);
-
-  data.allBooks.Books.forEach(item => {
-    let subjectsHeld = item.subjects
-      .filter(_id => subjectHash[_id])
-      .map(_id => (targetSubjectsLookup.has(_id) ? _id : getApplicableRootSubject(subjectHash[_id])._id));
-
-    let uniqueSubjects = Array.from(new Set(subjectsHeld));
-    let uniqueSubjectString = uniqueSubjects.sort().join(",");
-
-    if (!subjectResultsMap.has(uniqueSubjectString)) {
-      subjectResultsMap.set(uniqueSubjectString, 0);
-    }
-    subjectResultsMap.set(uniqueSubjectString, subjectResultsMap.get(uniqueSubjectString) + 1);
-  });
-
-  return Array.from(subjectResultsMap).map(([name, count], i) => {
-    let _ids = name.split(",").filter(s => s);
-    let names = _ids
-      .map(_id => subjectHash[_id].name)
-      .sort()
-      .join(",");
-
-    return {
-      groupId: name,
-      count,
-      display: names,
-      entries: _ids.map(_id => {
-        let subject = subjectHash[_id];
-        return {
-          name: subject.name,
-          color: subject.backgroundColor,
-          children: getChildSubjectsSorted(_id, subjectHash)
-        };
-      })
-    };
-  });
-
-  function getApplicableRootSubject(subject) {
-    let parentId = computeSubjectParentId(subject.path);
-
-    if (!parentId) {
-      return subject;
-    } else if (targetSubjectsLookup.has(parentId)) {
-      return subjectHash[parentId];
-    } else {
-      return getApplicableRootSubject(subjectHash[parentId]);
-    }
-  }
-};
+import { stackGraphData } from "./stackGraphData";
 
 const BarChart: FC<any> = memo(({ subjects, chartIndex, width, height, drilldown, maxWidth, header }) => {
   const [excluding, setExcluding] = useState({});
