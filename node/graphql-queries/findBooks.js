db65e51125f7edb4b336e6d66c49e6902481e78a
Adam Rackis <adamrackis@hotmail.com>
2021-11-02 07:47:19 -0500

    Delete old code

--- node/graphql-queries/findBooks.js
+++
@@ -1,9 +0,0 @@
-export default `query findBooks($ids: [String], $publicUserId: String) {
-  allBooks(_id_in: $ids, publicUserId: $publicUserId) {
-    Books {
-      _id
-      title
-      similarItems
-    }
-  }
-}`;
--- node/graphql-queries/findRecommendationMatches.js
+++
@@ -1,9 +0,0 @@
-export default `query findRecommendationMatches($isbns: [String], $eans: [String], $publicUserId: String) {
-  allBooks(publicUserId: $publicUserId, OR: [ { isbn_in: $isbns }, { ean_in: $eans } ]) {
-    Books {
-      title
-      isbn
-      ean
-    }
-  }
-}`;
