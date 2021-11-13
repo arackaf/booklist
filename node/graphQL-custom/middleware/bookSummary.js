ed4f6375a3f76180bd93a8c7a171bcb5eb5f6d3d
Adam Rackis <adamrackis@hotmail.com>
2021-11-13 13:21:40 -0600

    Small cleanup

--- node/graphQL-custom/middleware/bookSummary.js
+++ node/graphQL-custom/middleware/bookSummary.js
@@ -6,7 +6,6 @@ AWS.config.region = "us-east-1";
 
 export default class BooksMiddleware {
   async beforeUpdate(match, updates, { root, args, context, ast }) {
-    console.log("admin", context.user.admin);
     return !!context.user.admin;
   }
   adjustResults(results) {
