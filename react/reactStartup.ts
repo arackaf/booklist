e40d067dc6c35919de15186297690ed999bcd1ca
Adam Rackis <adamrackis@hotmail.com>
2021-11-18 11:55:02 -0600

    Fix HMR

--- react/reactStartup.ts
+++ react/reactStartup.ts
@@ -6,24 +6,13 @@ import "@reach/dialog/styles.css";
 import "./css/site-styles.scss";
 
 import { renderUI } from "app/renderUI";
-import { history } from "util/urlHelpers";
 
 import "util/ajaxUtil";
 import "util/graphql";
 
 import setupServiceWorker from "./util/setupServiceWorker";
-import { getCurrentUrlState } from "util/urlHelpers";
 
 console.log("HELLO WORLD 8");
 
 setupServiceWorker();
 renderUI();
-
-export function goto(module) {
-  var userId = getCurrentUrlState().searchState.userId;
-  let currentModule = history.location.pathname.replace(/\//g, "").toLowerCase();
-
-  if (currentModule !== module) {
-    history.push({ pathname: `/${module}`, search: userId ? `userId=${userId}` : void 0 });
-  }
-}
