2ca36f95fd0c6c00606fd9c7e5000ac4d723b0b4
Adam Rackis <adamrackis@hotmail.com>
2021-11-22 23:09:30 -0600

    Revert service worker change

--- react/util/setupServiceWorker.js
+++ react/util/setupServiceWorker.js
@@ -3,9 +3,7 @@ import { isLoggedIn } from "util/loginStatus";
 import { registerSW } from "virtual:pwa-register";
 
 export default function setupServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    // && !/localhost/.test(window.location) && !/lvh.me/.test(window.location)) {
-
+  if ("serviceWorker" in navigator && !/localhost/.test(window.location) && !/lvh.me/.test(window.location)) {
     const updateSW = registerSW({
       onNeedRefresh() {
         Promise.all([import("toastify-js"), import("toastify-js/src/toastify.css")]).then(([{ default: Toastify }]) => {
