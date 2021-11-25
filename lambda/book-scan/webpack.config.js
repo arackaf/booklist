b6f4f414c27c9323d59a22604e54fc06eec7d217
Adam Rackis <adamrackis@hotmail.com>
2021-11-25 17:33:52 -0600

    Start serverless-esbuild integration

--- lambda/book-scan/webpack.config.js
+++ lambda/book-scan/webpack.config.js
@@ -1,16 +1,5 @@
 const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
 const slsw = require("serverless-webpack");
-
-const ConditionalPlugin = (condition, plugin) => ({
-  apply: compiler => {
-    let fileName = Object.keys(compiler.options.entry)[0].split("/").pop();
-
-    if (condition(fileName)) {
-      plugin.apply(compiler);
-    }
-  }
-});
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -24,14 +13,6 @@ module.exports = {
         test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/
-      },
-      {
-        test: /.*.js$/,
-        loader: "string-replace-loader",
-        options: {
-          search: "require('optional-require')(require)",
-          replace: "__non_webpack_require__('optional-require')(__non_webpack_require__)"
-        }
       }
     ]
   },
@@ -40,21 +21,8 @@ module.exports = {
   },
   target: "node",
   mode: "production",
-  externals: ["aws-sdk", "optional-require", "mongodb-client-encryption", "saslprep"],
+  externals: ["aws-sdk"],
   optimization: {
     minimize: false
-  },
-  plugins: [
-    //ConditionalPlugin(
-    //fileName => fileName != "ws-connection",
-    new CopyPlugin({
-      patterns: [
-        { from: "../node_modules/saslprep", to: "node_modules/saslprep" },
-        { from: "../node_modules/sparse-bitfield", to: "node_modules/sparse-bitfield" },
-        { from: "../node_modules/memory-pager", to: "node_modules/memory-pager" },
-        { from: "../node_modules/optional-require", to: "node_modules/optional-require" }
-      ]
-    })
-    //)
-  ]
+  }
 };
