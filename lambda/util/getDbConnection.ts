1d26bca05cae1cffcff2583ab78ba500ace1e756
Adam Rackis <adamrackis@hotmail.com>
2022-03-31 17:10:34 -0500

    Tweak db connect module to allow for connection teardown

--- lambda/util/getDbConnection.ts
+++ lambda/util/getDbConnection.ts
@@ -9,3 +9,15 @@ export default async () => {
 
   return MongoClient.connect(connString).then(client => client.db(dbName));
 };
+
+export async function getDbConnection() {
+  const IS_DEV = process.env.stage === "dev";
+  const secrets = await getSecrets();
+  const connString = secrets[IS_DEV ? "mongo-connection-string-dev" : "mongo-connection-string"];
+  const dbName = secrets["db-name"];
+
+  const client = await MongoClient.connect(connString);
+  const db = await client.db(dbName);
+
+  return { db, client };
+}
