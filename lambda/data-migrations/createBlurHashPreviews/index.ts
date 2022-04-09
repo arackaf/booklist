11625f8ab1db833b46adedb4f7492072b1d20669
Adam Rackis <adamrackis@hotmail.com>
2022-04-09 07:02:41 -0400

    Cleanup

--- lambda/data-migrations/createBlurHashPreviews/index.ts
+++ lambda/data-migrations/createBlurHashPreviews/index.ts
@@ -12,9 +12,10 @@ function getImageData(image: Image) {
 
 async function getBlurHash(url: string) {
   const image = await loadImage(url);
-  console.log(" image = ", image.width, image.height);
+  console.log("image size", image.width, image.height);
   const imageData = getImageData(image);
   const blurHashValue = await blurhash.encode(imageData.data, imageData.width, imageData.height, 4, 4);
+  console.log("blurHashValue", blurHashValue);
   return { blurhash: blurHashValue, w: image.width, h: image.height };
 }
 
@@ -28,42 +29,80 @@ async function run() {
 
   const books = await db
     .collection("books")
-    .find({ previewsUpdated: { $ne: true }, $and: [{ smallImagePreview: { $exists: true } }, { smallImagePreview: { $ne: "" } }] })
+    .find({
+      $or: [{ mobileImagePreview: { $type: "string" } }, { smallImagePreview: { $type: "string" } }, { mediumImagePreview: { $type: "string" } }]
+    })
     .toArray();
 
   console.log(books.length, "books to process");
   let i = 1;
 
   for (const book of books) {
+    const ops: Promise<void>[] = [];
     console.log("\n====================================");
     console.log("Processing", book.title, "\n");
 
-    const { smallImage } = book;
+    const { mobileImage, mobileImagePreview, smallImage, smallImagePreview, mediumImage, mediumImagePreview } = book;
 
     console.log("Current img", smallImage);
     const idx = i++;
 
     try {
-      console.log("Downloading ...");
+      const updateObject = {} as any;
+      if (typeof mobileImagePreview === "string") {
+        ops.push(
+          (async () => {
+            try {
+              console.log("Updating mobile");
+              const blurhashValue = await getBlurHash(mobileImage);
 
-      const blurhashValue = await getBlurHash(smallImage);
+              console.log("Mobile downloaded. Blurhash value ===", blurhashValue);
+              updateObject.mobileImagePreview = blurhashValue;
+            } catch (er) {
+              console.log("Mobile failed");
+            }
+          })()
+        );
+      }
+      if (typeof smallImagePreview === "string") {
+        ops.push(
+          (async () => {
+            try {
+              console.log("Updating small");
+              const blurhashValue = await getBlurHash(smallImage);
 
-      console.log("Downloaded. Blurhash value ===", blurhashValue);
+              console.log("Small downloaded. Blurhash value ===", blurhashValue);
+              updateObject.smallImagePreview = blurhashValue;
+            } catch (er) {
+              console.log("Small failed");
+            }
+          })()
+        );
+      }
+      if (typeof mediumImagePreview === "string") {
+        ops.push(
+          (async () => {
+            try {
+              console.log("Updating medium");
+              const blurhashValue = await getBlurHash(mediumImage);
 
-      await db.collection("books").updateOne({ _id: book._id }, { $set: { smallImagePreview: JSON.stringify(blurhashValue) } });
-      console.log("#", idx, book.title, "saved");
+              console.log("Medium downloaded. Blurhash value ===", blurhashValue);
+              updateObject.mediumImagePreview = blurhashValue;
+            } catch (er) {
+              console.log("Medium failed");
+            }
+          })()
+        );
+      }
+      await Promise.all(ops);
 
-      // await db.collection("bookSummaries").updateOne(
-      //   { _id: book._id },
-      //   {
-      //     $set: { smallImage: "https://s.gr-assets.com/assets/nophoto/book/50x75-a91bf249278a81aabab721ef782c4a74.png", smallImagePreview: "" }
-      //   }
-      // );
-      console.log("#", idx, "Cover handling not successful");
-
-      //} else {
-      console.log("#", idx, "Download not successful");
-      //}
+      if (Object.keys(updateObject).length) {
+        console.log("#", idx, book.title, "Saving");
+        await db.collection("books").updateOne({ _id: book._id }, { $set: updateObject });
+      } else {
+        console.log("#", idx, "Nothing to save");
+      }
+      console.log("#", idx, book.title, "done");
 
       console.log("====================================\n");
     } catch (er) {
