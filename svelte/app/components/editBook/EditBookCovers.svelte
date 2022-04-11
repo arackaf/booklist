71c8ee584d03783e4377bc99717589ac7a85d1f3
Adam Rackis <adamrackis@hotmail.com>
2022-04-10 23:30:11 -0500

    Start updates

---
+++ svelte/app/components/editBook/EditBookCovers.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  import ManageBookCover from "./ManageBookCover.svelte";
+
+  /*
+export const EditBookCovers: FunctionComponent<Props> = ({ book, updateBook }) => {
+  const { runMutation: runBookMutation } = useMutation<MutationOf<Mutations["updateBook"]>>(UpdateBook);
+
+  const [coverProcessingResult, setCoverProcessingResult] = useState<UploadResultsType>(null);
+  const [useNewMobile, setUseNewMobile] = useState(false);
+  const [useNewSmall, setUseNewSmall] = useState(false);
+  const [useNewMedium, setUseNewMedium] = useState(false);
+
+  const isNew = () => !book?._id;
+
+  const saveEligible = useMemo(() => {
+    return coverProcessingResult?.success && (useNewMobile || useNewSmall || useNewMedium);
+  }, [coverProcessingResult, useNewMobile, useNewSmall, useNewMedium]);
+
+  const runSave = () => {
+    let { _id } = book;
+
+    const updateObject: Partial<
+      Pick<BookMutationInput, "mobileImage" | "mobileImagePreview" | "smallImage" | "smallImagePreview" | "mediumImage" | "mediumImagePreview">
+    > = {};
+    if (useNewMobile) {
+      updateObject.mobileImage = coverProcessingResult.mobile.image.url;
+      updateObject.mobileImagePreview = coverProcessingResult.mobile.image.preview;
+    }
+    if (useNewSmall) {
+      updateObject.smallImage = coverProcessingResult.small.image.url;
+      updateObject.smallImagePreview = coverProcessingResult.small.image.preview;
+    }
+    if (useNewMedium) {
+      updateObject.mediumImage = coverProcessingResult.medium.image.url;
+      updateObject.mediumImagePreview = coverProcessingResult.medium.image.preview;
+    }
+
+    if (_id) {
+      return runBookMutation({ _id, book: updateObject }).then(() => {
+        setCoverProcessingResult(null);
+        updateBook(b => ({ ...b, ...updateObject }));
+      });
+    } else {
+      updateBook(b => ({ ...b, ...updateObject }));
+      setCoverProcessingResult(null);
+    }
+  };
+
+  const [coverProcessingError, setCoverProcessingError] = useState(false);
+  const onCoverError = () => {
+    setCoverProcessingError(true);
+    setCoverProcessingResult(null);
+  };
+  const clearCoverError = () => setCoverProcessingError(false);
+
+  const onCoverResults = (obj: UploadResultsType) => {
+    clearCoverError();
+    setCoverProcessingResult(obj);
+    if (obj.success) {
+      setUseNewMobile(obj.mobile.STATUS === "success");
+      setUseNewSmall(obj.small.STATUS === "success");
+      setUseNewMedium(obj.medium.STATUS === "success");
+    } else {
+      setUseNewMobile(false);
+      setUseNewSmall(false);
+      setUseNewMedium(false);
+    }
+  };
+
+  return (
+
+  */
+</script>
+
+<div>
+  <!-- <CurrentCovers book={book} /> -->
+  <!-- <hr /> -->
+
+  <ManageBookCover />
+  <!-- onError={onCoverError} onResults={onCoverResults} /> -->
+  <!-- {coverProcessingError ? <div className="alert alert-danger">Error processing this cover</div> : null}
+      {coverProcessingResult ? (
+        <UploadResults {...{ ...coverProcessingResult, useNewMobile, setUseNewMobile, useNewSmall, setUseNewSmall, useNewMedium, setUseNewMedium }} />
+      ) : null} -->
+  <!-- {saveEligible && isNew() ? (
+        <Button className="margin-top" preset="primary" onClick={runSave}>
+          Set image info
+        </Button>
+      ) : null}
+      {saveEligible && !isNew() ? (
+        <ActionButton className="margin-top" preset="primary" onClick={runSave} finishedText="Saved" text="Save" runningText="Saving" />
+      ) : null} 
+    -->
+</div>
--- svelte/app/components/editBook/ManageBookCover.svelte
+++ svelte/app/components/editBook/ManageBookCover.svelte
@@ -1,29 +1,15 @@
 <script lang="ts">
   import { appState } from "app/state/appState";
-  import { MutationOf, Mutations } from "graphQL/graphql-typings";
   import Dropzone from "svelte-file-dropzone/src/components/Dropzone.svelte";
-
   import ajaxUtil from "util/ajaxUtil";
-  import { getCrossOriginAttribute } from "util/corsHelpers";
-
-  import UpdateBook from "graphQL/books/updateBook.graphql";
-  import { mutation } from "micro-graphql-svelte";
-
   import FlowItems from "../layout/FlowItems.svelte";
-  import Stack from "../layout/Stack.svelte";
 
-  export let _id;
-  export let img;
-  export let size;
-  export let imgKey;
-  export let updateBookObject;
+  export let onResults: (results: any) => void;
+  export let onError: () => void;
 
-  let currentUrl = img;
   let uploadState = { pendingImg: "", uploadError: "" };
 
   let uploading = false;
-  const { mutationState } = mutation<MutationOf<Mutations["updateBook"]>>(UpdateBook);
-  let { runMutation: updateBook } = $mutationState;
 
   $: ({ loginToken, userId } = $appState);
   $: ({ pendingImg, uploadError } = uploadState);
@@ -40,39 +26,8 @@
   const doRemoteSave = () => {
     remoteImageSaving = true;
 
-    const request = { userId, loginToken, url: remoteUrl, size };
-    ajaxUtil
-      .postWithCors(process.env.UPLOAD_BOOK_COVER_FROM_URL, request, processUrlResponse, processUrlError)
-      .then(() => (remoteImageSaving = false));
-  };
-
-  const runSave = () => {
-    let newUrl = uploadState.pendingImg;
-    if (_id) {
-      return updateBook({ _id, book: { [imgKey]: newUrl } }).then(() => {
-        currentUrl = newUrl;
-        uploadState = { pendingImg: "", uploadError: "" };
-      });
-    } else {
-      currentUrl = newUrl;
-      updateBookObject(book => ({ ...book, [imgKey]: newUrl }));
-    }
-  };
-
-  const processUrlResponse = res => {
-    if (res.error) {
-      uploadState = { pendingImg: "", uploadError: res.error };
-    } else if (!res.url) {
-      uploadState = { pendingImg: "", uploadError: "Error uploading" };
-    } else {
-      uploadState = { pendingImg: res.url, uploadError: "" };
-    }
-    uploading = false;
-  };
-
-  const processUrlError = res => {
-    uploadState = { pendingImg: "", uploadError: "Error uploading" };
-    uploading = false;
+    const request = { userId, loginToken, url: remoteUrl };
+    ajaxUtil.postWithCors(process.env.UPLOAD_BOOK_COVER_FROM_URL, request, onResults, onError).then(() => (remoteImageSaving = false));
   };
 
   const onDrop = evt => {
@@ -85,10 +40,9 @@
     request.append("fileUploaded", acceptedFiles[0]);
     request.append("loginToken", loginToken);
     request.append("userId", userId);
-    request.append("size", size);
 
     uploading = true;
-    ajaxUtil.postWithFilesCors(process.env.UPLOAD_BOOK_COVER, request, processUrlResponse, processUrlError);
+    ajaxUtil.postWithFilesCors(process.env.UPLOAD_BOOK_COVER, request, onResults, onError);
   };
 
   let dragging = false;
@@ -108,43 +62,25 @@
 </style>
 
 <FlowItems pushLast={true}>
-  {#if currentUrl}
-    <div style="min-width: 110px"><img alt="Current book cover" {...getCrossOriginAttribute(currentUrl)} src={currentUrl} /></div>
-  {:else}
-    <div style="align-self: flex-start; min-width: 110px;" class="alert alert-warning"><span>No Cover</span></div>
-  {/if}
+  <div style="flex: 1; position: relative;">
+    <Dropzone
+      accept={undefined}
+      disableDefaultStyles={true}
+      multiple={false}
+      on:drop={onDrop}
+      on:dragenter={() => (dragging = true)}
+      on:dragleave={() => (dragging = false)}
+      containerClasses="dropzone-container"
+      containerStyles={dropAddedStyles}
+    >
+      <div style="padding: 15px">Click or drag to upload</div>
+    </Dropzone>
+    {#if uploadError}
+      <div style="display: inline-block; margin-top: 2px; margin-bottom: 2px" class="label label-danger">{uploadError}</div>
+    {/if}
+  </div>
 
-  {#if !pendingImg}
-    <div style="min-width: 100px; max-width: 140px; position: relative;">
-      <Dropzone
-        accept={undefined}
-        disableDefaultStyles={true}
-        multiple={false}
-        on:drop={onDrop}
-        on:dragenter={() => (dragging = true)}
-        on:dragleave={() => (dragging = false)}
-        containerClasses="dropzone-container"
-        containerStyles={dropAddedStyles}
-      >
-        <div>Click or drag to upload a new cover</div>
-      </Dropzone>
-      {#if uploadError}
-        <div style="display: inline-block; margin-top: 2px; margin-bottom: 2px" class="label label-danger">{uploadError}</div>
-      {/if}
-    </div>
-  {/if}
-  {#if pendingImg}
-    <Stack>
-      <img alt="Pending book cover" src={pendingImg} {...getCrossOriginAttribute(pendingImg)} />
-      <FlowItems pushLast={true}>
-        <button on:click={runSave} class="btn btn-xs btn-light btn-square-icon"> <i class="fal fa-check" /> </button>
-        <button on:click={() => (uploadState = { pendingImg: "", uploadError: "" })} class="btn btn-xs btn-light btn-square-icon">
-          <i class="fal fa-undo" />
-        </button>
-      </FlowItems>
-    </Stack>
-  {/if}
-  <div>
+  <div style="flex: 1">
     <div class="btn-group">
       <input bind:value={remoteUrl} on:keydown={keyDown} style="min-width: 200px" class="form-control" placeholder="New Cover URL" tabIndex={-1} />
       <button class="btn btn-default" disabled={!remoteUrl || remoteImageSaving} on:click={doRemoteSave}>
