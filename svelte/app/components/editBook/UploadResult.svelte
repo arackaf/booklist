669e12291e1ecfa9d3ee1631187e93021129cea4
Adam Rackis <adamrackis@hotmail.com>
2022-04-14 21:03:21 -0500

    svelte cover upload style tweak

--- svelte/app/components/editBook/UploadResult.svelte
+++ svelte/app/components/editBook/UploadResult.svelte
@@ -16,13 +16,22 @@
   $: ImgComponent = size === "mobile" ? CoverMobile : size === "small" ? CoverSmall : CoverMedium;
 </script>
 
+<style>
+  .remove-button-focus :global(button:focus::before) {
+    box-shadow: 0px 0px 2px 3px var(--primary-7);
+  }
+  .remove-button-focus :global(button:focus) {
+    box-shadow: 0 0 0 0;
+  }
+</style>
+
 <div style="flex: 1">
   {#if success}
     <Stack inline={true} style="align-items: center; height: 100%">
       <div class="margin-bottom" style="opacity: {useNewImage ? 1 : 0.5}">
         <svelte:component this={ImgComponent} url={image.url} preview={image.preview} />
       </div>
-      <div style="margin-top: auto;">
+      <div style="margin-top: auto;" class="remove-button-focus">
         <Toggle
           toggledColor="var(--success-5)"
           untoggledColor="var(--neutral-5)"
