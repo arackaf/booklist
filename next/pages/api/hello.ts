b8bb3d878249ff0ab6c773cffb3b8df84c338388
Adam Rackis <adamrackis@hotmail.com>
2022-10-30 22:45:52 -0500

    auth work

--- next/pages/api/hello.ts
+++ next/pages/api/hello.ts
@@ -3,18 +3,10 @@ import { authOptions } from "./auth/[...nextauth]";
 import { getSession } from "../../lib/getSessionApi";
 import { NextRequest } from "next/server";
 
-let x: NextRequest;
-
 export default async function handler(req, res) {
   const session = await unstable_getServerSession(req, res, authOptions);
 
-  const cookie = req.headers["cookie"];
-  console.log("QQQ", cookie);
-
-  const session2 = await getSession(cookie);
-
   console.log("in api, session:", { session });
-  console.log("in api, session 2:", { session2 });
 
   res.status(200).json({ name: "Hello World", session });
 }
