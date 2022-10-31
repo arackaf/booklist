c589612ada7f964b227de1db25e4d17f8fe9b315
Adam Rackis <adamrackis@hotmail.com>
2022-10-31 14:23:09 -0500

    Update many works

--- next/pages/api/hello.ts
+++ next/pages/api/hello.ts
@@ -58,5 +58,31 @@ export default async function handler(req, res) {
       console.log({ err });
     });
 
+  fetch(process.env.MONGO_URL + "/action/updateMany", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Request-Headers": "*",
+      "api-key": process.env.MONGO_URL_API_KEY
+    },
+    body: JSON.stringify({
+      collection: "books",
+      database: "my-library",
+      dataSource: "Cluster0",
+      filter: { _id: { $in: [{ $oid: "6285a6596ea0d70a0c1a24bf" }, { $oid: "62915d0e1a727bc0a9dc7903" }] } },
+      update: { $set: { isRead: false } }
+    })
+  })
+    .then(res => {
+      console.log(res.status);
+      return res.json();
+    })
+    .then(res => {
+      console.log("SUCCESS", res);
+    })
+    .catch(err => {
+      console.log({ err });
+    });
+
   res.status(200).json({ name: "Hello World", session });
 }
