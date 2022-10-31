0dc934877941682d49b9791497c1ec26ff265d09
Adam Rackis <adamrackis@hotmail.com>
2022-10-31 15:09:13 -0500

    start data folder ... TODO

---
+++ data/books.ts
@@ -0,0 +1,12 @@
+//import { runAction } from "./mongo-action";
+import { xya } from "./mongo-action";
+
+export function readBooks() {
+  xya();
+  console.log("Ayoooooo");
+  //runAction("aggregate", {});
+}
+
+export function updateBooks() {
+  console.log("Ayoooooo");
+}
