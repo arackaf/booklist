3be19a70d4dc57631eba477384e7cd63a273febe
Adam Rackis <adamrackis@hotmail.com>
2022-11-02 16:01:10 -0500

    Home page copy tweaks

--- react/modules/home/home.tsx
+++ react/modules/home/home.tsx
@@ -131,19 +131,18 @@ const HomeIfNotLoggedIn = () => (
         <div style={{ maxWidth: "800px", marginTop: "25px", marginLeft: "auto", marginRight: "auto" }}>
           Welcome to <i>My Library</i>.<br />
           <br />
-          This site is my own little passion project, the purpose of which is to track your library. You scan in your books (or manually type in the
-          isbn) and the books' info is fetched from Amazon, and stored for you. You can then flexibly search and categorize your library.
+          This site is my own little passion project, which tracks your library. You scan your books (or manually type in the isbn) and the books'
+          info is fetched from ISBN DB, and stored for you. You can then search and categorize your library.
           <br />
           <br />
           So basically this site is of use to the extremely small percentage of people for whom the following are <i>all</i> true: they read a lot,
-          own the books they read, and read non-eBooks. As I said, this is more of a passion project than anything.
+          own the books they read, and read non-eBooks. Again, it's is more of a passion project than anything.
           <br />
           <br />
-          It's free to sign up, and store up to 500 books. In the remote chance someone actually wants to store more than that, there'll be some sort
-          of nominal fee to help defray storage costs.
+          It's free and I won't ever sell your info, so give it a try!
           <br />
           <br />
-          For those interested in seeing the code for this site, the GitHub repository is{" "}
+          If you're interested in seeing the code, the GitHub repository is{" "}
           <a target="_blank" href="https://github.com/arackaf/booklist">
             here
           </a>
