9ccbaa1c8971cedfea8ec0476084694070b99b53
Adam Rackis <adamrackis@hotmail.com>
2022-11-25 18:39:06 -0600

    Move utils over

---
+++ svelte-kit/src/lib/components/navigation/ModuleLoading.svelte
@@ -0,0 +1,23 @@
+<script>
+import SpinnerSvg from "./SpinnerSvg.svelte";
+
+</script>
+
+<style>
+  .module-loading {
+    position: fixed;
+    top: calc(50% - 100px);
+    left: calc(50% - 40px);
+    z-index: 10;
+    opacity: 0.7;
+  }
+  
+  :global(.module-loading > *) {
+    z-index: 1;
+    fill: var(--primary-4);
+  }
+</style>
+
+<div class="module-loading">
+  <SpinnerSvg class="fa-spin" width="80" />
+</div>
