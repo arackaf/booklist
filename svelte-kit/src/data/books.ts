6930b502fc0ebdb350ae4517a8f6aff4981d3228
Adam Rackis <adamrackis@hotmail.com>
2022-12-04 18:33:11 -0600

    Remove native db loading from books

--- svelte-kit/src/data/books.ts
+++ svelte-kit/src/data/books.ts
@@ -1,11 +1,6 @@
 import { env } from "$env/dynamic/private";
-import { getDbConnection } from "./dbUtils";
-
-const { db } = await getDbConnection();
 
 export const searchBooks = async (search: string) => {
-  const nativeStart = +new Date();
-
   const httpStart = +new Date();
   const httpResponse = fetch(env.MONGO_URL + "/action/aggregate", {
     method: "POST",
@@ -36,19 +31,5 @@ export const searchBooks = async (search: string) => {
       console.log({ err });
     });
 
-  const result = await db
-    .collection("books")
-    .aggregate([
-      { $match: { title: { $regex: search || "", $options: "i" }, userId: "60a93babcc3928454b5d1cc6" } },
-      { $project: { _id: 1, title: 1, userId: 1 } },
-      { $limit: 50 },
-      { $sort: { title: 1 } }
-    ])
-    .toArray();
-
-  const nativeEnd = +new Date();
-
-  console.log("Native time books", nativeEnd - nativeStart);
   return httpResponse;
-  return result.map(b => ({ ...b, _id: b._id.toString() }));
 };
