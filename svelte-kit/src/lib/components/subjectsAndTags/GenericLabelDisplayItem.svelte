d61722580366f5e218bdbc2bbb63907098aab256
Adam Rackis <adamrackis@hotmail.com>
2022-12-05 23:06:49 -0600

    Style tweak for label select

--- svelte-kit/src/lib/components/subjectsAndTags/GenericLabelDisplayItem.svelte
+++ svelte-kit/src/lib/components/subjectsAndTags/GenericLabelDisplayItem.svelte
@@ -5,7 +5,7 @@
   let effectiveChildLevel = item.childLevel - (item.prepend || []).length || 0;
 </script>
 
-<span style="margin-left: {effectiveChildLevel * 15}px">
+<span style="display: flex; margin-left: {effectiveChildLevel * 15}px">
   {#each item.prepend || [] as s}
     <LabelDisplay extraStyles="margin-right: 5px; cursor: pointer" item={s} disabled={true} />
     <i class="fal fa-level-up" style="transform: rotate(90deg) translateX(2px); margin: 0 7px 0 3px;" />
