3b68d4aa2164664e4ad6964669217bb5f2aa190a
Adam Rackis <adamrackis@hotmail.com>
2022-12-19 21:36:03 -0600

    Loading indicator

---
+++ svelte-kit/src/lib/components/ui/Loading.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let style = "";
+</script>
+
+<div><i {style} class="far fa-5x fa-spin fa-spinner" /></div>
+
+<style>
+  div {
+    position: fixed;
+    left: 50%;
+    top: 30%;
+    transform: translateX(-50%) translateY(-50%);
+    z-index: 99999;
+  }
+  i {
+    opacity: 0.5;
+    color: var(--primary-3);
+  }
+</style>
--- svelte-kit/src/routes/+layout.svelte
+++ svelte-kit/src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
+  import { onMount } from "svelte";
   import { writable } from "svelte/store";
+  import { beforeNavigate, afterNavigate } from "$app/navigation";
   import MainNavigation from "$lib/components/navigation/MainNavigation.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import Loading from "$lib/components/ui/Loading.svelte";
 
   import "$styles/fontawesome/css/all.min.css";
   import "$styles/site-styles.scss";
   import "./styles.scss";
-  import { onMount } from "svelte";
 
   const appState = writable({ isMobile: false });
 
@@ -14,12 +16,25 @@
   onMount(() => {
     document.body.classList.add(theme);
   });
+
+  let navigating = false;
+
+  beforeNavigate(({ type }) => {
+    navigating = true;
+  });
+
+  afterNavigate(({ type }) => {
+    navigating = false;
+  });
 </script>
 
 <div class={`app-container ${theme}`}>
   <div id="app">
     <MainNavigation />
     <main>
+      {#if navigating}
+        <Loading />
+      {/if}
       <slot />
 
       {#if $appState.isMobile}
