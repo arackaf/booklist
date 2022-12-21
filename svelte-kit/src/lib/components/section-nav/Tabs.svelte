87c3045544aa44f1660f86a62eccdf59ece49af9
Adam Rackis <adamrackis@hotmail.com>
2022-12-20 20:29:11 -0600

    Section nav started

---
+++ svelte-kit/src/lib/components/section-nav/Tabs.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+  import { writable } from "svelte/store";
+
+  import localStorageManager from "$lib/util/localStorage";
+
+  export let defaultTab = "";
+  export let localStorageName = "";
+
+  let currentTab = localStorageManager.get(localStorageName) || defaultTab;
+
+  const setTab = (tab: string) => tabState.update(state => ({ ...state, currentTab: tab }));
+  let tabState = writable({ localStorageName, currentTab, setTab });
+
+  setContext("tabs-state", tabState);
+</script>
+
+<slot />
--- svelte-kit/src/routes/(logged-in)/home/+layout.svelte
+++ svelte-kit/src/routes/(logged-in)/home/+layout.svelte
@@ -2,6 +2,17 @@
   <div style="margin-left: auto; margin-right: auto; max-width: var(--content-max-width)">
     <div>
       <div class="panel-body" style="position: relative">
+        <nav class="section-nav">
+          <div class="section-nav-item active">
+            <a>View</a>
+          </div>
+          <div class="section-nav-item">
+            <a>Discover</a>
+          </div>
+          <div class="section-nav-item">
+            <a>Recent scans</a>
+          </div>
+        </nav>
         <slot />
       </div>
     </div>
