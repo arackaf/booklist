5ae89ae5f46966c6f31770f69325937df5bd7bf6
Adam Rackis <adamrackis@hotmail.com>
2022-12-22 14:37:36 -0600

    start to set up Dynamo

--- svelte-kit/src/hooks.server.ts
+++ svelte-kit/src/hooks.server.ts
@@ -1,6 +1,44 @@
 import SvelteKitAuth from "@auth/sveltekit";
 import GoogleProvider from "@auth/core/providers/google";
-import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET } from "$env/static/private";
+import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, DYNAMO_AUTH_TABLE } from "$env/static/private";
+
+import { DynamoDB, type DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter";
+
+const dynamoConfig: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY
+  },
+
+  region: "us-east-1"
+};
+
+const client = DynamoDBDocument.from(new DynamoDB(dynamoConfig), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true
+  }
+});
+
+export const handle = SvelteKitAuth({
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_AUTH_CLIENT_ID,
+      clientSecret: GOOGLE_AUTH_SECRET
+    })
+  ],
+  session: {
+    maxAge: 60 * 60 * 24 * 365,
+    strategy: "jwt"
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  adapter: DynamoDBAdapter(client, { tableName: DYNAMO_AUTH_TABLE }) as any
+});
 
 // const PRELOAD = new Set(["font", "js", "css"]);
 
@@ -11,12 +49,3 @@ import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET } from "$env/static/private";
 
 //   return response;
 // }
-
-export const handle = SvelteKitAuth({
-  providers: [
-    GoogleProvider({
-      clientId: GOOGLE_AUTH_CLIENT_ID,
-      clientSecret: GOOGLE_AUTH_SECRET
-    })
-  ]
-});
