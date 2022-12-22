e0ce2e838330a13fefb07cbe4c6674edd3ec0241
Adam Rackis <adamrackis@hotmail.com>
2022-12-22 14:04:18 -0600

    Login / logout works

--- svelte-kit/src/lib/components/navigation/ModuleLink.svelte
+++ svelte-kit/src/lib/components/navigation/ModuleLink.svelte
@@ -2,8 +2,18 @@
   export let href: string;
   export let style = "";
   export let disabled = false;
+  export let onClick: (() => void) | null = null;
+
+  const linkClicked = (evt: any) => {
+    if (!onClick) {
+      return;
+    }
+
+    evt.preventDefault();
+    onClick();
+  };
 </script>
 
-<a href={disabled ? null : href} {style}>
+<a on:click={linkClicked} href={disabled ? null : href} {style}>
   <slot />
 </a>
--- svelte-kit/src/lib/components/navigation/NavBarItem.svelte
+++ svelte-kit/src/lib/components/navigation/NavBarItem.svelte
@@ -7,15 +7,16 @@
   export let disabled = false;
   let className = "";
   export { className as class };
+  export let onClick: (() => void) | null = null;
 
   $: currentModule = $page.route.id;
-  $: active = currentModule?.indexOf(href) !== -1;
+  $: active = href && currentModule?.indexOf(href) !== -1;
 
   const spreadClassNames = (baseCssClasses = "", ...userClasses: string[]) => `${baseCssClasses} ${userClasses.join(" ")}`;
 </script>
 
 <li class={spreadClassNames(className || "", !!disabled ? "disabled" : "", active ? "active" : "")}>
-  <ModuleLink {style} {href}>
+  <ModuleLink {onClick} {style} {href}>
     <slot />
   </ModuleLink>
 </li>
