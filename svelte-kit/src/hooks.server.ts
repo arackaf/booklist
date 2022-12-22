a74ac2f6dc9a935fb528bb949fa2be4dc954fb82
Adam Rackis <adamrackis@hotmail.com>
2022-12-22 13:39:13 -0600

    Login redirect

--- svelte-kit/src/hooks.server.ts
+++ svelte-kit/src/hooks.server.ts
@@ -1,9 +1,22 @@
-const PRELOAD = new Set(["font", "js", "css"]);
+import SvelteKitAuth from "@auth/sveltekit";
+import GoogleProvider from "@auth/core/providers/google";
+import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET } from "$env/static/private";
 
-export async function handle({ event, resolve }: any) {
-  const response = await resolve(event, {
-    preload: ({ type }: any) => PRELOAD.has(type)
-  });
+// const PRELOAD = new Set(["font", "js", "css"]);
 
-  return response;
-}
+// export async function handle({ event, resolve }: any) {
+//   const response = await resolve(event, {
+//     preload: ({ type }: any) => PRELOAD.has(type)
+//   });
+
+//   return response;
+// }
+
+export const handle = SvelteKitAuth({
+  providers: [
+    GoogleProvider({
+      clientId: GOOGLE_AUTH_CLIENT_ID,
+      clientSecret: GOOGLE_AUTH_SECRET
+    })
+  ]
+});
--- svelte-kit/src/routes/(logged-in)/+layout.server.ts
+++ svelte-kit/src/routes/(logged-in)/+layout.server.ts
@@ -1,7 +1,14 @@
+import { redirect } from "@sveltejs/kit";
+
 import { allSubjects } from "$data/subjects";
 import { allTags } from "$data/tags";
 
-export function load() {
+export async function load({ parent }: any) {
+  const { session } = await parent();
+  if (!session?.user) {
+    throw redirect(302, "/");
+  }
+
   const subjects = allSubjects();
   const tags = allTags();
 
