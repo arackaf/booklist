13a291a2f24873dae4585fc884d11e7be5c232ac
Adam Rackis <adamrackis@hotmail.com>
2023-01-04 18:09:03 -0600

    Remove native mongo connection utils

--- svelte-kit/src/data/dbUtils.ts
+++ svelte-kit/src/data/dbUtils.ts
@@ -1,15 +1,4 @@
-import { MongoClient } from "mongodb";
 import { env } from "$env/dynamic/private";
-
-const dbName = env.DB_NAME;
-const connString = env.MONGO_CONNECTION!;
-
-export async function getDbConnection() {
-  let client = await MongoClient.connect(connString);
-  let db = await client.db(dbName);
-
-  return { client, db };
-}
 
 export const runSingleUpdate = runRequest.bind(null, "updateOne");
 export const runMultiUpdate = runRequest.bind(null, "updateMany");
