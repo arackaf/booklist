dcec91f6b913263d96d726bc9db8725b35c7288c
Adam Rackis <adamrackis@hotmail.com>
2023-01-04 18:06:01 -0600

    Subjects using http api

--- svelte-kit/src/data/subjects.ts
+++ svelte-kit/src/data/subjects.ts
@@ -1,21 +1,15 @@
-import { getDbConnection } from "./dbUtils";
-import type { Subject } from "./types";
-
-const { db } = await getDbConnection();
+import { runAggregate } from "./dbUtils";
 
 export const allSubjects = async (userId: string) => {
   userId = userId || "";
-  const nativeStart = +new Date();
+  const httpStart = +new Date();
 
-  const result: Subject[] = (await db
-    .collection("subjects")
-    .aggregate([{ $match: { userId } }, { $project: { _id: 1, name: 1, path: 1, textColor: 1, backgroundColor: 1 } }, { $sort: { name: 1 } }])
-    .toArray()) as Subject[];
+  return runAggregate("subjects", {
+    pipeline: [{ $match: { userId } }, { $project: { _id: 1, name: 1, path: 1, textColor: 1, backgroundColor: 1 } }, { $sort: { name: 1 } }]
+  }).then(res => {
+    const httpEnd = +new Date();
 
-  const nativeEnd = +new Date();
-
-  console.log("Native time subjects", nativeEnd - nativeStart);
-  const allSubjectsSorted: Subject[] = result.map(s => ({ ...s, _id: s._id.toString() }));
-
-  return allSubjectsSorted;
+    console.log("Http time subjects", httpEnd - httpStart);
+    return res.documents;
+  });
 };
