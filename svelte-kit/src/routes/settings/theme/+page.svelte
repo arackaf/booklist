ed2dc14ac309fc820d6394383bfcdc83fde93720
Adam Rackis <adamrackis@hotmail.com>
2023-01-06 19:30:39 -0600

    Theme chooser page started

--- svelte-kit/src/routes/settings/theme/+page.svelte
+++ svelte-kit/src/routes/settings/theme/+page.svelte
@@ -1,1 +1,40 @@
-<h1>Hello</h1>
+<script lang="ts">
+  import "./theme-chooser.scss";
+  import type { PageData } from "./$types";
+
+  import classNames from "classnames";
+
+  import { NUM_THEMES } from "$lib/util/constants";
+  import Stack from "$lib/components/layout/Stack.svelte";
+
+  import DemoStyles from "./DemoStyles.svelte";
+
+  const themeNames = Array.from({ length: NUM_THEMES }, (v, i) => `scheme${i + 1}`);
+  const arrayOfTen = Array.from({ length: 10 }, (v, i) => i + 1);
+
+  export let data: PageData;
+  $: ({ theme, whiteBb } = data);
+
+  const setWhiteBackground = () => {};
+</script>
+
+<div class="theme-chooser-root">
+  <div class="theme-chooser-list">
+    <label style="font-size: 16px" class="checkbox margin-bottom">
+      <input type="checkbox" checked={whiteBb} on:change={setWhiteBackground} />
+      White background
+    </label>
+    {#each themeNames as name}
+      <Stack on:click={() => {}} class={classNames("theme-chooser-item", { active: theme == name })} tightest={true}>
+        <div class={classNames(name, "theme-chooser")}>
+          {#each arrayOfTen as val}
+            <div style={`background-color: var(--primary-${val})`} />
+          {/each}
+        </div>
+      </Stack>
+    {/each}
+  </div>
+  <div class="demo-container">
+    <DemoStyles />
+  </div>
+</div>
---
+++ svelte-kit/src/routes/settings/theme/DemoStyles.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import FlexRow from "$lib/components/layout/FlexRow.svelte";
+</script>
+
+<div class="demo-root">
+  <div data-reach-dialog-content class="mock-modal">
+    <h4>Book Search</h4>
+    <hr />
+    <FlexRow>
+      <div class="col-sm-6 col-xs-12">
+        <div class="form-group">
+          <label>Title</label>
+          <input placeholder="Search title" class="form-control" value="The Slefish Gene" />
+          <div class="alert alert-warning margin-bottom">Did you spell this right?</div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-xs-12">
+        <div class="form-group">
+          <label>Pages</label>
+          <div class="form-inline">
+            <div style="margin-right: 5px; display: inline-block" class="form-group">
+              <select class="form-control">
+                <option value="lt">{"<"}</option>
+                <option value="gt">{">"}</option>
+              </select>
+            </div>
+            <div class="form-group" style="display: inline-block">
+              <input style="width: 100px" type="number" placeholder="Pages" class="form-control" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="form-group">
+          <label>Publisher</label>
+          <input placeholder="Publisher" class="form-control" value="Basic Books" />
+          <div class="alert alert-success margin-bottom">Looks good to me!</div>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="form-group">
+          <label>Author</label>
+          <input placeholder="Author" class="form-control" />
+          <div class="alert alert-danger margin-bottom">Author is required!</div>
+        </div>
+      </div>
+    </FlexRow>
+    <hr />
+    <div style="display: flex">
+      <button class="btn btn-primary">Filter</button>
+      <button class="btn" style="margin-left: auto"> Close </button>
+    </div>
+  </div>
+  <div class="alert alert-info margin-top" style="max-width: 550px">
+    This color theme will be saved locally, and remembered the next time you browse here (on the same device)
+  </div>
+</div>
