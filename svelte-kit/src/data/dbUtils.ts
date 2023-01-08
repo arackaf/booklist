15262fe625822bb606ad444d95d4d5c65c5f8a45
Adam Rackis <adamrackis@hotmail.com>
2023-01-08 15:38:17 -0600

    db util cleanup

--- svelte-kit/src/data/dbUtils.ts
+++ svelte-kit/src/data/dbUtils.ts
@@ -1,24 +1,24 @@
 import { env } from "$env/dynamic/private";
 import type { Book, Subject, Tag } from "./types";
 
+type MongoQueryResponse<T> = {
+  documents: T[];
+};
+
 export const updateSingleBook = runSingleUpdate.bind(null, "books");
 export const updateMultipleBooks = runMultiUpdate.bind(null, "books");
 
-const runAggregate = runRequest.bind(null, "aggregate");
-
 export const deleteBookById = deleteById.bind(null, "books");
 
-function deleteById(collection: string, userId: string, _id: string) {
-  userId = userId || "";
+export const queryBooks = <TProjection = Book>(body: object) => runQuery<TProjection>("books", body);
+export const querySubjects = (body: object) => runQuery<Subject>("subjects", body);
+export const queryTags = (body: object) => runQuery<Tag>("tags", body);
 
-  return runRequest("deleteOne", collection, {
-    filter: { userId, _id: { $oid: _id } }
-  })
-    .then(res => res.json())
-    .catch(err => {
-      console.log({ err });
-    });
-}
+const runQuery = <T>(table: string, body: object) => {
+  return runAggregate(table, body).then((res: MongoQueryResponse<T>) => res.documents);
+};
+
+const runAggregate = runRequest.bind(null, "aggregate");
 
 export function runSingleUpdate(collection: string, userId: string, filter: object, update: object) {
   userId = userId || "";
@@ -46,17 +46,17 @@ export function runMultiUpdate(collection: string, userId: string, filter: objec
     });
 }
 
-type MongoQueryResponse<T> = {
-  documents: T[];
-};
+function deleteById(collection: string, userId: string, _id: string) {
+  userId = userId || "";
 
-export const queryBooks = <TProjection = Book>(body: object) => runQuery<TProjection>("books", body);
-export const querySubjects = (body: object) => runQuery<Subject>("subjects", body);
-export const queryTags = (body: object) => runQuery<Tag>("tags", body);
-
-const runQuery = <T>(table: string, body: object) => {
-  return runAggregate(table, body).then((res: MongoQueryResponse<T>) => res.documents);
-};
+  return runRequest("deleteOne", collection, {
+    filter: { userId, _id: { $oid: _id } }
+  })
+    .then(res => res.json())
+    .catch(err => {
+      console.log({ err });
+    });
+}
 
 export function runRequest(action: string, collection: string, body: object) {
   return fetch(`${env.MONGO_URL}/action/${action}`, {
