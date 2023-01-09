fe49825e23341eace888e0698f37557bc1e91077
Adam Rackis <adamrackis@hotmail.com>
2023-01-08 21:29:56 -0600

    subjects page started

--- svelte-kit/src/routes/(logged-in)/subjects/+page.svelte
+++ svelte-kit/src/routes/(logged-in)/subjects/+page.svelte
@@ -1,1 +1,78 @@
-<h1>SUBJECTS PAGE</h1>
+<script lang="ts">
+  import { setContext } from "svelte";
+  import { writable } from "svelte/store";
+
+  import Button from "$lib/components/buttons/Button.svelte";
+  //import EditSubject from "$lib/components/subjectsAndTags/subjects/EditSubject.svelte";
+  import Modal from "$lib/components/ui/Modal.svelte";
+  import { stackAndGetTopLevelSubjects } from "$lib/state/subjectsState";
+  //import { rootSubjects } from "app/state/subjectsState";
+
+  import SubjectDisplay from "./SubjectDisplay.svelte";
+
+  export let data: any;
+  $: ({ subjects } = data);
+
+  $: rootSubjects = stackAndGetTopLevelSubjects(subjects);
+
+  $: {
+    console.log({ data });
+  }
+
+  let editModalOpen = false;
+  let editingSubject = { name: "" };
+  const closeEditModal = () => (editModalOpen = false);
+  const editSubject = (subject: any) => {
+    editingSubject = subject;
+    editModalOpen = true;
+  };
+
+  setContext("subject-chain-disable-animation", writable(false));
+</script>
+
+<section class="flush-bottom subjectsRoot">
+  <div>
+    <Button class="margin-bottom" preset="primary" onClick={() => editSubject({ name: "" })}>New Subject</Button>
+  </div>
+
+  <div class="contentRoot">
+    <ul>
+      {#each rootSubjects as s (s._id)}
+        <SubjectDisplay subject={s} {editSubject} />
+      {/each}
+    </ul>
+  </div>
+
+  <Modal isOpen={editModalOpen} onHide={() => (editModalOpen = false)} headerCaption={"Edit Subject"} standardFooter={false}>
+    <!-- <EditSubject subject={editingSubject} onCancelEdit={closeEditModal} /> -->
+    <hr />
+    <Button onClick={closeEditModal}>Close</Button>
+  </Modal>
+</section>
+
+<style>
+  .subjectsRoot {
+    display: grid;
+    grid-template-rows: auto 1fr;
+  }
+
+  .subjectsRoot :global(ul) {
+    margin-left: 0;
+    padding-left: 0;
+  }
+
+  .subjectsRoot :global(ul ul) {
+    margin-left: 20px;
+  }
+
+  .contentRoot {
+    padding-left: 30px;
+    border-left: 2px solid var(--primary-4);
+  }
+  @media (max-width: 1000px) {
+    .contentRoot {
+      margin-left: 10px;
+      padding-left: 10px;
+    }
+  }
+</style>
---
+++ svelte-kit/src/routes/(logged-in)/subjects/SubjectDisplay.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+  import { getContext, onMount } from "svelte";
+
+  import { spring } from "svelte/motion";
+  import { fade } from "svelte/transition";
+
+  import type { FullSubject, Subject } from "$data/types";
+
+  import { syncHeight } from "$lib/util/animationHelpers";
+  import EditableExpandableLabelDisplay from "$lib/components/subjectsAndTags/EditableLabelDisplay.svelte";
+
+  //import { childMapSelector } from "app/state/subjectsState";
+
+  const disabledAnimationInChain: any = getContext("subject-chain-disable-animation");
+
+  let blockingUpstream: boolean;
+  let contentEl: HTMLElement;
+  let heightStore: any;
+
+  const SPRING_CONFIG_GROWING = { stiffness: 0.1, damping: 0.4, precision: 0.01 };
+  const SPRING_CONFIG_SHRINKING = { stiffness: 0.3, damping: 0.9, precision: 0.1 };
+  const subjectSpring = spring({ height: 0, opacity: 1, x: 0, y: 0 }, SPRING_CONFIG_GROWING);
+
+  onMount(() => {
+    heightStore = syncHeight(contentEl);
+  });
+
+  $: setSpring($heightStore, expanded);
+
+  let initialRender = false;
+  function setSpring(height: number, expanded: boolean) {
+    if (blockingUpstream) {
+      $disabledAnimationInChain = true;
+    }
+
+    const newHeight = expanded ? height : 0;
+    const existingHeight = $subjectSpring.height;
+    let animation = subjectSpring
+      .set(
+        { height: newHeight, opacity: expanded ? 1 : 0, x: expanded ? 0 : 20, y: expanded ? 0 : -20 },
+        { hard: !initialRender || ($disabledAnimationInChain && !blockingUpstream) }
+      )
+      .then(() => (initialRender = true));
+    Object.assign(subjectSpring, newHeight > existingHeight ? SPRING_CONFIG_GROWING : SPRING_CONFIG_SHRINKING);
+    if (blockingUpstream) {
+      Promise.resolve(animation).then(() => {
+        $disabledAnimationInChain = false;
+        blockingUpstream = false;
+      });
+    }
+  }
+
+  let expanded = true;
+  let setExpanded = (val: boolean) => {
+    blockingUpstream = true;
+    expanded = val;
+  };
+  export let editSubject = (subject: any) => {};
+  export let subject: FullSubject;
+
+  $: childSubjects = subject.children;
+  $: ({ height, opacity, x, y } = $subjectSpring);
+
+  //
+</script>
+
+<li style="padding-top: 0; padding-bottom: 0">
+  <div>
+    <div out:fade|local={{ duration: 300 }} class="padding-bottom-med subjectRow">
+      <EditableExpandableLabelDisplay {childSubjects} {expanded} {setExpanded} onEdit={() => editSubject(subject)} item={subject} />
+    </div>
+    <div style="height: {height}px; overflow: hidden;">
+      <div bind:this={contentEl} style="opacity: {opacity}; transform: translate3d({x}px, {y}px, 0)">
+        {#if childSubjects.length}
+          <ul>
+            {#each childSubjects as s (s._id)}
+              <svelte:self subject={s} {editSubject} />
+            {/each}
+          </ul>
+        {/if}
+      </div>
+    </div>
+  </div>
+</li>
+
+<style>
+  .subjectRow {
+    padding-left: 0;
+    margin-left: 0;
+  }
+
+  .subjectRow > :global(div) {
+    padding-left: 0;
+  }
+</style>
