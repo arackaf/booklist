2aab66d2e4b21e783a759893e5571774654e5490
Adam Rackis <adamrackis@hotmail.com>
2023-01-09 23:08:36 -0600

    Simplify books endpoint caching

--- svelte-kit/src/routes/api/books/+server.ts
+++ svelte-kit/src/routes/api/books/+server.ts
@@ -3,20 +3,16 @@ import type { BookSearch } from "$data/types";
 import { BOOKS_CACHE } from "$lib/state/cacheHelpers";
 import { json } from "@sveltejs/kit";
 
-export async function GET({ url, request, setHeaders, locals }: { url: URL; cookies: any; request: any; setHeaders: any; locals: any }) {
+export async function GET({ url, setHeaders, locals }: { url: URL; cookies: any; request: any; setHeaders: any; locals: any }) {
   const session = await locals.getSession();
   if (!session) {
     return json({});
   }
 
-  const currentCacheBust = request.headers.get(BOOKS_CACHE);
-
-  if (currentCacheBust) {
-    setHeaders({
-      "cache-control": "max-age=60",
-      Vary: BOOKS_CACHE
-    });
-  }
+  setHeaders({
+    "cache-control": "max-age=60",
+    Vary: BOOKS_CACHE
+  });
 
   const search = url.searchParams.get("search") || "";
   const sortString = url.searchParams.get("sort") || "";
