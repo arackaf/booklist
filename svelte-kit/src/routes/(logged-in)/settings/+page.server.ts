3724cfda9bf5fd643d837cf6e3045e2c5d345781
Adam Rackis <adamrackis@hotmail.com>
2023-01-14 15:13:52 -0600

    Revert "test subject save from other path"
    This reverts commit 50b241a5dc9780324ca5ef211a4792be1f5a37b7.

--- svelte-kit/src/routes/(logged-in)/settings/+page.server.ts
+++ svelte-kit/src/routes/(logged-in)/settings/+page.server.ts
@@ -1,11 +1,5 @@
 import { redirect } from "@sveltejs/kit";
 
-export const actions = {
-  async saveSubject() {
-    console.log("Save subject from settings");
-  }
-};
-
 export function load() {
   throw redirect(308, "/settings/account-sync");
 }
