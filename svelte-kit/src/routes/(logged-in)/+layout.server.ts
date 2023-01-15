6ddbdd203972e94fdff8842ec12b51c39bba9ccf
Adam Rackis <adamrackis@hotmail.com>
2023-01-14 19:41:36 -0600

    End Dynamo tags experiment

--- svelte-kit/src/routes/(logged-in)/+layout.server.ts
+++ svelte-kit/src/routes/(logged-in)/+layout.server.ts
@@ -3,21 +3,6 @@ import { redirect } from "@sveltejs/kit";
 import { allSubjects } from "$data/subjects";
 import { allTags } from "$data/tags";
 import { BOOKS_CACHE, updateCacheCookie } from "$lib/state/cacheHelpers";
-import { db, getQueryPacket } from "$data/dynamoHelpers";
-
-async function getDynamoTags(consistent: boolean = false) {
-  const start = +new Date();
-  const res = await db.query(
-    getQueryPacket(`pk = :pk`, {
-      ExpressionAttributeValues: { ":pk": "tag#1" },
-      ...(consistent ? { ConsistentRead: true } : {})
-    })
-  );
-  const end = +new Date();
-  console.log("Dynamo tags time", { consistent }, end - start);
-  // console.log({ dynamoTags: res });
-  return res;
-}
 
 export async function load({ cookies, locals, depends, isDataRequest }: any) {
   const initialRequest = !isDataRequest;
@@ -42,8 +27,6 @@ export async function load({ cookies, locals, depends, isDataRequest }: any) {
   return {
     booksCache,
     subjects,
-    tags,
-    dynamoTags: getDynamoTags(),
-    dynamoTags2: getDynamoTags(true)
+    tags
   };
 }
