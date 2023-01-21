b1d0fae4d146abc756d96e6d984ac9bee7354195
Adam Rackis <adamrackis@hotmail.com>
2023-01-21 15:43:13 -0600

    Remove unneeded code

--- svelte-kit/src/routes/(logged-in)/books/SearchModal.svelte
+++ svelte-kit/src/routes/(logged-in)/books/SearchModal.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { page } from "$app/stores";
   import Button from "$lib/components/buttons/Button.svelte";
 
   import Modal from "$lib/components/ui/Modal.svelte";
@@ -18,26 +17,14 @@
 
   import type { Subject, Tag } from "$data/types";
 
-  // import { currentSearch } from "./booksSearchState";
-  // import { applyFilters } from "./setBookFilters";
-  //import StandardModalFooter from "app/components/ui/StandardModalFooter.svelte";
-
   export let isOpen = false;
   export let onHide = () => {};
 
   export let allTags: Tag[];
   export let allSubjects: Subject[];
 
-  //let searchEl: any;
-  let isReadE: any;
-  let isRead0: any;
-  let childSubEl: any;
-  let authorEl: any;
-  let publisherEl: any;
-  let sortSelectEl: any;
-
-  let subjects = [] as any[]; //$currentSearch.subjectIds;
-  let tags = [] as any[]; //$currentSearch.tagIds;
+  let subjects = [] as any[];
+  let tags = [] as any[];
 
   $: {
     if (isOpen) {
@@ -49,7 +36,7 @@
     tags = $searchState.tags;
   }
 
-  let noSubjectsFilter = false; // = !!$currentSearch.noSubjects;
+  let noSubjectsFilter = false;
   const setNoSubjectsFilter = (evt: any) => {
     noSubjectsFilter = evt.target.checked;
   };
@@ -58,33 +45,6 @@
   const selectTag = (tag: any) => (tags = tags.concat(tag._id));
   const removeSubject = (subject: any) => (subjects = subjects.filter(_id => _id != subject._id));
   const removeTag = (tag: any) => (tags = tags.filter(_id => _id != tag._id));
-
-  const updateFilters = (evt: any) => {
-    evt.preventDefault();
-
-    let sort = "";
-    let sortDirection = "";
-    let sortValue = sortSelectEl.value;
-    if (sortValue !== "_id|desc") {
-      [sort, sortDirection] = sortValue.split("|");
-    }
-
-    const applyFilters: any = () => {};
-    // applyFilters({
-    //   subjects: noSubjectsFilter ? [] : subjects,
-    //   tags,
-    //   search: searchEl.value,
-    //   pages: pagesEl.value,
-    //   pagesOperator: pagesDirEl.value,
-    //   author: authorEl.value,
-    //   publisher: publisherEl.value,
-    //   isRead: isReadE.checked ? "" : isRead0.checked ? "0" : "1",
-    //   searchChildSubjects: childSubEl && childSubEl.checked,
-    //   noSubjects: noSubjectsFilter,
-    //   sort,
-    //   sortDirection
-    // });
-  };
 
   const onFormData = (evt: any) => {
     const searchParams: URLSearchParams = evt.formData;
@@ -127,7 +87,7 @@
           <label for="__" class="form-label">Is Read?</label>
           <FlowItems class="radio" style="display: flex; flex: 1; align-items: center;">
             <FlowItems tightest={true} vCenter={true}>
-              <input type="radio" checked={$searchState.isRead === ""} bind:this={isReadE} name="isRead" id="isReadE" value="off" />
+              <input type="radio" checked={$searchState.isRead === ""} name="isRead" id="isReadE" value="off" />
               <label for="isReadE">Either</label>
             </FlowItems>
             <FlowItems tightest={true} vCenter={true}>
@@ -135,7 +95,7 @@
               <label for="isReadY">Yes</label>
             </FlowItems>
             <FlowItems tightest={true} vCenter={true}>
-              <input type="radio" checked={$searchState.isRead === "false"} bind:this={isRead0} name="isRead" id="isReadN" value="false" />
+              <input type="radio" checked={$searchState.isRead === "false"} name="isRead" id="isReadN" value="false" />
               <label for="isReadN">No</label>
             </FlowItems>
           </FlowItems>
@@ -173,7 +133,7 @@
 
         <div class="col-xs-12">
           <label class="checkbox">
-            <input type="checkbox" name="child-subjects" value="true" bind:this={childSubEl} checked={false} />
+            <input type="checkbox" name="child-subjects" value="true" checked={false} />
             Also search child subjects
           </label>
         </div>
