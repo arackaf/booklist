629350ddf69f681413112a49a6a719aca5bd6bc6
Adam Rackis <adamrackis@hotmail.com>
2023-01-29 13:02:42 -0600

    Oops remove books layout group don't need it after all

--- svelte-kit/src/routes/(logged-in)/(view-books)/books/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import type { PageData } from "./$types";
-  import RenderBooksPage from "../RenderBooksPage.svelte";
-
-  export let data: PageData;
-</script>
-
-<RenderBooksPage {data} />
