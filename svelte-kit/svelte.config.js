364888976c949f667016da49b056e7a8345c7f8b
Adam Rackis <adamrackis@hotmail.com>
2023-01-31 11:11:22 -0600

    Revert "test Vercel adapter external field"
    This reverts commit 530409ea63e8096fb53eefd401ad86132147d4bd.

--- svelte-kit/svelte.config.js
+++ svelte-kit/svelte.config.js
@@ -3,12 +3,13 @@ import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess for more information about preprocessors
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
   preprocess: preprocess(),
 
   kit: {
     adapter: adapter({
-      external: ["@aws-sdk/client-lambda"]
+      external: []
     }),
     alias: {
       $styles: "src/styles",
