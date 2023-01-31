66fb2c3ad37b9bcb31af14bc03575c57d091894a
Adam Rackis <adamrackis@hotmail.com>
2023-01-31 10:35:00 -0600

    Explicitly add vercel adapter

--- svelte-kit/svelte.config.js
+++ svelte-kit/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,9 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      external: []
+    }),
     alias: {
       $styles: "src/styles",
       $lib: "src/lib",
