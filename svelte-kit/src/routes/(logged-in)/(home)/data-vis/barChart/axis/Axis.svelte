3e5b411f813d7c01d491ce5f268f1e22437a08aa
Adam Rackis <adamrackis@hotmail.com>
2023-02-01 14:11:19 -0600

    Fix truncating axis

--- svelte-kit/src/routes/(logged-in)/(home)/data-vis/barChart/axis/Axis.svelte
+++ svelte-kit/src/routes/(logged-in)/(home)/data-vis/barChart/axis/Axis.svelte
@@ -15,7 +15,7 @@
 
 <g transform={`translate(${$axisSpring.masterTransformX}, ${$axisSpring.masterTransformY})`}>
   <g font-size="10" {transform}>
-    <path fill="none" stroke="black" d="M0.5,6 V0.5 H{$axisSpring.width + 0.5} V 6" />
+    <path fill="none" stroke="black" d="M0.5,6 V0.5 H{$axisSpring.width - 1} V 6" />
     {#each data as d (d)}
       <Tick {scaleX} {d} />
     {/each}
