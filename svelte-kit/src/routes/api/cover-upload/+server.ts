32e8a1bee3a25c6a3bfd345b31f1d168416c26cc
Adam Rackis <adamrackis@hotmail.com>
2023-02-02 13:34:04 -0600

    Revert "Remove other aws services to check bundle size"
    This reverts commit 92f07dc18fd1e90ea563005a0899309a07e2ca12.

--- svelte-kit/src/routes/api/cover-upload/+server.ts
+++ svelte-kit/src/routes/api/cover-upload/+server.ts
@@ -1,5 +1,9 @@
 import { v4 } from "uuid";
 
+import { toUtf8, fromUtf8 } from "@aws-sdk/util-utf8";
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+
 import { json } from "@sveltejs/kit";
 
 import { AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, UPLOAD_FROM_URL_LAMBDA } from "$env/static/private";
@@ -27,7 +31,63 @@ export async function POST({ cookies, locals, request }: any) {
   const uploadKey = `upload-staging/${v4()}${extension}`;
 
   try {
-    return json({});
+    const client = new LambdaClient({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: AMAZON_ACCESS_KEY,
+        secretAccessKey: AMAZON_SECRET_KEY
+      }
+    });
+
+    const s3 = new S3Client({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: AMAZON_ACCESS_KEY,
+        secretAccessKey: AMAZON_SECRET_KEY
+      }
+    });
+
+    const arrayBuffer = await file.arrayBuffer();
+    const fileBuffer = Buffer.from(arrayBuffer);
+
+    await s3
+      .send(
+        new PutObjectCommand({
+          Bucket: "my-library-cover-uploads",
+          Key: uploadKey,
+          Body: fileBuffer
+        })
+      )
+      .catch(err => {
+        console.log({ err });
+      });
+
+    const url = `https://s3.amazonaws.com/my-library-cover-uploads/${uploadKey}`;
+
+    const command = new InvokeCommand({
+      FunctionName: UPLOAD_FROM_URL_LAMBDA,
+      Payload: fromUtf8(JSON.stringify({ url, userId: session.userId }))
+    });
+    const response = await client.send(command);
+
+    await s3
+      .send(
+        new DeleteObjectCommand({
+          Bucket: "my-library-cover-uploads",
+          Key: uploadKey
+        })
+      )
+      .catch(err => {
+        console.log({ err });
+      });
+
+    if (response.Payload) {
+      const respJson = JSON.parse(toUtf8(response.Payload));
+
+      return json(respJson);
+    } else {
+      return json({ error: true });
+    }
   } catch (er) {
     console.log("Error invoking lambda", er);
   }
--- svelte-kit/src/routes/api/get-recommendations/+server.ts
+++ svelte-kit/src/routes/api/get-recommendations/+server.ts
@@ -1,5 +1,8 @@
 import { json } from "@sveltejs/kit";
 
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import { toUtf8, fromUtf8 } from "@aws-sdk/util-utf8";
+
 import { AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, GET_RECOMMENDATIONS_LAMBDA } from "$env/static/private";
 
 export async function POST({ locals, request }: any) {
@@ -13,7 +16,26 @@ export async function POST({ locals, request }: any) {
   const { bookIds } = reqBody;
 
   try {
-    return json({});
+    const client = new LambdaClient({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: AMAZON_ACCESS_KEY,
+        secretAccessKey: AMAZON_SECRET_KEY
+      }
+    });
+    const command = new InvokeCommand({
+      FunctionName: GET_RECOMMENDATIONS_LAMBDA,
+      Payload: fromUtf8(JSON.stringify({ userId, bookIds }))
+    });
+    const response = await client.send(command);
+
+    if (response.Payload) {
+      const respJson = JSON.parse(toUtf8(response.Payload));
+
+      return json(respJson);
+    } else {
+      return json({ error: true });
+    }
   } catch (er) {
     console.log("Error invoking lambda", er);
     return json({ error: true });
