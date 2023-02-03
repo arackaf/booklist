65d5ae0278347e96380f60db1f5ed80a1c1c2f27
Adam Rackis <adamrackis@hotmail.com>
2023-02-03 16:25:57 -0600

    Remove unneeded logging

--- lambda/book-scan/handlers/getPendingScanCount.ts
+++ lambda/book-scan/handlers/getPendingScanCount.ts
@@ -3,10 +3,8 @@ import { getPendingCount } from "../util/data-helpers";
 export const handler = async event => {
   try {
     const { userId } = event;
-    console.log("CHECKING", userId);
     const pendingCount = await getPendingCount(userId);
 
-    console.log("SENDING", { pendingCount });
     return { pendingCount };
   } catch (er) {
     console.log("ERROR", er);
