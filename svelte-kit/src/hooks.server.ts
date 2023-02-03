effd022f1f37df865cf5847136984eb244137874
Adam Rackis <adamrackis@hotmail.com>
2023-02-02 20:43:56 -0600

    Upgrade dynamo adapter and re-add

--- svelte-kit/src/hooks.server.ts
+++ svelte-kit/src/hooks.server.ts
@@ -1,7 +1,28 @@
 import { sequence } from "@sveltejs/kit/hooks";
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GoogleProvider from "@auth/core/providers/google";
-import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET, NEXTAUTH_SECRET } from "$env/static/private";
+import { GOOGLE_AUTH_CLIENT_ID, GOOGLE_AUTH_SECRET, AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, DYNAMO_AUTH_TABLE } from "$env/static/private";
+
+import { DynamoDB, type DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter";
+
+const dynamoConfig: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: AMAZON_ACCESS_KEY,
+    secretAccessKey: AMAZON_SECRET_KEY
+  },
+
+  region: "us-east-1"
+};
+
+const client = DynamoDBDocument.from(new DynamoDB(dynamoConfig), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true
+  }
+});
 
 import { getUserSync } from "$data/legacyUser";
 // force deploy
@@ -20,6 +41,8 @@ const auth = SvelteKitAuth({
 
   trustHost: true,
   secret: "secret-xxx",
+
+  adapter: DynamoDBAdapter(client, { tableName: DYNAMO_AUTH_TABLE }) as any,
 
   callbacks: {
     async signIn({ account }) {
