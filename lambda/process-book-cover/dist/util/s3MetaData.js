4891fff3efb7798ed4dd69bc6f56585a7659c73e
Adam Rackis <adamrackis@hotmail.com>
2023-02-04 15:26:30 -0600

    Ignore dist folder

--- lambda/process-book-cover/dist/util/s3MetaData.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultMetaData = void 0;
-exports.defaultMetaData = {
-    CacheControl: "max-age=630720000,public",
-    ContentType: "image/jpeg"
-};
