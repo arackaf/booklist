16dd7d1281aeabc89fb24b58c7ba76109e2c2822
Adam Rackis <adamrackis@hotmail.com>
2023-02-03 18:32:11 -0600

    Update S3 to v3

--- lambda/util/uploadToS3.ts
+++ lambda/util/uploadToS3.ts
@@ -3,16 +3,19 @@ import { defaultMetaData } from "./s3MetaData";
 
 type S3UploadResult = { STATUS: "error"; message: any } | { STATUS: "success"; url: string };
 
+const Bucket = "my-library-cover-uploads";
+
 export default (fileName, body) => {
   const s3 = new S3Client({ region: "us-east-1" });
-  var params = new PutObjectCommand({ Bucket: "my-library-cover-uploads", Key: `${fileName}`, Body: body, ...defaultMetaData });
+  const key = fileName;
+  var params = new PutObjectCommand({ Bucket, Key: key, Body: body, ...defaultMetaData });
 
   return new Promise<S3UploadResult>(res => {
     s3.send(params, function (err, data) {
       if (err) {
         return res({ STATUS: "error", message: err });
       }
-      res({ STATUS: "success", url: `https://${params.Bucket}.s3.amazonaws.com/${params.Key}` });
+      res({ STATUS: "success", url: `https://${Bucket}.s3.amazonaws.com/${key}` });
     });
   });
 };
