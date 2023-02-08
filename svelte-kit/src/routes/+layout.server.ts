2394238039d095a6a0de313dc2939034ace2e678
Adam Rackis <adamrackis@hotmail.com>
2023-02-08 14:59:08 -0600

    Revert "test mysql bundle size"
    This reverts commit 05e38a2136737883ea206a48f79add3b58717674.

--- svelte-kit/src/routes/+layout.server.ts
+++ svelte-kit/src/routes/+layout.server.ts
@@ -1,12 +1,8 @@
 import { BOOKS_CACHE, updateCacheCookie } from "$lib/state/cacheHelpers";
 import { getUxState } from "$lib/util/uxState";
 
-import mysql from "mysql2";
-
 export async function load({ locals, isDataRequest, request, cookies, depends }: any) {
   depends("app:root");
-
-  console.log("typeof mysql", typeof mysql, typeof mysql?.createConnection);
 
   const userAgent = request.headers.get("User-Agent");
   const isMobile = /mobile/i.test(userAgent);
