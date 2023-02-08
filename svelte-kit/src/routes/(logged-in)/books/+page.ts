8aa115725262d22c2b5c81b79d18c98d7c5e9ff1
Adam Rackis <adamrackis@hotmail.com>
2023-02-08 13:24:51 -0600

    Go back to blocking books page on parent loader

--- svelte-kit/src/routes/(logged-in)/books/+page.ts
+++ svelte-kit/src/routes/(logged-in)/books/+page.ts
@@ -2,16 +2,15 @@ import { BOOKS_CACHE, getCurrentCookieValue } from "$lib/state/cacheHelpers";
 import { writable } from "svelte/store";
 import { BASIC_LIST_VIEW, GRID_VIEW } from "./bookViews/constants";
 
-export async function load({ url, parent, fetch, depends }: any) {
+export async function load({ url, parent, fetch, depends, ...rest }: any) {
   depends("reload:books");
 
-  const isClient = typeof document === "object";
-  const cache = isClient ? getCurrentCookieValue(BOOKS_CACHE) : global.initialBooksCache;
+  const parentData = await parent();
+  const cache = getCurrentCookieValue(BOOKS_CACHE) || parentData.booksCache;
 
   const resp = await fetch(`/api/books?${url.searchParams.toString()}&cache=${cache}`);
   const { books, totalBooks, page, totalPages } = await resp.json();
 
-  const parentData = await parent();
   const { uxState, showMobile } = parentData;
 
   const defaultBookView = uxState.bkVw ?? (showMobile ? BASIC_LIST_VIEW : GRID_VIEW);
