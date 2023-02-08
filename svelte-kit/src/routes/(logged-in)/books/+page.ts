af10c2170a1f35ac40a4ae1ebde2084a317bc6e2
Adam Rackis <adamrackis@hotmail.com>
2023-02-07 21:45:26 -0600

    Run books fetch without awaiting parent data

--- svelte-kit/src/routes/(logged-in)/books/+page.ts
+++ svelte-kit/src/routes/(logged-in)/books/+page.ts
@@ -2,16 +2,22 @@ import { BOOKS_CACHE, getCurrentCookieValue } from "$lib/state/cacheHelpers";
 import { writable } from "svelte/store";
 import { BASIC_LIST_VIEW, GRID_VIEW } from "./bookViews/constants";
 
-export async function load({ url, parent, fetch, depends }: any) {
+let loaderFirstRun = true;
+
+export async function load({ url, parent, fetch, depends, ...rest }: any) {
   depends("reload:books");
 
-  const parentData = await parent();
+  const isServer = typeof document === "undefined";
+  const currentCookieValue = getCurrentCookieValue(BOOKS_CACHE);
 
-  const cache = getCurrentCookieValue(BOOKS_CACHE) || parentData.booksCache;
+  const cache = isServer || loaderFirstRun ? "" : currentCookieValue;
+
+  loaderFirstRun = false;
 
   const resp = await fetch(`/api/books?${url.searchParams.toString()}&cache=${cache}`);
   const { books, totalBooks, page, totalPages } = await resp.json();
 
+  const parentData = await parent();
   const { uxState, showMobile } = parentData;
 
   const defaultBookView = uxState.bkVw ?? (showMobile ? BASIC_LIST_VIEW : GRID_VIEW);
