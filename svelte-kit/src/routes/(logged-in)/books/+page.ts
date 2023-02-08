25415aa26b93c4639297683a641ac9d54ff51db3
Adam Rackis <adamrackis@hotmail.com>
2023-02-07 22:05:36 -0600

    Revert "Run books fetch without awaiting parent data"
    This reverts commit af10c2170a1f35ac40a4ae1ebde2084a317bc6e2.

--- svelte-kit/src/routes/(logged-in)/books/+page.ts
+++ svelte-kit/src/routes/(logged-in)/books/+page.ts
@@ -2,22 +2,16 @@ import { BOOKS_CACHE, getCurrentCookieValue } from "$lib/state/cacheHelpers";
 import { writable } from "svelte/store";
 import { BASIC_LIST_VIEW, GRID_VIEW } from "./bookViews/constants";
 
-let loaderFirstRun = true;
-
-export async function load({ url, parent, fetch, depends, ...rest }: any) {
+export async function load({ url, parent, fetch, depends }: any) {
   depends("reload:books");
 
-  const isServer = typeof document === "undefined";
-  const currentCookieValue = getCurrentCookieValue(BOOKS_CACHE);
+  const parentData = await parent();
 
-  const cache = isServer || loaderFirstRun ? "" : currentCookieValue;
-
-  loaderFirstRun = false;
+  const cache = getCurrentCookieValue(BOOKS_CACHE) || parentData.booksCache;
 
   const resp = await fetch(`/api/books?${url.searchParams.toString()}&cache=${cache}`);
   const { books, totalBooks, page, totalPages } = await resp.json();
 
-  const parentData = await parent();
   const { uxState, showMobile } = parentData;
 
   const defaultBookView = uxState.bkVw ?? (showMobile ? BASIC_LIST_VIEW : GRID_VIEW);
