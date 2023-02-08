05e38a2136737883ea206a48f79add3b58717674
Adam Rackis <adamrackis@hotmail.com>
2023-02-08 14:18:39 -0600

    test mysql bundle size

--- svelte-kit/src/routes/+layout.server.ts
+++ svelte-kit/src/routes/+layout.server.ts
@@ -1,8 +1,12 @@
 import { BOOKS_CACHE, updateCacheCookie } from "$lib/state/cacheHelpers";
 import { getUxState } from "$lib/util/uxState";
 
+import mysql from "mysql2";
+
 export async function load({ locals, isDataRequest, request, cookies, depends }: any) {
   depends("app:root");
+
+  console.log("typeof mysql", typeof mysql, typeof mysql?.createConnection);
 
   const userAgent = request.headers.get("User-Agent");
   const isMobile = /mobile/i.test(userAgent);
