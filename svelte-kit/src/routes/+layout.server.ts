0e5cdfa43709f06e1b3bca2dbf68452a52f7ee17
Adam Rackis <adamrackis@hotmail.com>
2023-02-08 15:01:42 -0600

    test direct mongo connection

--- svelte-kit/src/routes/+layout.server.ts
+++ svelte-kit/src/routes/+layout.server.ts
@@ -1,8 +1,32 @@
+import { MONGO_CONNECTION } from "$env/static/private";
 import { BOOKS_CACHE, updateCacheCookie } from "$lib/state/cacheHelpers";
 import { getUxState } from "$lib/util/uxState";
 
+import { MongoClient } from "mongodb";
+
+async function connect() {
+  const start = +new Date();
+  const client = new MongoClient(MONGO_CONNECTION);
+  try {
+    await client.connect();
+    const end = +new Date();
+    console.log("Time", end - start);
+
+    await client.db("books").command({ ping: 1 });
+
+    console.log("Success");
+  } catch (err) {
+  } finally {
+    await client.close();
+  }
+}
+
 export async function load({ locals, isDataRequest, request, cookies, depends }: any) {
   depends("app:root");
+
+  try {
+    await connect();
+  } catch (er) {}
 
   const userAgent = request.headers.get("User-Agent");
   const isMobile = /mobile/i.test(userAgent);
