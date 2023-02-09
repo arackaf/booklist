744db51f92f66afc4bb707ca8739695b495240e6
Adam Rackis <adamrackis@hotmail.com>
2023-02-09 16:42:05 -0600

    Add outer layout for global markup. Add public landing page

--- svelte-kit/src/routes/(core-data)/+layout.svelte
+++ svelte-kit/src/routes/(core-data)/+layout.svelte
@@ -8,47 +8,13 @@
   import Toastify from "toastify-js";
   import "toastify-js/src/toastify.css";
 
-  import { beforeNavigate, afterNavigate } from "$app/navigation";
-
-  import MainNavigation from "$lib/components/navigation/MainNavigation.svelte";
-  import Footer from "$lib/components/Footer.svelte";
-  import Loading from "$lib/components/ui/Loading.svelte";
-
-  import { NUM_THEMES } from "$lib/util/constants";
   import { checkPendingCount, dispatchScanDataUpdate, getScanWebSocket } from "$lib/util/scanUtils";
-  import PublicLandingPage from "./PublicLandingPage.svelte";
   import type { Book } from "$data/types";
   import ScanToasterContent from "$lib/components/ui/ScanToasterContent.svelte";
 
   export let data: any;
 
-  $: ({ showMobile, uxState, loggedIn, hasPublicId, userId } = data);
-  $: ({ theme, wbg: whiteBg } = uxState);
-
-  $: {
-    if (typeof document === "object") {
-      document.body.classList.remove("white-bg");
-      for (let i = 0; i < NUM_THEMES; i++) {
-        document.body.classList.remove(`scheme${i}`);
-      }
-
-      if (whiteBg === "1") {
-        document.body.classList.add("white-bg");
-      }
-      document.body.classList.add(theme);
-    }
-  }
-
-  $: showContent = loggedIn || hasPublicId;
-
-  let navigating = false;
-  beforeNavigate(({ type }) => {
-    navigating = true;
-  });
-
-  afterNavigate(({ type }) => {
-    navigating = false;
-  });
+  $: ({ loggedIn, userId } = data);
 
   function startWebSocket() {
     const scanWebSocket = getScanWebSocket();
@@ -104,34 +70,4 @@
   });
 </script>
 
-<div class={`app-container ${theme}`} class:white-bg={whiteBg === "1"}>
-  <div id="app">
-    <MainNavigation />
-    <main>
-      {#if navigating}
-        <Loading />
-      {/if}
-
-      {#if showContent}
-        <slot />
-      {:else}
-        <PublicLandingPage />
-      {/if}
-
-      {#if showMobile}
-        <Footer />
-      {/if}
-    </main>
-    {#if !showMobile}
-      <Footer />
-    {/if}
-  </div>
-</div>
-
-<style>
-  .app-container {
-    overflow: hidden;
-    height: 100vh;
-    width: 100vw;
-  }
-</style>
+<slot />
---
+++ svelte-kit/src/routes/+layout.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import "$styles/fontawesome/css/all.min.css";
+  import "$styles/site-styles.scss";
+
+  import "toastify-js/src/toastify.css";
+
+  import { beforeNavigate, afterNavigate } from "$app/navigation";
+
+  import MainNavigation from "$lib/components/navigation/MainNavigation.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import Loading from "$lib/components/ui/Loading.svelte";
+
+  import { NUM_THEMES } from "$lib/util/constants";
+
+  export let data: any;
+
+  $: ({ showMobile, uxState } = data);
+  $: ({ theme, wbg: whiteBg } = uxState);
+
+  $: {
+    if (typeof document === "object") {
+      document.body.classList.remove("white-bg");
+      for (let i = 0; i < NUM_THEMES; i++) {
+        document.body.classList.remove(`scheme${i}`);
+      }
+
+      if (whiteBg === "1") {
+        document.body.classList.add("white-bg");
+      }
+      document.body.classList.add(theme);
+    }
+  }
+
+  let navigating = false;
+  beforeNavigate(({ type }) => {
+    navigating = true;
+  });
+
+  afterNavigate(({ type }) => {
+    navigating = false;
+  });
+</script>
+
+<div class={`app-container ${theme}`} class:white-bg={whiteBg === "1"}>
+  <div id="app">
+    <MainNavigation />
+    <main>
+      {#if navigating}
+        <Loading />
+      {/if}
+
+      <slot />
+
+      {#if showMobile}
+        <Footer />
+      {/if}
+    </main>
+    {#if !showMobile}
+      <Footer />
+    {/if}
+  </div>
+</div>
+
+<style>
+  .app-container {
+    overflow: hidden;
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
