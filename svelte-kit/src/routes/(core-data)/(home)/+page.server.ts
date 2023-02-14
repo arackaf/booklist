233486cd65742947f5b3591e5f9195a144070f66
Adam Rackis <adamrackis@hotmail.com>
2023-02-13 19:46:40 -0600

    Data vis usable with publicId

--- svelte-kit/src/routes/(core-data)/(home)/+page.server.ts
+++ svelte-kit/src/routes/(core-data)/(home)/+page.server.ts
@@ -1,12 +1,14 @@
 import { booksSubjectsDump } from "$data/books";
 import { ensureAnyUser } from "$lib/util/authCheck";
+import { getPublicId } from "$lib/util/getPublicId";
 
 export async function load({ locals, request }: any) {
   await ensureAnyUser({ locals, request });
 
   const session = await locals.getSession();
+  const parentId = getPublicId(request);
 
   return {
-    books: booksSubjectsDump(session.userId)
+    books: booksSubjectsDump(session?.userId || parentId)
   };
 }
