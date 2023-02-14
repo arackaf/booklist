88837107fae4768fabc94190378a038048220455
Adam Rackis <adamrackis@hotmail.com>
2023-02-13 20:15:00 -0600

    Last redirects

--- svelte-kit/src/routes/(core-data)/(home)/recent-scans/+page.ts
+++ svelte-kit/src/routes/(core-data)/(home)/recent-scans/+page.ts
@@ -3,6 +3,7 @@ import { writable } from "svelte/store";
 
 export async function load({ url, fetch, parent }: any) {
   await ensureLoggedIn({ parent });
+
   const resp = await fetch(`/api/recent-scans?${url.searchParams.toString()}`);
   const packet = await resp.json();
 
