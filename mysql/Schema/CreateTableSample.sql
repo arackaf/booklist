ef960c6a99cbd7f2080f140d4a45331a63116058
Adam Rackis <adamrackis@hotmail.com>
2023-02-22 17:55:48 -0600

    books table creation script

--- mysql/Schema/CreateTableSample.sql
+++ mysql/Schema/CreateTableSample.sql
@@ -1,6 +1,17 @@
-DROP TABLE junk;
+DROP TABLE books;
 
-CREATE TABLE IF NOT EXISTS junk (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name varchar(255)
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    isbn VARCHAR(15),
+    pages INT,
+    mobileImage VARCHAR(100),
+    mobileImagePreview JSON,
+    smallImage VARCHAR(100),
+    smallImagePreview JSON,
+    mediumImage VARCHAR(100),
+    mediumImagePreview JSON,
+    publicationDate VARCHAR(30),
+    publisher VARCHAR(50),
+    userId VARCHAR(25)
 )
