c78ff4c40fa3b0ec4d3849dcbd426c1a208d84f6
Adam Rackis <adamrackis@hotmail.com>
2023-02-22 19:29:47 -0600

    data import work

--- mysql/data-import/import.js
+++ mysql/data-import/import.js
@@ -1,4 +1,22 @@
+const { MongoClient } = require("mongodb");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
-console.log(process.env);
+const userIdToSkip = process.env.REPLICANT_USER;
+
+async function run() {
+  const client = await MongoClient.connect(process.env.MONGO_CONNECTION_LIVE);
+  const db = await client.db(process.env.DB_NAME);
+
+  try {
+    const books = await db.collection("books").find({}).toArray();
+    console.log({ books });
+  } catch (er) {
+    console.log(er);
+  } finally {
+    client.close();
+  }
+}
+
+run();
