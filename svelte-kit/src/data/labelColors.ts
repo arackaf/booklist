1ee084e4930b63a5a8b239de2fb7baa4e004f089
Adam Rackis <adamrackis@hotmail.com>
2023-02-24 18:44:39 -0600

    Make label colors static

--- svelte-kit/src/data/labelColors.ts
+++ svelte-kit/src/data/labelColors.ts
@@ -1,13 +1,154 @@
-import { queryLabelColors } from "./dbUtils";
-
-export const allLabelColors = async () => {
-  const start = +new Date();
-  return queryLabelColors({
-    pipeline: [{ $sort: { order: 1 } }]
-  }).then(results => {
-    const end = +new Date();
-    console.log("HTTP colors time", end - start);
-
-    return results;
-  });
-};
+export const labelColors = [
+  {
+    backgroundColor: "#ff0000",
+    order: 10
+  },
+  {
+    backgroundColor: "#cc3300",
+    order: 20
+  },
+  {
+    backgroundColor: "#800000",
+    order: 50
+  },
+  {
+    backgroundColor: "#ff6600",
+    order: 60
+  },
+  {
+    backgroundColor: "#ff9900",
+    order: 70
+  },
+  {
+    backgroundColor: "#ff9966",
+    order: 80
+  },
+  {
+    backgroundColor: "#ff6666",
+    order: 90
+  },
+  {
+    backgroundColor: "#ffcc00",
+    order: 110
+  },
+  {
+    backgroundColor: "#ffff00",
+    order: 120
+  },
+  {
+    backgroundColor: "#66ffff",
+    order: 140
+  },
+  {
+    backgroundColor: "#00ccff",
+    order: 150
+  },
+  {
+    backgroundColor: "#0099cc",
+    order: 160
+  },
+  {
+    backgroundColor: "#3366ff",
+    order: 190
+  },
+  {
+    backgroundColor: "#0000ff",
+    order: 210
+  },
+  {
+    backgroundColor: "#3333cc",
+    order: 230
+  },
+  {
+    backgroundColor: "#0000cc",
+    order: 240
+  },
+  {
+    backgroundColor: "#003399",
+    order: 250
+  },
+  {
+    backgroundColor: "#000066",
+    order: 260
+  },
+  {
+    backgroundColor: "#00ff99",
+    order: 270
+  },
+  {
+    backgroundColor: "#99ff99",
+    order: 280
+  },
+  {
+    backgroundColor: "#99ff33",
+    order: 290
+  },
+  {
+    backgroundColor: "#00cc00",
+    order: 320
+  },
+  {
+    backgroundColor: "#00cc66",
+    order: 330
+  },
+  {
+    backgroundColor: "#009933",
+    order: 340
+  },
+  {
+    backgroundColor: "#ff3399",
+    order: 360
+  },
+  {
+    backgroundColor: "#ff33cc",
+    order: 370
+  },
+  {
+    backgroundColor: "#ff0066",
+    order: 380
+  },
+  {
+    backgroundColor: "#cc0066",
+    order: 390
+  },
+  {
+    backgroundColor: "#cc0099",
+    order: 400
+  },
+  {
+    backgroundColor: "#9900cc",
+    order: 420
+  },
+  {
+    backgroundColor: "#993366",
+    order: 450
+  },
+  {
+    backgroundColor: "#660066",
+    order: 460
+  },
+  {
+    backgroundColor: "#999966",
+    order: 470
+  },
+  {
+    backgroundColor: "#663300",
+    order: 480
+  },
+  {
+    backgroundColor: "#996600",
+    order: 490
+  },
+  {
+    backgroundColor: "#666699",
+    order: 500
+  },
+  {
+    backgroundColor: "#669999",
+    order: 510
+  },
+  {
+    backgroundColor: "#000000",
+    order: 530
+  }
+];
