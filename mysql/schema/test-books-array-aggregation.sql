73fe253ae2f20272767922965a624251a7f60a35
Adam Rackis <adamrackis@hotmail.com>
2023-02-26 12:58:56 -0600

    books subjects dump query work

--- mysql/schema/test-books-array-aggregation.sql
+++ mysql/schema/test-books-array-aggregation.sql
@@ -1,1 +1,6 @@
-SELECT id, title, (SELECT JSON_ARRAYAGG(book) from books_tags WHERE book = id) tags FROM books
+SELECT COUNT(*) bookCount, (SELECT JSON_ARRAYAGG(tag) from books_tags bt WHERE book = b.id) tags
+FROM books b
+# WHERE b.id > 300
+GROUP BY tags
+HAVING tags IS NOT NULL
+ORDER BY bookCount DESC
