29219ef87411084bfbc5ae6a12e6661d8fad4e2c
Adam Rackis <adamrackis@hotmail.com>
2023-03-02 21:45:32 -0600

    data import work

--- mysql/data-import/insert-similar-book.js
+++ mysql/data-import/insert-similar-book.js
@@ -2,16 +2,9 @@ const { query, getLastId, mySqlConnection } = require("./db-utils");
 
 const similarBookFields = ["title", "authors", "isbn", "smallImage", "smallImagePreview"];
 
-module.exports = async function insertSimilarBook(book) {
-  if (!book.title) {
-    console.log("No title skipping");
-    return;
-  }
-  await query(`INSERT INTO similar_books (${similarBookFields.join(", ")}) VALUES (${similarBookFields.map(() => "?").join(", ")})`, [
-    book.title,
-    JSON.stringify(book.authors),
-    book.isbn,
-    book.smallImage,
-    JSON.stringify(book.smallImagePreview)
-  ]);
+module.exports = async function insertSimilarBook(books) {
+  const valuesPlaceholders = Array.from({ length: books.length }, () => "(?)").join(", ");
+  const values = books.map(book => [book.title, JSON.stringify(book.authors), book.isbn, book.smallImage, JSON.stringify(book.smallImagePreview)]);
+
+  await query(`INSERT INTO similar_books (${similarBookFields.join(", ")}) VALUES ${valuesPlaceholders}`, values);
 };
