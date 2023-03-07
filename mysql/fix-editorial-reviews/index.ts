a3ed6d1935decf715534016d60469593f9828f84
Adam Rackis <adamrackis@hotmail.com>
2023-03-06 18:50:27 -0600

    start module

---
+++ mysql/fix-editorial-reviews/index.ts
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+import { Client } from "@planetscale/database";
+import fetch from "node-fetch";
+
+async function run() {
+  console.log(fetch);
+}
+
+run();
