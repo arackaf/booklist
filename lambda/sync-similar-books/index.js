02e8a787239c25a7fba11691537a0077508b44c0
Adam Rackis <adamrackis@hotmail.com>
2023-03-07 22:47:02 -0600

    start to query books

--- lambda/sync-similar-books/index.js
+++ lambda/sync-similar-books/index.js
@@ -42,6 +42,23 @@ module.exports.handler = async evt => {
       });
 
   try {
+    const books = await query(
+      mySqlConnection,
+      `    
+      SELECT id, isbn
+      FROM books
+      WHERE id = 1596 AND (CHAR_LENGTH(isbn) = 10 OR CHAR_LENGTH(isbn) = 13)
+      ORDER BY id DESC
+      LIMIT 1`
+    );
+
+    if (!books.length || !books[0].isbn) {
+      mySqlConnection.end();
+      await browser.end();
+      return;
+    }
+
+    console.log(books);
     const { isbn } = evt;
 
     const page = await browser.newPage({
