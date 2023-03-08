8564116d1458b45c48e68f1291ecf80e9c8685ed
Adam Rackis <adamrackis@hotmail.com>
2023-03-07 22:18:08 -0600

    Secrets, MySql connection

--- lambda/sync-similar-books/index.js
+++ lambda/sync-similar-books/index.js
@@ -1,3 +1,12 @@
+const { SecretsManager } = require("@aws-sdk/client-secrets-manager");
+
+const region = "us-east-1";
+const secretName = "MyLibrary";
+
+const secretsClient = new SecretsManager({
+  region
+});
+
 const playwright = process.env.stage ? require("playwright-aws-lambda") : require("playwright");
 
 const mysql = require("mysql");
@@ -5,92 +14,104 @@ const mysql = require("mysql");
 const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
 const { toUtf8, fromUtf8 } = require("@aws-sdk/util-utf8");
 
-// const mySqlConnection = mysql.createConnection({
-//   host: process.env.MYSQL_HOST,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
 const client = new LambdaClient({
   region: "us-east-1"
 });
 
 module.exports.handler = async evt => {
-  const { isbn } = evt;
-  const browser = process.env.stage
-    ? await await playwright.launchChromium({
-        headless: true
-      })
-    : await playwright.chromium.launch({
-        headless: true
-      });
+  const secrets = await getSecrets();
 
-  const page = await browser.newPage({
-    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
-    extraHTTPHeaders: {
-      accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-encoding": "gzip, deflate, br",
-      "accept-language": "en",
-      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+  const { host, user, password } = splitMysqlConnectionString(secrets["mysql-connection-live"]);
+
+  const mySqlConnection = mysql.createConnection({
+    host,
+    user,
+    password,
+    ssl: {
+      rejectUnauthorized: false
     }
   });
+  mySqlConnection.connect();
 
-  await page.goto(`https://www.amazon.com/dp/${isbn}`, {});
+  try {
+    const { isbn } = evt;
+    const browser = process.env.stage
+      ? await await playwright.launchChromium({
+          headless: true
+        })
+      : await playwright.chromium.launch({
+          headless: true
+        });
 
-  for (let i = 1; i <= 15; i++) {
-    await page.evaluate(() => window.scrollTo(0, i * 700));
-    await page.waitForTimeout(1000);
-  }
+    const page = await browser.newPage({
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+      extraHTTPHeaders: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+      }
+    });
 
-  const allCarousels = await page.locator("[data-a-carousel-options]").all();
+    await page.goto(`https://www.amazon.com/dp/${isbn}`, {});
 
-  const allBookResults = new Map();
+    for (let i = 1; i <= 15; i++) {
+      await page.evaluate(() => window.scrollTo(0, i * 700));
+      await page.waitForTimeout(1000);
+    }
 
-  for (const carousel of allCarousels) {
-    const carouselEl = await carousel.elementHandle();
-    const headerEl = await carouselEl.$(".a-carousel-heading");
+    const allCarousels = await page.locator("[data-a-carousel-options]").all();
 
-    if (headerEl == null) {
-      continue;
-    } else {
-      const results = await processCarousel(page, carousel);
-      console.log("Found", results.length, "in", (await headerEl.innerText()).replace(/\n/g, " "));
+    const allBookResults = new Map();
 
-      for (const book of results) {
-        if (!allBookResults.has(book.isbn)) {
-          allBookResults.set(book.isbn, book);
+    for (const carousel of allCarousels) {
+      const carouselEl = await carousel.elementHandle();
+      const headerEl = await carouselEl.$(".a-carousel-heading");
+
+      if (headerEl == null) {
+        continue;
+      } else {
+        const results = await processCarousel(page, carousel);
+        console.log("Found", results.length, "in", (await headerEl.innerText()).replace(/\n/g, " "));
+
+        for (const book of results) {
+          if (!allBookResults.has(book.isbn)) {
+            allBookResults.set(book.isbn, book);
+          }
         }
       }
     }
-  }
 
-  const allResults = [...allBookResults.values()];
-  for (const book of allResults) {
-    const command = new InvokeCommand({
-      FunctionName: "process-book-cover-live-processCover",
-      Payload: fromUtf8(JSON.stringify({ url: book.img, userId: "similar-books" }))
-    });
-    const response = await client.send(command);
+    const allResults = [...allBookResults.values()];
+    for (const book of allResults) {
+      const command = new InvokeCommand({
+        FunctionName: "process-book-cover-live-processCover",
+        Payload: fromUtf8(JSON.stringify({ url: book.img, userId: "similar-books" }))
+      });
+      const response = await client.send(command);
 
-    if (response.Payload) {
-      const respJson = JSON.parse(toUtf8(response.Payload));
+      if (response.Payload) {
+        const respJson = JSON.parse(toUtf8(response.Payload));
 
-      delete book.img;
-      book.smallImage = respJson.smallImage;
-      book.smallImagePreview = respJson.smallImagePreview;
-      book.mobileImage = respJson.mobileImage;
-      book.mobileImagePreview = respJson.mobileImagePreview;
-    } else {
-      console.log("can't process");
+        delete book.img;
+        book.smallImage = respJson.smallImage;
+        book.smallImagePreview = respJson.smallImagePreview;
+        book.mobileImage = respJson.mobileImage;
+        book.mobileImagePreview = respJson.mobileImagePreview;
+      } else {
+        console.log("can't process");
+      }
     }
+    console.log({ allResults });
+  } catch (er) {
+    console.log("Error: ", er);
+  } finally {
+    try {
+      await browser?.close();
+    } catch (er) {}
+    mySqlConnection?.end();
   }
-  console.log({ allResults });
-
-  await browser.close();
 };
 
 async function processCarousel(page, carousel) {
@@ -188,4 +209,36 @@ async function getAuthor(card) {
   for (const a of author) {
     return a.innerText();
   }
+}
+
+async function getSecrets() {
+  try {
+    const result = await secretsClient.getSecretValue({ SecretId: secretName });
+    return JSON.parse(result.SecretString);
+  } catch (er) {
+    console.log("Error reading secrets", er);
+    throw er;
+  }
+}
+
+function splitMysqlConnectionString(connString) {
+  connString = connString.replace("mysql://", "");
+  connString = connString.replace(/\/.*$/, "");
+
+  const [userPwd, host] = connString.split("@");
+  const [user, password] = userPwd.split(":");
+
+  return { host, user, password };
+}
+
+function query(mySqlConnection, ...args) {
+  return new Promise((res, rej) => {
+    mySqlConnection.query(...args, (err, results) => {
+      if (err) {
+        rej(err);
+      } else {
+        res(results);
+      }
+    });
+  });
 }
