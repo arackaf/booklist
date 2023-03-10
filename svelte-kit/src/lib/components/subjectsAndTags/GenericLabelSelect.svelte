3faa50d374a4495263b11831498fe080f297ecd1
Adam Rackis <adamrackis@hotmail.com>
2023-03-09 23:24:12 -0600

    Fix z-index issue

--- svelte-kit/src/lib/components/subjectsAndTags/GenericLabelSelect.svelte
+++ svelte-kit/src/lib/components/subjectsAndTags/GenericLabelSelect.svelte
@@ -12,7 +12,7 @@
   let inputStyles = "width: 100px; border-top-width: 0; border-right-width: 0; border-left-width: 0; border-radius: 0;";
 </script>
 
-<div>
+<div class="root">
   <AutoSuggest
     keyField="id"
     {options}
@@ -29,3 +29,9 @@
     </span>
   </AutoSuggest>
 </div>
+
+<style>
+  .root :global(.options-root) {
+    z-index: 9999;
+  }
+</style>
