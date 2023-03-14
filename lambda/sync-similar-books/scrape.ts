4f3901f6e0c8a323a4b57a8a263035076152ede8
Adam Rackis <adamrackis@hotmail.com>
2023-03-13 18:47:16 -0700

    Tweak UserAgent string

--- lambda/sync-similar-books/scrape.ts
+++ lambda/sync-similar-books/scrape.ts
@@ -207,7 +207,7 @@ export async function getAuthorFromBookPage(isbn: string) {
       });
   try {
     const page: Page = await browser.newPage({
-      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+      userAgent: "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
       extraHTTPHeaders: {
         accept:
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
