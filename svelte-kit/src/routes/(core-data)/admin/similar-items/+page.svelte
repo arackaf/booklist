a2377271f9a536e47c4ed26ce2fc1271303bcdba
Adam Rackis <adamrackis@hotmail.com>
2023-04-20 23:12:12 -0500

    styling

--- svelte-kit/src/routes/(core-data)/admin/similar-items/+page.svelte
+++ svelte-kit/src/routes/(core-data)/admin/similar-items/+page.svelte
@@ -13,8 +13,11 @@
       <div class="entry">
         <BookCover size="small" book={b} preview={b.smallImagePreview} />
         <div class="book-info">
-          <span class="book-title">{b.title}</span>
-          <span>{b.authors}</span>
+          <span class="book-title no-overflow">{b.title}</span>
+          <span class="book-author">{b.authors}</span>
+        </div>
+        <div class="similar-items">
+          <div class="book-title">Unweaving the Rainbow: Science, Delusion and the Appetite for Wonder</div>
         </div>
       </div>
     {/each}
@@ -27,11 +30,19 @@
     border: 1px solid var(--neutral-border);
     border-radius: 5px;
     padding: 15px;
+    gap: 10px;
+    margin-bottom: 10px;
   }
 
   .book-info {
     display: flex;
     flex-direction: column;
     margin-left: 5px;
+    flex: 1;
+    min-width: 0;
+  }
+
+  .similar-items {
+    flex: 1;
   }
 </style>
