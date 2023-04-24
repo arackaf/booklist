7310eaad22f411e14bcba6ef89360fc2becf8630
Adam Rackis <adamrackis@hotmail.com>
2023-04-23 22:13:15 -0500

    Next lambda converted

--- svelte-kit/src/routes/api/get-recommendations/+server.ts
+++ svelte-kit/src/routes/api/get-recommendations/+server.ts
@@ -1,9 +1,7 @@
 import { json } from "@sveltejs/kit";
 
-import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
-import { toUtf8, fromUtf8 } from "@aws-sdk/util-utf8";
-
-import { AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, GET_RECOMMENDATIONS_LAMBDA } from "$env/static/private";
+import { GET_RECOMMENDATIONS_LAMBDA } from "$env/static/private";
+import { invokeLambda } from "$lib/lambda-utils.js";
 
 export async function POST({ locals, request }) {
   const session = await locals.getSession();
@@ -16,26 +14,9 @@ export async function POST({ locals, request }) {
   const { bookIds } = reqBody;
 
   try {
-    const client = new LambdaClient({
-      region: "us-east-1",
-      credentials: {
-        accessKeyId: AMAZON_ACCESS_KEY,
-        secretAccessKey: AMAZON_SECRET_KEY
-      }
-    });
-    const command = new InvokeCommand({
-      FunctionName: GET_RECOMMENDATIONS_LAMBDA,
-      Payload: fromUtf8(JSON.stringify({ userId, bookIds }))
-    });
-    const response = await client.send(command);
+    const respJson = await invokeLambda(GET_RECOMMENDATIONS_LAMBDA, { userId, bookIds });
 
-    if (response.Payload) {
-      const respJson = JSON.parse(toUtf8(response.Payload));
-
-      return json(respJson);
-    } else {
-      return json({ error: true });
-    }
+    return json(respJson);
   } catch (er) {
     console.log("Error invoking lambda", er);
     return json({ error: true });
