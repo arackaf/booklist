d0685e74ed65871b1105cc872c33be68bae4e0a4
Adam Rackis <adamrackis@hotmail.com>
2023-05-06 12:04:18 -0500

    Start to add themes

--- svelte-kit/src/lib/components/ui/Button/Button.svelte
+++ svelte-kit/src/lib/components/ui/Button/Button.svelte
@@ -1,3 +1,7 @@
-<button class="m-0 rounded py-1.5 px-3 bg-primary-5 text-primary-10 text-base">
+<script lang="ts">
+  export let primary = true;
+</script>
+
+<button class:bg-primary-5={primary} class:text-primary-10={primary} class="m-0 rounded py-1.5 px-3 text-base">
   <slot />
 </button>
