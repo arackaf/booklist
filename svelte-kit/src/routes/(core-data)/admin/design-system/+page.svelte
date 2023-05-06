405e83590b5e02c55ae3420705ffbb9adebd7c25
Adam Rackis <adamrackis@hotmail.com>
2023-05-06 11:37:27 -0500

    design system layout

--- svelte-kit/src/routes/(core-data)/admin/design-system/+page.svelte
+++ svelte-kit/src/routes/(core-data)/admin/design-system/+page.svelte
@@ -5,19 +5,23 @@
 <div class="buttons">
   <div>
     <h1>Old</h1>
-    <button class="btn btn-primary">Button</button>
   </div>
   <div>
     <h1>New</h1>
+  </div>
+
+  <div>
+    <button class="btn btn-primary">Button</button>
+  </div>
+  <div>
     <Button>Button</Button>
   </div>
 </div>
 
 <style>
   .buttons {
-    display: flex;
-  }
-  .buttons > * {
-    flex: 1;
+    display: grid;
+    gap: 15px;
+    grid-template-columns: min-content min-content;
   }
 </style>
