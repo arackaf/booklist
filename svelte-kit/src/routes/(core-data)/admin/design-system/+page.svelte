6d44e88cffc2ba5aa9001d28dae1b32cad93df4c
Adam Rackis <adamrackis@hotmail.com>
2023-05-05 21:12:56 -0500

    New section added

---
+++ svelte-kit/src/routes/(core-data)/admin/design-system/+page.svelte
@@ -0,0 +1,19 @@
+<div class="buttons">
+  <div>
+    <h1>Old</h1>
+    <button class="btn btn-primary">Button</button>
+  </div>
+  <div>
+    <h1>New</h1>
+    <button class="btn btn-primary">Button</button>
+  </div>
+</div>
+
+<style>
+  .buttons {
+    display: flex;
+  }
+  .buttons > * {
+    flex: 1;
+  }
+</style>
