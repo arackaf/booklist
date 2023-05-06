2f45e80463e635ce912c7067758959b1506a6967
Adam Rackis <adamrackis@hotmail.com>
2023-05-06 11:58:41 -0500

    Button work

--- svelte-kit/tailwind.config.js
+++ svelte-kit/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "primary-1": "var(--primary-1)",
+        "primary-2": "var(--primary-2)",
+        "primary-3": "var(--primary-3)",
+        "primary-4": "var(--primary-4)",
+        "primary-5": "var(--primary-5)",
+        "primary-6": "var(--primary-6)",
+        "primary-7": "var(--primary-7)",
+        "primary-8": "var(--primary-8)",
+        "primary-9": "var(--primary-9)",
+        "primary-10": "var(--primary-10)",
+        "primary-11": "var(--primary-11)"
+      }
+    }
   },
   plugins: []
 };
