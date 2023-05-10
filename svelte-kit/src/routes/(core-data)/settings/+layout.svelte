3b90fe7ed383c70e40bc41f3b241419cb888b0d4
Adam Rackis <adamrackis@hotmail.com>
2023-05-09 20:59:09 -0500

    Misc fixes

--- svelte-kit/src/routes/(core-data)/settings/+layout.svelte
+++ svelte-kit/src/routes/(core-data)/settings/+layout.svelte
@@ -3,14 +3,14 @@
   import TabbedPage from "$lib/components/layout/TabbedPage.svelte";
   import { SectionNavItem } from "$lib/components/section-nav";
 
-  $: ({ isPublic } = $page.data);
+  $: ({ hasPublicId } = $page.data);
 </script>
 
 <TabbedPage>
   <svelte:fragment slot="nav">
     <SectionNavItem href="/settings/theme">Theme</SectionNavItem>
-    <SectionNavItem disabled={isPublic} href="/settings/public-sharing">Public sharing</SectionNavItem>
-    <SectionNavItem disabled={isPublic} href="/settings/account-sync">Sync old login</SectionNavItem>
+    <SectionNavItem disabled={hasPublicId} href="/settings/public-sharing">Public sharing</SectionNavItem>
+    <SectionNavItem disabled={hasPublicId} href="/settings/account-sync">Sync old login</SectionNavItem>
   </svelte:fragment>
   <slot />
 </TabbedPage>
