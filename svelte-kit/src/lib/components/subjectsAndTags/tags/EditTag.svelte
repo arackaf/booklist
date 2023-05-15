de2198fe31d70a1dc14e7744662946411b05f432
Adam Rackis <adamrackis@hotmail.com>
2023-05-14 22:03:20 -0500

    Next FlexRow removed

--- svelte-kit/src/lib/components/subjectsAndTags/tags/EditTag.svelte
+++ svelte-kit/src/lib/components/subjectsAndTags/tags/EditTag.svelte
@@ -93,100 +93,96 @@
 
 {#if !editingTag}
   <div />
-{:else}
-  <FlexRow>
-    {#if !deleteShowing}
-      <form method="POST" action="?/saveTag" use:enhance={runSave}>
-        <FlexRow>
-          <input type="hidden" name="id" value={editingTag.id} />
-          <div class="col-xs-12 col-lg-6">
-            <div class="form-group">
-              <label for="tag-name">Name</label>
-              <input
-                id="tag-name"
-                bind:this={inputEl}
-                bind:value={editingTag.name}
-                name="name"
-                placeholder="Tag name"
-                class={cn("form-control", { error: missingName })}
-              />
-              {#if missingName}
-                <span style="margin-top: 5px; display: inline-block;" class="label label-danger">Tags need names!</span>
-                <br />
-              {/if}
-              <div
-                class="label label-default"
-                style="background-color: {editingTag.backgroundColor}; color: {editingTag.textColor}; max-width: 100%; overflow: hidden; align-self: flex-start;"
-              >
-                {editingTag.name.trim() || "<label preview>"}
-              </div>
-            </div>
+{:else if !deleteShowing}
+  <form method="POST" action="?/saveTag" use:enhance={runSave}>
+    <input type="hidden" name="id" value={editingTag.id} />
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-4">
+      <div class="md:col-span-2">
+        <div class="form-group">
+          <label for="tag-name">Name</label>
+          <input
+            id="tag-name"
+            bind:this={inputEl}
+            bind:value={editingTag.name}
+            name="name"
+            placeholder="Tag name"
+            class={cn("form-control", { error: missingName })}
+          />
+          {#if missingName}
+            <span style="margin-top: 5px; display: inline-block;" class="label label-danger">Tags need names!</span>
+            <br />
+          {/if}
+          <div
+            class="label label-default"
+            style="background-color: {editingTag.backgroundColor}; color: {editingTag.textColor}; max-width: 100%; overflow: hidden; align-self: flex-start;"
+          >
+            {editingTag.name.trim() || "<label preview>"}
           </div>
-          <div class="col-xs-12 col-lg-6" />
-          <div class="col-xs-12 col-sm-6">
-            <div class="form-group">
-              <span>Label Color</span>
-              <ColorsPalette
-                currentColor={editingTag.backgroundColor}
-                colors={colors.map(c => c.backgroundColor)}
-                onColorChosen={color => (editingTag.backgroundColor = color)}
-              />
-              <CustomColorPicker
-                labelStyle="margin-left: 3px"
-                onColorChosen={color => (editingTag.backgroundColor = color)}
-                currentColor={editingTag.backgroundColor}
-              />
-              <input type="hidden" name="backgroundColor" value={editingTag.backgroundColor} />
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6">
-            <div class="form-group">
-              <span>Text Color</span>
-              <ColorsPalette currentColor={editingTag.textColor} colors={textColors} onColorChosen={color => (editingTag.textColor = color)} />
-              <CustomColorPicker
-                labelStyle="margin-left: 3px"
-                onColorChosen={color => (editingTag.textColor = color)}
-                currentColor={editingTag.backgroundColor}
-              />
-              <input type="hidden" name="textColor" value={editingTag.textColor} />
-            </div>
-          </div>
-          <div class="col-xs-12">
-            <FlowItems pushLast={true}>
-              <Button disabled={saving} preset="primary-xs">Save</Button>
-              <Button disabled={saving} preset="default-xs" onClick={onCancelEdit}>Cancel</Button>
-              {#if editingTag.id}
-                <Button disabled={saving} preset="danger-xs" onClick={() => (deleteShowing = true)}>
-                  Delete
-                  {originalName}
-                  <i class="fal fa-fw fa-trash-alt" />
-                </Button>
-              {/if}
-            </FlowItems>
-          </div>
-        </FlexRow>
-      </form>
-    {:else}
-      <form method="POST" action="?/deleteTag" use:enhance={runDelete}>
-        <input type="hidden" name="id" value={editingTag.id} />
-        <div class="col-xs-12">
-          <Stack>
-            <div class="alert alert-danger alert-slim" style="align-self: flex-start">
-              <FlowItems tighter={true}><span>Delete {originalName}?</span></FlowItems>
-            </div>
-            <FlowItems>
-              <Button disabled={deleting} preset="danger-xs">
-                {#if deleting}
-                  <span> Deleting <i class="far fa-spinner fa-spin" /></span>
-                {:else}
-                  <span>Delete it!</span>
-                {/if}
-              </Button>
-              <Button disabled={deleting} onClick={() => (deleteShowing = false)} class="btn btn-xs">Cancel</Button>
-            </FlowItems>
-          </Stack>
         </div>
-      </form>
-    {/if}
-  </FlexRow>
+      </div>
+
+      <div>
+        <div class="form-group">
+          <span>Label Color</span>
+          <ColorsPalette
+            currentColor={editingTag.backgroundColor}
+            colors={colors.map(c => c.backgroundColor)}
+            onColorChosen={color => (editingTag.backgroundColor = color)}
+          />
+          <CustomColorPicker
+            labelStyle="margin-left: 3px"
+            onColorChosen={color => (editingTag.backgroundColor = color)}
+            currentColor={editingTag.backgroundColor}
+          />
+          <input type="hidden" name="backgroundColor" value={editingTag.backgroundColor} />
+        </div>
+      </div>
+      <div>
+        <div class="form-group">
+          <span>Text Color</span>
+          <ColorsPalette currentColor={editingTag.textColor} colors={textColors} onColorChosen={color => (editingTag.textColor = color)} />
+          <CustomColorPicker
+            labelStyle="margin-left: 3px"
+            onColorChosen={color => (editingTag.textColor = color)}
+            currentColor={editingTag.backgroundColor}
+          />
+          <input type="hidden" name="textColor" value={editingTag.textColor} />
+        </div>
+      </div>
+      <div class="md:col-span-2">
+        <FlowItems pushLast={true}>
+          <Button disabled={saving} preset="primary-xs">Save</Button>
+          <Button disabled={saving} preset="default-xs" onClick={onCancelEdit}>Cancel</Button>
+          {#if editingTag.id}
+            <Button disabled={saving} preset="danger-xs" onClick={() => (deleteShowing = true)}>
+              Delete
+              {originalName}
+              <i class="fal fa-fw fa-trash-alt" />
+            </Button>
+          {/if}
+        </FlowItems>
+      </div>
+    </div>
+  </form>
+{:else}
+  <form method="POST" action="?/deleteTag" use:enhance={runDelete}>
+    <input type="hidden" name="id" value={editingTag.id} />
+    <div class="col-xs-12">
+      <Stack>
+        <div class="alert alert-danger alert-slim" style="align-self: flex-start">
+          <FlowItems tighter={true}><span>Delete {originalName}?</span></FlowItems>
+        </div>
+        <FlowItems>
+          <Button disabled={deleting} preset="danger-xs">
+            {#if deleting}
+              <span> Deleting <i class="far fa-spinner fa-spin" /></span>
+            {:else}
+              <span>Delete it!</span>
+            {/if}
+          </Button>
+          <Button disabled={deleting} onClick={() => (deleteShowing = false)} class="btn btn-xs">Cancel</Button>
+        </FlowItems>
+      </Stack>
+    </div>
+  </form>
 {/if}
