bd05ffd059589d48ee9eb9e005e63a05c9f18612
Adam Rackis <adamrackis@hotmail.com>
2023-05-16 20:01:46 -0500

    Tailwind cleanup

--- svelte-kit/src/lib/components/editBook/CurrentCovers.svelte
+++ svelte-kit/src/lib/components/editBook/CurrentCovers.svelte
@@ -1,30 +1,24 @@
 <script lang="ts">
   import BookCover from "../ui/BookCover.svelte";
 
-  import FlowItems from "../layout/FlowItems.svelte";
-  import Stack from "../layout/Stack.svelte";
   import type { Book } from "$data/types";
 
   export let book: Book;
 </script>
 
-<FlowItems>
-  <Stack style="flex: 1" tightest={true}>
-    <div>
-      <span class="form-label">Mobile</span>
-      <BookCover size="mobile" {book} />
-    </div>
-  </Stack>
-  <Stack style="flex: 1" tightest={true}>
-    <div>
-      <span class="form-label">Standard</span>
-      <BookCover size="small" {book} />
-    </div>
-  </Stack>
-  <Stack style="flex: 1" tightest={true}>
-    <div>
-      <span class="form-label">Full</span>
-      <BookCover size="medium" {book} />
-    </div>
-  </Stack>
-</FlowItems>
+<div class="flex flex-row">
+  <div class="flex-1 flex flex-col">
+    <span class="form-label">Mobile</span>
+    <BookCover size="mobile" {book} />
+  </div>
+
+  <div class="flex-1 flex flex-col">
+    <span class="form-label">Standard</span>
+    <BookCover size="small" {book} />
+  </div>
+
+  <div class="flex-1 flex flex-col">
+    <span class="form-label">Full</span>
+    <BookCover size="medium" {book} />
+  </div>
+</div>
