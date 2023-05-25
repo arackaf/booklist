8a456fbebe5ba82197456548e339abebc051480d
Adam Rackis <adamrackis@hotmail.com>
2023-05-26 00:37:05 +0200

    Input work

--- svelte-kit/src/lib/components/ui/Input/Input.svelte
+++ svelte-kit/src/lib/components/ui/Input/Input.svelte
@@ -4,6 +4,7 @@
 
 <input
   class:border-neutral-400={!error}
+  class:focus:shadow-danger-7={error}
   class:border-danger-5={error}
-  class="bg-white text-neutral-600 border rounded h-8 py-1 px-3 text-base md:text-sm block w-full"
+  class="bg-white text-neutral-600 border rounded h-8 py-1 px-3 text-base md:text-sm block w-full transition-box-shadow focus:shadow-md"
 />
