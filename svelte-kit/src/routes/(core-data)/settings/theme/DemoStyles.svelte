fff119a9dc305fa5da916fd15112e4c958cae7fa
Adam Rackis <adamrackis@hotmail.com>
2023-05-30 18:43:36 -0400

    convert demoStyles page

--- svelte-kit/src/routes/(core-data)/settings/theme/DemoStyles.svelte
+++ svelte-kit/src/routes/(core-data)/settings/theme/DemoStyles.svelte
@@ -1,26 +1,36 @@
+<script>
+  import Input from "$lib/components/ui/Input/Input.svelte";
+  import InputGroup from "$lib/components/ui/Input/InputGroup.svelte";
+</script>
+
 <div class="demo-root">
   <div data-reach-dialog-content class="mock-modal">
     <h4>Book Search</h4>
     <hr />
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div class="md:col-span-2">
-        <div class="form-group">
-          <label for="demo-title">Title</label>
-          <input id="demo-title" placeholder="Search title" class="form-control" value="The Slefish Gene" />
+        <div class="flex flex-col gap-2">
+          <InputGroup labelText="Title">
+            <Input slot="input" placeholder="Search title" value="The Slefish Gene" />
+          </InputGroup>
+
           <div class="alert alert-warning margin-bottom">Did you spell this right?</div>
         </div>
       </div>
       <div>
-        <div class="form-group">
-          <label for="demo-publisher">Publisher</label>
-          <input id="demo-publisher" placeholder="Publisher" class="form-control" value="Basic Books" />
+        <div class="flex flex-col gap-2">
+          <InputGroup labelText="Publisher">
+            <Input slot="input" placeholder="Publisher" class="form-control" value="Basic Books" />
+          </InputGroup>
+
           <div class="alert alert-success margin-bottom">Looks good to me!</div>
         </div>
       </div>
       <div>
-        <div class="form-group">
-          <label for="demo-author">Author</label>
-          <input id="demo-author" placeholder="Author" class="form-control" />
+        <div class="flex flex-col gap-2">
+          <InputGroup labelText="Author">
+            <Input slot="input" placeholder="Author" />
+          </InputGroup>
           <div class="alert alert-danger margin-bottom">Author is required!</div>
         </div>
       </div>
