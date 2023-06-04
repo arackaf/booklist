3b2bf68f1aa2016effa96aed08bcaa9c9af54276
Adam Rackis <adamrackis@hotmail.com>
2023-06-03 21:36:44 -0400

    Update section nav styles

--- svelte-kit/src/lib/components/section-nav/SectionNavItem.svelte
+++ svelte-kit/src/lib/components/section-nav/SectionNavItem.svelte
@@ -7,16 +7,18 @@
   $: targetPathname = href.replace(/\?.*/, "");
 
   $: active = targetPathname === $page.url.pathname;
+
+  $: addedClasses = active ? "bg-[var(--primary-10)]" : "";
 </script>
 
-<div class="section-nav-item flex p-0" class:active class:disabled>
-  <svelte:element this={active || disabled ? "span" : "a"} href={active || disabled ? null : href}>
+<div class="section-nav-item flex p-0" class:active class:disabled class:cursor-default={active || disabled}>
+  <svelte:element
+    this={active || disabled ? "span" : "a"}
+    class="p-4 {addedClasses}"
+    class:text-neutral-500={disabled}
+    class:font-bold={active}
+    href={active || disabled ? null : href}
+  >
     <slot />
   </svelte:element>
 </div>
-
-<style>
-  .disabled {
-    cursor: not-allowed;
-  }
-</style>
