418ad9c1be860b161d1b48fabc88f36137e51a2f
Adam Rackis <adamrackis@hotmail.com>
2023-06-03 20:17:59 -0400

    Button conversions, landing page cleanup

--- svelte-kit/src/routes/landing/PublicLandingPage.svelte
+++ svelte-kit/src/routes/landing/PublicLandingPage.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { signIn } from "@auth/sveltekit/client";
+  import Button from "$lib/components/ui/Button/Button.svelte";
 </script>
 
 <section>
@@ -7,33 +8,35 @@
     <div>
       <div style="position: relative">
         <div style="display: flex">
-          <div style="max-width: 800px; margin-top: 25px; margin-left: auto; margin-right: auto">
-            Welcome to
-            <i>My Library</i>.<br />
-            <br />
-            This site is my own little passion project, which tracks your library. You scan your books (or manually type in the isbn) and the books' info
-            is fetched from ISBN DB, and stored for you. You can then search and categorize your library.
-            <br />
-            <br />
-            So basically this site is of use to the extremely small percentage of people for whom the following are <i>all</i> true: they read a lot,
-            own the books they read, and read non-eBooks. Again, it's is more of a passion project than anything.
-            <br />
-            <br />
-            It's free and I won't ever sell your info, so give it a try!
-            <br />
-            <br />
-            For those interested in seeing the code for this site, the GitHub repository is{" "}
-            <a target="_blank" rel="noreferrer" href="https://github.com/arackaf/booklist"> here </a>
-            <br />
-            <br />
-            <button
-              style="text-decoration: none"
-              class="btn btn-primary"
+          <div class="flex flex-col gap-6" style="max-width: 800px; margin-top: 25px; margin-left: auto; margin-right: auto">
+            <span class="text-lg font-bold">
+              Welcome to <span class="italic">My Library</span>
+            </span>
+            <span>
+              This site is my own little passion project, which tracks your library. You scan your books (or manually type in the isbn) and the books'
+              info is fetched from ISBN DB, and stored for you. You can then search and categorize your library.
+            </span>
+
+            <span>
+              So basically this site is of use to the extremely small percentage of people for whom the following are <i>all</i> true: they read a lot,
+              own the books they read, and read non-eBooks. Again, it's is more of a passion project than anything.
+            </span>
+            <span> It's free and I won't ever sell your info, so give it a try! </span>
+            <span>
+              For those interested in seeing the code for this site, the GitHub repository is
+              <a target="_blank" rel="noreferrer" href="https://github.com/arackaf/booklist">here</a>
+            </span>
+
+            <Button
+              class="self-start"
+              theme="primary"
               on:click={() =>
                 signIn("google", {
                   callbackUrl: "/books"
-                })}>Login or create an account</button
+                })}
             >
+              Login or create an account
+            </Button>
           </div>
         </div>
       </div>
