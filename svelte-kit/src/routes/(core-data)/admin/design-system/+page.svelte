636c2c83a3ad98f2cc7bc892d9f6c5dc361d4e07
Adam Rackis <adamrackis@hotmail.com>
2023-06-09 09:03:21 -0400

    Remove danger label

--- svelte-kit/src/routes/(core-data)/admin/design-system/+page.svelte
+++ svelte-kit/src/routes/(core-data)/admin/design-system/+page.svelte
@@ -43,20 +43,6 @@
     <Label display="inline-block">Hello Worlding</Label>
   </div>
 
-  <div>
-    <div class="label label-danger">Error Message</div>
-  </div>
-  <div>
-    <Label theme="error">Error Message</Label>
-  </div>
-
-  <div>
-    <div class="label label-danger">Error Message</div>
-  </div>
-  <div>
-    <Label display="inline-block" theme="error">Error Message</Label>
-  </div>
-
   <h1>Book cover</h1>
   <div />
 
