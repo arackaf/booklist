c5cbf6ff61b18ccffc9646d02fa9db8161894d7f
Adam Rackis <adamrackis@hotmail.com>
2023-06-13 23:28:18 -0500

    Finish tab header conversion

--- svelte-kit/src/lib/components/layout/tabs/TabHeader.svelte
+++ svelte-kit/src/lib/components/layout/tabs/TabHeader.svelte
@@ -19,17 +19,7 @@
 
 <div class="border-primary-9" class:bg-primary-10={active} class:border-b={active}>
   <button on:click={onClick} type="button" class="raw-button overlay-holder" style="padding: 4px 8px" class:cursor-default={active}>
-    <span class:active><slot /></span>
-    <span class="placeholder"><slot /></span>
+    <span class:font-bold={active}><slot /></span>
+    <span class="invisible font-bold"><slot /></span>
   </button>
 </div>
-
-<style>
-  span.active {
-    font-weight: bold;
-  }
-  span.placeholder {
-    visibility: hidden;
-    font-weight: bold;
-  }
-</style>
