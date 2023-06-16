82ebaa828bf639aa944e2bc8d126c03324b0fff5
Adam Rackis <adamrackis@hotmail.com>
2023-06-15 22:20:52 -0500

    Tooltip style tag removed

--- svelte-kit/src/routes/(core-data)/(home)/data-vis/barChart/Tooltip.svelte
+++ svelte-kit/src/routes/(core-data)/(home)/data-vis/barChart/Tooltip.svelte
@@ -9,7 +9,7 @@
   const runDrilldown = () => drilldown(data.childSubjects, data.display);
 </script>
 
-<div class={"root " + position}>
+<div class={"tooltip-root " + position}>
   <div class="content">
     <span class="name">{data.display}: {data.count}</span>
     <button class="raw-button flex" on:click={() => removeBar(data.groupId)}><i class="fad fa-times-circle" /></button>
@@ -26,75 +26,3 @@
 
   <div class={"arrow " + position} />
 </div>
-
-<style>
-  .root {
-    --bg-color: var(--info-9);
-    --arrow-size: 10px;
-    --arrow-diagonal: 7px; /* half of sqrt(10^2 + 10^2)  */
-    --arrow-offset: 5px; /* half of sqrt(10^2 + 10^2)  */
-    color: var(--neutral-3);
-    background-color: var(--bg-color);
-    padding: 10px;
-    border-radius: 5px;
-  }
-
-  .root.left {
-    margin-top: 10px;
-    transform: translateX(calc(100% + var(--arrow-diagonal)));
-  }
-  .root.right {
-    margin-top: 10px;
-    transform: translateX(calc(-1 * (100% + var(--arrow-diagonal))));
-  }
-  .root.top {
-    transform: translateY(calc(-1 * var(--arrow-diagonal)));
-  }
-
-  .content {
-    display: flex;
-    align-items: baseline;
-  }
-
-  .name {
-    font-size: 22px;
-    margin-right: 12px;
-  }
-
-  .content button {
-    font-size: 22px;
-  }
-  button {
-    cursor: pointer;
-  }
-
-  .arrow,
-  .arrow::before {
-    background-color: var(--bg-color);
-    position: absolute;
-    width: var(--arrow-size);
-    height: var(--arrow-size);
-    background: inherit;
-  }
-
-  .arrow {
-    visibility: hidden;
-  }
-  .arrow.left {
-    top: calc(50% - var(--arrow-offset));
-    left: calc(-1 * var(--arrow-offset));
-  }
-  .arrow.right {
-    top: calc(50% - var(--arrow-offset));
-    right: calc(-1 * var(--arrow-offset));
-  }
-  .arrow.top {
-    left: calc(50% - var(--arrow-offset));
-    bottom: calc(-1 * var(--arrow-offset));
-  }
-  .arrow::before {
-    visibility: visible;
-    content: "";
-    transform: rotate(45deg);
-  }
-</style>
