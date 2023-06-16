9edb7fbb8b12127ee566b31f3f173aaad664830e
Adam Rackis <adamrackis@hotmail.com>
2023-06-15 22:42:58 -0500

    Prior fix

--- svelte-kit/src/lib/components/ui/Loading.svelte
+++ svelte-kit/src/lib/components/ui/Loading.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <div class="fixed top-1/3 left-1/2 z-[99999] -translate-x-1/2 -translate-y-1/2" in:fade={{ delay: 50, duration: 200 }}>
-  <i class="far fa-5x fa-spin fa-spinner" />
+  <i class="far fa-5x fa-spin fa-spinner text-primary-3 opacity-50" />
 </div>
