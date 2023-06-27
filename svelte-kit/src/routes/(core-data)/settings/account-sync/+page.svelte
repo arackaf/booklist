9e611f5f1c54343a351c0cebc6196a03d816d14f
Adam Rackis <adamrackis@hotmail.com>
2023-06-26 23:00:47 -0400

    Next alerts converted

--- svelte-kit/src/routes/(core-data)/settings/account-sync/+page.svelte
+++ svelte-kit/src/routes/(core-data)/settings/account-sync/+page.svelte
@@ -7,6 +7,7 @@
   import { invalidateAll } from "$app/navigation";
   import Input from "$lib/components/ui/Input/Input.svelte";
   import InputGroup from "$lib/components/ui/Input/InputGroup.svelte";
+  import Alert from "$lib/components/ui/Alert.svelte";
 
   export let data;
 
@@ -37,16 +38,16 @@
     <div class="basis-full lg:basis-1/2">
       <div class="flex flex-col gap-4">
         {#if legacySync}
-          <div class="alert alert-info">You've already sync'd your account</div>
+          <Alert type="info">You've already sync'd your account</Alert>
         {:else if success}
-          <div class="alert alert-success">Your account is now sync'd. You'll be logged out in a few moments. Log back in to see your books.</div>
+          <Alert type="success">Your account is now sync'd. You'll be logged out in a few moments. Log back in to see your books.</Alert>
         {:else}
-          <div class="alert alert-info">
+          <Alert type="info">
             If you had an account with the old version of this site, with an email/password, your books are safe, where you left them
             <br />
             <br />
             Just enter your email address and password, and your current account will become sync'd with your old one
-          </div>
+          </Alert>
           <InputGroup labelText="Email">
             <Input slot="input" name="email" />
           </InputGroup>
@@ -61,7 +62,7 @@
         {#if notFound}
           <div>
             <br />
-            <div class="alert alert-danger">Could not find an account with that email and password</div>
+            <Alert type="error">Could not find an account with that email and password</Alert>
           </div>
         {/if}
       </div>
