7222aa57fcc8a72c9fd4dcec5e11f4736f546b6a
Adam Rackis <adamrackis@gmail.com>
2023-09-12 23:08:10 -0500

    style tweaks

--- svelte-kit/src/routes/(core-data)/books/bookViews/BasicViewItem.svelte
+++ svelte-kit/src/routes/(core-data)/books/bookViews/BasicViewItem.svelte
@@ -26,15 +26,19 @@
   on:swipe={onSwipe}
   class="py-1 border-b border-b-neutral-400 listGroupItem first:border-t-primary-8 first:border-t-[2px] hover:bg-primary-10"
 >
+<div class="py-1 border-b border-b-neutral-400 listGroupItem first:border-t-primary-8 first:border-t-[2px] hover:bg-primary-10">
   <div style="display: flex">
-    <div style="margin-right: 5px; min-width: 55px">
+    <div style="margin-right: 5px; min-width: 40px">
       <BookCover size="mobile" {book} />
     </div>
-    <div style="overflow: hidden">
+    <div class="overflow-hidden flex-1 mr-1">
       <div class="flex flex-col h-full overflow-hidden">
         <span class="text-sm leading-[normal] truncate">{book.title}</span>
         <SubTitleText>{book.authors.length ? book.authors.join(", ") : ""}</SubTitleText>
       </div>
+    </div>
+    <div class="self-center opacity-70">
+      <i class="fal fa-pencil fa-fw" />
     </div>
   </div>
 </div>
