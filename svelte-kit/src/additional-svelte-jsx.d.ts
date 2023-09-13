f66414a1f0b421ab8081a71d9fc41bc50f598c64
Adam Rackis <adamrackis@gmail.com>
2023-09-12 22:40:09 -0500

    Fix typescript events

--- svelte-kit/src/additional-svelte-jsx.d.ts
+++ svelte-kit/src/additional-svelte-jsx.d.ts
@@ -1,33 +1,33 @@
 declare namespace svelteHTML {
   interface HTMLAttributes<T> {
-    onpan?: (event: CustomEvent<{ x: number; y: number; target: EventTarget & T }>) => void;
-    onpanup?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onpandown?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onpanmove?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onpinch?: (event: CustomEvent<{ scale: number; center: { x: number; y: number } }>) => void;
-    onpinchup?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onpinchdown?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onpinchmove?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onrotate?: (event: CustomEvent<{ rotation: number; center: { x: number; y: number } }>) => void;
-    onrotateup?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onrotatedown?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onrotatemove?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onswipe?: (
+    "on:pan"?: (event: CustomEvent<{ x: number; y: number; target: EventTarget & T }>) => void;
+    "on:panup"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:pandown"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:panmove"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:pinch"?: (event: CustomEvent<{ scale: number; center: { x: number; y: number } }>) => void;
+    "on:pinchup"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:pinchdown"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:pinchmove"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:rotate"?: (event: CustomEvent<{ rotation: number; center: { x: number; y: number } }>) => void;
+    "on:rotateup"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:rotatedown"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:rotatemove"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:swipe"?: (
       event: CustomEvent<{
         direction: "top" | "right" | "bottom" | "left";
         target: EventTarget;
       }>
     ) => void;
-    onswipeup?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onswipedown?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onswipemove?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    ontap?: (event: CustomEvent<{ x: number; y: number; target: EventTarget }>) => void;
-    ontapup?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    ontapdown?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    ontapmove?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onpress?: (event: CustomEvent<{ x: number; y: number; target: EventTarget }>) => void;
-    onpressup?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onpressdown?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
-    onpressmove?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:swipeup"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:swipedown"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:swipemove"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:tap"?: (event: CustomEvent<{ x: number; y: number; target: EventTarget }>) => void;
+    "on:tapup"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:tapdown"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:tapmove"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:press"?: (event: CustomEvent<{ x: number; y: number; target: EventTarget }>) => void;
+    "on:pressup"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:pressdown"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
+    "on:pressmove"?: (event: CustomEvent<{ event: PointerEvent; pointersCount: number }>) => void;
   }
 }
