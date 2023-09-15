7bddd80d8b88be037bb210e2f0e20977060afed4
Adam Rackis <adamrackis@gmail.com>
2023-09-14 21:44:53 -0500

    Schema update

--- svelte-kit/src/db/schema.ts
+++ svelte-kit/src/db/schema.ts
@@ -25,6 +25,19 @@ export const books = mysqlTable("books", {
   editorialReviews: json("editorialReviews")
 });
 
+export const similarBooks = mysqlTable("similar_books", {
+  id: int("id").primaryKey().autoincrement(),
+  title: varchar("title", { length: 250 }).notNull(),
+  authors: json("authors").$type<string[]>(),
+  authorsLastManualSync: datetime("authorsLastManualSync"),
+  isbn: varchar("isbn", { length: 25 }),
+
+  mobileImage: varchar("mobileImage", { length: 250 }),
+  mobileImagePreview: json("mobileImagePreview"),
+  smallImage: varchar("smallImage", { length: 250 }),
+  smallImagePreview: json("smallImagePreview")
+});
+
 export const tags = mysqlTable("tags", {
   id: int("id").primaryKey().autoincrement(),
   userId: varchar("userId", { length: 50 }).notNull(),
@@ -37,4 +50,19 @@ export const booksTags = mysqlTable("books_tags", {
   id: int("id").notNull().autoincrement(),
   book: int("book").notNull(),
   tag: int("tag").notNull()
+});
+
+export const subjects = mysqlTable("subjects", {
+  id: int("id").primaryKey().autoincrement(),
+  userId: varchar("userId", { length: 50 }).notNull(),
+  name: varchar("name", { length: 255 }).notNull(),
+  path: varchar("path", { length: 255 }),
+  textColor: varchar("textColor", { length: 255 }),
+  backgroundColor: varchar("backgroundColor", { length: 255 })
+});
+
+export const booksSubjects = mysqlTable("books_subjects", {
+  id: int("id").notNull().autoincrement(),
+  book: int("book").notNull(),
+  subject: int("subject").notNull()
 });
