33904a0c3717d5fecf9f7759b1a060801abea088
Adam Rackis <adamrackis@gmail.com>
2023-09-17 20:46:45 -0500

    Next drizzle conversions

--- svelte-kit/src/data/similar-books.ts
+++ svelte-kit/src/data/similar-books.ts
@@ -1,7 +1,6 @@
-import { executeCommand, executeQuery } from "./dbUtils";
-import type { BookWithSimilarItems, SimilarBook } from "./types";
-
-const LIMIT = 50;
+import { and, eq, exists, inArray, getTableColumns, or, sql, type SQLWrapper, like, desc, isNotNull } from "drizzle-orm";
+import { db, executeDrizzle } from "./dbUtils";
+import { booksSubjects, books as booksTable, similarBooks } from "./drizzle-schema";
 
 type QueryProps = {
   userId?: string;
@@ -9,59 +8,73 @@ type QueryProps = {
 };
 
 export const getBooksWithSimilarBooks = async ({ userId, subjects }: QueryProps = {}) => {
-  let filters = [];
-  const args: any[] = [];
+  const filters: SQLWrapper[] = [];
+
   if (userId) {
-    filters.push(" userId = ? ");
-    args.push(userId);
+    filters.push(eq(booksTable.userId, userId));
   }
 
   if (subjects?.length) {
-    filters.push(" EXISTS (SELECT 1 FROM books_subjects bs WHERE bs.book = b.id AND bs.subject IN (?)) ");
-    args.push(subjects);
+    filters.push(
+      exists(
+        db
+          .select({ _: sql`1` })
+          .from(booksSubjects)
+          .where(
+            and(
+              eq(booksSubjects.book, booksTable.id),
+              inArray(
+                booksSubjects.subject,
+                subjects.map(s => Number(s))
+              )
+            )
+          )
+      )
+    );
   }
 
-  const filtersString = filters.length ? ` AND (${filters.join(" AND ")}) ` : "";
-
-  const eligibleBooks = await executeQuery<BookWithSimilarItems>(
+  const { id, title, authors, isbn, smallImage, smallImagePreview, similarBooks } = getTableColumns(booksTable);
+  const eligibleBooks = await executeDrizzle(
     "books that might have similar books",
-    `
-      SELECT id, title, authors, isbn, smallImage, smallImagePreview, similarBooks, DATE_FORMAT(similarBooksLastSync, '%Y-%m-%dT%TZ') similarBooksLastSync
-      FROM books b
-      WHERE (CHAR_LENGTH(isbn) = 10 OR (CHAR_LENGTH(isbn) = 13 AND isbn LIKE '978%')) ${filtersString}
-      ORDER BY id DESC
-      LIMIT 50;
-    `,
-    args
+    db
+      .select({
+        id,
+        title,
+        authors,
+        isbn,
+        smallImage,
+        smallImagePreview,
+        similarBooks,
+        similarBooksLastSync: sql`DATE_FORMAT(${booksTable.similarBooksLastSync}, '%Y-%m-%dT%TZ')`
+      })
+      .from(booksTable)
+      .where(
+        and(
+          // isbn 10 or old-school 13
+          or(sql`CHAR_LENGTH(${booksTable.isbn}) = 10`, and(sql`CHAR_LENGTH(${booksTable.isbn}) = 13`, like(booksTable.isbn, "978%"))),
+          ...filters
+        )
+      )
+      .orderBy(desc(booksTable.id))
+      .limit(50)
   );
 
   return eligibleBooks;
 };
 
 export const clearSync = async (id: number) => {
-  const eligibleBooks = await executeCommand(
-    "clear sync",
-    `
-      UPDATE books
-      SET similarBooksLastSync = NULL
-      WHERE id = ?;
-    `,
-    [id]
-  );
+  await executeDrizzle("clear sync", db.update(booksTable).set({ similarBooksLastSync: null }).where(eq(booksTable.id, id)));
 };
 
 export const getSimilarBooksForBook = async (id: number) => {
-  const similarBooks = await executeQuery<SimilarBook>(
+  const result = await executeDrizzle(
     "similar books for book",
-    `
-      SELECT sb.*
-      FROM books b
-      LEFT JOIN similar_books sb
-      ON JSON_SEARCH(b.similarBooks, 'one', sb.isbn)
-      WHERE b.id = ? AND sb.id IS NOT NULL;
-    `,
-    [id]
+    db
+      .select(getTableColumns(similarBooks))
+      .from(booksTable)
+      .leftJoin(similarBooks, sql`JSON_SEARCH(${booksTable.similarBooks}, 'one', ${similarBooks.isbn})`)
+      .where(and(eq(booksTable.id, id), isNotNull(similarBooks.id)))
   );
 
-  return similarBooks;
+  return result;
 };
