2d6a5cc11ad78b6c313d2f1237aca876c04925e2
Adam Rackis <adamrackis@hotmail.com>
2023-10-03 08:37:45 -0500

    Tooltip tweaks

--- svelte-kit/src/routes/(core-data)/(home)/data-vis/barChart/Tooltip.svelte
+++ svelte-kit/src/routes/(core-data)/(home)/data-vis/barChart/Tooltip.svelte
@@ -10,19 +10,26 @@
 </script>
 
 <div class={"tooltip-root flex flex-col gap-3 bg-slate-400 text-black rounded p-2 " + position}>
-  <div class="flex items-baseline gap-2 text-lg">
-    <span class="name">{data.display}: {data.count} Books</span>
-    <button class="raw-button flex" on:click={() => removeBar(data.groupId)}><i class="fad fa-times-circle" /></button>
-  </div>
-
-  {#if data.childSubjects?.length}
-    <div class="text-base">
-      <button on:click={runDrilldown} class="raw-button flex gap-2">
-        <span class="leading-none text-black">View</span>
-        <i class="far fa-chart-bar leading-none text-black" />
-      </button>
+  <div class="flex flex-col gap-2">
+    <div class="flex flex-col gap-2">
+      <div class="flex items-baseline gap-2 md:text-lg text-xs">
+        <span class="name">{data.display}</span>
+        <button class="raw-button flex" on:click={() => removeBar(data.groupId)}><i class="fad fa-times-circle" /></button>
+      </div>
+      <hr class="border-slate-600 m-0" />
     </div>
-  {/if}
+    <div class="md:text-base text-xs">
+      <span>{data.count} Books</span>
+    </div>
+    {#if data.childSubjects?.length}
+      <div class="md:text-base text-xs">
+        <button on:click={runDrilldown} class="raw-button flex gap-2">
+          <span class="leading-none text-black">View</span>
+          <i class="far fa-chart-bar leading-none text-black" />
+        </button>
+      </div>
+    {/if}
+  </div>
 
   <div class={"arrow " + position} />
 </div>
