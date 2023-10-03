3932eb56ce6e7deeddac1705b3efa84b9fdf97e9
Adam Rackis <adamrackis@hotmail.com>
2023-10-03 17:06:15 -0500

    Tooltip tweaks

--- svelte-kit/src/routes/(core-data)/(home)/data-vis/barChart/Tooltip.svelte
+++ svelte-kit/src/routes/(core-data)/(home)/data-vis/barChart/Tooltip.svelte
@@ -22,22 +22,22 @@
   const runDrilldown = () => drilldown(data.childSubjects, data.display);
 </script>
 
-<div class={"tooltip-root flex flex-col gap-3 bg-slate-400 text-black rounded p-2 " + position}>
+<div class={"tooltip-root flex flex-col gap-3 bg-slate-400 text-black rounded md:p-2 p-[6px] " + position}>
   <div class="flex flex-col gap-2">
     <div class="flex flex-col gap-2">
       <div class="flex items-baseline gap-2 md:text-lg text-xs">
         <span class="name">{data.display}</span>
         <button class="raw-button flex" on:click={() => removeBar(data.groupId)}><i class="fad fa-times-circle" /></button>
       </div>
-      <hr class="border-slate-600 m-0" />
+      <hr class="border-slate-600 my-0" />
     </div>
-    <div class="md:text-base text-xs">
-      <span>{data.count} Books</span>
+    <div class="md:text-base text-xs pl-1">
+      <span>{data.count} {data.count === 1 ? "Book" : "Books"}</span>
     </div>
     {#if data.childSubjects?.length}
-      <div class="md:text-base text-xs">
+      <div class="md:text-base text-xs pl-1">
         <button on:click={runDrilldown} class="raw-button flex gap-2">
-          <span class="leading-none text-black">View</span>
+          <span class="leading-none text-black">Drilldown</span>
           <i class="far fa-chart-bar leading-none text-black" />
         </button>
       </div>
