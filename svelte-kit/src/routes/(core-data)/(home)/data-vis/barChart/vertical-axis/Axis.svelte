626298dde6a33013cb062d1d7eb3d1a9e75fd74b
Adam Rackis <adamrackis@gmail.com>
2023-10-21 19:41:39 -0500

    cleanup

--- svelte-kit/src/routes/(core-data)/(home)/data-vis/barChart/vertical-axis/Axis.svelte
+++ svelte-kit/src/routes/(core-data)/(home)/data-vis/barChart/vertical-axis/Axis.svelte
@@ -11,11 +11,9 @@
   $: axisSpring.update(state => ({ ...state, height: graphHeight }));
 </script>
 
-<g>
-  <g {transform}>
-    <path fill="none" stroke="black" d="M50,0 H50.5 V{$axisSpring.height}" />
-    {#each data.filter((_, idx) => idx !== 0).map((d, idx) => [d, idx]) as d (d[1])}
-      <Tick {scale} d={d[0]} />
-    {/each}
-  </g>
+<g {transform}>
+  <path fill="none" stroke="black" d="M50,0 H50.5 V{$axisSpring.height}" />
+  {#each data.filter((_, idx) => idx !== 0).map((d, idx) => [d, idx]) as d (d[1])}
+    <Tick {scale} d={d[0]} />
+  {/each}
 </g>
