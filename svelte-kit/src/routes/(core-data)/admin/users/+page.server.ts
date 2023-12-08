89d873b26b2fe0f548ad072e102e730fce877675
Adam Rackis <adamrackis@hotmail.com>
2023-12-08 00:50:21 -0600

    query

--- svelte-kit/src/routes/(core-data)/admin/users/+page.server.ts
+++ svelte-kit/src/routes/(core-data)/admin/users/+page.server.ts
@@ -6,3 +6,20 @@ GROUP BY userId
 ORDER BY MAX(dateAdded) DESC
 
 */
+
+/*
+
+GS1
+
+GSI1PK
+ACCOUNT#google
+GSI1SK
+ACCOUNT#203374502437829341114
+
+TABLE
+
+pk: USER#c347xxx
+sk: USER#c347xxx
+
+
+*/
