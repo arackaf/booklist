b2c62f5fe47ed50bb70adaa1d06b026680d03e8b
Adam Rackis <adamrackis@hotmail.com>
2023-12-28 22:18:02 -0600

    remove raw sql query

--- svelte-kit/src/data/user-summary.ts
+++ svelte-kit/src/data/user-summary.ts
@@ -5,51 +5,6 @@ import { db, executeQuery } from "./dbUtils";
 
 export const userSummary = async (userId: string): Promise<{}> => {
   try {
-    false &&
-      (await executeQuery(
-        "Raw",
-        `
-      SELECT 'MIN SUBJECT' label, s.name, COUNT(*)
-      FROM books b
-      JOIN books_subjects bs
-          ON b.id = bs.book AND userId = '60a93babcc3928454b5d1cc6'
-      JOIN subjects s
-          ON bs.subject = s.id
-      GROUP BY bs.subject, s.name
-      HAVING COUNT(*) = (
-          SELECT COUNT(*)
-          FROM books b2
-          JOIN books_subjects bs2
-              ON b2.id = bs2.book
-              AND b2.userId = '60a93babcc3928454b5d1cc6'
-          GROUP BY bs2.subject
-          ORDER BY COUNT(*)
-          LIMIT 1
-      )
-      UNION ALL
-      SELECT 'MAX SUBJECT' label, s.name, COUNT(*)
-      FROM books b
-      JOIN books_subjects bs
-          ON b.id = bs.book AND userId = '60a93babcc3928454b5d1cc6'
-      JOIN subjects s
-          ON bs.subject = s.id
-      GROUP BY bs.subject, s.name
-      HAVING COUNT(*) = (
-          SELECT COUNT(*)
-          FROM books b2
-          JOIN books_subjects bs2
-              ON b2.id = bs2.book
-              AND b2.userId = '60a93babcc3928454b5d1cc6'
-          GROUP BY bs2.subject
-          ORDER BY COUNT(*) DESC
-          LIMIT 1
-      )
-      ORDER BY label, name    
-    `
-      ).then(val => {
-        console.log(val);
-      }));
-
     const subjectCounts = () =>
       db
         .select({ _: sql<number>`COUNT(*)` })
