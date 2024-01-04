1535a14c94f2037eb1fa88e6733922b0f49c34b0
Adam Rackis <adamrackis@gmail.com>
2024-01-03 19:23:58 -0600

    rename

--- svelte-kit/src/lib/components/section-nav/SectionNav.svelte
+++ svelte-kit/src/lib/components/section-nav/SectionNav.svelte
@@ -1,4 +1,4 @@
-<div class="sticky-sub-content mb-1 z-[3] pb-1 bg-[var(--content-background)]">
+<div class="sticky-content mb-1 z-[3] pb-1 bg-[var(--content-background)]">
   <nav class="flex border-b border-b-[var(--primary-9)] mt-[calc(-1*var(--main-spacing-top))]">
     <slot />
   </nav>
