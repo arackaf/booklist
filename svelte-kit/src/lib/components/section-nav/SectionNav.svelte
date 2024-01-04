b0aa6baab09425c98a7dfcc114946c718ef6ec5a
Adam Rackis <adamrackis@gmail.com>
2024-01-03 19:14:10 -0600

    Update other position sticky's

--- svelte-kit/src/lib/components/section-nav/SectionNav.svelte
+++ svelte-kit/src/lib/components/section-nav/SectionNav.svelte
@@ -1,4 +1,4 @@
-<div class="mb-1 z-[3] sticky top-0 pb-1 bg-[var(--content-background)]">
+<div class="mb-1 z-[3] sticky top-12 pb-1 bg-[var(--content-background)]">
   <nav class="flex border-b border-b-[var(--primary-9)] mt-[calc(-1*var(--main-spacing-top))]">
     <slot />
   </nav>
