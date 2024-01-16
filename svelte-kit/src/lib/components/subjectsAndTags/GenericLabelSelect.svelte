bb5b921dc345d2e07c26a05b9f8cc0d5ee6f34de
Adam Rackis <adamrackis@gmail.com>
2024-01-15 22:59:22 -0600

    start to set up combobox

--- svelte-kit/src/lib/components/subjectsAndTags/GenericLabelSelect.svelte
+++ svelte-kit/src/lib/components/subjectsAndTags/GenericLabelSelect.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
+  import { Check, ChevronsUpDown } from "lucide-svelte";
+  import * as Command from "$lib/components/ui/command";
+  import * as Popover from "$lib/components/ui/popover";
+  import { Button } from "$lib/components/ui/button";
+  import { cn } from "$lib/utils";
+  import { tick } from "svelte";
+  import * as AlertDialog from "$lib/components/ui/alert-dialog";
+
   import AutoSuggest from "svelte-helpers/AutoSuggest.svelte";
   import GenericLabelDisplayItem from "./GenericLabelDisplayItem.svelte";
 
@@ -22,9 +30,81 @@
   }
 
   let inputStyles = "width: 100px; border-top-width: 0; border-right-width: 0; border-left-width: 0; border-radius: 0;";
+
+  const frameworks = [
+    {
+      value: "sveltekit",
+      label: "SvelteKit"
+    },
+    {
+      value: "next.js",
+      label: "Next.js"
+    },
+    {
+      value: "nuxt.js",
+      label: "Nuxt.js"
+    },
+    {
+      value: "remix",
+      label: "Remix"
+    },
+    {
+      value: "astro",
+      label: "Astro"
+    }
+  ];
+  let open = false;
+  let value = "";
+  $: selectedValue = frameworks.find(f => f.value === value)?.label ?? "Select a framework...";
+  // We want to refocus the trigger button when the user selects
+  // an item from the list so users can continue navigating the
+  // rest of the form with the keyboard.
+  function closeAndFocusTrigger(triggerId: string) {
+    open = false;
+    tick().then(() => {
+      document.getElementById(triggerId)?.focus();
+    });
+  }
+
+  function trap(evt: MouseEvent) {
+    evt.stopPropagation();
+    evt.stopImmediatePropagation();
+    evt.preventDefault();
+  }
 </script>
 
-<div class="generic-label-select {className}">
+<Popover.Root portal={document.body} bind:open let:ids>
+  <Popover.Trigger asChild let:builder>
+    <Button builders={[builder]} variant="outline" role="combobox" aria-expanded={open} class="w-[200px] justify-between">
+      {selectedValue}
+      <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+    </Button>
+  </Popover.Trigger>
+  <Popover.Content class="w-[200px] p-0">
+    <div id="sss" on:click={trap} on:mouseup={trap} on:pointerup={trap}>
+      <Command.Root>
+        <Command.Input placeholder="Search framework..." />
+        <Command.Empty>No framework found.</Command.Empty>
+        <Command.Group>
+          {#each frameworks as framework}
+            <Command.Item
+              value={framework.value}
+              onSelect={currentValue => {
+                value = currentValue;
+                closeAndFocusTrigger(ids.trigger);
+              }}
+            >
+              <Check class={cn("mr-2 h-4 w-4", value !== framework.value && "text-transparent")} />
+              {framework.label}
+            </Command.Item>
+          {/each}
+        </Command.Group>
+      </Command.Root>
+    </div>
+  </Popover.Content>
+</Popover.Root>
+
+<!-- <div class="generic-label-select {className}">
   <AutoSuggest
     keyField="id"
     {options}
@@ -40,4 +120,4 @@
       <GenericLabelDisplayItem item={option} />
     </span>
   </AutoSuggest>
-</div>
+</div> -->
