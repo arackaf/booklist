2febe232c836450808b7a0b5282efacd8e09ca52
Adam Rackis <adamrackis@gmail.com>
2024-01-15 21:07:47 -0600

    modal sizing tweaks

--- svelte-kit/src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
+++ svelte-kit/src/lib/components/ui/alert-dialog/alert-dialog-content.svelte
@@ -18,7 +18,9 @@
     {transition}
     {transitionConfig}
     class={cn(
-      "fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-50 grid w-full max-w-lg gap-4 border bg-background p-6 shadow-lg sm:rounded-lg md:w-full",
+      "fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-50",
+      "w-[min(95vw,600px)] sm:w-[600px]",
+      "grid gap-4 border bg-background p-6 shadow-lg sm:rounded-lg",
       className
     )}
     {...$$restProps}
