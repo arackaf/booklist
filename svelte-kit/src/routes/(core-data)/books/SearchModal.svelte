6c6cd3effd73a3f0a3772f7105f5ec0e2eb5f9bf
Adam Rackis <adamrackis@hotmail.com>
2024-01-17 20:29:59 -0600

    Simplify code

--- svelte-kit/src/routes/(core-data)/books/SearchModal.svelte
+++ svelte-kit/src/routes/(core-data)/books/SearchModal.svelte
@@ -32,10 +32,8 @@
   let localSubjects = [] as any[];
   let localTags = [] as any[];
   let noSubjects: boolean;
-  let key = 1;
 
   const onOpen = () => {
-    key++;
     syncSearchState();
     setTimeout(() => {
       titleEl?.focus();
@@ -80,91 +78,89 @@
   };
 </script>
 
-<Modal on:closed={() => key++} {isOpen} {onHide} headerCaption={"Full Search"} standardFooter={false}>
-  {#key key}
-    <form action="/books" on:formdata={onFormData} on:submit={onHide}>
-      {#if $publicUser}
-        <input type="hidden" name="user" value={$publicUser} />
-      {/if}
-      <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
-        <InputGroup labelText="Title">
-          <Input bind:inputEl={titleEl} slot="input" name="search" placeholder="Title" value={localSearchValues.search} />
-        </InputGroup>
+<Modal {isOpen} {onHide} headerCaption={"Full Search"} standardFooter={false}>
+  <form action="/books" on:formdata={onFormData} on:submit={onHide}>
+    {#if $publicUser}
+      <input type="hidden" name="user" value={$publicUser} />
+    {/if}
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
+      <InputGroup labelText="Title">
+        <Input bind:inputEl={titleEl} slot="input" name="search" placeholder="Title" value={localSearchValues.search} />
+      </InputGroup>
 
-        <InputGroup labelText="Publisher">
-          <Input slot="input" name="publisher" value={localSearchValues.publisher} placeholder="Publisher" />
-        </InputGroup>
+      <InputGroup labelText="Publisher">
+        <Input slot="input" name="publisher" value={localSearchValues.publisher} placeholder="Publisher" />
+      </InputGroup>
 
-        <InputGroup labelText="Author">
-          <Input slot="input" name="author" value={localSearchValues.author} placeholder="Author" />
-        </InputGroup>
+      <InputGroup labelText="Author">
+        <Input slot="input" name="author" value={localSearchValues.author} placeholder="Author" />
+      </InputGroup>
 
-        <div class="flex flex-col">
-          <label for="isReadE" class="text-sm">Is Read?</label>
-          <div class="flex-1 flex flex-row gap-4 items-center mt-1 sm:mt-0">
-            <div class="flex flex-row items-center gap-1">
-              <input type="radio" checked={localSearchValues.isRead === ""} name="is-read" id="isReadE" value="off" />
-              <label for="isReadE">Either</label>
-            </div>
-            <div class="flex flex-row items-center gap-1">
-              <input type="radio" checked={localSearchValues.isRead === "true"} name="is-read" id="isReadY" value="true" />
-              <label for="isReadY">Yes</label>
-            </div>
-            <div class="flex flex-row items-center gap-1">
-              <input type="radio" checked={localSearchValues.isRead === "false"} name="is-read" id="isReadN" value="false" />
-              <label for="isReadN">No</label>
-            </div>
+      <div class="flex flex-col">
+        <label for="isReadE" class="text-sm">Is Read?</label>
+        <div class="flex-1 flex flex-row gap-4 items-center mt-1 sm:mt-0">
+          <div class="flex flex-row items-center gap-1">
+            <input type="radio" checked={localSearchValues.isRead === ""} name="is-read" id="isReadE" value="off" />
+            <label for="isReadE">Either</label>
+          </div>
+          <div class="flex flex-row items-center gap-1">
+            <input type="radio" checked={localSearchValues.isRead === "true"} name="is-read" id="isReadY" value="true" />
+            <label for="isReadY">Yes</label>
+          </div>
+          <div class="flex flex-row items-center gap-1">
+            <input type="radio" checked={localSearchValues.isRead === "false"} name="is-read" id="isReadN" value="false" />
+            <label for="isReadN">No</label>
           </div>
         </div>
-        <SelectGroup labelText="Sort">
-          <Select slot="select" name="sort" value={localSearchValues.sortPacket || "id-desc"}>
-            {#each Object.entries(sortDisplayLookup) as [sortVal, display]}
-              <option value={sortVal}>{display}</option>
-            {/each}
-          </Select>
-        </SelectGroup>
+      </div>
+      <SelectGroup labelText="Sort">
+        <Select slot="select" name="sort" value={localSearchValues.sortPacket || "id-desc"}>
+          {#each Object.entries(sortDisplayLookup) as [sortVal, display]}
+            <option value={sortVal}>{display}</option>
+          {/each}
+        </Select>
+      </SelectGroup>
 
-        <SelectAndDisplayContainer isEmpty={!localTags.length} class="sm:col-span-2 pt-2">
-          <SelectAvailableTags placeholder="Tags" slot="select" {tags} currentlySelected={localTags} onSelect={selectTag} />
-          <DisplaySelectedTags slot="display" {tags} currentlySelected={localTags} onRemove={removeTag} />
-        </SelectAndDisplayContainer>
+      <SelectAndDisplayContainer isEmpty={!localTags.length} class="sm:col-span-2 pt-2">
+        <SelectAvailableTags placeholder="Tags" slot="select" {tags} currentlySelected={localTags} onSelect={selectTag} />
+        <DisplaySelectedTags slot="display" {tags} currentlySelected={localTags} onRemove={removeTag} />
+      </SelectAndDisplayContainer>
 
-        <SelectAndDisplayContainer isEmpty={!localSubjects.length} class="sm:col-span-2">
-          <SelectAvailableSubjects
-            placeholder="Subjects"
-            inputProps={{ disabled: noSubjects }}
-            slot="select"
-            subjects={allSubjects}
-            currentlySelected={localSubjects}
-            onSelect={selectSubject}
-          />
-          <DisplaySelectedSubjects
-            disabled={noSubjects}
-            slot="display"
-            subjects={allSubjects}
-            currentlySelected={localSubjects}
-            onRemove={removeSubject}
-          />
-        </SelectAndDisplayContainer>
+      <SelectAndDisplayContainer isEmpty={!localSubjects.length} class="sm:col-span-2">
+        <SelectAvailableSubjects
+          placeholder="Subjects"
+          inputProps={{ disabled: noSubjects }}
+          slot="select"
+          subjects={allSubjects}
+          currentlySelected={localSubjects}
+          onSelect={selectSubject}
+        />
+        <DisplaySelectedSubjects
+          disabled={noSubjects}
+          slot="display"
+          subjects={allSubjects}
+          currentlySelected={localSubjects}
+          onRemove={removeSubject}
+        />
+      </SelectAndDisplayContainer>
 
-        <div class="sm:col-span-2">
-          <label class="checkbox">
-            <input type="checkbox" name="child-subjects" value="true" checked={!!localSearchValues.childSubjects} />
-            Also search child subjects
-          </label>
-        </div>
-
-        <div class="sm:col-span-2">
-          <label class="checkbox">
-            <input type="checkbox" name="no-subjects" value="true" bind:checked={noSubjects} />
-            Search books with no subjects set
-          </label>
-        </div>
+      <div class="sm:col-span-2">
+        <label class="checkbox">
+          <input type="checkbox" name="child-subjects" value="true" checked={!!localSearchValues.childSubjects} />
+          Also search child subjects
+        </label>
       </div>
 
-      <div class="mt-5">
-        <Button theme="primary" text="Filter">Search</Button>
+      <div class="sm:col-span-2">
+        <label class="checkbox">
+          <input type="checkbox" name="no-subjects" value="true" bind:checked={noSubjects} />
+          Search books with no subjects set
+        </label>
       </div>
-    </form>
-  {/key}
+    </div>
+
+    <div class="mt-5">
+      <Button theme="primary" text="Filter">Search</Button>
+    </div>
+  </form>
 </Modal>
