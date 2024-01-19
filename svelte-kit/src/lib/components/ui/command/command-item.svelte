34feb8cdaa1e0e6aa761e2a5ae10c4df826716dd
Adam Rackis <adamrackis@hotmail.com>
2024-01-19 13:28:10 -0600

    command-item disabled state cursor

--- svelte-kit/src/lib/components/ui/command/command-item.svelte
+++ svelte-kit/src/lib/components/ui/command/command-item.svelte
@@ -10,7 +10,7 @@
 
 <CommandPrimitive.Item
   class={cn(
-    "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+    "relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:cursor-not-allowed data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
     className
   )}
   {...$$restProps}
