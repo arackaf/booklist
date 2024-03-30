13e2f787d8070ab7f87af332e12d14b2eca149c9
Adam Rackis <adamrackis@hotmail.com>
2024-03-30 15:53:40 -0500

    remove comments

--- svelte-kit/src/routes/(core-data)/admin/users/+page.server.ts
+++ svelte-kit/src/routes/(core-data)/admin/users/+page.server.ts
@@ -67,29 +67,3 @@ export const load = async ({ parent }) => {
 
   return { userUsageInfo, missingUserInfo };
 };
-
-/*
-
-SELECT userId, DATEDIFF(NOW(), MAX(dateAdded)) daysAgo, MAX(dateAdded) latest
-FROM books
-GROUP BY userId
-ORDER BY MAX(dateAdded) DESC
-
-*/
-
-/*
-
-GS1
-
-GSI1PK
-ACCOUNT#google
-GSI1SK
-ACCOUNT#203374502437829341114
-
-TABLE
-
-pk: USER#c347xxx
-sk: USER#c347xxx
-
-
-*/
