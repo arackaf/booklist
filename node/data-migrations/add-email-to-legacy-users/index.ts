f4dc9ac0f88f55f3883105c8474a0ef729eecc97
Adam Rackis <adamrackis@hotmail.com>
2024-03-30 23:54:39 -0500

    last tweaks to user sync script

--- node/data-migrations/add-email-to-legacy-users/index.ts
+++ node/data-migrations/add-email-to-legacy-users/index.ts
@@ -1,14 +1,12 @@
 import { DynamoDB, type ScanCommandInput, type DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-import { Client, type Transaction, type ExecutedQuery, type Connection } from "@planetscale/database";
+import { Client } from "@planetscale/database";
 
 require("dotenv").config();
 
 export const mySqlConnectionFactory = new Client({
   url: process.env.MYSQL_CONNECTION_STRING
 });
-
-console.log(DynamoDB, DynamoDBDocument);
 
 const AMAZON_ACCESS_KEY = process.env.AMAZON_ACCESS_KEY;
 const AMAZON_SECRET_KEY = process.env.AMAZON_SECRET_KEY;
@@ -21,9 +19,10 @@ const dynamoConfig: DynamoDBClientConfig = {
 
   region: "us-east-1"
 };
+const conn = mySqlConnectionFactory.connection();
 
 const TABLE_NAME = "My_Library_dev";
-const LIMIT = 5;
+const LIMIT = 50;
 
 const dynamo = DynamoDBDocument.from(new DynamoDB(dynamoConfig));
 
@@ -32,18 +31,25 @@ const scanInput: ScanCommandInput = {
   Limit: LIMIT
 };
 
+const lookup = await getUnsyncdUsers();
+
+console.log("Looking for", lookup.size);
+console.log([...lookup.values()].join(", "));
+
+let i = 1;
 let lastKey = undefined;
+
 do {
   const results = await getNext(lastKey);
   lastKey = results.LastEvaluatedKey;
 
   for (const item of results.Items) {
-    console.log(item.pk);
+    console.log(i++, item.pk);
 
     if (item.pk.startsWith("User#")) {
       const { userId, email } = item;
 
-      if (userId && email) {
+      if (userId && email && lookup.has(userId)) {
         await updateUser(userId, email);
       }
     }
@@ -51,7 +57,25 @@ do {
 } while (lastKey);
 
 async function getNext(lastKey: Record<string, string> | undefined) {
+  await new Promise(res => setTimeout(res, 1000));
   return dynamo.scan({ ...scanInput, ExclusiveStartKey: lastKey });
+}
+
+async function getUnsyncdUsers() {
+  try {
+    const result = await executeSQLRaw(
+      `
+      SELECT userId 
+      FROM user_info_cache
+      WHERE (email IS NULL OR email = '') AND provider = 'Legacy'
+      `,
+      []
+    );
+
+    return new Set(result.rows.map(r => r.userId));
+  } catch (er) {
+    console.log("Error updating user", er);
+  }
 }
 
 async function updateUser(userId: string, email: string) {
@@ -70,8 +94,10 @@ async function updateUser(userId: string, email: string) {
 }
 
 async function executeSQLRaw(sql: string, args: any[] = []) {
-  const conn = mySqlConnectionFactory.connection();
+  await new Promise(res => setTimeout(res, 150));
   const result = await conn.execute(sql, args);
 
   console.log("Size --->", result.rowsAffected, "\n");
+
+  return result;
 }
