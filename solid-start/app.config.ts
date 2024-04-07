9572f9550823ec98a4025dd67fa1b6b8834d8aa4
Adam Rackis <adamrackis@hotmail.com>
2024-04-07 16:17:09 -0500

    attempt to use vercel adapter

--- solid-start/app.config.ts
+++ solid-start/app.config.ts
@@ -1,3 +1,10 @@
+import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    // @ts-ignore
+    plugins: solid({ ssr: true, adapter: vercel({ edge: true }) })
+  }
+});
