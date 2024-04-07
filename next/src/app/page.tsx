f53afe2fa73fe61e2c669fab3794ed1e675841a9
Adam Rackis <adamrackis@hotmail.com>
2024-04-07 16:25:40 -0500

    Add delay

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { getJunkValue } from "../../../data/junk";
 
 export default async function Home() {
+  await new Promise(res => setTimeout(res, 1500));
   const val = await getJunkValue();
   return <div className="m-5">Hello World {val}</div>;
 }
