55596cdfe8fda3f4d94beae3f7380e54fe5da0b6
Adam Rackis <adamrackis@hotmail.com>
2024-04-07 16:21:11 -0500

    Revert "attempt to use vercel adapter"
    This reverts commit 9572f9550823ec98a4025dd67fa1b6b8834d8aa4.

--- solid-start/app.config.ts
+++ solid-start/app.config.ts
@@ -1,10 +1,3 @@
-import solid from "solid-start/vite";
-import vercel from "solid-start-vercel";
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({
-  vite: {
-    // @ts-ignore
-    plugins: solid({ ssr: true, adapter: vercel({ edge: true }) })
-  }
-});
+export default defineConfig({});
