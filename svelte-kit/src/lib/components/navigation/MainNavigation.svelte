b7e36cab5626dd065d98c087f1a8f80594b2d76e
Adam Rackis <adamrackis@hotmail.com>
2024-04-27 18:40:31 -0500

    Revert "small fix"
    This reverts commit 7951b20bfc9ef9bc29defcb87ed74bac9c4c161b.

--- svelte-kit/src/lib/components/navigation/MainNavigation.svelte
+++ svelte-kit/src/lib/components/navigation/MainNavigation.svelte
@@ -69,7 +69,7 @@
     <ProfilePanel {userSummary} {loggedInUser} open={profilePanelOpen} onClose={() => (profilePanelOpen = false)} />
   {/if}
   <nav class="nav flex bg-[var(--primary-4)] h-12 text-base">
-    {#if loggedInUser}
+    {#if loggedIn}
       <div class="items-center mx-2 my-auto">
         <button
           on:mouseenter={fetchUserSummaryIfNeeded}
