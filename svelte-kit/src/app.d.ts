55e1a953f88acef6b385ae621621fe8878670e61
Adam Rackis <adamrackis@hotmail.com>
2024-05-04 22:11:02 -0500

    type cleanup

--- svelte-kit/src/app.d.ts
+++ svelte-kit/src/app.d.ts
@@ -1,14 +1,12 @@
 // See https://kit.svelte.dev/docs/types#app for information about these interfaces and what to do when importing types
 
 import type { ScanWebSocketManage } from "$lib/util/scanUtils";
-import { DefaultSession } from "@auth/core/types";
 
 declare module "@auth/core/types" {
   interface Session {
     userId: string;
     provider: string;
     legacySync: boolean;
-    user: DefaultSession["user"];
   }
 }
 
