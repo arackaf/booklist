7cef0a4176f3586578314d6c141460b4f4e99701
Adam Rackis <adamrackis@hotmail.com>
2024-07-01 13:39:37 -0500

    work

--- svelte-kit/src/lib/components/editBook/EditBookInfo.svelte
+++ svelte-kit/src/lib/components/editBook/EditBookInfo.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-  import Button from "$lib/components/Button/Button.svelte";
-  import Input from "../form-elements/Input/Input.svelte";
-  import InputGroup from "../form-elements/Input/InputGroup.svelte";
-
   export let book: any;
 
   const updateBook = (updates: any) => {
@@ -14,18 +10,8 @@
   };
 </script>
 
-<fieldset>
-  <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
-    <div class="sm:col-span-2 grid grid-cols-3 gap-x-5 gap-y-4">
-      {#each book.authors || [] as author, index (index)}
-        <InputGroup labelText="Author">
-          <Input slot="input" name="authors" bind:value={author} placeholder={`Author ${index + 1}`} />
-        </InputGroup>
-      {/each}
-    </div>
+{#each book.authors || [] as author, index (index)}
+  <input name="authors" bind:value={author} placeholder={`Author ${index + 1}`} />
+{/each}
 
-    <div class="sm:col-span-2">
-      <Button size="sm" type="button" on:click={addAuthor}><i class="far fa-fw fa-plus"></i>Add author</Button>
-    </div>
-  </div>
-</fieldset>
+<button type="button" on:click={addAuthor}><i class="far fa-fw fa-plus"></i>Add author</button>
