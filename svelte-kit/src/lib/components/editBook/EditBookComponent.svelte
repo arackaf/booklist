f3d507fe03131e47c7e0d22a8f674e691984eeb5
Adam Rackis <adamrackis@hotmail.com>
2024-07-01 13:24:18 -0500

    work

--- svelte-kit/src/lib/components/editBook/EditBookComponent.svelte
+++ svelte-kit/src/lib/components/editBook/EditBookComponent.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import cloneDeep from "lodash.clonedeep";
-
   import EditBook from "./EditBook.svelte";
 
   export let book: any;
 
-  $: editingBook = book ? cloneDeep(book) : book;
+  $: editingBook = book;
 </script>
 
 <EditBook book={editingBook} />
