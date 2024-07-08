79adaea23e005969181f0e93f7effd369df8cec4
Adam Rackis <adamrackis@hotmail.com>
2024-07-08 18:06:43 -0500

    start

---
+++ svelte-kit/src/routes/blog/Foo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" generics="T extends {id: number }">
+  type Props = {
+    data: T[];
+    addItems: (item: T) => void;
+  };
+
+  let { data, addItems }: Props = $props();
+</script>
+
+<span>{data.length}</span>
---
+++ svelte-kit/src/routes/blog/page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Foo from "./Foo.svelte";
+
+  type X = {
+    //id: number;
+    name: string;
+  };
+
+  function add(item: X) {}
+
+  let data: X[] = [];
+</script>
+
+<Foo {data} addItems={add}></Foo>
