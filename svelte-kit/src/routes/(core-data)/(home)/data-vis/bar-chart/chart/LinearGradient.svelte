6f2969f9e97e6bad107161f33342bca95c180ea9
Adam Rackis <adamrackis@hotmail.com>
2024-08-09 19:23:12 -0500

    work

---
+++ svelte-kit/src/routes/(core-data)/(home)/data-vis/bar-chart/chart/LinearGradient.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let id: string;
+  export let colors: string[];
+
+  $: interval = 100 / colors.length;
+</script>
+
+<linearGradient {id} x1="0%" y1="0%" x2="0%" y2="100%">
+  {#each colors as color, idx}
+    <stop offset={(interval * (idx + 1)).toFixed(1) + "%"} style="stop-color: {color}; stop-opacity:1" />
+    {#if idx < colors.length - 1}
+      <stop offset={(interval * (idx + 1)).toFixed(1) + "%"} style="stop-color: {colors[idx + 1]}; stop-opacity:1" />
+    {/if}
+  {/each}
+</linearGradient>
