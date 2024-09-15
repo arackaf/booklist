4801bff372cd22ae5828543f34c49a570f3c7095
Adam Rackis <adamrackis@hotmail.com>
2024-09-15 09:20:24 -0500

    convert next component

--- svelte-kit/src/lib/components/subjectsAndTags/GenericLabelDisplayItem.svelte
+++ svelte-kit/src/lib/components/subjectsAndTags/GenericLabelDisplayItem.svelte
@@ -2,8 +2,9 @@
   import type { HierarchicalLabel } from "./types";
   import LabelDisplay from "./LabelDisplay.svelte";
 
-  export let item: HierarchicalLabel;
-  let effectiveChildLevel = item.childLevel - (item.prepend || []).length || 0;
+  let { item }: { item: HierarchicalLabel } = $props();
+
+  let effectiveChildLevel = $derived(item.childLevel - (item.prepend || []).length || 0);
 </script>
 
 <span style="display: flex; margin-left: {effectiveChildLevel * 15}px">
