f70ecd1a87279373687cf4fdd79f22f0bc8525dd
Adam Rackis <adamrackis@hotmail.com>
2024-09-19 22:13:10 -0500

    ColorsPalette component

--- svelte-kit/src/lib/components/ColorsPalette.svelte
+++ svelte-kit/src/lib/components/ColorsPalette.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-  export let currentColor: string | null;
-  export let colors: string[];
-  export let onColorChosen: (color: string) => {};
+  type Props = {
+    currentColor: string | null;
+    colors: string[];
+    onColorChosen: (color: string) => void;
+  };
+
+  let { currentColor, colors, onColorChosen }: Props = $props();
 </script>
 
 <div>
   {#each colors as color}
     <button
       class="float-left border border-black h-[20px] w-[20px] m-1 cursor-pointer transition-transform"
-      class:scale-125={color == currentColor}
-      on:click={() => onColorChosen(color)}
+      class:scale-125={color === currentColor}
+      onclick={() => onColorChosen(color)}
       type="button"
       style="background-color: {color}"
-   ></button>
+    >
+    </button>
   {/each}
 </div>
