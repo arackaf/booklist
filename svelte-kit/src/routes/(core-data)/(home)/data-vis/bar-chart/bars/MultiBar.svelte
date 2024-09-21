1b6c8e7cec00039beb5ea51872070353274017aa
Adam Rackis <adamrackis@hotmail.com>
2024-09-21 15:57:39 -0500

    MultiBar converted

--- svelte-kit/src/routes/(core-data)/(home)/data-vis/bar-chart/bars/MultiBar.svelte
+++ svelte-kit/src/routes/(core-data)/(home)/data-vis/bar-chart/bars/MultiBar.svelte
@@ -1,76 +1,48 @@
 <script lang="ts">
   import { spring } from "svelte/motion";
-  import { type Position } from "../../tooltip/tooltipUtils";
+  import type { Position } from "../../tooltip/tooltipUtils";
   import { getContext } from "svelte";
   import type { createTooltipState } from "../../tooltip/tooltipState";
   import BarPath from "./BarPath.svelte";
 
-  export let drilldown: any;
-  export let data: any;
-  export let height: any;
-  export let width: any;
-  export let x: any;
-  export let totalSvgWidth: any;
-  export let position: Position;
-  export let removeBar: (id: string) => void;
+  type Props = {
+    drilldown: any;
+    data: any;
+    height: number;
+    width: number;
+    x: number;
+    totalSvgWidth: number;
+    position: Position;
+    removeBar: (id: string) => void;
+    noInitialAnimation: boolean;
+  };
 
-  export let noInitialAnimation: boolean;
+  let { drilldown, data, height, width, x, totalSvgWidth, position, removeBar, noInitialAnimation }: Props = $props();
 
-  let g: SVGElement;
-  let initialRenderFinished = false;
+  let g = $state<SVGElement>();
+  let initialRenderFinished = $state(false);
   const tooltipState = getContext("tooltip-state") as ReturnType<typeof createTooltipState>;
-
-  $: _colors = data.entries.map((e: any) => e.color);
-  let colors: any[] = [];
-
-  let itemsClamped: any = {};
-
-  $: {
-    let heightUsed = 0;
-    let count = _colors.length;
-    let currentHeight = $barSpring.height;
-    colors = _colors.map((color: any, i: any) => {
-      let isLast = i + 1 == count;
-      let sectionHeight = ~~(currentHeight / count);
-      let finalSectionHeight = ~~(height / count);
-      let barHeight = isLast ? currentHeight - heightUsed : sectionHeight;
-
-      if (!isLast && barHeight > finalSectionHeight) {
-        itemsClamped[i] = true;
-      }
-
-      if (itemsClamped[i]) {
-        barHeight = finalSectionHeight;
-      }
-
-      const sectionVerticalStartingPoint = heightUsed;
-      heightUsed += barHeight;
-
-      return { x, y: sectionVerticalStartingPoint, height: barHeight, fill: color };
-    });
-  }
 
   const initialValues = noInitialAnimation ? { height, x } : { height: 0, x: totalSvgWidth };
   let barSpring = spring(initialValues, { stiffness: 0.1, damping: 0.4 });
 
-  $: {
-    itemsClamped = {};
+  $effect(() => {
     barSpring.set({ height, x }, { hard: noInitialAnimation && !initialRenderFinished }).then(() => {
       initialRenderFinished = true;
     });
-  }
+  });
 
   function mouseOver() {
-    tooltipState.onHover(g, { position, data, drilldown, remove: removeBar });
+    tooltipState.onHover(g!, { position, data, drilldown, remove: removeBar });
   }
 
   function mouseOut() {
     tooltipState.onMouseLeave();
   }
 
-  $: gradientId = data.groupId.replace(/,/g, "-");
+  let gradientId = $derived(data.groupId.replace(/,/g, "-"));
 </script>
 
-<g role="contentinfo" on:mouseover={mouseOver} on:mouseout={mouseOut} bind:this={g} on:focus={() => {}} on:blur={() => {}}>
+<g role="contentinfo" onmouseover={mouseOver} onmouseout={mouseOut} bind:this={g} onfocus={() => {}} onblur={() => {}}>
   <BarPath x={$barSpring.x} height={$barSpring.height} {width} fill={`url(#${gradientId})`} />
 </g>
