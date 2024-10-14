f264cf3d126e083d7f5d695fca2868f9baa027bd
Adam Rackis <adamrackis@hotmail.com>
2024-10-14 10:59:42 -0500

    work

--- lambda/my-library-pg-backup/index.ts
+++ lambda/my-library-pg-backup/index.ts
@@ -7,15 +7,11 @@ const REGION = "us-east-1";
 const params = {
   Bucket: "my-library-backups",
   Key: "my-file-2",
-  Body: fs.readFileSync(path.resolve(__dirname, "dump-output.sql"))
+  Body: fs.readFileSync(path.resolve(__dirname, "/tmp/dump-output.sql"))
 };
 
 const s3 = new S3Client({
-  region: REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ID!,
-    secretAccessKey: process.env.AWS_SECRET!
-  }
+  region: REGION
 });
 
 s3.send(new PutObjectCommand(params))
--- lambda/my-library-pg-backup/run-backup.sh
+++ lambda/my-library-pg-backup/run-backup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo "HELLO"
+echo ${PG_URL}
 PG_URI_CLEANED=$(echo ${PG_URI} | sed -e 's/^"//' -e 's/"$//')
 
-pg_dump ${PG_URI_CLEANED} -f dump-output.sql
+pg_dump ${PG_URI_CLEANED} -f /tmp/dump-output.sql
+
+echo "pg_dump ${PG_URI_CLEANED} -f dump-output.sql"
 
 ./run-pg_dump
