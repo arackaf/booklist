9546f14e0f8a7e00741a18a9b0dbfdf64732023e
Adam Rackis <adamrackis@hotmail.com>
2024-10-16 10:18:01 -0500

    convert schema to pg

--- svelte-kit/src/data/drizzle-schema.ts
+++ svelte-kit/src/data/drizzle-schema.ts
@@ -1,19 +1,20 @@
 import type { EditorialReview, PreviewPacket } from "$data/types";
-import { int, datetime, tinyint, json, mysqlTable, varchar, longtext } from "drizzle-orm/mysql-core";
 
-export const books = mysqlTable("books", {
-  id: int("id").primaryKey().autoincrement(),
+import { integer, serial, timestamp, boolean, json, pgTable, varchar, text } from "drizzle-orm/pg-core";
+
+export const books = pgTable("books", {
+  id: serial("id").primaryKey(),
   userId: varchar("userId", { length: 50 }).notNull(),
-  dateAdded: datetime("dateAdded").notNull(),
+  dateAdded: timestamp("dateAdded").notNull(),
   title: varchar("title", { length: 250 }).notNull(),
   authors: json("authors").$type<string[]>(),
   isbn: varchar("isbn", { length: 25 }),
-  pages: int("pages"),
-  isRead: tinyint("isRead").notNull(),
+  pages: integer("pages"),
+  isRead: boolean("isRead").notNull(),
 
   similarBooks: json("similarBooks").$type<string[] | null>(),
-  similarBooksLastSync: datetime("similarBooksLastSync"),
-  similarBooksLastSyncSuccess: tinyint("similarBooksLastSyncSuccess"),
+  similarBooksLastSync: timestamp("similarBooksLastSync"),
+  similarBooksLastSyncSuccess: boolean("similarBooksLastSyncSuccess"),
   similarBooksLastSyncFailureReason: longtext("similarBooksLastSyncFailureReason"),
   mobileImage: varchar("mobileImage", { length: 250 }),
   mobileImagePreview: json("mobileImagePreview").$type<string | PreviewPacket | null>(),
@@ -26,11 +27,11 @@ export const books = mysqlTable("books", {
   editorialReviews: json("editorialReviews").$type<EditorialReview[]>()
 });
 
-export const similarBooks = mysqlTable("similar_books", {
-  id: int("id").primaryKey().autoincrement(),
+export const similarBooks = pgTable("similar_books", {
+  id: serial("id").primaryKey(),
   title: varchar("title", { length: 250 }).notNull(),
   authors: json("authors").$type<string[]>(),
-  authorsLastManualSync: datetime("authorsLastManualSync"),
+  authorsLastManualSync: timestamp("authorsLastManualSync"),
   isbn: varchar("isbn", { length: 25 }).notNull(),
 
   mobileImage: varchar("mobileImage", { length: 250 }),
@@ -39,23 +40,23 @@ export const similarBooks = mysqlTable("similar_books", {
   smallImagePreview: json("smallImagePreview").$type<string | PreviewPacket | null>()
 });
 
-export const tags = mysqlTable("tags", {
-  id: int("id").primaryKey().autoincrement(),
+export const tags = pgTable("tags", {
+  id: serial("id").primaryKey(),
   userId: varchar("userId", { length: 50 }).notNull(),
   name: varchar("name", { length: 255 }).notNull(),
   textColor: varchar("textColor", { length: 255 }),
   backgroundColor: varchar("backgroundColor", { length: 255 })
 });
 
-export const booksTags = mysqlTable("books_tags", {
-  id: int("id").notNull().autoincrement(),
+export const booksTags = pgTable("books_tags", {
+  id: serial("id").primaryKey(),
   userId: varchar("userId", { length: 50 }).notNull(),
-  book: int("book").notNull(),
-  tag: int("tag").notNull()
+  book: integer("book").notNull(),
+  tag: integer("tag").notNull()
 });
 
-export const subjects = mysqlTable("subjects", {
-  id: int("id").primaryKey().autoincrement(),
+export const subjects = pgTable("subjects", {
+  id: serial("id").primaryKey(),
   userId: varchar("userId", { length: 50 }).notNull(),
   name: varchar("name", { length: 255 }).notNull(),
   path: varchar("path", { length: 255 }),
@@ -63,19 +64,19 @@ export const subjects = mysqlTable("subjects", {
   backgroundColor: varchar("backgroundColor", { length: 255 })
 });
 
-export const booksSubjects = mysqlTable("books_subjects", {
-  id: int("id").notNull().autoincrement(),
+export const booksSubjects = pgTable("books_subjects", {
+  id: serial("id").primaryKey(),
   userId: varchar("userId", { length: 50 }).notNull(),
-  book: int("book").notNull(),
-  subject: int("subject").notNull()
+  book: integer("book").notNull(),
+  subject: integer("subject").notNull()
 });
 
-export const userInfoCache = mysqlTable("user_info_cache", {
+export const userInfoCache = pgTable("user_info_cache", {
   userId: varchar("userId", { length: 50 }).notNull(),
   name: varchar("name", { length: 50 }),
   provider: varchar("provider", { length: 50 }),
   email: varchar("email", { length: 50 }),
   avatar: varchar("avatar", { length: 50 }),
   aliasUserId: varchar("aliasUserId", { length: 50 }),
-  lastSync: int("lastSync").notNull()
+  lastSync: integer("lastSync").notNull()
 });
