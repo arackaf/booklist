2f03811107ad4b75b1dc3b777bc64643a7a16e43
Adam Rackis <adamrackis@hotmail.com>
2024-10-19 00:00:02 -0600

    remove mysql utils and cleanup

--- svelte-kit/src/data/dbUtils.ts
+++ svelte-kit/src/data/dbUtils.ts
@@ -1,18 +1,13 @@
-import { Client, type Connection } from "@planetscale/database";
-import { env } from "$env/dynamic/private";
-const { MYSQL_CONNECTION_STRING } = env;
-
-import * as schema from "./drizzle-schema";
-
-import type { PgDatabase } from "drizzle-orm/pg-core";
-
 import pg from "pg";
 
+import { env } from "$env/dynamic/private";
+
+import type { PgDatabase } from "drizzle-orm/pg-core";
 import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
+import * as schema from "./drizzle-schema";
 
 import { building } from "$app/environment";
 
-export let mySqlConnectionFactory = null as any;
 export let db: PgDatabase<any, any>;
 
 if (building) {
@@ -25,28 +20,7 @@ if (building) {
   });
 
   db = drizzlePg({ schema, client: pool });
-
-  mySqlConnectionFactory = new Client({
-    url: MYSQL_CONNECTION_STRING
-  });
 }
-
-const executeSQLRaw = async (description: string, sql: string, args: any[] = []): ReturnType<Connection["execute"]> => {
-  const start = +new Date();
-  const conn = mySqlConnectionFactory.connection();
-
-  const result = await conn.execute(sql, args);
-
-  const end = +new Date();
-  console.log(description, "latency:", end - start, "query time:", result.time.toFixed(1));
-
-  return result;
-};
-
-export const executeQuery = async <T = unknown>(description: string, sql: string, args: any[] = []): Promise<T[]> => {
-  const resultRaw = await executeSQLRaw("Query: " + description, sql, args);
-  return resultRaw.rows as T[];
-};
 
 export type SubjectEditFields = {
   name: string;
