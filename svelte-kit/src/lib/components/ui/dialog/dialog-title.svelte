baca98800399f74892085d595c0641e941c14d03
Adam Rackis <adamrackis@hotmail.com>
2024-10-28 14:16:08 -0500

    upgrade popover and dialog

--- svelte-kit/src/lib/components/ui/dialog/dialog-title.svelte
+++ svelte-kit/src/lib/components/ui/dialog/dialog-title.svelte
@@ -1,16 +1,8 @@
 <script lang="ts">
-	import { Dialog as DialogPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils";
+  import { Dialog as DialogPrimitive } from "bits-ui";
+  import { cn } from "$lib/utils.js";
 
-	type $$Props = DialogPrimitive.TitleProps;
-
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+  let { ref = $bindable(null), class: className, ...restProps }: DialogPrimitive.TitleProps = $props();
 </script>
 
-<DialogPrimitive.Title
-	class={cn("text-lg font-semibold leading-none tracking-tight", className)}
-	{...$$restProps}
->
-	<slot />
-</DialogPrimitive.Title>
+<DialogPrimitive.Title bind:ref class={cn("text-lg font-semibold leading-none tracking-tight", className)} {...restProps} />
