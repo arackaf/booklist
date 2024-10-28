275676870c7dac0f4dc2412a9dcd311da7e8cd0c
Adam Rackis <adamrackis@hotmail.com>
2024-10-28 14:27:12 -0500

    install command

--- svelte-kit/src/lib/components/ui/command/command-shortcut.svelte
+++ svelte-kit/src/lib/components/ui/command/command-shortcut.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-  import { cn } from "$lib/utils";
+  import type { WithElementRef } from "bits-ui";
   import type { HTMLAttributes } from "svelte/elements";
+  import { cn } from "$lib/utils.js";
 
-  type $$Props = HTMLAttributes<HTMLSpanElement>;
-
-  let className: string | undefined | null = undefined;
-  export { className as class };
+  let { ref = $bindable(null), class: className, children, ...restProps }: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
 </script>
 
-<span class={cn("ml-auto text-xs tracking-widest text-muted-foreground", className)} {...$$restProps}>
-  <slot />
+<span bind:this={ref} class={cn("text-muted-foreground ml-auto text-xs tracking-widest", className)} {...restProps}>
+  {@render children?.()}
 </span>
