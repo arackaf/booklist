e0db200ca84370ebc40a3c7a33bcf6221a3a7dfe
Adam Rackis <adamrackis@hotmail.com>
2024-10-28 14:54:41 -0500

    work

--- svelte-kit/src/lib/components/subjectsAndTags/GenericLabelSelect.svelte
+++ svelte-kit/src/lib/components/subjectsAndTags/GenericLabelSelect.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { ChevronsUpDown } from "lucide-svelte";
   import * as Command from "$lib/components/ui/command";
-  import * as Popover from "$lib/components/ui/popover";
+  import { Popover } from "bits-ui";
   import { Button } from "$lib/components/ui/button";
   import { tick, type Snippet } from "svelte";
 
   import GenericLabelDisplayItem from "./GenericLabelDisplayItem.svelte";
+  import { Portal } from "../ui/alert-dialog";
 
   type Props = {
     options: () => any[];
@@ -30,15 +31,12 @@
       document.getElementById(triggerId)?.focus();
     });
   }
-
-  const portal = typeof document === "object" ? document.body : null;
 </script>
 
-<Popover.Root {portal} bind:open let:ids>
-  <Popover.Trigger asChild let:builder>
+<Popover.Portal>
+  <Popover.Trigger>
     <Button
       size="sm"
-      builders={[builder]}
       variant="outline"
       role="combobox"
       aria-expanded={open}
@@ -52,29 +50,31 @@
       <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
     </Button>
   </Popover.Trigger>
-  <Popover.Content avoidCollisions={false} side="bottom" class="w-[200px] p-0">
-    <div class="max-h-72 overflow-auto">
-      <Command.Root shouldFilter={false}>
-        <Command.Input bind:value={search} placeholder="Search" />
-        <Command.Empty>Nothing to select</Command.Empty>
-        <Command.Group>
-          {#each options() as option (option.id)}
-            <Command.Item
-              disabled={option.disabled}
-              value={option.id + ""}
-              onSelect={currentValue => {
-                const item = options().find(opt => opt.id == currentValue);
-                if (item) {
-                  onItemSelected(item);
-                }
-                closeAndFocusTrigger(ids.trigger);
-              }}
-            >
-              <GenericLabelDisplayItem item={option} />
-            </Command.Item>
-          {/each}
-        </Command.Group>
-      </Command.Root>
-    </div>
-  </Popover.Content>
-</Popover.Root>
+
+  <Popover.Portal>
+    <Popover.Content avoidCollisions={false} side="bottom" class="w-[200px] p-0">
+      <div class="max-h-72 overflow-auto">
+        <Command.Root
+          shouldFilter={false}
+          onValueChange={currentValue => {
+            const item = options().find(opt => opt.id == currentValue);
+            if (item) {
+              onItemSelected(item);
+            }
+            //closeAndFocusTrigger(ids.trigger);
+          }}
+        >
+          <Command.Input bind:value={search} placeholder="Search" />
+          <Command.Empty>Nothing to select</Command.Empty>
+          <Command.Group>
+            {#each options() as option (option.id)}
+              <Command.Item disabled={option.disabled} value={option.id + ""}>
+                <GenericLabelDisplayItem item={option} />
+              </Command.Item>
+            {/each}
+          </Command.Group>
+        </Command.Root>
+      </div>
+    </Popover.Content>
+  </Popover.Portal>
+</Popover.Portal>
