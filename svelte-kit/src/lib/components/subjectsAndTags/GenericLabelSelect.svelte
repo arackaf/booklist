ec12b9d6e21c3aa266a2e88a4c8d264140ae5727
Adam Rackis <adamrackis@hotmail.com>
2024-10-28 21:26:19 -0500

    work

--- svelte-kit/src/lib/components/subjectsAndTags/GenericLabelSelect.svelte
+++ svelte-kit/src/lib/components/subjectsAndTags/GenericLabelSelect.svelte
@@ -21,25 +21,15 @@
   let { options, placeholder, search = $bindable(""), onItemSelected, size = "default", triggerClasses = "", renderPlaceholder }: Props = $props();
 
   let open = $state(false);
-
-  // We want to refocus the trigger button when the user selects
-  // an item from the list so users can continue navigating the
-  // rest of the form with the keyboard.
-  function closeAndFocusTrigger(triggerId: string) {
-    open = false;
-    tick().then(() => {
-      document.getElementById(triggerId)?.focus();
-    });
-  }
 </script>
 
-<Popover.Root>
+<Popover.Root {open} onOpenChange={newVal => (open = newVal)}>
   <Popover.Trigger>
     <Button
       size="sm"
       variant="outline"
       role="combobox"
-      aria-expanded={open}
+      aria-expanded={true}
       class="w-[150px] justify-between {size === 'sm' ? 'h-8' : ''} border rounded border-neutral-400 {triggerClasses}"
     >
       {#if renderPlaceholder}
@@ -53,22 +43,22 @@
 
   <Popover.Content avoidCollisions={false} side="bottom" class="w-[200px] p-0">
     <div class="max-h-72 overflow-auto">
-      <Command.Root
-        shouldFilter={false}
-        onValueChange={currentValue => {
-          // console.log({ currentValue });
-          const item = options().find(opt => opt.id == currentValue);
-          if (item) {
-            //onItemSelected(item);
-          }
-          //closeAndFocusTrigger(ids.trigger);
-        }}
-      >
+      <Command.Root shouldFilter={false}>
         <Command.Input bind:value={search} placeholder="Search" />
         <Command.Empty>Nothing to select</Command.Empty>
         <Command.Group>
           {#each options() as option (option.id)}
-            <Command.Item disabled={option.disabled} value={option.id + ""}>
+            <Command.Item
+              disabled={option.disabled}
+              value={option.id + ""}
+              onSelect={() => {
+                const item = options().find(opt => opt.id == option.id);
+                if (item) {
+                  onItemSelected(item);
+                }
+                open = false;
+              }}
+            >
               <GenericLabelDisplayItem item={option} />
             </Command.Item>
           {/each}
