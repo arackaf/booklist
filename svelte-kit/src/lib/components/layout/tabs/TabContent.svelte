e74864da03ea927052618f6b57fb61a64b493b94
Adam Rackis <adamrackis@hotmail.com>
2024-12-14 14:53:42 -0600

    Next slot removed

--- svelte-kit/src/lib/components/layout/tabs/TabContent.svelte
+++ svelte-kit/src/lib/components/layout/tabs/TabContent.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-  import { getContext } from "svelte";
+  import { getContext, type Snippet } from "svelte";
   import type { HTMLAttributes } from "svelte/elements";
 
   type Props = {
     tabName: string;
+    children: Snippet;
   } & HTMLAttributes<HTMLDivElement>;
-  let { tabName, class: className, ...rest }: Props = $props();
+  let { tabName, class: className, children, ...rest }: Props = $props();
 
   const tabsState: any = getContext("tabs-state");
   let currentTab = $derived($tabsState.currentTab);
@@ -13,5 +14,5 @@
 </script>
 
 <div {...rest} class="mt-2 ml-0 md:ml-1 {className}" class:hidden={!isActive}>
-  <slot {isActive} />
+  {@render children()}
 </div>
