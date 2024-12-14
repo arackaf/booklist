3d3baaa48257ff0c20d3d57bccb60ae3caac7a5e
Adam Rackis <adamrackis@hotmail.com>
2024-12-14 13:38:44 -0600

    Remove design system page

--- svelte-kit/src/routes/(core-data)/admin/+layout.svelte
+++ svelte-kit/src/routes/(core-data)/admin/+layout.svelte
@@ -11,7 +11,6 @@
   <svelte:fragment slot="nav">
     <SectionNavItem href="/admin/similar-books">Similar Items</SectionNavItem>
     <SectionNavItem href="/admin/users">Users</SectionNavItem>
-    <SectionNavItem href="/admin/design-system">Design system</SectionNavItem>
   </svelte:fragment>
   {@render children()}
 </TabbedPage>
