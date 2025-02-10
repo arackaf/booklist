a56229bf1221091acc7bb89da7a4a2fae0eb99ea
Adam Rackis <adamrackis@hotmail.com>
2025-02-10 13:56:03 -0600

    refactor

---
+++ svelte-kit/src/lib/components/navigation/NavItems.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import MainNavigationLink from "../navigation/MainNavigationLink.svelte";
+  import type { NavigationItem } from "../navigation/types";
+
+  type Props = {
+    navItems: NavigationItem[];
+  };
+
+  let { navItems }: Props = $props();
+</script>
+
+<div class="flex gap-2">
+  {#each navItems.filter(item => !item.hidden) as item}
+    <MainNavigationLink Icon={item.Icon} active={item.active} disabled={item.disabled} href={item.href} label={item.label} badge={item.badge} />
+  {/each}
+</div>
--- svelte-kit/src/lib/components/section-nav/SectionNav.svelte
+++ svelte-kit/src/lib/components/section-nav/SectionNav.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import MainNavigationLink from "../navigation/MainNavigationLink.svelte";
+  import NavItems from "../navigation/NavItems.svelte";
   import type { NavigationItem } from "../navigation/types";
 
   type Props = {
@@ -11,10 +11,6 @@
 
 <header class="z-50 sticky-content w-full border-b bg-background pl-1">
   <nav class="container flex h-14 items-center">
-    <div class="flex gap-2">
-      {#each navItems.filter(item => !item.hidden) as item}
-        <MainNavigationLink Icon={item.Icon} active={item.active} disabled={item.disabled} href={item.href} label={item.label} badge={item.badge} />
-      {/each}
-    </div>
+    <NavItems {navItems} />
   </nav>
 </header>
