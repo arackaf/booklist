7dfd815973c5fbcd858aa9408792da448e3cec88
Adam Rackis <adamrackis@hotmail.com>
2025-02-10 13:58:13 -0600

    refactor

--- svelte-kit/src/lib/components/navigation/PageWithNavigation.svelte
+++ svelte-kit/src/lib/components/navigation/PageWithNavigation.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
-  import SectionNav from "../section-nav/SectionNav.svelte";
   import type { NavigationItem } from "./types";
+  import NavItems from "./NavItems.svelte";
 
   type Props = {
     navItems: NavigationItem[];
@@ -13,7 +13,11 @@
 
 <section class="p-0">
   <div style="max-width: var(--content-max-width)">
-    <SectionNav {navItems}></SectionNav>
+    <header class="z-50 sticky-content w-full border-b bg-background pl-1">
+      <nav class="container flex h-14 items-center">
+        <NavItems {navItems} />
+      </nav>
+    </header>
     <section class="p-[var(--main-spacing)]">
       {@render children()}
     </section>
