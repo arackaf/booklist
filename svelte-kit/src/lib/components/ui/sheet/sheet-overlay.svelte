5c0735c3f39eb097ff5f938f50ee2a676c615f6f
Adam Rackis <adamrackis@hotmail.com>
2025-02-11 22:32:20 -0600

    make overlay invisible

--- svelte-kit/src/lib/components/ui/sheet/sheet-overlay.svelte
+++ svelte-kit/src/lib/components/ui/sheet/sheet-overlay.svelte
@@ -10,7 +10,7 @@
 <SheetPrimitive.Overlay
   bind:ref
   class={cn(
-    "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0  fixed inset-0 z-50 bg-black/10",
+    "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0  fixed inset-0 z-50 bg-white/0",
     className
   )}
   {...restProps}
