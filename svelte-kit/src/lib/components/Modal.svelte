1d39021ab744ab7e8248b35978e2778fa5f2e425
Adam Rackis <adamrackis@hotmail.com>
2025-02-21 15:37:50 -0600

    modal sizing fixes

--- svelte-kit/src/lib/components/Modal.svelte
+++ svelte-kit/src/lib/components/Modal.svelte
@@ -30,7 +30,7 @@
   <AlertDialog.Content
     interactOutsideBehavior="close"
     onOpenAutoFocus={() => openFocus?.focus()}
-    class="translate-y-[0] top-16 p-5 pt-6 overflow-auto"
+    class="translate-y-[0] top-8 sm:top-16 p-5 pt-6 overflow-auto"
   >
     <AlertDialog.Header class="px-1">
       <AlertDialog.Title>
