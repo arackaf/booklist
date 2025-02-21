c01f5cd7da3ec448121e7e2e6886dbc1b6e94930
Adam Rackis <adamrackis@hotmail.com>
2025-02-20 21:14:00 -0600

    format label.svelte

--- svelte-kit/src/lib/components/ui/label/label.svelte
+++ svelte-kit/src/lib/components/ui/label/label.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
-	import { Label as LabelPrimitive } from "bits-ui";
-	import { cn } from "$lib/utils.js";
+  import { Label as LabelPrimitive } from "bits-ui";
+  import { cn } from "$lib/utils.js";
 
-	let {
-		ref = $bindable(null),
-		class: className,
-		...restProps
-	}: LabelPrimitive.RootProps = $props();
+  let { ref = $bindable(null), class: className, ...restProps }: LabelPrimitive.RootProps = $props();
 </script>
 
 <LabelPrimitive.Root
-	bind:ref
-	class={cn(
-		"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-		className
-	)}
-	{...restProps}
+  bind:ref
+  class={cn("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
+  {...restProps}
 />
