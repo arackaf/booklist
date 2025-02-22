9e57af195e7e672e902e525b7ec8d6ed1e598661
Adam Rackis <adamrackis@hotmail.com>
2025-02-21 18:20:05 -0600

    remove old tabs

--- svelte-kit/src/lib/components/layout/tabs/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Tabs } from "./Tabs.svelte";
-export { default as TabHeaders } from "./TabHeaders.svelte";
-export { default as TabHeader } from "./TabHeader.svelte";
-export { default as TabContents } from "./TabContents.svelte";
-export { default as TabContent } from "./TabContent.svelte";
