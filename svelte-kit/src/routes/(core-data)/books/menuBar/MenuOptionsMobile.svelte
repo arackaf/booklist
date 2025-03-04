ad0f3041456ea050690209128e6fb885e147ec69
Adam Rackis <adamrackis@hotmail.com>
2025-03-03 22:34:03 -0600

    work

--- svelte-kit/src/routes/(core-data)/books/menuBar/MenuOptionsMobile.svelte
+++ svelte-kit/src/routes/(core-data)/books/menuBar/MenuOptionsMobile.svelte
@@ -5,10 +5,10 @@
   import { enhance } from "$app/forms";
   import { invalidate } from "$app/navigation";
 
+  import Button from "$lib/components/ui/button/button.svelte";
+  import Input from "$lib/components/ui/input/input.svelte";
   import Separator from "$lib/components/ui/separator/separator.svelte";
 
-  import Input from "$lib/components/form-elements/Input/Input.svelte";
-  import Button from "$lib/components/Button/Button.svelte";
   import BookReadSetter from "../BookReadSetter.svelte";
 
   import { endSaving, startSaving } from "../state/booksReadSavingState.svelte";
@@ -27,7 +27,7 @@
   let bulkReadSaving = $state(false);
   let bulkUnReadSaving = $state(false);
   let reloading = $state(false);
-  let quickSearchEl = $state<any>({});
+  let quickSearchEl = $state<HTMLInputElement | null>(null);
 
   let selectedBooksIds = $derived(Object.keys(selectionState.selectedBooksLookup).map(s => +s));
   let selectedBooksCount = $derived(selectedBooksIds.length);
@@ -58,7 +58,7 @@
 
   const runSearch = () => {
     const searchParams = new URLSearchParams($page.url.searchParams);
-    const newSearch = quickSearchEl.value;
+    const newSearch = quickSearchEl!.value;
 
     if (!newSearch) {
       searchParams.delete("search");
@@ -73,7 +73,7 @@
   };
 
   const resetSearch = () => {
-    quickSearchEl.value = searchState.value.search;
+    quickSearchEl!.value = searchState.value.search;
   };
 
   $effect(() => {
@@ -88,7 +88,7 @@
 {#if !selectedBooksCount}
   <Input
     autocomplete="off"
-    bind:inputEl={quickSearchEl}
+    bind:ref={quickSearchEl}
     value={searchState.value.search}
     onblur={resetSearch}
     onkeydown={(evt: any) => {
@@ -97,23 +97,23 @@
       }
     }}
     name="search"
-    class="h-8 m-0 mr-0"
+    class=""
     placeholder="Title search"
   />
 
   <Separator class="my-0 h-[2px]" />
 
-  <Button title="Filter search" onclick={mobileHandler(openFilterModal)} class="h-8">
+  <Button variant="outline" size="sm" title="Filter search" onclick={mobileHandler(openFilterModal)}>
     <span>Set Filters</span>
     <i class="fal fa-fw fa-filter ml-auto"></i>
   </Button>
   <Separator class="my-0 h-[2px]" />
   {#if !isPublic}
-    <Button title="Edit subjects" onclick={mobileHandler(editSubjects)} class="h-8">
+    <Button variant="outline" size="sm" title="Edit subjects" onclick={mobileHandler(editSubjects)}>
       <span>Edit Subjects</span>
       <i class="fal fa-fw fa-sitemap ml-auto"></i>
     </Button>
-    <Button title="Edit tags" onclick={mobileHandler(editTags)} class="h-8">
+    <Button variant="outline" size="sm" title="Edit tags" onclick={mobileHandler(editTags)}>
       <span>Edit Tags</span>
       <i class="fal fa-fw fa-tags ml-auto"></i>
     </Button>
@@ -121,7 +121,7 @@
   {/if}
 
   <form method="POST" action="?/reloadBooks" class="contents" use:enhance={reload}>
-    <Button class="h-8" type="submit" disabled={reloading}>
+    <Button variant="outline" size="sm" type="submit" disabled={reloading}>
       <span>Reload Books</span>
       <i class="fal fa-fw fa-sync ml-auto" class:fa-spin={reloading}></i>
     </Button>
@@ -129,22 +129,22 @@
   <Separator class="my-0 h-[2px]" />
 {:else if !isPublic}
   <Separator class="my-0 h-[2px]" />
-  <Button class="h-8" title="Add/remove subjects" onclick={mobileHandler(editSubjectsForSelectedBooks)}>
+  <Button variant="outline" size="sm" title="Add/remove subjects" onclick={mobileHandler(editSubjectsForSelectedBooks)}>
     <span>Edit Subjects</span>
     <i class="fal fa-fw fa-sitemap ml-auto"></i>
   </Button>
-  <Button class="h-8" title="Add/remove tags" onclick={mobileHandler(editTagsForSelectedBooks)}>
+  <Button variant="outline" size="sm" title="Add/remove tags" onclick={mobileHandler(editTagsForSelectedBooks)}>
     <span>Edit Tags</span>
     <i class="fal fa-fw fa-tags ml-auto"></i>
   </Button>
   <BookReadSetter ids={selectedBooksIds} value={true} bind:saving={bulkReadSaving}>
-    <Button class="h-8" title="Set read" disabled={bulkReadSaving || bulkUnReadSaving}>
+    <Button variant="outline" size="sm" title="Set read" disabled={bulkReadSaving || bulkUnReadSaving}>
       <span>Set Read</span>
       <i class="fal fa-fw fa-eye ml-auto"></i>
     </Button>
   </BookReadSetter>
   <BookReadSetter ids={selectedBooksIds} value={false} bind:saving={bulkUnReadSaving}>
-    <Button class="h-8" title="Set un-read" disabled={bulkReadSaving || bulkUnReadSaving}>
+    <Button variant="outline" size="sm" title="Set un-read" disabled={bulkReadSaving || bulkUnReadSaving}>
       <span>Set Un-Read</span>
       <i class="fal fa-fw fa-eye-slash ml-auto"></i>
     </Button>
