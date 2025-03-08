5268c110173777c216e262fa7b5114c071614090
Adam Rackis <adamrackis@hotmail.com>
2025-03-08 17:40:52 -0600

    remove font-awesome

--- svelte-kit/src/lib/components/Loading.svelte
+++ svelte-kit/src/lib/components/Loading.svelte
@@ -1,9 +1,10 @@
 <svelte:options runes={true} />
 
 <script lang="ts">
+  import { LoaderCircleIcon } from "lucide-svelte";
   import { fade } from "svelte/transition";
 </script>
 
 <div class="fixed top-1/3 left-1/2 z-[99999] -translate-x-1/2 -translate-y-1/2" in:fade={{ delay: 50, duration: 200 }}>
-  <i class="far fa-5x fa-spin fa-spinner text-primary-3 opacity-50"></i>
+  <LoaderCircleIcon class="animate-spin text-primary/30" size={92} />
 </div>
