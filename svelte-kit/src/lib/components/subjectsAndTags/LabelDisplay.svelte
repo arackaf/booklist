ad4af426a7fd8fc91b35bc8bbc9a3edde4e8477b
Adam Rackis <adamrackis@hotmail.com>
2025-03-08 19:54:10 -0600

    tweak label display

--- svelte-kit/src/lib/components/subjectsAndTags/LabelDisplay.svelte
+++ svelte-kit/src/lib/components/subjectsAndTags/LabelDisplay.svelte
@@ -18,7 +18,10 @@
 
 {#if href}
   <a {href} style={extraStyles} class="{disabled ? disabledClasses : ''} {extraClasses}">
-    <Badge class="bg-neutral-500 text-white px-2 text-xs leading-none" style="background-color: {item.backgroundColor}; color: {item.textColor};">
+    <Badge
+      class="whitespace-nowrap truncate max-w-44 block bg-neutral-500 text-white px-2 text-xs leading-none"
+      style="background-color: {item.backgroundColor}; color: {item.textColor};"
+    >
       {item.name}
     </Badge>
   </a>
