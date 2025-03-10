bf77ba3fb7c37649b698c9aa1ed5b83782ac4194
Adam Rackis <adamrackis@hotmail.com>
2025-03-10 00:26:07 -0500

    remove primary colors

--- svelte-kit/tailwind.config.js
+++ svelte-kit/tailwind.config.js
@@ -64,18 +64,6 @@ const config = {
         "info-9": "var(--info-9)",
         "info-10": "var(--info-10)",
 
-        "primary-1": "var(--primary-1)",
-        "primary-2": "var(--primary-2)",
-        "primary-3": "var(--primary-3)",
-        "primary-4": "var(--primary-4)",
-        "primary-5": "var(--primary-5)",
-        "primary-6": "var(--primary-6)",
-        "primary-7": "var(--primary-7)",
-        "primary-8": "var(--primary-8)",
-        "primary-9": "var(--primary-9)",
-        "primary-10": "var(--primary-10)",
-        "primary-11": "var(--primary-11)",
-
         "success-1": "var(--success-1)",
         "success-2": "var(--success-2)",
         "success-3": "var(--success-3)",
