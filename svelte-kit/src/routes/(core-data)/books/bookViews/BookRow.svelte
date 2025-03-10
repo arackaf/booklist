cdbc8ff6d5ffb4a3998760ecc845afaca00b4a4b
Adam Rackis <adamrackis@hotmail.com>
2025-03-10 00:42:40 -0500

    style tweaks

--- svelte-kit/src/routes/(core-data)/books/bookViews/BookRow.svelte
+++ svelte-kit/src/routes/(core-data)/books/bookViews/BookRow.svelte
@@ -65,7 +65,7 @@
   }
 </script>
 
-<tr class="hover:bg-primary/5">
+<tr class="hover:bg-secondary">
   {#if !isPublic}
     <td>
       <button style="font-size: 12pt" class="raw-button" onclick={() => selectionState.toggle(id)} aria-label="Select book">
