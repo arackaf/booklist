2ea8d5d452b8687ef1745307dcfe5b1f4462d97a
Adam Rackis <adamrackis@hotmail.com>
2025-03-10 00:23:03 -0500

    manually fire scan toasts

--- svelte-kit/src/routes/(core-data)/+layout.svelte
+++ svelte-kit/src/routes/(core-data)/+layout.svelte
@@ -8,6 +8,32 @@
   import type { Book } from "$data/types";
   import ScanToasterSuccessContent from "$lib/components/ScanToasterResultContent.svelte";
 
+  $effect(() => {
+    setTimeout(() => {
+      const fail = {
+        _id: "b26fe39d-67ab-4482-8027-823d8db87c7c",
+        title: "Failed lookup for 9988776655"
+      };
+
+      const success = {
+        success: true,
+        item: {
+          title: "The Forging of the Union, 1781-1789 (New American Nation Series)",
+          authors: ["Richard B. Morris"],
+          smallImage:
+            "https://my-library-cover-uploads.s3.amazonaws.com/medium-covers/573d1b97120426ef0078aa92/61af7adf-80c4-4122-848d-0938fbd86245.jpg",
+          smallImagePreview: {
+            w: 50,
+            h: 75,
+            b64: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAAECAIAAADETxJQAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAM0lEQVR4nAEoANf/ALymQZSBJsOrSAB+chUXEABnYAkAx6RCTTsAtpc6AP/vgOi9Vv/ufjI6ESGQHZPBAAAAAElFTkSuQmCC"
+          }
+        }
+      };
+
+      showBookToast(success.item as any);
+      showBookFailureToast(fail as any);
+    }, 1000);
+  });
   let { data, children } = $props();
   let { loggedIn, userId } = $derived(data);
 
