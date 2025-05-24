cf35a3f06bc1e9b6201c972da95ad683732b96ba
Adam Rackis <adamrackis@hotmail.com>
2025-05-24 15:03:22 -0400

    overwrite dialog with new install

--- svelte-kit/src/lib/components/ui/dialog/dialog-header.svelte
+++ svelte-kit/src/lib/components/ui/dialog/dialog-header.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   import type { HTMLAttributes } from "svelte/elements";
-  import type { WithElementRef } from "bits-ui";
-  import { cn } from "$lib/utils.js";
+  import { cn, type WithElementRef } from "$lib/utils.js";
 
   let { ref = $bindable(null), class: className, children, ...restProps }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
 </script>
 
-<div bind:this={ref} class={cn("flex flex-col space-y-1.5 text-center sm:text-left", className)} {...restProps}>
+<div bind:this={ref} data-slot="dialog-header" class={cn("flex flex-col gap-2 text-center sm:text-left", className)} {...restProps}>
   {@render children?.()}
 </div>
