f0d4c7b50b4b47c754ee9c93f93b67203f115377
Adam Rackis <adamrackis@hotmail.com>
2025-05-25 10:37:32 -0400

    work

--- svelte-kit/src/lib/components/ui/dialog/dialog-content.svelte
+++ svelte-kit/src/lib/components/ui/dialog/dialog-content.svelte
@@ -22,7 +22,7 @@
   <DialogPrimitive.Content
     bind:ref
     class={cn(
-      "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 bg-background fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 sm:rounded-lg",
+      "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 !slide-in-from-left-[-50%] data-[state=open]:zoom-in-95 bg-background fixed left-[50%] top-[50%] z-50 grid max-h-[calc(100dvh-64px)] sm:max-h-[calc(100vh-100px)] max-w-[min(95vw,600px)] w-[min(95vw,600px)] !-translate-x-1/2 gap-4 border p-6 shadow-lg duration-200 sm:rounded-lg",
       className
     )}
     {...restProps}
