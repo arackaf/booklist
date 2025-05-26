c214f54c5b2d7507e6bd9874f3173713b08dd874
Adam Rackis <adamrackis@hotmail.com>
2025-05-26 16:15:43 -0500

    update alert

--- svelte-kit/src/lib/components/ui/alert/alert-description.svelte
+++ svelte-kit/src/lib/components/ui/alert/alert-description.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-  import type { WithElementRef } from "bits-ui";
   import type { HTMLAttributes } from "svelte/elements";
-  import { cn } from "$lib/utils.js";
+  import { cn, type WithElementRef } from "$lib/utils.js";
 
   let { ref = $bindable(null), class: className, children, ...restProps }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
 </script>
 
-<div bind:this={ref} class={cn("text-sm [&_p]:leading-relaxed", className)} {...restProps}>
+<div
+  bind:this={ref}
+  data-slot="alert-description"
+  class={cn("text-muted-foreground col-start-2 grid justify-items-start gap-1 text-sm [&_p]:leading-relaxed", className)}
+  {...restProps}
+>
   {@render children?.()}
 </div>
--- svelte-kit/src/lib/components/ui/alert/alert.svelte
+++ svelte-kit/src/lib/components/ui/alert/alert.svelte
@@ -2,11 +2,11 @@
   import { type VariantProps, tv } from "tailwind-variants";
 
   export const alertVariants = tv({
-    base: "[&>svg]:text-foreground relative w-full rounded-lg border p-4 [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg~*]:pl-7",
+    base: "relative grid w-full grid-cols-[0_1fr] items-start gap-y-0.5 rounded-lg border px-4 py-3 text-sm has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] has-[>svg]:gap-x-3 [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current",
     variants: {
       variant: {
-        default: "bg-background text-foreground",
-        destructive: "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive"
+        default: "bg-card text-card-foreground",
+        destructive: "text-destructive bg-card *:data-[slot=alert-description]:text-destructive/90 [&>svg]:text-current"
       }
     },
     defaultVariants: {
@@ -19,8 +19,7 @@
 
 <script lang="ts">
   import type { HTMLAttributes } from "svelte/elements";
-  import type { WithElementRef } from "bits-ui";
-  import { cn } from "$lib/utils.js";
+  import { cn, type WithElementRef } from "$lib/utils.js";
 
   let {
     ref = $bindable(null),
@@ -33,6 +32,6 @@
   } = $props();
 </script>
 
-<div bind:this={ref} class={cn(alertVariants({ variant }), className)} {...restProps} role="alert">
+<div bind:this={ref} data-slot="alert" class={cn(alertVariants({ variant }), className)} {...restProps} role="alert">
   {@render children?.()}
 </div>
