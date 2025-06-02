3a3225ce629e63f3bee0b0b87b22677308f3a45d
Adam Rackis <adamrackis@hotmail.com>
2025-06-02 00:45:38 -0500

    work

--- fly-io/sync-similar-books/full-sync.ts
+++ fly-io/sync-similar-books/full-sync.ts
@@ -1,14 +1,70 @@
 import "./util/config";
+import { desc, eq, isNull, lt, or, InferSelectModel } from "drizzle-orm";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import { addMonths, format } from "date-fns";
 
 import { initializePostgres } from "./util/dbUtils";
+import * as schema from "./drizzle/drizzle-schema";
 import { books as booksTable } from "./drizzle/drizzle-schema";
+import { isbn13To10 } from "./util/isbn13to10";
 
-const db = initializePostgres();
+type Book = InferSelectModel<typeof booksTable>;
+
+async function getNextBook(db: NodePgDatabase<typeof schema>) {
+  const today = new Date();
+  const cutoff = addMonths(today, -3);
+  const cutoffString = format(cutoff, "yyyy-MM-dd");
+  const { id, title, lastAmazonSync } = booksTable;
+
+  const books = await db
+    .select()
+    .from(booksTable)
+    .where(or(isNull(booksTable.lastAmazonSync), lt(booksTable.lastAmazonSync, cutoffString)))
+    .orderBy(desc(booksTable.id))
+    .limit(1);
+  const x = books[0];
+
+  return books[0] as Book;
+}
+
+async function failSync(db: NodePgDatabase<typeof schema>, book: Book, message: string) {
+  console.log("Failing book", book.title, message);
+  await db
+    .update(booksTable)
+    .set({ lastAmazonSync: new Date().toISOString(), lastAmazonSyncSuccess: false, lastAmazonSyncError: message })
+    .where(eq(booksTable.id, book.id));
+}
+
+async function syncBook(db: NodePgDatabase<typeof schema>, book: Book) {
+  let isbn = book.isbn;
+
+  if (!isbn || (isbn.length !== 10 && isbn.length !== 13)) {
+    const message = !isbn ? "No ISBN" : "ISBN has an invalid length";
+    await failSync(db, book, message);
+    return;
+  }
+
+  if (isbn.length === 13) {
+    isbn = isbn13To10(isbn);
+  }
+  if (isbn == null) {
+    await failSync(db, book, "Could not convert to an ISBN-10");
+    return;
+  }
+
+  console.log("Syncing book", book.title, isbn);
+}
 
 async function main() {
-  const books = await db.select({ id: booksTable.id, title: booksTable.title }).from(booksTable).limit(10);
-
-  console.log({ books });
+  const { db, dispose } = initializePostgres();
+  try {
+    const book = await getNextBook(db);
+    await syncBook(db, book);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    dispose();
+  }
 }
 
 main();
