5897e8f51bd4844c195bf1fa20d8ff04aae84e08
Adam Rackis <adamrackis@hotmail.com>
2025-06-07 15:42:20 -0500

    cleanup

--- fly-io/sync-similar-books/index.ts
+++ fly-io/sync-similar-books/index.ts
@@ -1,4 +1,4 @@
-import "./config";
+import "./util/config";
 import { localSync } from "./sync";
 
 Promise.resolve(localSync()).then(() => {
--- fly-io/sync-similar-books/util/config.ts
+++ fly-io/sync-similar-books/util/config.ts
@@ -1,3 +1,2 @@
 import dotenv from "dotenv";
-
 dotenv.config({ path: ".env.local" });
